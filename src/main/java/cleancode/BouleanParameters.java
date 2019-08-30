eeec9664c04d47578fd0e82f63fd1a4067e8b886
Victor Rentea <victorrentea@gmail.com>
2019-08-30 11:26:29 +0300

    boulean

--- src/main/java/cleancode/BouleanParameters.java
+++ src/main/java/cleancode/BouleanParameters.java
@@ -7,21 +7,23 @@ public class BouleanParameters {
 
     public static void main(String[] args) {
         // The method is called from various foreign places in the codebase
-        bigUglyMethod(1,2,3,4,5);
-        bigUglyMethod(1,2,3,4,5);
-        bigUglyMethod(1,2,3,4,5);
-        bigUglyMethod(1,2,3,4,5);
-        bigUglyMethod(1,2,3,4,5);
+        bigUglyMethod(1,2,3,4,5, false);
+        bigUglyMethod(1,2,3,4,5, false);
+        bigUglyMethod(1,2,3,4,5, false);
+        bigUglyMethod(1,2,3,4,5, false);
+        bigUglyMethod(1,2,3,4,5, false);
 
-        // TODO From my use-case, I call it too, to do more within:
-        bigUglyMethod(1,2,3,4,5);
-
+        // TODO From **MY** use-case #323, I call it too, to do more within:
+        bigUglyMethod(1,2,3,4,5, true);
     }
+    static void bigUglyMethod(int a, int b, int c, int d, int e, boolean cr323) {
+        // complex logic
+        // complex logic
+        // complex logic
 
-    static void bigUglyMethod(int a, int b, int c, int d, int e) {
-        // complex logic
-        // complex logic
-        // complex logic
+        if (cr323) {
+            // sI eu
+        }
 
         // more complex logic
         // more complex logic
