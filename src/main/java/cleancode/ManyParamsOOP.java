d46c3133b988fb1d279ab673b10768fa88cddfe4
Victor Rentea <victorrentea@gmail.com>
2019-11-19 13:49:01 +0200

    Parameters to fields

--- src/main/java/cleancode/ManyParamsOOP.java
+++ src/main/java/cleancode/ManyParamsOOP.java
@@ -1,51 +1,63 @@
 package cleancode;
 
-import cleancode.pretend.Autowired;
 import cleancode.pretend.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ManyParamsOOP {
-    @Autowired
-    private Validator validator;
+
+    private final OtherDependency dep;
+
+    public ManyParamsOOP(OtherDependency dep) {
+        this.dep = dep;
+    }
+
 
     public void bizLogic() {
-        List<String> errors = new ArrayList<>();
-        validator.m1(errors, "a",1);
-        validator.m2(errors, "b",1);
-        validator.m3(errors, "file.txt", 1L,"ref");
-        validator.m4(errors, "a", 1L,5L, "g");
-        validator.m5(errors, 1);
+         Validator validator = new Validator(dep);
+        validator.m1("a",1);
+        validator.m2("b",1);
+        validator.m3("file.txt", 1L,"ref");
+        validator.m4("a", 1L,5L, "g");
+        validator.m5(1);
+        List<String> errors = validator.getErrors();
         if (!errors.isEmpty()) {
             throw new IllegalArgumentException(errors.toString());
         }
     }
 }
-@Service
 class Validator {
-    @Autowired
-	private OtherDependency dep;
+    private final OtherDependency dep;
+    private List<String> errors = new ArrayList<>();
 
-    public void m1(List<String> errors, String a, int b) {
+    public Validator(OtherDependency dep) {
+        this.dep = dep;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    public void m1(String a, int b) {
         if (a == null) {
             errors.add("a must not be null");
         }
         // stuff
     }
-    public void m2(List<String> errors, String s, int c) {
+    public void m2(String s, int c) {
         if (c < 0) {
             errors.add("negative c");
         }
         // stuff
     }
-    public void m3(List<String> errors, String fileName, long versionId, String reference) {
+    public void m3(String fileName, long versionId, String reference) {
         // stuff
     }
-    public void m4(List<String> errors, String a, long listId, long recordId, String g) {
+    public void m4(String a, long listId, long recordId, String g) {
         // stuff
     }
-    public void m5(List<String> errors, int b) {
+    public void m5(int b) {
         // stuff
     }
 }
