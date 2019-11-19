40d59ad828bb2544806c1c080cb6eaea74e80665
Victor Rentea <victorrentea@gmail.com>
2019-11-19 10:28:45 +0200

    Utils vs VO prep

---
+++ src/main/java/cleancode/UtilsVsVO.java
@@ -0,0 +1,62 @@
+package cleancode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UtilsVsVO {
+    public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> models) {
+        List<CarModel> results = new ArrayList<>(models);
+        results.removeIf(model -> ! intervalsIntersect(
+                model.getStartYear(), model.getEndYear(),
+                criteria.getStartYear(), criteria.getEndYear()));
+        System.out.println("More filtering logic");
+        return results;
+    }
+    // http://world.std.com/~swmcd/steven/tech/interval.html
+    private boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
+        return start1 <= end2 && start2 <= end1;
+    }
+}
+
+
+
+
+
+
+class CarSearchCriteria {
+    private final int startYear;
+    private final int endYear;
+
+    public CarSearchCriteria(int startYear, int endYear) {
+        if (startYear > endYear) throw new IllegalArgumentException("start larger than end");
+        this.startYear = startYear;
+        this.endYear = endYear;
+    }
+
+    public int getStartYear() {
+        return startYear;
+    }
+
+    public int getEndYear() {
+        return endYear;
+    }
+}
+
+class CarModel {
+    private final int startYear;
+    private final int endYear;
+
+    public CarModel(int startYear, int endYear) {
+        if (startYear > endYear) throw new IllegalArgumentException("start larger than end");
+        this.startYear = startYear;
+        this.endYear = endYear;
+    }
+
+    public int getEndYear() {
+        return endYear;
+    }
+
+    public int getStartYear() {
+        return startYear;
+    }
+}
