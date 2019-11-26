6dca7e079f15bf32e44dccb40838f19b1817ea9a
Victor Rentea <victorrentea@gmail.com>
2019-11-26 16:32:45 +0200

    clean up. urmeaza haos

--- src/main/java/videostore/dirty/MovieType.java
+++ src/main/java/videostore/dirty/MovieType.java
@@ -1,30 +1,7 @@
 package videostore.dirty;
 
 public enum MovieType {
-	CHILDREN(2, ChildrenPriceCalculator.class),
-	REGULAR(0) {
-		@Override
-		public double computePrice(int daysRented) {
-			double price = 2;
-			if (daysRented > 2)
-				price += (daysRented - 2) * 1.5;
-			return price;
-		}
-	},
-//	BABACIUNI(5),
-	NEW_RELEASE(1) {
-	@Override
-	public double computePrice(int daysRented) {
-		return 0;
-	}
-};
-
-	private final int id;
-	private final Class<? extends MoviePriceCalculator> strategyClass;
-
-	MovieType(int id, Class<? extends MoviePriceCalculator> strategyClass) {
-		this.id = id;
-		this.strategyClass = strategyClass;
-	}
-
+	CHILDREN,
+	REGULAR,
+	NEW_RELEASE
 }
