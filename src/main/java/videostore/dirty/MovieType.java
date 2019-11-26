3fcc930f9fd19d83591e711de560527792a08a2a
Victor Rentea <victorrentea@gmail.com>
2019-11-26 16:30:27 +0200

    metode abstracte in ENUM!!! WTH

--- src/main/java/videostore/dirty/MovieType.java
+++ src/main/java/videostore/dirty/MovieType.java
@@ -1,14 +1,34 @@
 package videostore.dirty;
 
 public enum MovieType {
-	CHILDREN(2),
-	REGULAR(0),
+	CHILDREN(2){
+		@Override
+		public double computePrice(int daysRented) {
+			return 0;
+		}
+	},
+	REGULAR(0) {
+		@Override
+		public double computePrice(int daysRented) {
+			double price = 2;
+			if (daysRented > 2)
+				price += (daysRented - 2) * 1.5;
+			return price;
+		}
+	},
 //	BABACIUNI(5),
-	NEW_RELEASE(1);
+	NEW_RELEASE(1) {
+	@Override
+	public double computePrice(int daysRented) {
+		return 0;
+	}
+};
 
 	private final int id;
 
 	MovieType(int id) {
 		this.id = id;
 	}
+
+	public abstract double computePrice(int daysRented);
 }
--- src/main/java/videostore/dirty/Rental.java
+++ src/main/java/videostore/dirty/Rental.java
@@ -27,67 +27,17 @@ class Rental {
     }
 
 
-    interface MoviePriceCalculator {
-        double computePrice();
-        double computePrice2();
-        double computePrice3();
-    }
-    class NewReleasePriceCalculator implements MoviePriceCalculator {
-        public double computePrice() {
-            return 0;
-        }
-        public double computePrice2() {
-            return 0;
-        }
-        public double computePrice3() {
-            return 0;
-        }
-    }
-    class RegularPriceCalculator implements MoviePriceCalculator {
-        public double computePrice() {
-            return 0;
-        }
-        public double computePrice2() {
-            return 0;
-        }
-        public double computePrice3() {
-            return 0;
-        }
-    }
 
-
-    public MoviePriceCalculator selectPriceCalculator(MovieType type) {
-        switch (type) {
-            case NEW_RELEASE: return new NewReleasePriceCalculator();
-        }
-    }
 
     public double computePrice() {
-        switch (getMovie().getMovieType()) {
-            case REGULAR: return computeRegularMoviePrice();
-            case NEW_RELEASE: return computeNewReleaseMoviePrice();
-            case CHILDREN: return computeChildrenMoviePrice();
-            default:
-                throw new IllegalStateException("JDD Unexpected value: " + getMovie().getMovieType());
-        }
-    }
-    public double computePrice2() {
-        switch (getMovie().getMovieType()) {
-            case REGULAR: return computeRegularMoviePrice();
-            case NEW_RELEASE: return computeNewReleaseMoviePrice();
-            case CHILDREN: return computeChildrenMoviePrice();
-            default:
-                throw new IllegalStateException("JDD Unexpected value: " + getMovie().getMovieType());
-        }
-    }
-    public double computePrice3() {
-        switch (getMovie().getMovieType()) {
-            case REGULAR: return computeRegularMoviePrice();
-            case NEW_RELEASE: return computeNewReleaseMoviePrice();
-            case CHILDREN: return computeChildrenMoviePrice();
-            default:
-                throw new IllegalStateException("JDD Unexpected value: " + getMovie().getMovieType());
-        }
+        return movie.getMovieType().computePrice(daysRented);
+//        switch (getMovie().getMovieType()) {
+//            case REGULAR: return computeRegularMoviePrice();
+//            case NEW_RELEASE: return computeNewReleaseMoviePrice();
+//            case CHILDREN: return computeChildrenMoviePrice();
+//            default:
+//                throw new IllegalStateException("JDD Unexpected value: " + getMovie().getMovieType());
+//        }
     }
 
 	private double computeRegularMoviePrice() {
