118dd422cfbd3f09a0fc6fb082c119218342769e
Victor Rentea <victorrentea@gmail.com>
2019-12-05 10:09:32 +0200

    Am pus la cos

--- src/main/java/cleancode/UtilsVsVO.java
+++ src/main/java/cleancode/UtilsVsVO.java
@@ -6,14 +6,17 @@ import java.util.List;
 public class UtilsVsVO {
     public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> models) {
         List<CarModel> results = new ArrayList<>(models);
-        results.removeIf(model -> ! intervalsIntersect(
+        results.removeIf(model -> ! MateUtil.intervalsIntersect(
                 model.getStartYear(), model.getEndYear(),
                 criteria.getStartYear(), criteria.getEndYear()));
         System.out.println("More filtering logic");
         return results;
     }
+}
+
+class MateUtil {
     // http://world.std.com/~swmcd/steven/tech/interval.html
-    private boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
+    public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
         return start1 <= end2 && start2 <= end1;
     }
 }
