2a64230c1cc73542fe4a8e672e5dfa374ddac6fc
Victor Rentea <victorrentea@gmail.com>
2020-02-10 23:46:41 +0200

    adjust

--- src/main/java/victor/training/refactoring/Immutable.java
+++ src/main/java/victor/training/refactoring/Immutable.java
@@ -2,6 +2,10 @@ package victor.training.refactoring;
 
 import java.util.List;
 
+class MutableHost { //think "Entity"
+    public Immutable immutable;
+}
+
 public class Immutable {
     public int x;
     public List<Integer> numbers;
