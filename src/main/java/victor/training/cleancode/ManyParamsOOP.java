f914308e8fea69996ea9d2d738014673e622b9a9
Victor Rentea <victorrentea@gmail.com>
2020-02-11 16:14:03 +0200

    Spring + OOP = HATE

--- src/main/java/victor/training/cleancode/ManyParamsOOP.java
+++ src/main/java/victor/training/cleancode/ManyParamsOOP.java
@@ -1,51 +1,58 @@
 package victor.training.cleancode;
 
+import lombok.RequiredArgsConstructor;
 import victor.training.cleancode.pretend.Autowired;
 import victor.training.cleancode.pretend.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class ManyParamsOOP {
-    @Autowired
-    private Validator validator;
+
+    private OtherDependency dep1;
 
     public void bizLogic() {
-        List<String> errors = new ArrayList<>();
-        validator.m1("a",1, errors);
-        validator.m2("b",1, errors);
-        validator.m3("file.txt", 1L,"ref", errors);
-        validator.m4("a", 1L,5L, "g", errors);
-        validator.m5(1, errors);
-        if (!errors.isEmpty()) {
-            throw new IllegalArgumentException(errors.toString());
+        Validator validator = new Validator(dep1);
+        validator.m1("a",1);
+        validator.m2("b",1);
+        validator.m3("file.txt", 1L,"ref");
+        validator.m4("a", 1L,5L, "g");
+        validator.m5(1);
+        if (!validator.getErrors().isEmpty()) {
+            throw new IllegalArgumentException(validator.getErrors().toString());
         }
     }
 }
-@Service
+@RequiredArgsConstructor
 class Validator {
-    @Autowired
-	private OtherDependency dep;
+	private final OtherDependency dep;
 
-    public void m1(String a, int b, List<String> errors) {
+    private final List<String> errors = new ArrayList<>();
+
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    public void m1(String a, int b) {
         if (a == null) {
             errors.add("a must not be null");
         }
         // stuff
     }
-    public void m2(String s, int c, List<String> errors) {
+    public void m2(String s, int c) {
         if (c < 0) {
             errors.add("negative c");
         }
         // stuff
     }
-    public void m3(String fileName, long versionId, String reference, List<String> errors) {
+    public void m3(String fileName, long versionId, String reference) {
         // stuff
     }
-    public void m4(String a, long listId, long recordId, String g, List<String> errors) {
+    public void m4(String a, long listId, long recordId, String g) {
         // stuff
     }
-    public void m5(int b, List<String> errors) {
+    public void m5(int b) {
         // stuff
     }
 }
