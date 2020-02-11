02c8ab8945abc020692485060f6ec6dacd3a2c30
Victor Rentea <victorrentea@gmail.com>
2020-02-11 08:37:45 +0200

    initial commit

---
+++ src/main/java/victor/training/refactoring/SplitLoop.java
@@ -0,0 +1,34 @@
+package victor.training.refactoring;
+
+import lombok.Data;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SplitLoop {
+
+    public static void main(String[] args) {
+        computeStats(Arrays.asList(
+                new Employee(24, 1500),
+                new Employee(30, 2500)));
+    }
+
+    private static void computeStats(List<Employee> employees) {
+        int averageAge = 0;
+        double averageSalary = 0;
+        for (Employee employee : employees) {
+            averageAge += employee.getAge();
+            averageSalary += employee.getSalary();
+        }
+        averageAge = averageAge / employees.size();
+        averageSalary = averageSalary / employees.size();
+        System.out.println("avg age = " + averageAge + "\navg sal = " + averageSalary);
+    }
+}
+
+
+@Data
+class Employee {
+    private final int age;
+    private final double salary;
+}
