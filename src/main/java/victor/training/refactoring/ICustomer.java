c5b4f260b6804d8a08030f134cb4c698a55f38ea
Victor Rentea <victorrentea@gmail.com>
2020-02-11 12:15:41 +0200

    NullObject pattern

---
+++ src/main/java/victor/training/refactoring/ICustomer.java
@@ -0,0 +1,7 @@
+package victor.training.refactoring;
+
+public interface ICustomer {
+    boolean isActive();
+
+    String getName();
+}
--- src/main/java/victor/training/refactoring/NullObject.java
+++ src/main/java/victor/training/refactoring/NullObject.java
@@ -1,21 +1,56 @@
 package victor.training.refactoring;
 
 class NullObject {
-    public void process(Customer customer) {
+    public static void main(String[] args) {
+        new NullObject().process(Customer.NULL_CUSTOMER);
+    }
+    public void process(ICustomer customer) {
         String customerName;
-        if (customer == null) {
-            customerName = "occupant";
-        } else {
+//        if (customer == null) {
+//            customerName = "occupant";
+//        } else {
             customerName = customer.getName();
+//        }
+        if (customer.isActive()) {
+            System.out.printf("ALOA");
         }
         // ...
     }
 
 }
 
-class Customer {
+// Hard-core version: re-implementare cu logica alternativa
+class NullCustomer implements ICustomer {
 
+    @Override
+    public boolean isActive() {
+        return false;
+    }
+
+    @Override
     public String getName() {
-        return "blah";
+        return "occupant";
+    }
+}
+
+class Customer implements ICustomer {
+    // LIGHT version: date dummy intr-o constanta
+    public static final ICustomer NULL_CUSTOMER = new Customer("occupant");
+
+    private final String name;
+
+    Customer(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean isActive() {
+        // logica multa
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return name;
     }
 }
