8bfd1e7710f28ccf098ed96c0cfe39b5409831eb
Victor Rentea <victorrentea@gmail.com>
2020-02-11 15:40:30 +0200

    Extract Method Object

--- src/main/java/victor/training/refactoring/ExtractMethodObject.java
+++ src/main/java/victor/training/refactoring/ExtractMethodObject.java
@@ -1,5 +1,6 @@
 package victor.training.refactoring;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang.StringUtils;
 
@@ -14,24 +15,54 @@ public class ExtractMethodObject {
     private final EntityManager em;
 
     public List<Long> search(CustomerSearchCriteria criteria) {
-        String jpql = "SELECT c.id FROM Customer c WHERE 1=1 ";
-        Map<String, Object> params = new HashMap<>();
+        CustomerQueryBuilder customerQueryBuilder =
+                new CustomerQueryBuilder(criteria).build();
 
-        if (StringUtils.isNotBlank(criteria.name)) {
-            jpql += " AND UPPER(c.name) LIKE '%' || UPPER(:name) || '%' ";
-            params.put("name", criteria.name);
-        }
-
-        if (criteria.countryId != null) {
-            jpql += " AND (c.residenceCountry.id = :countryId OR ..<5 lines of JPQL>..)";
-            params.put("countryId", criteria.countryId);
-        }
+        String jpql = customerQueryBuilder.getJpql();
+        Map<String, Object> params = customerQueryBuilder.getParams();
 
         TypedQuery<Long> query = em.createQuery(jpql + " ", Long.class);
         for (String param : params.keySet()) {
             query.setParameter(param, params.get(param));
         }
         return query.getResultList();
+    }
+
+    private class CustomerQueryBuilder {
+        private final CustomerSearchCriteria criteria;
+        private String jpql;
+        @Getter
+        private Map<String, Object> params = new HashMap<>();
+
+        public String getJpql() {
+            if (jpql ==null) throw  new IllegalStateException();
+            return jpql;
+        }
+
+        public CustomerQueryBuilder(CustomerSearchCriteria criteria) {
+            this.criteria = criteria;
+        }
+
+        public CustomerQueryBuilder build() {
+            jpql = "SELECT c.id FROM Customer c WHERE 1=1 ";
+            addNameCriterion();
+            addCountryCriterion();
+            return this;
+        }
+
+        private void addCountryCriterion() {
+            if (criteria.countryId != null) {
+                jpql += " AND (c.residenceCountry.id = :countryId OR ..<5 lines of JPQL>..)";
+                params.put("countryId", criteria.countryId);
+            }
+        }
+
+        private void addNameCriterion() {
+            if (StringUtils.isNotBlank(criteria.name)) {
+                jpql += "   AND UPPER(c.name) LIKE '%' || UPPER(:name) || '%' ";
+                params.put("name", criteria.name);
+            }
+        }
     }
 }
 
