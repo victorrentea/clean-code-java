eee522f7dadf316d6d005524a20f23cef8e6ccde
Victor Rentea <victorrentea@gmail.com>
2020-03-05 11:47:47 +0200

    Aplatizarea functiilor scotand boolean parameters. Urma Split Loop refactor (dublarea forului)

--- src/main/java/victor/training/cleancode/BouleanParameters.java
+++ src/main/java/victor/training/cleancode/BouleanParameters.java
@@ -44,23 +44,31 @@ public class BouleanParameters {
 
     // ============== "BOSS" LEVEL: A lot harder to break down =================
 
-    static void bossLevel(boolean stuff, boolean fluff, List<Integer> tasks) {
-        int i = 0; // TODO localize
-        int j = 1;
+    static void bossLevelStuffFluff(List<Integer> tasks) {
         System.out.println("Logic1");
-        if (stuff) {
-            System.out.println("Logic2");
-            if (fluff) {
-                System.out.println("Logic3");
-                for (int task : tasks) {
-                    i++;
-                    System.out.println("Logic4 " + task);
-                    // TODO HERE, when call this method, I want MY own custom code to run here
-                    System.out.println("Logic5 " + i);
-                }
-                System.out.println("Logic6 " + j++);
-            }
+        System.out.println("Logic2");
+        System.out.println("Logic3");
+        // TODO localize
+        int i = 0;
+        for (int task : tasks) {
+            i++;
+            System.out.println("Logic4 " + task);
+            // TODO HERE, when call this method, I want MY own custom code to run here
+            System.out.println("Logic5 " + i);
         }
+        int j = 1;
+        System.out.println("Logic6 " + j++);
+        System.out.println("Logic7");
+    }
+
+    static void bossLevelStuffNoFluff() {
+        System.out.println("Logic1");
+        System.out.println("Logic2");
+        System.out.println("Logic7");
+    }
+
+    static void bossLevelNoStuff() {
+        System.out.println("Logic1");
         System.out.println("Logic7");
     }
 }
