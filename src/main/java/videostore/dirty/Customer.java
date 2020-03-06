946d9e898a635324d73254e33b2c12743d638785
Victor Rentea <victorrentea@gmail.com>
2020-03-06 02:17:31 +0200

    adjust kata

--- src/main/java/videostore/dirty/Customer.java
+++ src/main/java/videostore/dirty/Customer.java
@@ -27,15 +27,15 @@ class Customer {
 			Rental each = (Rental) rentals.next();
 			// determine amounts for each line
 			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
+			case Movie.CATEGORY_REGULAR:
 				thisAmount += 2;
 				if (each.getDaysRented() > 2)
 					thisAmount += (each.getDaysRented() - 2) * 1.5;
 				break;
-			case Movie.NEW_RELEASE:
+			case Movie.CATEGORY_NEW_RELEASE:
 				thisAmount += each.getDaysRented() * 3;
 				break;
-			case Movie.CHILDRENS:
+			case Movie.CATEGORY_CHILDRENS:
 				thisAmount += 1.5;
 				if (each.getDaysRented() > 3)
 					thisAmount += (each.getDaysRented() - 3) * 1.5;
@@ -44,12 +44,12 @@ class Customer {
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+			if ((each.getMovie().getPriceCode() == Movie.CATEGORY_NEW_RELEASE)
 					&& each.getDaysRented() > 1)
 				frequentRenterPoints++;
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
+					+ thisAmount + "\n";
 			totalAmount += thisAmount;
 		}
 		// add footer lines
--- src/main/java/videostore/dirty/Movie.java
+++ src/main/java/videostore/dirty/Movie.java
@@ -1,8 +1,8 @@
 package videostore.dirty;
 public class Movie {
-	public static final int CHILDRENS = 2;
-	public static final int REGULAR = 0;
-	public static final int NEW_RELEASE = 1;
+	public static final int CATEGORY_CHILDRENS = 2;
+	public static final int CATEGORY_REGULAR = 0;
+	public static final int CATEGORY_NEW_RELEASE = 1;
 	private String _title;
 	private int _priceCode;
 
