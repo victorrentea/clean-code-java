43d97b17b8e1edfe054ab6b81d9a2ba7847a0c15
Victor Rentea <victorrentea@gmail.com>
2020-06-03 12:52:23 +0300

    Common data can become fields in the class

--- src/main/java/victor/training/cleancode/ManyParamsOOP.java
+++ src/main/java/victor/training/cleancode/ManyParamsOOP.java
@@ -8,44 +8,63 @@ import java.util.List;
 
 public class ManyParamsOOP {
     @Autowired
-    private Validator validator;
+    private OtherDependency dep;
+//    @Autowired
+//    private Validator validator;
 
     public void bizLogic() {
-        List<String> errors = new ArrayList<>();
-        validator.m1("a",1, errors);
-        validator.m2("b",1, errors);
-        validator.m3("file.txt", 1L,"ref", errors);
-        validator.m4("a", 1L,5L, "g", errors);
-        validator.m5(1, errors);
+        Validator validator = new Validator(dep);
+        validator.m1("a",1);
+        validator.m2("b",1);
+        validator.m3("file.txt", 1L,"ref");
+        validator.m4("a", 1L,5L, "g");
+        validator.m5(1);
+
+        List<String> errors = validator.getErrors();
         if (!errors.isEmpty()) {
             throw new IllegalArgumentException(errors.toString());
         }
     }
 }
-@Service
+//@Service cannot be // avoid: @Scope("prototype") + @Autowire ObjectFactory<Validator> !!!
 class Validator {
-    @Autowired
-	private OtherDependency dep;
+    List<String> errors = new ArrayList<>();
 
-    public void m1(String a, int b, List<String> errors) {
+//    public Validator(List<String> errors) {
+//        this.errors = errors;
+//    }
+
+//    @Autowired
+	private final OtherDependency dep;
+
+    Validator(OtherDependency dep) {
+        this.dep = dep;
+    }
+
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    public void m1(String a, int b) {
         if (a == null) {
             errors.add("a must not be null");
         }
         // stuff
     }
-    public void m2(String s, int c, List<String> errors) {
+    public void m2(String s, int c) {
         if (c < 0) {
             errors.add("negative c");
         }
         // stuff
     }
-    public void m3(String fileName, long versionId, String reference, List<String> errors) {
+    public void m3(String fileName, long versionId, String reference) {
         // stuff
     }
-    public void m4(String a, long listId, long recordId, String g, List<String> errors) {
+    public void m4(String a, long listId, long recordId, String g) {
         // stuff
     }
-    public void m5(int b, List<String> errors) {
+    public void m5(int b) {
         // stuff
     }
 }
