6520cbb4dcab4134c6d3c2292d8681e27f114459
Victor Rentea <victorrentea@gmail.com>
2020-06-03 12:58:08 +0300

    JavaxValidation groups crashcourse

--- src/main/java/victor/training/cleancode/ManyParamsOOP.java
+++ src/main/java/victor/training/cleancode/ManyParamsOOP.java
@@ -1,8 +1,11 @@
 package victor.training.cleancode;
 
+import org.hibernate.validator.constraints.Length;
 import victor.training.cleancode.pretend.Autowired;
 import victor.training.cleancode.pretend.Service;
 
+import javax.validation.ValidatorFactory;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,10 +29,31 @@ public class ManyParamsOOP {
         }
     }
 }
+//class ValidationSessionAtDraft {
+//class ValidationSessionAtEdit {
+//class ValidationSessionAtExport {
+//class ValidationSessionAtImport {
+//    private final Validator validator;
+//    ValidationSession(Validator validator) {
+//        this.validator = validator;
+//    }
+//
+//    List<String> errors = new ArrayList<>();
+//    validate(0)
+//}
 //@Service cannot be // avoid: @Scope("prototype") + @Autowire ObjectFactory<Validator> !!!
 class Validator {
-    List<String> errors = new ArrayList<>();
-
+//    interface DRAFT {}
+//    interface ACCEPTED {}
+//    @NotNull(groups = {DRAFT.class, ACCEPTED.class})
+//    @Length(min=20, groups = ACCEPTED.class)
+//    private String s;
+//    {
+//        javax.validation.Validator v;
+//        if (entity.state == Draft) {
+//            v.validate(entity, DRAFT.class);
+//        }
+//    }
 //    public Validator(List<String> errors) {
 //        this.errors = errors;
 //    }
