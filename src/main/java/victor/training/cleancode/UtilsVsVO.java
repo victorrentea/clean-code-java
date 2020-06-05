b45b49c30520bfbbdb4c1a9d39cf84a9bf999271
Victor Rentea <victorrentea@gmail.com>
2020-06-05 12:49:06 +0300

    Mutarea de metode statice + TEMA

--- src/main/java/victor/training/cleancode/UtilsVsVO.java
+++ src/main/java/victor/training/cleancode/UtilsVsVO.java
@@ -6,18 +6,31 @@ import java.util.List;
 public class UtilsVsVO {
     public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> models) {
         List<CarModel> results = new ArrayList<>(models);
-        results.removeIf(model -> ! intervalsIntersect(
+        results.removeIf(model -> ! MathUtil.intervalsIntersect(
                 model.getStartYear(), model.getEndYear(),
                 criteria.getStartYear(), criteria.getEndYear()));
         System.out.println("More filtering logic");
         return results;
     }
-    private boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
+}
+//class Interval {
+//    boolean intersects(Interval other) {}
+//} // TODO tema de weekend
+class AlteClase {
+    {
+        MathUtil.intervalsIntersect(1, 2, 3, 4);
+    }
+}
+class Constants {
+    public static final String SPACE = " ";
+}
+class MathUtil {
+
+    public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
         // http://world.std.com/~swmcd/steven/tech/interval.html
         return start1 <= end2 && start2 <= end1;
     }
 }
-
 
 
 
