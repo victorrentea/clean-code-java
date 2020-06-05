141a5103462c6fae7c1a0e6632f84f52397fa533
Victor Rentea <victorrentea@gmail.com>
2020-06-05 12:29:35 +0300

    Dublezi metoda ca sa scapi de param booleani. Grija e sa nu dublezi LOGICA -> de aia extragi intai mai multe functii

--- src/main/java/victor/training/cleancode/BouleanParameters.java
+++ src/main/java/victor/training/cleancode/BouleanParameters.java
@@ -5,62 +5,63 @@ import java.util.List;
 @SuppressWarnings("all")
 public class BouleanParameters {
 
-	public static void main(String[] args) {
-		// The method is called from various foreign places in the codebase
-		bigUglyMethod(2, 1, false);
-		bigUglyMethod(2, 1, false);
-		bigUglyMethod(2, 1, false);
-		bigUglyMethod(2, 1, false);
-		bigUglyMethod(2, 1, false);
+   public static void main(String[] args) {
+      // The method is called from various foreign places in the codebase
+      bigUglyMethod(2, 1);
+      bigUglyMethod(2, 1);
+      bigUglyMethod(2, 1);
+      bigUglyMethod(2, 1);
+      bigUglyMethod(2, 1);
 
-		// TODO From my use-case, I call it too, to do more within:
-		bigUglyMethod(2, 1, true);
+      // TODO From my use-case, I call it too, to do more within:
+      bigUglyMethodMyUC(2, 1);
 
-	}
+   }
 
-	static void bigUglyMethod(int b, int a, boolean myUC) {
-		System.out.println("Complex Logic " + a);
-		System.out.println("Complex Logic " + a);
-		System.out.println("Complex Logic " + a);
+   static void bigUglyMethod(int b, int a) {
+      beforeLogic(a);
+      afterLogic(b);
+   }
 
-		if (myUC) {
-			// in matzele ei: vrei sa faci si tu - doar cand tu chemi functia
-			System.out.println("ASTA");
-		}
+   static void bigUglyMethodMyUC(int b, int a) {
+      beforeLogic(a);
+      // in matzele ei: vrei sa faci si tu - doar cand tu chemi functia
+      System.out.println("ASTA");
+      afterLogic(b);
+   }
 
-		System.out.println("More Complex Logic " + b);
-		System.out.println("More Complex Logic " + b);
-		System.out.println("More Complex Logic " + b);
-	}
+   private static void afterLogic(int b) {
+      System.out.println("More Complex Logic " + b);
+      System.out.println("More Complex Logic " + b);
+      System.out.println("More Complex Logic " + b);
+   }
+
+   private static void beforeLogic(int a) {
+      System.out.println("Complex Logic " + a);
+      System.out.println("Complex Logic " + a);
+      System.out.println("Complex Logic " + a);
+   }
 
 
+   // ============== "BOSS" LEVEL: A lot harder to break down =================
 
-
-
-
-
-
-
-
-	// ============== "BOSS" LEVEL: A lot harder to break down =================
-
-	static void bossLevel(boolean stuff, boolean fluff, List<Integer> tasks) {
-		int i = 0; // TODO localize
-		int j = 1;
-		System.out.println("Logic1");
-		if (stuff) {
-			System.out.println("Logic2");
-			if (fluff) {
-				System.out.println("Logic3");
-				for (int task : tasks) {
-					i++;
-					System.out.println("Logic4 " + task);
-					// TODO HERE, when call this method, I want MY own custom code to run here
-					System.out.println("Logic5 " + i);
-				}
-				System.out.println("Logic6 " + j++);
-			}
-		}
-		System.out.println("Logic7");
-	}
+   static void bossLevel(boolean stuff, boolean fluff, List<Integer> tasks) {
+      int i = 0; // TODO localize
+      int j = 1;
+      System.out.println("Logic1");
+      if (stuff) {
+         System.out.println("Logic2");
+         if (fluff) {
+            System.out.println("Logic3");
+            for (int task : tasks) {
+               i++;
+               System.out.println("Logic4 " + task);
+               // TODO HERE, when call this method, I want MY own custom code to run here
+               System.out.println("Logic5 " + i);
+            }
+            System.out.println("Logic6 " + j++);
+         }
+      }
+      System.out.println("Logic7");
+   }
 }
