f5bfac262cff270c16e2cd974c514bc6b76c8663
Victor Rentea <victorrentea@gmail.com>
2020-06-06 23:02:09 +0300

    ParameterizeAndExtract - ENG

--- src/main/java/victor/training/refactoring/ParameterizeAndExtract.java
+++ src/main/java/victor/training/refactoring/ParameterizeAndExtract.java
@@ -3,16 +3,16 @@ package victor.training.refactoring;
 class ParameterizeAndExtract {
 
     public void f() {
-        System.out.println("Logica f");
+        System.out.println("Logic F");
         for (int i = 0; i < 4; i++) {
-            System.out.println("Cod " + i);
+            System.out.println("Code " + i);
         }
     }
 
     public void g() {
-        System.out.println("Logica g");
+        System.out.println("Logic G");
         for (int i = 0; i < 3; i++) {
-            String x = "Cod" +
+            String x = "Code" +
                     " " + i;
             System.out.println(x);
         }
