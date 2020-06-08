0a6a4d343bd39f9da6acd6055a4c688d737c047d
Victor Rentea <victorrentea@gmail.com>
2020-06-08 08:22:33 +0300

    Params vs VO

--- src/main/java/victor/training/cleancode/ManyParamsVO.java
+++ src/main/java/victor/training/cleancode/ManyParamsVO.java
@@ -1,58 +1,65 @@
 package victor.training.cleancode;
 
 public class ManyParamsVO {
-    public static void main(String[] args) {
-        new ManyParamsVO().placeOrder("John", "Doe", "St. Albergue", "Paris", 99);
-    }
-    public void placeOrder(String fName, String lName, String city, String streetName, Integer streetNumber) {
-    	if (fName == null || lName == null) throw new IllegalArgumentException();
-    	
-    	System.out.println("Some Logic");
-    }
+   public static void main(String[] args) {
+      new ManyParamsVO().placeOrder("John", "Doe", "St. Albergue", "Paris", 99);
+   }
+
+   public void placeOrder(String fName, String lName, String city, String streetName, Integer streetNumber) {
+      if (fName == null || lName == null) throw new IllegalArgumentException();
+
+      System.out.println("Some Logic");
+      System.out.println("Shipping to " + city + " on St. " + streetName + " " + streetNumber);
+
+   }
 }
 
 class AnotherClass {
-    public void otherMethod(String firstName, String lastName, int x) {
-    	if (firstName == null || lastName == null) throw new IllegalArgumentException();
-    	
-    	System.out.println("Another distant Logic");
-    }
+   public void otherMethod(String firstName, String lastName, int x) {
+      if (firstName == null || lastName == null) throw new IllegalArgumentException();
+
+      System.out.println("Another distant Logic " + x);
+      System.out.println("Person: " + lastName);
+   }
 }
 
 class Person {
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String phone;
+   private Long id;
+   private String firstName;
+   private String lastName;
+   private String phone;
 
-    public Person(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        if (firstName == null || lastName == null) throw new IllegalArgumentException();
-        // TODO think: is this sufficient enforcing ?
-    }
+   public Person(String firstName, String lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      if (firstName == null || lastName == null) throw new IllegalArgumentException();
+   }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    // TODO hard-core: implement setter
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    public String getFirstName() {
-        return firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   // TODO hard-core: implement setter
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
 }
 
 class PersonService {
-    public void f(Person person) {
-        String fullNameStr = person.getFirstName() + " " + person.getLastName().toUpperCase();
-        System.out.println(fullNameStr);
-    }
-    public void p(String city, String streetName, Integer streetNumber) {
-        System.out.println("Living in " + city + " on St. " + streetName + " " + streetNumber);
-    }
+   public void f(Person person) {
+      String fullNameStr = person.getFirstName() + " " + person.getLastName().toUpperCase();
+      System.out.println(fullNameStr);
+   }
+
+   public void p(String city, String streetName, Integer streetNumber) {
+      System.out.println("Living in " + city + " on St. " + streetName + " " + streetNumber);
+   }
 }
