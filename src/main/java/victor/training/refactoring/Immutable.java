128fcf197bd6198b050394902b87ad793c0d5fc8
Victor Rentea <victorrentea@gmail.com>
2020-06-09 12:04:57 +0300

    Immutable + cum pierzi un elem in HashSet

--- src/main/java/victor/training/refactoring/Immutable.java
+++ src/main/java/victor/training/refactoring/Immutable.java
@@ -1,21 +1,103 @@
 package victor.training.refactoring;
 
-import java.util.List;
+import java.sql.Connection;
+import java.util.*;
 
 class MutableHost { //think "Entity"
     public Immutable immutable;
 
     public static void main(String[] args) {
-        Immutable immutable = new Immutable();
-        immutable.x = 2;
-        System.out.println(immutable.x);
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        Immutable immutable = new Immutable(3, list, new Other(17));
+        list.add(2);
+        System.out.println(immutable.getX());
+        for (Integer number : immutable.getNumbers()) {
+            System.out.println("Elem " + number);
+        }
+//        immutable.getNumbers().add(3); // nu compileaza datorita genericelor (pt forma geek)
+//        immutable.getNumbers().containsAll(1,2,4)
+        System.out.println(immutable.getNumbers());
+
+        // cum pierzi copilul la Mall
+
+        Set<Object> puiiMei = new HashSet<>();
+        Copil childOne = new Copil("Emma");
+
+        puiiMei.add(childOne);
+
+        System.out.println(childOne.hashCode());
+        System.out.println("Unde e copilu? " + puiiMei.contains(childOne));
+//        childOne.setName("Emma-Simona"); // hashcode equals trebuie implem doar pe campuri nemodificabile!!
+        System.out.println(childOne.hashCode());
+        System.out.println("Unde e copilu? " + puiiMei.contains(childOne));
     }
 }
 
-public class Immutable {
-    public int x;
-    public List<Integer> numbers;
-    public Other other;
+class Copil{
+    private final String name;
+//    String prieten;
+
+    public Copil(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Copil copil = (Copil) o;
+        return Objects.equals(name, copil.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
 
-class Other {}
+
+public class Immutable {
+    private final int x;
+    private final List<Integer> numbers;
+    private final Other other;
+
+    public Immutable(int x, List<Integer> numbers, Other other) {
+        this.x = x;
+        this.numbers = new ArrayList<>(numbers);
+        this.other = other;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+//    public List<Integer> getNumbers() { // standard
+//        return Collections.unmodifiableList(numbers);
+//    }
+//    public Iterable<Integer> getNumbers() { // zgarcita
+//        return Collections.unmodifiableList(numbers);
+//    }
+    public Collection<? extends Integer> getNumbers() { // geek
+        return numbers;
+    }
+}
+
+class Other {
+    private final int x;
+
+    Other(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
