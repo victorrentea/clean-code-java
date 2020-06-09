d66e09842a5dc011b11ae052f358a288badf697a
Victor Rentea <victorrentea@gmail.com>
2020-06-09 10:20:20 +0300

    Eliminarea duplicarii radicala poate evidentia noi tipare in cod. NU-ti fie jena sa extragi functii microscopice.

--- src/main/java/victor/training/refactoring/ReplaceTempWithQuery.java
+++ src/main/java/victor/training/refactoring/ReplaceTempWithQuery.java
@@ -2,35 +2,37 @@ package victor.training.refactoring;
 
 public class ReplaceTempWithQuery {
 
-    private int quantity;
-    private double itemPrice;
+   private int quantity;
+   private double itemPrice;
 
-    public ReplaceTempWithQuery(int quantity, double itemPrice) {
-        this.quantity = quantity;
-        this.itemPrice = itemPrice;
-    }
+   public ReplaceTempWithQuery(int quantity, double itemPrice) {
+      this.quantity = quantity;
+      this.itemPrice = itemPrice;
+   }
 
 
-    public double computePrice() {
-        if (quantity > 10)
-            return basePrice() * 0.95;
-        else
-            return computeNormalPrice();
-    }
+   public double computePrice() {
+      if (quantity > 10)
+         return basePrice() * 0.95;
+      else
+         return basePrice() * normalPriceFactor();
+   }
 
-    private double basePrice() {
-        return quantity * itemPrice ;
-    }
+   //    @Contract(pure=true)
+   private double basePrice() {
+//        sql.insert(BOU);
+      return quantity * itemPrice;
+   }
 
-    private double computeNormalPrice() {
-        if (basePrice() > 1000) {
-            return basePrice() * 0.95;
-        } else {
-            return basePrice() * 0.98;
-        }
-    }
+   private double normalPriceFactor() {
+      if (basePrice() > 1000) {
+         return 0.95;
+      } else {
+         return 0.98;
+      }
+   }
 
-    public int computeFidelityPoints() {
-        return (int) (basePrice() / 2);
-    }
+   public int computeFidelityPoints() {
+      return (int) (basePrice() / 2);
+   }
 }
