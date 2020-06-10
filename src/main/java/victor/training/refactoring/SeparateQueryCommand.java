877f0e75989ec6f85bcefc8a060443bfe9ee408e
Victor Rentea <victorrentea@gmail.com>
2020-06-10 10:25:33 +0300

    Am copiat functia si am facut-o pure query

--- src/main/java/victor/training/refactoring/SeparateQueryCommand.java
+++ src/main/java/victor/training/refactoring/SeparateQueryCommand.java
@@ -2,6 +2,8 @@ package victor.training.refactoring;
 
 import java.util.List;
 
+import static java.util.Arrays.asList;
+
 public class SeparateQueryCommand {
 
     public String alertForMiscreant(List<String> people) {
@@ -13,6 +15,17 @@ public class SeparateQueryCommand {
             if (person.equals("John")) {
                 setOffAlarms();
                 return "John";
+            }
+        }
+        return "";
+    }
+
+
+    // pure query function
+    public String getMiscreant(List<String> people) {
+        for (String person : people) {
+            if (asList("Don", "John").contains(person)) {
+                return person;
             }
         }
         return "";
