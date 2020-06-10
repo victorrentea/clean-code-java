fb7e5efaee80d3cab4e04a6b37e787b3c577f40d
Victor Rentea <victorrentea@gmail.com>
2020-06-10 10:18:28 +0300

    Extract Method Object

--- src/main/java/victor/training/refactoring/ExtractMethodObject.java
+++ src/main/java/victor/training/refactoring/ExtractMethodObject.java
@@ -8,37 +8,56 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ExtractMethodObject {
-    private final EntityManager em;
+class CustomerRepo2 {
+   private final EntityManager em;
 
-    public ExtractMethodObject(EntityManager em) {
-        this.em = em;
-    }
+   public CustomerRepo2(EntityManager em) {
+      this.em = em;
+   }
 
-    public List<Long> search(CustomerSearchCriteria criteria) {
-        String jpql = "SELECT c.id FROM Customer c WHERE 1=1 ";
-        Map<String, Object> params = new HashMap<>();
+   public List<Long> search(CustomerSearchCriteria criteria) {
+      return new CustomerSearchQuery(em, criteria).search();
+   }
+}
 
-        if (StringUtils.isNotBlank(criteria.name)) {
-            jpql += " AND UPPER(c.name) LIKE '%' || UPPER(:name) || '%' ";
-            params.put("name", criteria.name);
-        }
+// method object
+class CustomerSearchQuery {
+   private final EntityManager em;
+   private final CustomerSearchCriteria criteria;
+   private String jpql = "SELECT c.id FROM Customer c WHERE 1=1 ";
+   private Map<String, Object> params = new HashMap<>();
 
-        if (criteria.countryId != null) {
-            jpql += " AND (c.residenceCountry.id = :countryId OR ..<5 lines of JPQL>..)";
-            params.put("countryId", criteria.countryId);
-        }
+   public CustomerSearchQuery(EntityManager em, CustomerSearchCriteria criteria) {
+      this.em = em;
+      this.criteria = criteria;
+   }
 
-        TypedQuery<Long> query = em.createQuery(jpql + " ", Long.class);
-        for (String param : params.keySet()) {
-            query.setParameter(param, params.get(param));
-        }
-        return query.getResultList();
-    }
+   public List<Long> search() {
+      addNameCriteria();
+      addCountryCriteria();
+
+      TypedQuery<Long> query = em.createQuery(jpql + " ", Long.class);
+      for (String param : params.keySet()) {
+         query.setParameter(param, params.get(param));
+      }
+      return query.getResultList();
+   }
+   private void addCountryCriteria() {
+      if (criteria.countryId != null) {
+         jpql += " AND (c.residenceCountry.id = :countryId OR ..<5 lines of JPQL>..)";
+         params.put("countryId", criteria.countryId);
+      }
+   }
+   private void addNameCriteria() {
+      if (StringUtils.isNotBlank(criteria.name)) {
+         jpql += " AND UPPER(c.name) LIKE '%' || UPPER(:name) || '%' ";
+         params.put("name", criteria.name);
+      }
+   }
 }
 
 class CustomerSearchCriteria {
 
-    public String name;
-    public Long countryId;
+   public String name;
+   public Long countryId;
 }
