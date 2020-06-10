eb77fc6200bbcce2c164b706c6d9c606ea2df097
Victor Rentea <victorrentea@gmail.com>
2020-06-10 11:01:43 +0300

    Regulile unui switch curat

--- src/main/java/victor/training/refactoring/SwitchHygiene.java
+++ src/main/java/victor/training/refactoring/SwitchHygiene.java
@@ -3,28 +3,46 @@ package victor.training.refactoring;
 public class SwitchHygiene {
     public void f(String messageType) {
         System.out.println("Cod inainte");
+        System.out.println("Cod inainte");
+        System.out.println("Cod inainte");
+        System.out.println("Cod inainte");
+        System.out.println("Cod inainte");
+        System.out.println("Cod inainte");
+        System.out.println("Cod inainte");
+        System.out.println("Cod inainte");
+        System.out.println("Cod inainte");
+        processByType(messageType);
+        System.out.println("Cod dupa");
+    }
+
+    private void processByType(String messageType) {
         switch (messageType) {
             case "PLACE_ORDER":
-                System.out.println("Marcel: pun #sieu aici ceva \n");
-                if (true) {
-                    try {
-                        System.out.println("Maricica: pun #sieu aici ceva \n");
-                    } catch (Exception $e) {
-                    }
-                }
-
-                System.out.println("Florin: pun #sieu aici ceva \n");
-                System.out.println("Marcel: pun #sieu aici ceva \n");
-                System.out.println("COD\n");
+                handlePlaceOrder();
+                break;
             case "CANCEL_ORDER":
                 System.out.println("COD2\n");
                 break;
             case "SHIP_ORDER":
                 System.out.println("COD3\n");
                 break;
+
             default:
-                throw new IllegalArgumentException("Unexpected value" + messageType);
+                throw new IllegalStateException("JDD: Unexpected value: " + messageType);
         }
-        System.out.println("Cod dupa");
+    }
+
+    private void handlePlaceOrder() {
+        System.out.println("Marcel: pun #sieu aici ceva \n");
+        if (true) {
+            try {
+                System.out.println("Maricica: pun #sieu aici ceva \n");
+            } catch (Exception $e) {
+            }
+        }
+
+        System.out.println("Florin: pun #sieu aici ceva \n");
+        System.out.println("Marcel: pun #sieu aici ceva \n");
+        System.out.println("COD\n");
     }
 }
