5b1b93922af7f8cb1fd770ec2eb5f194bf6a9de6
Victor Rentea <victorrentea@gmail.com>
2020-06-10 17:41:49 +0300

    Guard clauses: merge tests

--- src/main/java/victor/training/refactoring/GuardClauses.java
+++ src/main/java/victor/training/refactoring/GuardClauses.java
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
-public class GuardClauses {
+class GuardService {
    public int getPayAmount(Marine marine) {
       int result;
       if (!marine.isDead()) {
@@ -43,6 +43,53 @@ public class GuardClauses {
    }
 
 }
+
+
+public class GuardClauses {
+   private GuardService guards = new GuardService();
+
+   @Test
+   public void dead() {
+      Marine marine = new Marine().setDead(true);
+      assertEquals(1, guards.getPayAmount(marine));
+   }
+
+   @Test
+   public void retired() {
+      Marine marine = new Marine().setRetired(true);
+      assertEquals(2, guards.getPayAmount(marine));
+   }
+
+   @Test(expected = IllegalStateException.class)
+   public void nullYears() {
+      Marine marine = new Marine().setYearsService(null);
+      guards.getPayAmount(marine);
+   }
+
+   @Test
+   public void noAwards() {
+      Marine marine = new Marine().setYearsService(5);
+      assertEquals(500, guards.getPayAmount(marine));
+   }
+
+   @Test
+   public void oneAward() {
+      Marine marine = new Marine()
+          .setYearsService(5)
+          .addAward(new Award());
+      assertEquals(1500, guards.getPayAmount(marine));
+   }
+
+   @Test
+   public void manyAwards() {
+      Marine marine = new Marine()
+          .setYearsService(5)
+          .addAward(new Award(), new Award(), new Award());
+      assertEquals(3500, guards.getPayAmount(marine));
+   }
+}
+
+
 
 class Marine {
    private boolean dead;
