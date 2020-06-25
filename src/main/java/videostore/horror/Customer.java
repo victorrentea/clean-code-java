ba1fa976c4571227f11c0b4b828e706b295be4e1
Victor Rentea <victorrentea@gmail.com>
2020-06-25 16:48:48 +0300

    rename amount. Extrac header + footer

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -8,7 +8,7 @@ public class Customer {
 
 	public Customer(String name) {
 		this.name = name;
-	};
+	}
 
 	public void addRental(Movie movie, int days) {
 		rentals.add(new Rental(movie, days));
@@ -19,28 +19,31 @@ public class Customer {
 	}
 
 	public String statement() {
-		double totalAmount = 0;
+		double totalPrice = 0;
 		int frequentRenterPoints = 0;
-		String result = "Rental Record for " + name + "\n";
+		String result = createHeader();
 
 		for (Rental rental : rentals) {
-			double thisAmount = computeAmount(rental);
-			frequentRenterPoints += addRenterPoints(rental);
-
-
+			double price = computePrice(rental);
+			frequentRenterPoints += computeRenterPoints(rental);
 			// show figures line for this rental
-			result += "\t" + rental.getMovie().getTitle() + "\t"
-				+ String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + rental.getMovie().getTitle() + "\t" + price + "\n";
+			totalPrice += price;
 		}
-		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
-				+ " frequent renter points";
+		result += createFooter(totalPrice, frequentRenterPoints);
 		return result;
 	}
 
-	private int addRenterPoints(Rental rental) {
+	private String createHeader() {
+		return "Rental Record for " + name + "\n";
+	}
+
+	private String createFooter(double totalPrice, int frequentRenterPoints) {
+		return "Amount owed is " + totalPrice + "\n" +
+			"You earned " + frequentRenterPoints + " frequent renter points";
+	}
+
+	private int computeRenterPoints(Rental rental) {
 		int points = 1;
 		boolean isNewRelease = rental.getMovie().getPriceCode() == PriceCode.NEW_RELEASE;
 		if (isNewRelease && rental.getDaysRented() >= 2) {
@@ -49,26 +52,25 @@ public class Customer {
 		return points;
 	}
 
-	private double computeAmount(Rental rental) {
-		double thisAmount = 0;
-		// determine amounts for each line
+	private double computePrice(Rental rental) {
+		double price = 0;
 		switch (rental.getMovie().getPriceCode()) {
 			case REGULAR:
-				thisAmount += 2;
+				price += 2;
 				if (rental.getDaysRented() > 2) {
-					thisAmount += (rental.getDaysRented() - 2) * 1.5;
+					price += (rental.getDaysRented() - 2) * 1.5;
 				}
 				break;
 			case NEW_RELEASE:
-				thisAmount += rental.getDaysRented() * 3;
+				price += rental.getDaysRented() * 3;
 				break;
 			case CHILDRENS:
-				thisAmount += 1.5;
+				price += 1.5;
 				if (rental.getDaysRented() > 3) {
-					thisAmount += (rental.getDaysRented() - 3) * 1.5;
+					price += (rental.getDaysRented() - 3) * 1.5;
 				}
 				break;
 		}
-		return thisAmount;
+		return price;
 	}
 }
