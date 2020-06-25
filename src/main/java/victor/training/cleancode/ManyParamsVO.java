4d0fb4660a62fbbbc92b9508dbf6449fab8de18b
Victor Rentea <victorrentea@gmail.com>
2020-06-25 11:06:33 +0300

    Introduce Paramter Object si impins peste tot in cod unde era folosita grama de date numita "firstname, lastname"

--- src/main/java/victor/training/cleancode/ManyParamsVO.java
+++ src/main/java/victor/training/cleancode/ManyParamsVO.java
@@ -2,11 +2,12 @@ package victor.training.cleancode;
 
 public class ManyParamsVO {
    public static void main(String[] args) {
-      new ManyParamsVO().placeOrder("John", "Doe", "St. Albergue", "Paris", 99);
+      FullName fullName = new FullName("John", "Doe");
+      new ManyParamsVO().placeOrder(fullName, "St. Albergue", "Paris", 99);
    }
 
-   public void placeOrder(String fName, String lName, String city, String streetName, Integer streetNumber) {
-      if (fName == null || lName == null) throw new IllegalArgumentException();
+   public void placeOrder(FullName fullName, String city, String streetName, Integer streetNumber) {
+      if (fullName.getFirstName() == null || fullName.getLastName() == null) throw new IllegalArgumentException();
 
       System.out.println("Some Logic");
       System.out.println("Shipping to " + city + " on St. " + streetName + " " + streetNumber);
@@ -14,36 +15,15 @@ public class ManyParamsVO {
    }
 }
 
-class AnotherClass {
-   public void otherMethod(String firstName, String lastName, int x) {
-      if (firstName == null || lastName == null) throw new IllegalArgumentException();
+//class Name { gresit. prea vag si prea general
+class FullName {
+   private final String firstName;
+   private final String lastName;
 
-      System.out.println("Another distant Logic " + x);
-      System.out.println("Person: " + lastName);
-   }
-}
-
-class Person {
-   private Long id;
-   private String firstName;
-   private String lastName;
-   private String phone;
-
-   public Person(String firstName, String lastName) {
+   public FullName(String firstName, String lastName) {
       this.firstName = firstName;
       this.lastName = lastName;
-      if (firstName == null || lastName == null) throw new IllegalArgumentException();
    }
-
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
-
-   // TODO hard-core: implement setter
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
-
    public String getFirstName() {
       return firstName;
    }
@@ -53,9 +33,41 @@ class Person {
    }
 }
 
+class AnotherClass {
+   // nu e codu tau si totusi il imbunatatesti
+   public void otherMethod(FullName fullName, int x) {
+      if (fullName.getFirstName() == null || fullName.getLastName() == null) throw new IllegalArgumentException();
+
+      System.out.println("Another distant Logic " + x);
+      System.out.println("Person: " + fullName.getLastName());
+   }
+}
+
+//@Entity
+class Person {
+   private Long id;
+   private FullName fullName;
+   private String phone;
+
+   public Person(String firstName, String lastName) {
+      this.fullName = new FullName(firstName, lastName);
+      if (firstName == null || lastName == null) throw new IllegalArgumentException();
+   }
+
+
+   // TODO hard-core: implement setter
+//   public void setLastName(String lastName) {
+//      this.lastName = lastName;
+//   }
+   public FullName getFullName() {
+      return fullName;
+   }
+
+}
+
 class PersonService {
    public void f(Person person) {
-      String fullNameStr = person.getFirstName() + " " + person.getLastName().toUpperCase();
+      String fullNameStr = person.getFullName().getFirstName() + " " + person.getFullName().getLastName().toUpperCase();
       System.out.println(fullNameStr);
    }
 
