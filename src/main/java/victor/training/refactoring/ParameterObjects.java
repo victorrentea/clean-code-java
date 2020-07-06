a53a0e52f5d7e08b1cb736c2a4cdb1d004b5b600
Victor Rentea <victorrentea@gmail.com>
2020-07-06 16:48:25 +0300

    Introduce Parameter Object - group params together

--- src/main/java/victor/training/refactoring/ParameterObjects.java
+++ src/main/java/victor/training/refactoring/ParameterObjects.java
@@ -1,18 +1,53 @@
 package victor.training.refactoring;
 
+import java.math.BigDecimal;
+
+class Salary {
+   private final BigDecimal amount;
+
+   Salary(BigDecimal amount) {
+      this.amount = amount;
+   }
+   public Salary increase(double percent) {
+      return new Salary(amount.multiply(BigDecimal.valueOf(percent)));
+   }
+}
+
 public class ParameterObjects {
    public static void main(String[] args) {
-      new ParameterObjects().placeOrder("John", "Doe", "St. Albergue", "Paris", 99);
+      FullName fullName = new FullName("John", "Doe");
+      new ParameterObjects().placeOrder(fullName, "St. Albergue", "Paris", 99);
    }
 
-   public void placeOrder(String fName, String lName, String city, String streetName, Integer streetNumber) {
-      if (fName == null || lName == null) throw new IllegalArgumentException();
+   public void placeOrder(FullName fullName, String city, String streetName, Integer streetNumber) {
+      if (fullName.getFirstName() == null || fullName.getLastName() == null) throw new IllegalArgumentException();
 
       System.out.println("Some Logic");
       System.out.println("Shipping to " + city + " on St. " + streetName + " " + streetNumber);
-
    }
 }
+
+//class Customer { // to generic, will grow enormously
+//class Name { // to generic. a company also has a "name"
+//class CustomerName { // to specific. a User also has first and last
+class FullName { //perfect
+   private final String firstName;
+   private final String lastName;
+
+   public FullName(String firstName, String lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+}
+
 
 class AnotherClass {
    public void otherMethod(String firstName, String lastName, int x) {
