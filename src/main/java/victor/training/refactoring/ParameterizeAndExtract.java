080ebdef445751a5e7c19cde8ed199e0dc4b1983
Victor Rentea <victorrentea@gmail.com>
2020-07-06 15:59:55 +0300

    1) Extract Variable to make the 2+ pieces of code synctactically equivalent, 2) Extract Method : IDE should tellyou that it will replace duplicated code 3) Inline (some) variables from step1 back into the method calls.

--- src/main/java/victor/training/refactoring/ParameterizeAndExtract.java
+++ src/main/java/victor/training/refactoring/ParameterizeAndExtract.java
@@ -4,16 +4,19 @@ class ParameterizeAndExtract {
 
     public void f() {
         System.out.println("Logic F");
-        for (int i = 0; i < 4; i++) {
-            System.out.println("Code " + i);
-        }
+
+        extracted(4);
     }
     public void g() {
         System.out.println("Logic G");
-        for (int i = 0; i < 3; i++) {
-            String x = "Code" +
-                    " " + i;
-            System.out.println(x);
+
+
+        extracted(3);
+    }
+
+    private void extracted(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("Code 10lines " + i);
         }
     }
 
