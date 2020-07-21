0aea25b5edba7fb5bbec78c1a9b6c41815093650
Victor Rentea <victorrentea@gmail.com>
2020-07-21 10:16:31 +0300

    Am introdus un mic Value Object (doar campuri finale, mic, si dragutz)

--- src/main/java/victor/training/cleancode/UtilsVsVO.java
+++ src/main/java/victor/training/cleancode/UtilsVsVO.java
@@ -8,25 +8,44 @@ public class UtilsVsVO {
 
     // Autovit.ro
     public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> models) {
+        Interval criteriaInterval = new Interval(criteria.getStartYear(), criteria.getEndYear());
         List<CarModel> results = new ArrayList<>(models);
-        results.removeIf(model -> ! MathUtil.intervalsIntersect(
-                model.getStartYear(), model.getEndYear(),
-                criteria.getStartYear(), criteria.getEndYear()));
+        results.removeIf(model -> !MathUtil.intervalIntersect(
+            new Interval(model.getStartYear(), model.getEndYear()), criteriaInterval));
         System.out.println("More filtering logic");
         return results;
     }
 }
 class Alta {
     {
-        MathUtil.intervalsIntersect(1, 3, 2, 4);
+        MathUtil.intervalIntersect(new Interval(1, 3), new Interval(2, 4));
     }
 }
+class Interval  {
+    private final int start;
+    private final int end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
+
 class MathUtil {
 
-    public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
+    public static boolean intervalIntersect(Interval interval1, Interval interval2) {
         // http://world.std.com/~swmcd/steven/tech/interval.html
-        return start1 <= end2 && start2 <= end1;
+        return interval1.getStart() <= interval2.getEnd() && interval2.getStart() <= interval2.getEnd();
     }
+
 }
 
 
