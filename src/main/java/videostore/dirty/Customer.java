82d5cd245cf5cbedb1aba0b0fc13b7e1fb10b717
Victor Rentea <victorrentea@gmail.com>
2020-07-28 18:43:13 +0300

    Autoformat

--- src/main/java/videostore/dirty/Customer.java
+++ src/main/java/videostore/dirty/Customer.java
@@ -24,18 +24,16 @@ class Customer {
 		String result = "Rental Record for " + getName() + "\n";
 		
 		for (Rental rental:rentals) {
-			
+
 			// determine amounts for each line
 			double currentPrice = determineAmount(rental);
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if ((rental.getMovie().getCategory() == Movie.Category.NEW_RELEASE)
-					&& rental.getDaysRented() > 1)
+			if ((rental.getMovie().getCategory() == Movie.Category.NEW_RELEASE) && rental.getDaysRented() > 1)
 				frequentRenterPoints++;
 			// show figures for this rental
-			result += "\t" + rental.getMovie().getTitle() + "\t"
-					+ currentPrice + "\n";
+			result += "\t" + rental.getMovie().getTitle() + "\t" + currentPrice + "\n";
 			totalPrice += currentPrice;
 		}
 		//pe master direct
