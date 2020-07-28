a15991438af752669b92aaace5d8bbf78a350f78
Victor Rentea <victorrentea@gmail.com>
2020-07-28 17:48:10 +0300

    Take1

--- src/main/java/videostore/dirty/Movie.java
+++ src/main/java/videostore/dirty/Movie.java
@@ -1,25 +1,32 @@
 package videostore.dirty;
+
+import static org.apache.commons.lang.StringUtils.isBlank;
+
+import org.apache.commons.lang.StringUtils;
+import org.hamcrest.core.IsNot;
+
 public class Movie {
+	
+	
 	public static final int CATEGORY_CHILDRENS = 2;
 	public static final int CATEGORY_REGULAR = 0;
 	public static final int CATEGORY_NEW_RELEASE = 1;
-	private String _title;
-	private int _priceCode;
+	private final String title;
+	private final int priceCode;
 
 	public Movie(String title, int priceCode) {
-		_title = title;
-		_priceCode = priceCode;
+		if (isBlank(title)) {
+			throw new IllegalArgumentException();
+		}
+		this.title = title;
+		this.priceCode = priceCode;
 	}
 
 	public int getPriceCode() {
-		return _priceCode;
-	}
-
-	public void setPriceCode(int arg) {
-		_priceCode = arg;
+		return priceCode;
 	}
 
 	public String getTitle() {
-		return _title;
-	};
+		return title;
+	}
 }
