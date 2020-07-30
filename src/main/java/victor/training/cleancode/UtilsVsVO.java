6da4506604f15aa0ad7acba6fd4f547c207dd150
Victor Rentea <victorrentea@gmail.com>
2020-07-30 17:51:23 +0300

    remove doc

--- src/main/java/victor/training/cleancode/UtilsVsVO.java
+++ src/main/java/victor/training/cleancode/UtilsVsVO.java
@@ -13,7 +13,6 @@ public class UtilsVsVO {
         return results;
     }
     private boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
-        // http://world.std.com/~swmcd/steven/tech/interval.html
         return start1 <= end2 && start2 <= end1;
     }
 }
