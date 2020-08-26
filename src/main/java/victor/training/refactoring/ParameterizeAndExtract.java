d556e6c858f12ad0a3da3855c42fe697e6378d83
Victor Rentea <victorrentea@gmail.com>
2020-08-26 21:12:09 +0300

    Parameterize a function

--- src/main/java/victor/training/refactoring/ParameterizeAndExtract.java
+++ src/main/java/victor/training/refactoring/ParameterizeAndExtract.java
@@ -4,17 +4,18 @@ class ParameterizeAndExtract {
 
     public void f() {
         System.out.println("Logic F");
-        for (int i = 0; i < 4; i++) {
+        howTheHeck(4);
+    }
+
+    private void howTheHeck(int n) {
+        for (int i = 0; i < n; i++) {
             System.out.println("Code " + i);
         }
     }
+
     public void g() {
         System.out.println("Logic G");
-        for (int i = 0; i < 3; i++) {
-            String x = "Code" +
-                    " " + i;
-            System.out.println(x);
-        }
+        howTheHeck(3);
     }
 
 }
