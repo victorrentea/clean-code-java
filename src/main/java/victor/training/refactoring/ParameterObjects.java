f445450ff9367916fbb83214a10ff608407e356a
Victor Rentea <victorrentea@gmail.com>
2020-08-26 21:34:45 +0300

    Introduced parameter object to carry 2 strings (PersonName)

--- src/main/java/victor/training/refactoring/ParameterObjects.java
+++ src/main/java/victor/training/refactoring/ParameterObjects.java
@@ -2,17 +2,44 @@ package victor.training.refactoring;
 
 public class ParameterObjects {
    public static void main(String[] args) {
-      new ParameterObjects().placeOrder("John", "Doe", "St. Albergue", "Paris", 99);
+      PersonName name = new PersonName("John", "Doe");
+      new ParameterObjects().placeOrder(name, "St. Albergue", "Paris", 99);
    }
 
-   public void placeOrder(String fName, String lName, String city, String streetName, Integer streetNumber) {
-      if (fName == null || lName == null) throw new IllegalArgumentException();
+   public void placeOrder(PersonName name, String city, String streetName, Integer streetNumber) {
+      if (name.getFirstName() == null || name.getLastName() == null) throw new IllegalArgumentException();
 
       System.out.println("Some Logic");
       System.out.println("Shipping to " + city + " on St. " + streetName + " " + streetNumber);
 
    }
 }
+
+//class OrderPlaceRequest { // too specific. Overengineering: won't ever be reused. It's just a blind action.
+//class Person { // overly generic, can explode with too much data.
+//class Customer {// overly generic, can explode with too much data.
+//class Name { // not precise. A company has a Name too
+//@Data :)
+// record java 13
+class PersonName { // not precise. A company has a Name too
+   private final String firstName;
+   private final String lastName;
+
+   public PersonName(String firstName, String lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+}
+
+
 
 class AnotherClass {
    public void otherMethod(String firstName, String lastName, int x) {
