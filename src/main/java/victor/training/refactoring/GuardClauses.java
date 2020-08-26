9d66a4e9fc4a7578b9668606782b16f6cc928196
Victor Rentea <victorrentea@gmail.com>
2020-08-26 20:20:19 +0300

    introduced guard clauses and extract a conditional into a private function next to me

--- src/main/java/victor/training/refactoring/GuardClauses.java
+++ src/main/java/victor/training/refactoring/GuardClauses.java
@@ -11,27 +11,27 @@ import static org.junit.Assert.assertEquals;
 
 class GuardService {
    public int getPayAmount(Marine marine) {
-      int result;
-      if (!marine.isDead()) {
-         if (!marine.isRetired()) {
-            if (marine.getYearsService() != null) {
-               result = marine.getYearsService() * 100;
-               if (!marine.getAwards().isEmpty()) {
-                  result += 1000;
-               }
-               if (marine.getAwards().size() >= 3) {
-                  result += 2000;
-               }
-            } else {
-               throw new IllegalStateException("Any marine should have the years of service set");
-            }
-         } else {
-            result = retiredAmount();
-         }
-      } else {
-         result = deadAmount();
+      if (marine.isDead()) {
+         return deadAmount();
+      }
+      if (marine.isRetired()) {
+         return retiredAmount();
+      }
+      if (marine.getYearsService() == null) {
+         throw new IllegalStateException("Any marine should have the years of service set");
+      }
+      int result = marine.getYearsService() * 100;
+      if (wasAwarded(marine)) {
+         result += 1000;
+      }
+      if (marine.getAwards().size() >= 3) {
+         result += 2000;
       }
       return result;
+   }
+
+   private boolean wasAwarded(Marine marine) {
+      return !marine.getAwards().isEmpty();
    }
 
    private int deadAmount() {
