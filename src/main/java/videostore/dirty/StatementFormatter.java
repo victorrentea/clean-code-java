2f74e01cd9a4867cf195f3517411f8229065f69f
Victor Rentea <victorrentea@gmail.com>
2020-09-07 17:50:51 +0300

    Split Loop + .stream() + mult extract/inline duce la cod incredibil de concis. Dar e prea mult pentru viata reala

--- src/main/java/videostore/dirty/StatementFormatter.java
+++ src/main/java/videostore/dirty/StatementFormatter.java
@@ -2,31 +2,44 @@ package videostore.dirty;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.joining;
+
 public class StatementFormatter {
    public String generateStatement(String customerName, List<Rental> rentals) {
-      double totalPrice = 0;
-      int frequentRenterPoints = 0; // TODO =.size()
-      String result = formatHeader(customerName);
-      for (Rental rental : rentals) {
-         frequentRenterPoints += rental.determineFrequentRenterPoints();
-         result += formatRentalLine(rental);
-         totalPrice += rental.determinePrice();
-      }
-      result += formatFooter(totalPrice, frequentRenterPoints);
-      return result;
+      return formatHeader(customerName)
+             + formatBody(rentals)
+             + formatFooter(rentals);
    }
 
    private String formatHeader(String customerName) {
       return "Rental Record for " + customerName + "\n";
    }
 
-   private String formatFooter(double totalPrice, int frequentRenterPoints) {
-      return "Amount owed is " + totalPrice + "\n"
-             + "You earned " + frequentRenterPoints + " frequent renter points";
+   private String formatBody(List<Rental> rentals) {
+      return rentals.stream().map(this::formatRentalLine).collect(joining());
    }
 
+   private int determineTotalPoints(List<Rental> rentals) {
+      return rentals.stream().mapToInt(Rental::determineFrequentRenterPoints).sum();
+   }
+
+   private double determineTotalPrice(List<Rental> rentals) {
+      return rentals.stream().mapToDouble(Rental::determinePrice).sum();
+   }
+
+//   class StatementData {
+//      double totalPrice = 0;
+//      int frequentRenterPoints = 0; // TODO =.size()
+//      String result;
+
+//   }
    private String formatRentalLine(Rental rental) {
       return "\t" + rental.getMovie().getTitle() + "\t" + rental.determinePrice() + "\n";
+   }
+
+   private String formatFooter(List<Rental> rentals) {
+      return "Amount owed is " + determineTotalPrice(rentals) + "\n"
+             + "You earned " + determineTotalPoints(rentals) + " frequent renter points";
    }
 
 }
