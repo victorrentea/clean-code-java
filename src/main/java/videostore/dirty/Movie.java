31e3b89a308b7d97a688fdf181d6a80ecc0f26bb
Victor Rentea <victorrentea@gmail.com>
2020-09-07 15:31:25 +0300

    cleaned up Movie: next: constant->enum

--- src/main/java/videostore/dirty/Movie.java
+++ src/main/java/videostore/dirty/Movie.java
@@ -1,25 +1,23 @@
 package videostore.dirty;
+
 public class Movie {
-	public static final int CATEGORY_CHILDRENS = 2;
-	public static final int CATEGORY_REGULAR = 0;
-	public static final int CATEGORY_NEW_RELEASE = 1;
-	private String _title;
-	private int _priceCode;
+   public static final int CATEGORY_CHILDRENS = 2;
+   public static final int CATEGORY_REGULAR = 0;
+   public static final int CATEGORY_NEW_RELEASE = 1;
 
-	public Movie(String title, int priceCode) {
-		_title = title;
-		_priceCode = priceCode;
-	}
+   private final String title;
+   private final int priceCode;
 
-	public int getPriceCode() {
-		return _priceCode;
-	}
+   public Movie(String title, int priceCode) {
+      this.title = title;
+      this.priceCode = priceCode;
+   }
 
-	public void setPriceCode(int arg) {
-		_priceCode = arg;
-	}
+   public int getPriceCode() {
+      return priceCode;
+   }
 
-	public String getTitle() {
-		return _title;
-	};
+   public String getTitle() {
+      return title;
+   }
 }
