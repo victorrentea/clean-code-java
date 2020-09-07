7ae6fe62144013a01479325fa3e73df8c9b76428
Victor Rentea <victorrentea@gmail.com>
2020-09-07 11:00:43 +0300

    Utils ---> Value Object + OOP (metode in el)

--- src/main/java/victor/training/cleancode/UtilsVsVO.java
+++ src/main/java/victor/training/cleancode/UtilsVsVO.java
@@ -4,76 +4,91 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UtilsVsVO {
-    public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> models) {
-        List<CarModel> results = new ArrayList<>(models);
-        results.removeIf(model -> ! intervalsIntersect(
-                model.getStartYear(), model.getEndYear(),
-                criteria.getStartYear(), criteria.getEndYear()));
-        System.out.println("More filtering logic");
-        return results;
-    }
-    private boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
-        return start1 <= end2 && start2 <= end1;
-    }
+   public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> models) {
+         Interval criteriaInterval = new Interval(criteria.getStartYear(), criteria.getEndYear());
+      List<CarModel> results = new ArrayList<>(models);
+      results.removeIf(model -> !model.getYearInterval().intersects(criteriaInterval));
+      System.out.println("More filtering logic");
+      return results;
+   }
 }
+class Altu {
+   public static void main(String[] args) {
+      System.out.println(new Interval(1, 3).intersects(new Interval(2, 4)));
+   }
+}
+class Interval {
+   private final int start;
+   private final int end;
+   public Interval(int start, int end) {
+      this.start = start;
+      this.end = end;
+   }
+   public boolean intersects(Interval other) {
+      return start <= other.end && other.start <= end;
+   }
+}
+class MathUtil {
 
-
-
-
+}
 
 
 class CarSearchCriteria {
-    private final int startYear;
-    private final int endYear;
-    private final String make;
+   private final int startYear;
+   private final int endYear;
+   private final String make;
 
-    public CarSearchCriteria(int startYear, int endYear, String make) {
-        this.make = make;
-        if (startYear > endYear) throw new IllegalArgumentException("start larger than end");
-        this.startYear = startYear;
-        this.endYear = endYear;
-    }
+   public CarSearchCriteria(int startYear, int endYear, String make) {
+      this.make = make;
+      if (startYear > endYear) throw new IllegalArgumentException("start larger than end");
+      this.startYear = startYear;
+      this.endYear = endYear;
+   }
 
-    public int getStartYear() {
-        return startYear;
-    }
+   public int getStartYear() {
+      return startYear;
+   }
 
-    public int getEndYear() {
-        return endYear;
-    }
+   public int getEndYear() {
+      return endYear;
+   }
 
-    public String getMake() {
-        return make;
-    }
+   public String getMake() {
+      return make;
+   }
 }
 
 class CarModel {
-    private final String make;
-    private final String model;
-    private final int startYear;
-    private final int endYear;
+   private final String make;
+   private final String model;
+   private final int startYear;
+   private final int endYear;
 
-    public CarModel(String make, String model, int startYear, int endYear) {
-        this.make = make;
-        this.model = model;
-        if (startYear > endYear) throw new IllegalArgumentException("start larger than end");
-        this.startYear = startYear;
-        this.endYear = endYear;
-    }
+   public CarModel(String make, String model, int startYear, int endYear) {
+      this.make = make;
+      this.model = model;
+      if (startYear > endYear) throw new IllegalArgumentException("start larger than end");
+      this.startYear = startYear;
+      this.endYear = endYear;
+   }
 
-    public int getEndYear() {
-        return endYear;
-    }
+   public int getEndYear() {
+      return endYear;
+   }
 
-    public int getStartYear() {
-        return startYear;
-    }
+   public int getStartYear() {
+      return startYear;
+   }
 
-    public String getMake() {
-        return make;
-    }
+   public String getMake() {
+      return make;
+   }
 
-    public String getModel() {
-        return model;
-    }
+   public String getModel() {
+      return model;
+   }
+
+   public Interval getYearInterval() {
+      return new Interval(startYear, endYear);
+   }
 }
