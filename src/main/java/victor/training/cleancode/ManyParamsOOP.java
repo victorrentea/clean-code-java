4786f59aac2ac0a101c059c2d9fea0fe16be47bc
Victor Rentea <victorrentea@gmail.com>
2020-09-07 11:46:23 +0300

    format

--- src/main/java/victor/training/cleancode/ManyParamsOOP.java
+++ src/main/java/victor/training/cleancode/ManyParamsOOP.java
@@ -8,49 +8,55 @@ import java.util.List;
 
 public class ManyParamsOOP {
 
-    public void bizLogic() {
-         Validator validator = new Validator();
-        validator.m1("a",1);
-        validator.m2("b",1);
-        validator.m3("file.txt", 1L,"ref");
-        validator.m4("a", 1L,5L, "g");
-        validator.m5(1);
-        List<String> errors = validator.getErrors();
-        if (!errors.isEmpty()) {
-            throw new IllegalArgumentException(errors.toString());
-        }
-    }
+
+   public void bizLogic() {
+      Validator validator = new Validator();
+      validator.m1("a", 1);
+      validator.m2("b", 1);
+      validator.m3("file.txt", 1L, "ref");
+      validator.m4("a", 1L, 5L, "g");
+      validator.m5(1);
+      List<String> errors = validator.getErrors();
+      if (!errors.isEmpty()) {
+         throw new IllegalArgumentException(errors.toString());
+      }
+   }
 }
+
 class Validator {
-	private OtherDependency dep;
-    private List<String> errors = new ArrayList<>();
+   private OtherDependency dep;
+   private List<String> errors = new ArrayList<>();
 
-    public List<String> getErrors() {
-        return errors;
-    }
+   public List<String> getErrors() {
+      return errors;
+   }
 
-    public void m1(String a, int b) {
-        if (a == null) {
-            errors.add("a must not be null");
-        }
-        // stuff
+   public void m1(String a, int b) {
+      if (a == null) {
+         errors.add("a must not be null");
+      }
+      // stuff
 
-    }
-    public void m2(String s, int c) {
-        if (c < 0) {
-            errors.add("negative c");
-        }
-        // stuff
-    }
-    public void m3(String fileName, long versionId, String reference) {
-        // stuff
-    }
-    public void m4(String a, long listId, long recordId, String g) {
-        // stuff
-    }
-    public void m5(int b) {
-        // stuff
-    }
+   }
+
+   public void m2(String s, int c) {
+      if (c < 0) {
+         errors.add("negative c");
+      }
+      // stuff
+   }
+
+   public void m3(String fileName, long versionId, String reference) {
+      // stuff
+   }
+
+   public void m4(String a, long listId, long recordId, String g) {
+      // stuff
+   }
+
+   public void m5(int b) {
+      // stuff
+   }
 }
 
 @Service
