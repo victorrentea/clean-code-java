2161ddd1a8d3464a412bf9fe2c7835e8d60a726c
Victor Rentea <victorrentea@gmail.com>
2020-09-07 12:28:20 +0300

    Eliminarea unui Boolean din semnatura unei functii de cele mai multe ori implica dublare de cod. E datoria noastra sa reducem nr de linii de cod copiate extragand metode in prealabil.

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -5,22 +5,24 @@ import java.util.List;
 public class BooleanParameters {
    public static void main(String[] args) {
       // The method is called from various foreign places in the codebase
-      bigUglyMethod(1, 5, false);
-      bigUglyMethod(2, 4, false);
-      bigUglyMethod(3, 3, false);
-      bigUglyMethod(4, 2, false);
-      bigUglyMethod(5, 1, false);
+      bigUglyMethod(1, 5);
+      bigUglyMethod(2, 4);
+      bigUglyMethod(3, 3);
+      bigUglyMethod(4, 2);
+      bigUglyMethod(5, 1);
 
       // TODO From my use-case CR323, I call it too, to do more within:
-      bigUglyMethod(2, 1, true);
+      bigUglyMethod323(2, 1);
 
    }
 
-   static void bigUglyMethod(int b, int a, boolean cr323) {
+   static void bigUglyMethod(int b, int a) {
       beforeLogic(b, a);
-      if (cr323) {
-         System.out.println("Si asta " + a + b);
-      }
+      afterLogic(b);
+   }
+   static void bigUglyMethod323(int b, int a) {
+      beforeLogic(b, a);
+      System.out.println("Si asta " + a + b);
       afterLogic(b);
    }
 
