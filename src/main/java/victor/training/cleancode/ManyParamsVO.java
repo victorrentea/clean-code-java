1e3531940d82c276a7bc37e4830275e767931a77
Victor Rentea <victorrentea@gmail.com>
2020-09-07 11:34:40 +0300

    introdus VO + validari in constructorul lui + folosirea in Entity model

--- src/main/java/victor/training/cleancode/ManyParamsVO.java
+++ src/main/java/victor/training/cleancode/ManyParamsVO.java
@@ -2,60 +2,93 @@ package victor.training.cleancode;
 
 public class ManyParamsVO {
    public static void main(String[] args) {
-      new ManyParamsVO().placeOrder("John", "Doe", "St. Albergue", "Paris", 99);
+      new ManyParamsVO().placeOrder(new FullName("John", "Doe"), new Address("St. Albergue", "Paris", 99));
    }
 
-   public void placeOrder(String fName, String lName, String city, String streetName, Integer streetNumber) {
-      if (fName == null || lName == null) throw new IllegalArgumentException();
-
+   public void placeOrder(FullName fullName, Address address) {
       System.out.println("Some Logic");
-      System.out.println("Shipping to " + city + " on St. " + streetName + " " + streetNumber);
+      System.out.println("Shipping to " + address.getCity() + " on St. " + address.getStreetName() + " " + address.getStreetNumber());
 
    }
 }
+//class CreateOrderCommand { // poate fi folosit DOAR la functia de mai sus
+// 100% safe refactring:
+// rename: private functions, local var, param (!nu campuri)
+// extract method
 
-class AnotherClass {
-   public void otherMethod(String firstName, String lastName, int x) {
+//class Client { // prea generica. o sa sfarseasca foarte mare
+//class Person
+//class Name {// si un SRL are nume dar nu first
+//class ClientName { // prea restrictiv
+
+//@Data
+class FullName {
+   // 20 campuri
+   private final String firstName;
+   private final String lastName;
+
+   public FullName(String firstName, String lastName) {
       if (firstName == null || lastName == null) throw new IllegalArgumentException();
-
-      System.out.println("Another distant Logic " + x);
-      System.out.println("Person: " + lastName);
-   }
-}
-
-class Person {
-   private Long id;
-   private String firstName;
-   private String lastName;
-   private String phone;
-
-   public Person(String firstName, String lastName) {
       this.firstName = firstName;
       this.lastName = lastName;
-      if (firstName == null || lastName == null) throw new IllegalArgumentException();
-   }
-
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
-
-   // TODO hard-core: implement setter
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
-
-   public String getFirstName() {
-      return firstName;
    }
 
    public String getLastName() {
       return lastName;
    }
+
+   public String getFirstName() {
+      return firstName;
+   }
+}
+
+class AnotherClass {
+   public void otherMethod(FullName fullName, int x) {
+      System.out.println("Another distant Logic " + x);
+      System.out.println("Person: " + fullName.getLastName());
+   }
+}
+
+
+// Entite / Document
+class Person {
+   private Long id;
+   private FullName fullName;
+   private String phone;
+
+   public Person(String firstName, String lastName) {
+      this.fullName = new FullName(firstName, lastName);
+
+   }
+
+
+   // TODO hard-core: implement setter
+//   public void setLastName(String lastName) {
+//      this.lastName = lastName;
+//   }
+
+   public FullName getFullName() {
+      return fullName;
+   }
 }
 
 class PersonService {
    public void f(Person person) {
-      String fullNameStr = person.getFirstName() + " " + person.getLastName().toUpperCase();
+      String fullNameStr = person.getFullName().getFirstName() + " " + person.getFullName().getLastName().toUpperCase();
+
+
+      System.out.println(person.getFullName().getFirstName() + " " + person.getFullName().getLastName().toUpperCase());
+      System.out.println(person.getFullName().getFirstName() + " " + person.getFullName().getLastName().toUpperCase());
+
+
+      System.out.println(person.getFullName().getFirstName() + " " + person.getFullName().getLastName().toUpperCase());
+      System.out.println(person.getFullName().getFirstName() + " " + person.getFullName().getLastName().toUpperCase());
+      System.out.println(person.getFullName().getFirstName() + " " + person.getFullName().getLastName().toUpperCase());
+      System.out.println(person.getFullName().getFirstName() + " " + person.getFullName().getLastName().toUpperCase());
+      System.out.println(person.getFullName().getFirstName() + " " + person.getFullName().getLastName().toUpperCase());
+      System.out.println(person.getFullName().getFirstName() + " " + person.getFullName().getLastName().toUpperCase());
+      System.out.println(person.getFullName().getFirstName() + " " + person.getFullName().getLastName().toUpperCase());
+      System.out.println(person.getFullName().getFirstName() + " " + person.getFullName().getLastName().toUpperCase());
       System.out.println(fullNameStr);
    }
 
