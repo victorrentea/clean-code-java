cabe3c2bc2f79a4e16c718f4e3d2dc2992c0cfa1
Victor Rentea <victorrentea@gmail.com>
2020-09-25 15:47:20 +0300

    Enum cu coduri

--- src/main/java/videostore/horror/Movie.java
+++ src/main/java/videostore/horror/Movie.java
@@ -1,25 +1,41 @@
 package videostore.horror;
+
+
+// Daniel, dupa 2 min: "nu facem si noi Movie abstract" <- extinsa de ChildrenMovie, RegularMovie...
 public class Movie {
+
+	enum Category {
+   REGULAR(0),
+   NEW_RELEASE(1),
+	CHILDRENS(2);
+
+		private final int code; // daca era din DB, UI, Fisier
+
+		Category(int code) {
+
+			this.code = code;
+		}
+	}
+
 	public static final int CHILDRENS = 2;
-	public static final int REGULAR = 0;
-	public static final int NEW_RELEASE = 1;
-	private String _title;
-	private Integer _priceCode;
+   public static final int REGULAR = 0;
+   public static final int NEW_RELEASE = 1;
 
-	public Movie(String title, Integer priceCode) {
-		_title = title;
-		_priceCode = priceCode;
-	}
+   private final String _title;
+   private final Integer _priceCode;
 
-	public Integer getPriceCode() {
-		return _priceCode;
-	}
+   public Movie(String title, Integer priceCode) {
+      _title = title;
+      _priceCode = priceCode;
+   }
 
-	public void setPriceCode(Integer arg) {
-		_priceCode = arg;
-	}
+   public Integer getPriceCode() {
+      return _priceCode;
+   }
 
 	public String getTitle() {
-		return _title;
-	};
+      return _title;
+   }
+
+   ;
 }
