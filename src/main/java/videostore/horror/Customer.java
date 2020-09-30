36b3874422e2dc1de858450d408f17a6df38cdf2
Victor Rentea <victorrentea@gmail.com>
2020-09-30 16:10:21 +0300

    Polymorphism over Switch

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -3,15 +3,15 @@ package videostore.horror;
 import java.util.*;
 
 class Customer {
-	private String name;
-	private Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order
+	private final String name;
+	private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order
 
 	public Customer(String name) {
 		this.name = name;
 	};
 
-	public void addRental(Movie m, int d) {
-		rentals.put(m, d);
+	public void addRental(Movie movie, int daysRented) {
+		rentals.put(movie, daysRented);
 	}
 
 	public String getName() {
@@ -21,38 +21,52 @@ class Customer {
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
-		Iterator<Movie> rentals = this.rentals.keySet().iterator();
 		String result = "Rental Record for " + getName() + "\n";
-		while (rentals.hasNext()) {
+
+		for (Movie movie : rentals.keySet()) {
 			double thisAmount = 0;
-			Movie each = (Movie) rentals.next();
 			// determine amounts for each line
-			int dr = this.rentals.get(each);
-			switch (each.getCategory()) {
-			case REGULAR:
-				thisAmount += 2;
-				if (dr > 2)
-					thisAmount += (dr - 2) * 1.5;
-				break;
-			case NEW_RELEASE:
-				thisAmount += dr * 3;
-				break;
-			case CHILDREN:
-				thisAmount += 1.5;
-				if (dr > 3)
-					thisAmount += (dr - 3) * 1.5;
-				break;
+			int daysRented = rentals.get(movie);
+
+
+			/**
+			 * abstract class Movie{
+			 *    public abstract computeAmount(daysRented);
+			 * }
+			 * class NewReleaseMovie extends Movie {
+			 * 	public computeAmount(daysRented) {
+			 * 		return daysRented * 3;
+			 * 	}
+			 * }
+			 *
+			 * movie.computeAmount(daysRented); // polymorphic dispatch
+			 */
+
+			switch (movie.getCategory()) {
+				case REGULAR:
+					thisAmount += 2;
+					if (daysRented > 2)
+						thisAmount += (daysRented - 2) * 1.5;
+					break;
+				case NEW_RELEASE:
+					thisAmount += daysRented * 3;
+					break;
+				case CHILDREN:
+					thisAmount += 1.5;
+					if (daysRented > 3)
+						thisAmount += (daysRented - 3) * 1.5;
+					break;
 			}
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if (each.getCategory() != null &&
-				 (each.getCategory() == Movie.Category.NEW_RELEASE)
-				 && dr > 1)
+			if (movie.getCategory() != null &&
+				 (movie.getCategory() == Movie.Category.NEW_RELEASE)
+				 && daysRented > 1)
 				frequentRenterPoints++;
 			// show figures line for this rental
-			result += "\t" + each.getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
+			result += "\t" + movie.getTitle() + "\t"
+						 + String.valueOf(thisAmount) + "\n";
 			totalAmount += thisAmount;
 		}
 		// add footer lines
