779884ad526d48b994b596975302fffa4f8bbbe6
Victor Rentea <victorrentea@gmail.com>
2020-10-05 21:51:55 +0300

    We remove boolean parameters by duplicating methods. A method does ONE thing: a boolean meant that the function did 2 things

--- src/main/java/victor/training/refactoring/BooleanParameters.java
+++ src/main/java/victor/training/refactoring/BooleanParameters.java
@@ -5,37 +5,38 @@ import java.util.List;
 public class BooleanParameters {
    public static void main(String[] args) {
       // The method is called from various foreign places in the codebase
-      bigUglyMethod(5, 1, false);
-      bigUglyMethod(4, 2, false);
-      bigUglyMethod(3, 3, false);
-      bigUglyMethod(2, 4, false);
-      bigUglyMethod(1, 5, false);
+      bigUglyMethod(5, 1);
+      bigUglyMethod(4, 2);
+      bigUglyMethod(3, 3);
+      bigUglyMethod(2, 4);
+      bigUglyMethod(1, 5);
 
       // TODO From my use-case #323, I call it too, to do more within:
-      bigUglyMethod(1, 2, true);
+      bigUglyMethod323(1, 2);
 
    }
 
-   static void bigUglyMethod(int a, int b, boolean cr323) {
+   static void bigUglyMethod(int a, int b) {
+      beforeLogic(a, b);
+      afterLogic(b);
+   }
+   static void bigUglyMethod323(int a, int b) {
+      beforeLogic(a, b);
+      System.out.println("Someting extra, just for me " + a);
+      afterLogic(b);
+   }
+
+   private static void afterLogic(int b) {
+      System.out.println("More Complex Logic " + b);
+      System.out.println("More Complex Logic " + b);
+      System.out.println("More Complex Logic " + b);
+   }
+
+   private static void beforeLogic(int a, int b) {
       System.out.println("Complex Logic 1 " + a + " and " + b);
       System.out.println("Complex Logic 2 " + a);
       System.out.println("Complex Logic 3 " + a);
-
-      if (cr323) {
-         System.out.println("Someting extra, just for me " + a);
-      }
-
-      System.out.println("More Complex Logic " + b);
-      System.out.println("More Complex Logic " + b);
-      System.out.println("More Complex Logic " + b);
    }
-
-
-
-
-
-
-
 
 
    // ============== "BOSS" LEVEL: Deeply nested functions are a lot harder to break down =================
