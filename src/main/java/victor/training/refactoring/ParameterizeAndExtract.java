f63ac976e3c8a129f260b7a0731ac94bfbb8078a
Victor Rentea <victorrentea@gmail.com>
2020-10-05 22:06:54 +0300

    two similarly looking methods in 2 separate classes

--- src/main/java/victor/training/refactoring/ParameterizeAndExtract.java
+++ src/main/java/victor/training/refactoring/ParameterizeAndExtract.java
@@ -2,17 +2,21 @@ package victor.training.refactoring;
 
 class ParameterizeAndExtract {
 
+    // existing
     public void f() {
         System.out.println("Logic F");
         for (int i = 0; i < 4; i++) {
-            System.out.println("Code " + i);
+            System.out.println("Code " + i * (i - 1));
         }
     }
+
+}
+class MyStory {
     public void g() {
         System.out.println("Logic G");
         for (int i = 0; i < 3; i++) {
-            String x = "Code" +
-                    " " + i;
+            int temp = i * (i - 1);
+            String x = "Code " + temp;
             System.out.println(x);
         }
     }
