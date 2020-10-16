f8bc90d75bcc55adb31a898d86a4ef0f391fe5af
Victor Rentea <victorrentea@gmail.com>
2020-10-16 16:39:39 +0300

    Broke loop + Streams OMG!

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -1,55 +1,13 @@
 package videostore.horror;
 
+import static java.util.stream.Collectors.joining;
+
 import java.util.*;
 import java.util.Map.Entry;
 
 
-class Rental {
-	private final Movie movie;
-	private final int daysRented;
-	public Rental(Movie movie, int daysRentals) {
-		this.movie = movie;
-		this.daysRented = daysRentals;
-	}
-	public int getDaysRented() {
-		return daysRented;
-	}
-	public Movie getMovie() {
-		return movie;
-	}
-	
-	public double calculatePrice() {
-		double price = 0;
-		switch (movie.getType()) {
-		case REGULAR:
-			price += 2;
-			if (daysRented > 2)
-				price += (daysRented - 2) * 1.5;
-			return price;
-		case NEW_RELEASE:
-			return daysRented * 3;
-		case CHILDRENS:
-			price += 1.5;
-			if (daysRented > 3)
-				price += (daysRented - 3) * 1.5;
-			return price;
-		default: throw new IllegalArgumentException(getMovie().getType().name());
-		}
-	}
-	public int calculateRenterPoints() {
-		// add frequent renter points
-		int frequentRenterPoints = 1;
-		// add bonus for a two day new release rental
-		if ((getMovie().getType() == Movie.Type.NEW_RELEASE)
-				&& getDaysRented() > 1)
-			frequentRenterPoints++;
-		return frequentRenterPoints;
-	}
-}
-
 class Customer {
 	private String name;
-	private Map<Movie, Integer> rentalsMap = new LinkedHashMap<>(); // preserves order
 	private List<Rental> rentals = new ArrayList<>();
 
 	public Customer(String name) {
@@ -57,7 +15,6 @@ class Customer {
 	}
 
 	public void addRental(Movie movie, int daysRented) {
-		rentalsMap.put(movie, daysRented);
 		rentals.add(new Rental(movie, daysRented));
 	}
 
@@ -65,38 +22,43 @@ class Customer {
 		return name;
 	}
 	
-	// -1) remove rentalsMap 
-	// 0) Body line has 2 params ---> 1 is enough
-	// -) calculateRenterPoints() clean
 	// 1) break for. extract to mehods?  maybe use streams?
 	// 2) why is this in customer
 	// 3) SWITCH - extract from it
 	// 4) separate the presentation fro mcomputation : MVC
 
 	public String createStatement() {
-		double totalPrice = 0;
-		int frequentRenterPoints = 0;
 		String result = createHeader();
 		
-		for (Rental rental: rentals) {
-			frequentRenterPoints += rental.calculateRenterPoints();
-			result += createBodyLine(rental.getMovie(), rental.calculatePrice());
-			totalPrice += rental.calculatePrice();
-		}
-		result += createFooter(totalPrice, frequentRenterPoints);
+		result += createBody();
+		
+		
+		result += createFooter();
 		return result;
 	}
 
-	private String createBodyLine(Movie movie, double price) {
-		return "\t" + movie.getTitle() + "\t" + price + "\n";
+	private String createBody() {
+		return rentals.stream().map(this::createBodyLine).collect(joining());
+	}
+
+	private String createFooter() {
+		return "Amount owed is " + calculateTotalPrice() + "\n" +
+				"You earned " + calculateTotalRenterPoints() + " frequent renter points";
+	}
+
+	private double calculateTotalPrice() {
+		return rentals.stream().mapToDouble(Rental::calculatePrice).sum();
+	}
+
+	private int calculateTotalRenterPoints() {
+		return rentals.stream().mapToInt(Rental::calculateRenterPoints).sum();
+	}
+
+	private String createBodyLine(Rental rental) {
+		return "\t" + rental.getMovie().getTitle() + "\t" + rental.calculatePrice() + "\n";
 	}
 
 	private String createHeader() {
 		return "Rental Record for " + name + "\n";
-	}
-
-	private String createFooter(double totalPrice, int frequentRenterPoints) {
-		return "Amount owed is " + totalPrice + "\n" +
-				"You earned " + frequentRenterPoints + " frequent renter points";
 	}
 }
