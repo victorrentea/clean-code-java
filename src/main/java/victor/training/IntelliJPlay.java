7080cced7cd1f20efe9b8f5d65a2878339a9348f
Victor Rentea <victorrentea@gmail.com>
2020-10-29 16:42:13 +0200

    IJ play

---
+++ src/main/java/victor/training/IntelliJPlay.java
@@ -0,0 +1,31 @@
+package victor.training;
+
+import lombok.Data;
+
+public class IntelliJPlay {
+   public static void main(String[] args) {
+      // numberList
+      // .for
+      // 1 year ago
+      // log field + .log
+      // open file read line
+      // ALT-J
+      // define class pf get
+   }
+}
+
+@Data
+class Player {
+   private Address address;
+}
+
+@Data
+class Address {
+   private City city;
+   private String streetName;
+   private String streetNumber;
+}
+
+class City {
+
+}
