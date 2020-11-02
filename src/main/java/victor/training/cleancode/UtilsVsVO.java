0a14a3da50ccc9eedcf1c0b26b8d167065405f86
Victor Rentea <victorrentea@gmail.com>
2020-11-02 11:11:36 +0200

    move static method

--- src/main/java/victor/training/cleancode/UtilsVsVO.java
+++ src/main/java/victor/training/cleancode/UtilsVsVO.java
@@ -4,18 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UtilsVsVO {
+
+    // Ford Focus:     [2012 ---- 2016]
+    // Search:              [2014 ---- 2018]
+
     public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> models) {
         List<CarModel> results = new ArrayList<>(models);
-        results.removeIf(model -> ! intervalsIntersect(
+        results.removeIf(model -> ! MathUtil.intervalsIntersect(
                 model.getStartYear(), model.getEndYear(),
                 criteria.getStartYear(), criteria.getEndYear()));
         System.out.println("More filtering logic");
         return results;
     }
-    private boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
+}
+
+class Alta {
+    public void method() {
+        boolean ok = MathUtil.intervalsIntersect(1, 3, 2, 4);
+        System.out.println(ok);
+    }
+}
+
+
+class MathUtil {
+
+    public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
         return start1 <= end2 && start2 <= end1;
     }
 }
+
+
+
+
+
+
+
+
+
 
 
 
