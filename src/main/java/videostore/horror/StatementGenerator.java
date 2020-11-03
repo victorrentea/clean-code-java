b6903d33645ba96fade59eef0be6e69e33b1a454
Victor Rentea <victorrentea@gmail.com>
2020-11-03 14:08:29 +0200

    END of day 2

--- src/main/java/videostore/horror/StatementGenerator.java
+++ src/main/java/videostore/horror/StatementGenerator.java
@@ -4,21 +4,32 @@ import java.util.List;
 
 import static java.util.stream.Collectors.joining;
 
+//class CustomerService {
+//   private final StatementGenerator statementGenerator;
+//
+//   CustomerService(StatementGenerator statementGenerator) {
+//      this.statementGenerator = statementGenerator;
+//   }
+//}
+
 public class StatementGenerator {
 
    public String generateStatement(String customerName, List<Rental> rentals) {
+      double totalPrice = computeTotalPrice(rentals);
+      int totalPoints = computeTotalPoints(rentals);
+
       return createHeader(customerName) +
              createBody(rentals) +
-             createFooter(rentals);
+             createFooter(totalPrice, totalPoints);
    }
 
    private String createBody(List<Rental> rentals) {
       return rentals.stream().map(this::createBodyLine).collect(joining());
    }
 
-   private String createFooter(List<Rental> rentals) {
-      return "Amount owed is " + computeTotalPrice(rentals) + "\n"
-             + "You earned " + computeTotalPoints(rentals) + " frequent renter points";
+   private String createFooter(double totalPrice, int totalPoints) {
+      return "Amount owed is " + totalPrice + "\n"
+             + "You earned " + totalPoints + " frequent renter points";
    }
 
    private double computeTotalPrice(List<Rental> rentals) {
