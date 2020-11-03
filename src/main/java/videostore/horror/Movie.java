659143e19e8e4eb3c5774a78cebe6ea6b02d2fd6
Victor Rentea <victorrentea@gmail.com>
2020-11-03 12:06:01 +0200

    sa fim corecti, pentru Mihaela

--- src/main/java/videostore/horror/Movie.java
+++ src/main/java/videostore/horror/Movie.java
@@ -16,6 +16,10 @@ public class Movie {
 	public Movie(String title, Type type) {
 		this.title = title;
 		this.type = requireNonNull(type);
+		// requires a db migration: UPDATE MOVIES SET TYPE='CHILDREN' WHERE TYPE IS NULL;
+		// + ALTER TABLE MOVIES SET TYPE NOT NULL;
+
+
 	}
 
 	public Type getType() {
