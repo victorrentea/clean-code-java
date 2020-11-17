9e69d289411c1dbfcbc46ecc92ac774d78c628e5
Victor Rentea <victorrentea@gmail.com>
2020-11-17 12:27:44 +0200

    discovered the Interval class from a method signature; added logic to that new class. and constraints to constructor ! then used it to enrich the data models in our app (Criteira)

--- src/main/java/victor/training/cleancode/UtilsVsVO.java
+++ src/main/java/victor/training/cleancode/UtilsVsVO.java
@@ -1,23 +1,52 @@
 package victor.training.cleancode;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class UtilsVsVO {
    // Ford Focus:     [2012 ---- 2016]
    // Search:              [2014 ---- 2018]
 
    public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> models) {
-      List<CarModel> results = new ArrayList<>(models);
-      results.removeIf(model -> !intervalsIntersect(
-          model.getStartYear(), model.getEndYear(),
-          criteria.getStartYear(), criteria.getEndYear()));
+      List<CarModel> results = models.stream()
+          .filter(criteria::matchesYearCriteria)
+          .collect(toList());
+
       System.out.println("More filtering logic");
       return results;
    }
 
-   private boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
-      return start1 <= end2 && start2 <= end1;
+   {
+        System.out.println(new Interval(1, 3).intersects(new Interval(2, 4)));
+     }
+
+}
+class IntervalUtil {
+
+}
+class Interval {
+   private final int start;
+   private final int end;
+
+   public Interval(int start, int end) {
+      if (start > end) {
+         throw new IllegalArgumentException();
+      }
+      this.start = start;
+      this.end = end;
+   }
+
+   public boolean intersects(Interval other) {
+      return start <= other.end && other.start <= end;
+   }
+
+   public int getStart() {
+      return start;
+   }
+
+   public int getEnd() {
+      return end;
    }
 }
 
@@ -35,27 +64,24 @@ public class UtilsVsVO {
 
 
 class CarSearchCriteria {
-   private final int startYear;
-   private final int endYear;
+   private final Interval yearInterval;
    private final String make;
 
-   public CarSearchCriteria(int startYear, int endYear, String make) {
+   public CarSearchCriteria(Interval yearInterval, String make) {
+      this.yearInterval = yearInterval;
       this.make = make;
-      if (startYear > endYear) throw new IllegalArgumentException("start larger than end");
-      this.startYear = startYear;
-      this.endYear = endYear;
-   }
-
-   public int getStartYear() {
-      return startYear;
-   }
-
-   public int getEndYear() {
-      return endYear;
    }
 
    public String getMake() {
       return make;
+   }
+
+   public Interval getYearInterval() {
+      return yearInterval;
+   }
+
+   public boolean matchesYearCriteria(CarModel model) {
+      return yearInterval.intersects(model.getYearInterval());
    }
 }
 
@@ -87,5 +113,9 @@ class CarModel {
 
    public String getModel() {
       return model;
+   }
+
+   public Interval getYearInterval() {
+      return new Interval(getStartYear(), getEndYear());
    }
 }
