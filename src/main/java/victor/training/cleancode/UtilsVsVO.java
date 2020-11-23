b15194d5b3d5d0a106f5aa84033840f0d481bbe0
Victor Rentea <victorrentea@gmail.com>
2020-11-23 11:01:57 +0200

    UtilsVsVO improv

--- src/main/java/victor/training/cleancode/UtilsVsVO.java
+++ src/main/java/victor/training/cleancode/UtilsVsVO.java
@@ -1,11 +1,23 @@
 package victor.training.cleancode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class UtilsVsVO {
    // Ford Focus:     [2012 ---- 2016]
    // Search:              [2014 ---- 2018]
+   public static void main(String[] args) {
+      // can't afford a 2021 car
+      CarSearchCriteria criteria = new CarSearchCriteria(2014, 2018, "Ford");
+      CarModel fordFocusMk2 = new CarModel("Ford", "Focus", 2012, 2016);
+      List<CarModel> models = new SearchEngine().filterCarModels(criteria, Arrays.asList(fordFocusMk2));
+      System.out.println(models);
+   }
+}
+
+
+class SearchEngine {
 
    public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> models) {
       List<CarModel> results = new ArrayList<>(models);
@@ -16,12 +28,14 @@ public class UtilsVsVO {
       return results;
    }
 
+   private void applyCapacityFilter() {
+      System.out.println(intervalsIntersect(1000, 1600, 1250, 2000));
+   }
+
    private boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
       return start1 <= end2 && start2 <= end1;
    }
 }
-
-
 
 
 
@@ -87,5 +101,13 @@ class CarModel {
 
    public String getModel() {
       return model;
+   }
+
+   @Override
+   public String toString() {
+      return "CarModel{" +
+             "make='" + make + '\'' +
+             ", model='" + model + '\'' +
+             '}';
    }
 }
