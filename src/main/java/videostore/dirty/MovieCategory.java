d51d57899bc2a6350016936c890cea63fdc9f3b7
Victor Rentea <victorrentea@gmail.com>
2020-11-23 16:36:01 +0200

    abstract methods in enum

--- src/main/java/videostore/dirty/MovieCategory.java
+++ src/main/java/videostore/dirty/MovieCategory.java
@@ -1,7 +1,30 @@
 package videostore.dirty;
 
 public enum MovieCategory {
-   CHILDREN,
-   REGULAR,
-   NEW_RELEASE
+   CHILDREN {
+      @Override
+      public double calculatePrice(int daysRented) {
+         double price = 1.5;
+         if (daysRented > 3)
+            price += (daysRented - 3) * 1.5;;
+            return price;
+      }
+   },
+   REGULAR {
+      @Override
+      public double calculatePrice(int daysRented) {
+         double price = 2;
+         if (daysRented > 2)
+            price += (daysRented - 2) * 1.5;
+         return price;
+      }
+   },
+   NEW_RELEASE {
+      @Override
+      public double calculatePrice(int daysRented) {
+         return daysRented * 3;
+      }
+   };
+
+   public abstract double calculatePrice(int daysRented);
 }
--- src/main/java/videostore/dirty/Rental.java
+++ src/main/java/videostore/dirty/Rental.java
@@ -32,25 +32,6 @@ class Rental {
    }
 
    public double calculatePrice() {
-      double price = 0;
-      switch (movie.getCategory()) {
-      case REGULAR:
-         price += 2;
-         if (daysRented > 2)
-            price += (daysRented - 2) * 1.5;
-         break;
-      case NEW_RELEASE:
-         price += daysRented * 3;
-         break;
-      case CHILDREN:
-         price += 1.5;
-         if (daysRented > 3)
-            price += (daysRented - 3) * 1.5;
-         break;
-
-         default:
-            throw new IllegalStateException("Unexpected value: " + getMovie().getCategory());
-      }
-      return price;
+      return movie.getCategory().calculatePrice(daysRented);
    }
 }
