072aa3f2d2fcfbc8c417706dd98dbd575d5e2988
Victor Rentea <victorrentea@gmail.com>
2020-11-23 14:27:44 +0200

    gata cu incalzirea

--- src/main/java/videostore/dirty/Movie.java
+++ src/main/java/videostore/dirty/Movie.java
@@ -1,38 +1,21 @@
 package videostore.dirty;
 
 public class Movie {
-   public static final int CATEGORY_CHILDRENS = 2;
-   public static final int CATEGORY_REGULAR = 0;
-   public static final int CATEGORY_NEW_RELEASE = 1;
-   
-   enum Category {
-		CHILDREN(2),
-		REGULAR(0),
-		NEW_RELEASE(1);
-      private final int code;
-      Category(int code) {
-         this.code = code;
-      }
-      public int getCode() {
-         return code;
-      }
-   }
-   
-   private String title;
-   private Category priceCode;
 
-   public Movie(String title, Category priceCode) {
+   private final String title;
+   private final MovieCategory category;
+
+   public Movie(String title, MovieCategory category) {
       this.title = title;
-      this.priceCode = priceCode;
+      this.category = category;
    }
 
-   public Category getPriceCode() {
-      return priceCode;
+   public MovieCategory getCategory() {
+      return category;
    }
 
    public String getTitle() {
       return title;
    }
 
-   ;
 }
