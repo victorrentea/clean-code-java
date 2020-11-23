90dbf66da3ca20467e9044ba8b18c51244571ed0
Victor Rentea <victorrentea@gmail.com>
2020-11-23 14:22:36 +0200

    cleanup + curatat movie + rental

--- src/main/java/videostore/dirty/Movie.java
+++ src/main/java/videostore/dirty/Movie.java
@@ -1,25 +1,38 @@
 package videostore.dirty;
+
 public class Movie {
-	public static final int CATEGORY_CHILDRENS = 2;
-	public static final int CATEGORY_REGULAR = 0;
-	public static final int CATEGORY_NEW_RELEASE = 1;
-	private String _title;
-	private int _priceCode;
+   public static final int CATEGORY_CHILDRENS = 2;
+   public static final int CATEGORY_REGULAR = 0;
+   public static final int CATEGORY_NEW_RELEASE = 1;
+   
+   enum Category {
+		CHILDREN(2),
+		REGULAR(0),
+		NEW_RELEASE(1);
+      private final int code;
+      Category(int code) {
+         this.code = code;
+      }
+      public int getCode() {
+         return code;
+      }
+   }
+   
+   private String title;
+   private Category priceCode;
 
-	public Movie(String title, int priceCode) {
-		_title = title;
-		_priceCode = priceCode;
-	}
+   public Movie(String title, Category priceCode) {
+      this.title = title;
+      this.priceCode = priceCode;
+   }
 
-	public int getPriceCode() {
-		return _priceCode;
-	}
+   public Category getPriceCode() {
+      return priceCode;
+   }
 
-	public void setPriceCode(int arg) {
-		_priceCode = arg;
-	}
+   public String getTitle() {
+      return title;
+   }
 
-	public String getTitle() {
-		return _title;
-	};
+   ;
 }
--- src/main/java/videostore/dirty/Rental.java
+++ src/main/java/videostore/dirty/Rental.java
@@ -1,18 +1,26 @@
 package videostore.dirty;
+
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+
 class Rental {
-	private Movie _movie;
-	private int _daysRented;
+   private final Movie movie;
+   private final int daysRented;
 
-	public Rental(Movie movie, int daysRented) {
-		_movie = movie;
-		_daysRented = daysRented;
-	}
+   public Rental(Movie movie, int daysRented) {
+		if (daysRented <= 0) {
+			throw new IllegalArgumentException();
+		}
+		this.movie = requireNonNull(movie);
+      this.daysRented = daysRented;
+   }
 
-	public int getDaysRented() {
-		return _daysRented;
-	}
+   public int getDaysRented() {
+      return daysRented;
+   }
 
-	public Movie getMovie() {
-		return _movie;
-	}
+   public Movie getMovie() {
+      return movie;
+   }
 }
