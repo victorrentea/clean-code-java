fd25d525cddddff39c15b8d6e14b8dcd159ff732
Victor Rentea <victorrentea@gmail.com>
2020-11-26 15:54:59 +0200

    Scoaterea unui param boolean inseamna intotdeauna copiere de cod (o alta functie). Treaba ta e sa copii cat mai putin, extragand functii inprealabil

--- src/main/java/victor/training/cleancode/BouleanParameters.java
+++ src/main/java/victor/training/cleancode/BouleanParameters.java
@@ -5,36 +5,42 @@ import java.util.List;
 public class BouleanParameters {
    public static void main(String[] args) {
       // The big method is called from various foreign places in the codebase
-      bigUglyMethod(1, 5, false);
-      bigUglyMethod(2, 4, false);
-      bigUglyMethod(3, 3, false);
-      bigUglyMethod(4, 2, false);
-      bigUglyMethod(5, 1, false);
+      bigUglyMethod(1, 5);
+      bigUglyMethod(2, 4);
+      bigUglyMethod(3, 3);
+      bigUglyMethod(4, 2);
+      bigUglyMethod(5, 1);
 
       // TODO From my CR323 I call it too, to do more within:
-      bigUglyMethod(2, 1, true);
+      bigUglyMethod323(2, 1);
 
    }
 
-   static void bigUglyMethod(int b, int a, boolean cr323) {
+   static void bigUglyMethod(int b, int a) {
+      beforeLogic(b, a);
+      afterLogic(b);
+   }
+
+   static void bigUglyMethod323(int b, int a) {
+      beforeLogic(b, a);
+      System.out.println("Si eu ceva pt cr 323 " + a);
+      afterLogic(b);
+   }
+
+
+
+
+   private static void afterLogic(int b) {
+      System.out.println("More Complex Logic " + b);
+      System.out.println("More Complex Logic " + b);
+      System.out.println("More Complex Logic " + b);
+   }
+
+   private static void beforeLogic(int b, int a) {
       System.out.println("Complex Logic 1 " + a + " and " + b);
       System.out.println("Complex Logic 2 " + a);
       System.out.println("Complex Logic 3 " + a);
-
-      if (cr323) {
-         System.out.println("Si eu ceva pt cr 323 " + a);
-      }
-
-      System.out.println("More Complex Logic " + b);
-      System.out.println("More Complex Logic " + b);
-      System.out.println("More Complex Logic " + b);
    }
-
-
-
-
-
-
 
 
    // ============== "BOSS" LEVEL: Deeply nested functions are a lot harder to break down =================
