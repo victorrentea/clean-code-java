3d807e216ea4b921f8260331eb748520fd8e3fa1
Victor Rentea <victorrentea@gmail.com>
2020-11-27 14:31:20 +0200

    Immutable play

--- src/main/java/victor/training/refactoring/ImmutablePlay.java
+++ src/main/java/victor/training/refactoring/ImmutablePlay.java
@@ -1,13 +1,20 @@
 package victor.training.refactoring;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class ImmutablePlay {
     public static void main(String[] args) {
+        List<Integer> numbers = Stream.of(1, 2, 3, 4, 5).collect(toList());
+
         Immutable immutable = new Immutable();
         immutable.x = 2;
-        immutable.numbers = Arrays.asList(1, 2, 3, 4, 5);
+        immutable.numbers = numbers;
         immutable.other = new Other(13);
         System.out.println(immutable.x);
     }
