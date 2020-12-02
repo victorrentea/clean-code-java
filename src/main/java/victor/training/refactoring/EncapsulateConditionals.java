13855913a5ff75b452c08f8c03a220ca2a8a55b6
Victor Rentea <victorrentea@gmail.com>
2020-12-02 15:49:40 +0200

    EncapsulateConditionals

--- src/main/java/victor/training/refactoring/EncapsulateConditionals.java
+++ src/main/java/victor/training/refactoring/EncapsulateConditionals.java
@@ -1,26 +1,72 @@
 package victor.training.refactoring;
 
-import lombok.Data;
-
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
 
 public class EncapsulateConditionals {
-    public double getQuote(Date date, RatesPlan plan, int quantity, float clientFidelityFactor) {
-        double charge;
-        if (!(date.before(plan.getSummerStart())) && date.before(plan.getSummerEnd()))
-            charge = quantity * plan.getSummerRate();
-        else
-            charge = quantity * plan.getRegularRate() + plan.getRegularServiceCharge();
-        return charge - clientFidelityFactor;
-    }
+   public double getQuote(Date date, RatesPlan plan, int quantity, float clientFidelityFactor) {
+
+      List<Date> dates = asList(new Date(), new Date(), new Date(), new Date());
+
+      List<Date> summerDates = dates.stream().filter(plan::isDuringSummer).collect(Collectors.toList());
+
+      double charge;
+      if (plan.isDuringSummer(date))
+         charge = quantity * plan.getSummerRate();
+      else
+         charge = quantity * plan.getRegularRate() + plan.getRegularServiceCharge();
+      return charge - clientFidelityFactor;
+   }
+
 }
 
 
-@Data
+
 class RatesPlan {
-    private final Date summerStart;
-    private final Date summerEnd;
-    private final double summerRate;
-    private final double regularRate;
-    private final double regularServiceCharge;
+   private final Date summerStart;
+   private final Date summerEnd;
+   private final double summerRate;
+   private final double regularRate;
+   private final double regularServiceCharge;
+
+   RatesPlan(Date summerStart, Date summerEnd, double summerRate, double regularRate, double regularServiceCharge) {
+      if (summerStart.after(summerEnd)) {
+         throw new IllegalArgumentException("summer date start > end");
+      }
+      this.summerStart = summerStart;
+      this.summerEnd = summerEnd;
+      this.summerRate = summerRate;
+      this.regularRate = regularRate;
+      this.regularServiceCharge = regularServiceCharge;
+   }
+
+//   public boolean shouldBeDisplayedGrayedOut() { // MVC violation: nu ai ce sa cauti in entitatea cu treburi de prezentar
+//   public boolean isEnabled() {    } // OK
+
+   public boolean isDuringSummer(Date date) {
+      return !(date.before(summerStart)) && date.before(summerEnd);
+   }
+
+   public Date getSummerEnd() {
+      return summerEnd;
+   }
+
+   public Date getSummerStart() {
+      return summerStart;
+   }
+
+   public double getRegularRate() {
+      return regularRate;
+   }
+
+   public double getRegularServiceCharge() {
+      return regularServiceCharge;
+   }
+
+   public double getSummerRate() {
+      return summerRate;
+   }
 }
