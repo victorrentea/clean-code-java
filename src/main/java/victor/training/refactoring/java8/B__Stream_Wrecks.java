5fe68c74531920254fa408f67d1811366e5f2a8c
Victor Rentea <victorrentea@gmail.com>
2020-12-14 10:46:23 +0200

    add java8 clean code

---
+++ src/main/java/victor/training/refactoring/java8/B__Stream_Wrecks.java
@@ -0,0 +1,55 @@
+package victor.training.refactoring.java8;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map.Entry;
+
+import static java.util.stream.Collectors.*;
+
+// get the products frequently ordered during the past year
+
+
+class ProductService {
+	private ProductRepo productRepo;
+
+	public List<Product> getFrequentOrderedProducts(List<Order> orders) {
+		return orders.stream()
+				.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
+				.flatMap(o -> o.getOrderLines().stream())
+				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)))
+				.entrySet()
+				.stream()
+				.filter(e -> e.getValue() >= 10)
+				.map(Entry::getKey)
+				.filter(p -> !p.isDeleted())
+				.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
+				.collect(toList());
+	}
+}
+
+
+//VVVVVVVVV ==== supporting (dummy) code ==== VVVVVVVVV
+@Data
+class Product {
+	private Long id;
+	private boolean deleted;
+}
+
+@Data
+class Order {
+	private Long id;
+	private List<OrderLine> orderLines;
+	private LocalDate creationDate;
+}
+
+@Data
+class OrderLine {
+	private Product product;
+	private int itemCount;
+}
+
+interface ProductRepo {
+	List<Long> getHiddenProductIds();
+}
--- src/main/java/videostore/dirty/Customer.java
+++ src/main/java/videostore/dirty/Customer.java
@@ -23,24 +23,8 @@ class Customer {
 		Iterator rentals = this.rentals.iterator();
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasNext()) {
-			double thisAmount = 0;
 			Rental each = (Rental) rentals.next();
 			// determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.CATEGORY_REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.CATEGORY_NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CATEGORY_CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-			}
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
@@ -48,14 +32,33 @@ class Customer {
 					&& each.getDaysRented() > 1)
 				frequentRenterPoints++;
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ thisAmount + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + each.getMovie().getTitle() + "\t" + computePrice(each) + "\n";
+			totalAmount += computePrice(each);
 		}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
+	}
+
+	private double computePrice(Rental each) {
+		double thisAmount = 0;
+		switch (each.getMovie().getPriceCode()) {
+		case Movie.CATEGORY_REGULAR:
+			thisAmount += 2;
+			if (each.getDaysRented() > 2)
+				thisAmount += (each.getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.CATEGORY_NEW_RELEASE:
+			thisAmount += each.getDaysRented() * 3;
+			break;
+		case Movie.CATEGORY_CHILDRENS:
+			thisAmount += 1.5;
+			if (each.getDaysRented() > 3)
+				thisAmount += (each.getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
 	}
 }
