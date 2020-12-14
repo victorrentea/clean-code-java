4f708db42c8b91054253aafb7580500fef284229
Victor Rentea <victorrentea@gmail.com>
2020-12-14 13:15:16 +0200

    lunch break

--- src/main/java/victor/training/refactoring/java8/B__Stream_Wrecks.java
+++ src/main/java/victor/training/refactoring/java8/B__Stream_Wrecks.java
@@ -3,7 +3,9 @@ package victor.training.refactoring.java8;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import static java.util.stream.Collectors.*;
@@ -15,15 +17,16 @@ class ProductService {
 	private ProductRepo productRepo;
 
 	public List<Product> getFrequentOrderedProducts(List<Order> orders) {
-		return orders.stream()
-				.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
-				.flatMap(o -> o.getOrderLines().stream())
-				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)))
-				.entrySet()
+		Map<Product, Integer> productFrequencies = orders.stream()
+			.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
+			.flatMap(o -> o.getOrderLines().stream()).collect(toMap(OrderLine::getProduct, OrderLine::getItemCount, Integer::sum));
+
+		return productFrequencies.entrySet()
 				.stream()
 				.filter(e -> e.getValue() >= 10)
 				.map(Entry::getKey)
-				.filter(p -> !p.isDeleted())
+//				.filter(p -> !p.isDeleted())
+				.filter(Product::isNotDeleted)
 				.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
 				.collect(toList());
 	}
@@ -35,6 +38,10 @@ class ProductService {
 class Product {
 	private Long id;
 	private boolean deleted;
+
+	public boolean isNotDeleted() {
+		return !deleted;
+	}
 }
 
 @Data
