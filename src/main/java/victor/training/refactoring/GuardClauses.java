f71271e9d0cefe4c0c6197ad209daff6d8062952
Victor Rentea <victorrentea@gmail.com>
2020-12-17 18:25:15 +0200

    Guard Clauses: start with the trivial branches. typically checks

--- src/main/java/victor/training/refactoring/GuardClauses.java
+++ src/main/java/victor/training/refactoring/GuardClauses.java
@@ -5,30 +5,29 @@ import java.util.List;
 
 public class GuardClauses {
    public int getPayAmount(Marine marine) {
-      int result;
-      if (!retrieveDeadStatus()) { // network call
-         if (marine != null) {
-            if (!marine.isRetired()) {
-               if (marine.getYearsService() != null) {
-                  result = marine.getYearsService() * 100;
-                  if (!marine.getAwards().isEmpty()) {
-                     result += 1000;
-                  }
-                  if (marine.getAwards().size() >= 3) {
-                     result += 2000;
-                  }
-                  // much more logic here...
-               } else {
-                  throw new IllegalArgumentException("Any marine should have the years of service set");
-               }
-            } else result = retiredAmount();
-         } else {
-            throw new RuntimeException("Marine is null");
-         }
-      } else {
+      if (retrieveDeadStatus()) {
          // some logic here
-         result = deadAmount();
+         return deadAmount();
       }
+      if (marine == null) {
+         throw new RuntimeException("Marine is null");
+      }
+      if (marine.isRetired()) {
+         return retiredAmount();
+      }
+      if (marine.getYearsService() == null) {
+         throw new IllegalArgumentException("Any marine should have the years of service set");
+      }
+
+
+      int result = marine.getYearsService() * 100;
+      if (!marine.getAwards().isEmpty()) {
+         result += 1000;
+      }
+      if (marine.getAwards().size() >= 3) {
+         result += 2000;
+      }
+      // much more logic here...
       return result;
    }
 
