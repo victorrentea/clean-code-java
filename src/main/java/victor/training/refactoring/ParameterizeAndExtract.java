2710f6d27e28e96c2481f6953db39fc926edf620
Victor Rentea <victorrentea@gmail.com>
2020-12-17 20:04:26 +0200

    improve

--- src/main/java/victor/training/refactoring/ParameterizeAndExtract.java
+++ src/main/java/victor/training/refactoring/ParameterizeAndExtract.java
@@ -2,20 +2,31 @@ package victor.training.refactoring;
 
 class ParameterizeAndExtract {
 
-    public void f() {
+    public void f(int n) {
         System.out.println("Logic F");
         for (int i = 0; i < 4; i++) {
-            System.out.println("Code " + i);
+            if (n + i < 0) {
+                System.out.println("Code " + i);
+            } else {
+                throw new IllegalArgumentException();
+            }
         }
     }
 
-    public void g() {
+
+}
+class AnotherClass {
+
+    public void g(int n) {
         System.out.println("Logic G");
-        for (int i = 0; i < 3; i++) {
-            String x = "Code" +
-                    " " + i;
-            System.out.println(x);
+        for (int j = 0; j < 3; j++) {
+            if (n + j < 0) {
+                String x = "Code" +
+                        " " + j;
+                System.out.println(x);
+            } else {
+                throw new IllegalArgumentException();
+            }
         }
     }
-
 }
