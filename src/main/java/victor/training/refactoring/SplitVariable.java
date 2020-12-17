2a24567f5b52faec6b61ceef83417db37738b0a4
Victor Rentea <victorrentea@gmail.com>
2020-12-17 17:45:16 +0200

    i gotta leave

--- src/main/java/victor/training/refactoring/SplitVariable.java
+++ src/main/java/victor/training/refactoring/SplitVariable.java
@@ -7,11 +7,24 @@ import static org.junit.Assert.assertEquals;
 public class SplitVariable {
 
     public int discount(int price, int quantity) {
-        if (price > 50) price = price - 2;
-        if (quantity > 100) price = price - 1;
-        return price;
+        return price - computePriceDiscount(price) - computeQuantityDiscount(quantity);
     }
 
+    private int computeQuantityDiscount(int quantity) {
+        if (quantity > 100) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    private int computePriceDiscount(int price) {
+        if (price > 50) {
+            return 2;
+        } else {
+            return 0;
+        }
+    }
 
 
     @Test
