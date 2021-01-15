23be180103326ea9b8fea76e0e045d802eb9c5d2
Victor Rentea <victorrentea@gmail.com>
2021-01-15 13:57:01 +0200

    Immutable cleaner

--- src/main/java/victor/training/refactoring/ImmutablePlay.java
+++ src/main/java/victor/training/refactoring/ImmutablePlay.java
@@ -9,35 +9,44 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.toList;
 
 public class ImmutablePlay {
-    public static void main(String[] args) {
-        List<Integer> numbers = Stream.of(1, 2, 3, 4, 5).collect(toList());
+   public static void main(String[] args) {
+      List<Integer> numbers = Stream.of(1, 2, 3, 4, 5).collect(toList());
 
-        Immutable immutable = new Immutable();
-        immutable.x = 2;
-        immutable.numbers = numbers;
-        immutable.other = new Other(13);
-        System.out.println(immutable.x);
-    }
+      Immutable immutable = new Immutable();
+
+
+      immutable.x = 2;
+      immutable.numbers = numbers;
+      immutable.other = new Other(13);
+
+      // LOTS OF BUSINESS LOGIC HERE
+
+      System.out.println(immutable);
+   }
 }
 
 class Immutable {
-    public int x;
-    public List<Integer> numbers;
-    public Other other;
+   public int x;
+   public List<Integer> numbers;
+   public Other other;
+
+   public String toString() {
+      return String.format("Immutable{x=%d, numbers=%s, other=%s}", x, numbers, other);
+   }
 }
 
 class Other {
-    private int a;
+   private int a;
 
-    public Other(int a) {
-        this.a = a;
-    }
+   public Other(int a) {
+      this.a = a;
+   }
 
-    public int getA() {
-        return a;
-    }
+   public int getA() {
+      return a;
+   }
 
-    public void setA(int a) {
-        this.a = a;
-    }
+   public void setA(int a) {
+      this.a = a;
+   }
 }
