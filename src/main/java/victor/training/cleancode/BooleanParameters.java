e1cf96618e416c0653d71ddab1d3d26b1af083d9
Victor Rentea <victorrentea@gmail.com>
2021-01-29 17:50:45 +0200

    Revert "Split Loop Refactoring + comments"
    This reverts commit edc532db

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -1,88 +1,66 @@
 package victor.training.cleancode;
 
-import io.vavr.collection.Seq;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class BooleanParameters {
    public static void main(String[] args) {
       // The big method is called from various foreign places in the codebase
-      bigUglyMethod(1, 5);
-      bigUglyMethod(2, 4);
-      bigUglyMethod(3, 3);
-      bigUglyMethod(4, 2);
-      bigUglyMethod(5, 1);
+      bigUglyMethod(1, 5, false);
+      bigUglyMethod(2, 4, false);
+      bigUglyMethod(3, 3, false);
+      bigUglyMethod(4, 2, false);
+      bigUglyMethod(5, 1, false);
 
       // TODO From my use-case #323, I call it too, to do more within:
-      bigUglyMethod323(2, 1);
+      bigUglyMethod(2, 1, true);
 
    }
 
-   static void bigUglyMethod(int b, int a) {
-      beforeLogic(b, a);
-      afterLogic(b);
-   }
-   static void bigUglyMethod323(int b, int a) {
-      beforeLogic(b, a);
-      System.out.println("For CR323: LOGIC");
-      afterLogic(b);
-   }
-
-   private static void afterLogic(int b) {
-      System.out.println("More Complex Logic " + b);
-      System.out.println("More Complex Logic " + b);
-      System.out.println("More Complex Logic " + b);
-   }
-
-   private static void beforeLogic(int b, int a) {
+   static void bigUglyMethod(int b, int a, boolean cr323) {
       System.out.println("Complex Logic 1 " + a + " and " + b);
       System.out.println("Complex Logic 2 " + a);
       System.out.println("Complex Logic 3 " + a);
+
+      if (cr323) {
+         System.out.println("For CR323: LOGIC");
+      }
+
+      System.out.println("More Complex Logic " + b);
+      System.out.println("More Complex Logic " + b);
+      System.out.println("More Complex Logic " + b);
    }
 
 
    // ============== "BOSS" LEVEL: Deeply nested functions are a lot harder to break down =================
 
-   public void bossLevelStuffFluff(List<Task> tasks) {
+   public void bossLevel(boolean stuff, boolean fluff, List<Task> tasks) {
+      int index = 0; // TODO move closer to usages
+      int j = tasks.size();
       System.out.println("Logic1");
       List<Long> taskIds = new ArrayList<>();
-      System.out.println("Logic2");
-      System.out.println("Logic3");
-      int index = 0;
+      if (stuff) {
+         System.out.println("Logic2");
+         if (fluff) {
+            System.out.println("Logic3");
+            for (Task task : tasks) {
+               System.out.println("Logic4: Validate " + task);
+               task.start();
 
-      for (Task task : tasks) {  // Performance ?  1_000_000 --> what are you doing with them in memory
+               taskIds.add(task.getId());
 
-         // if the 2nd task had to start AFTER the first one FINISHED --> BUG: there is a hidden temporary coupling between === side effects around
+               // TODO When **I** call this method, I want this to run HERE, too:
+               // System.out.println("My Logic: " + task);
 
-         System.out.println("Logic4: Validate " + task);
-         task.start(); // NOT BUG since the change is on my current item.
-
-         index++;// BUG ==> did side effects to global state (outside of the current item)
-         taskIds.add(task.getId());
+               index++;
+               System.out.println("Logic5 " + index + " on " + task.isRunning());
+            }
+            System.out.println("Logic6 " + j);
+            System.out.println("Task Ids: " + taskIds);
+         } else {
+            System.out.println("Logic7 " + tasks);
+         }
       }
-
-      for (Task task : tasks) {
-         // TODO When **I** call this method, I want this to run HERE, too:
-         System.out.println("My Logic: " + task);
-      }
-
-      for (Task task : tasks) {
-         System.out.println("Logic5 " + index + " on " + task.isRunning());
-      }
-
-      System.out.println("Logic6 " + tasks.size());
-      System.out.println("Task Ids: " + taskIds);
-      System.out.println("Logic7");
-   }
-   public void bossLevelStuffNoFluff(List<Task> tasks) {
-      System.out.println("Logic1");
-      System.out.println("Logic2");
-      System.out.println("Logic7 " + tasks);
-      System.out.println("Logic7");
-   }
-   public void bossLevelNoStuff() {
-      System.out.println("Logic1");
       System.out.println("Logic7");
    }
 
