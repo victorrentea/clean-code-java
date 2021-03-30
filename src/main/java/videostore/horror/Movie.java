d2fd4f82d022634adafba3078c80aed4d44060b3
Victor Rentea <victorrentea@gmail.com>
2021-03-30 16:40:09 +0300

    Logic, switches, and enum

--- src/main/java/videostore/horror/Movie.java
+++ src/main/java/videostore/horror/Movie.java
@@ -6,12 +6,40 @@ import static java.util.Objects.requireNonNull;
 
 public class Movie {
 	enum Category {
-		REGULAR,
-		CHILDRENS,
-		NEW_RELEASE
+		REGULAR {
+         @Override
+         public double computePrice(int daysRented) {
+            double price = 2;
+            if (daysRented > 2) {
+               price += (daysRented - 2) * 1.5;
+            }
+            return price;
+         }
+      },
+		CHILDRENS {
+         @Override
+         public double computePrice(int daysRented) {
+            double price = 1.5;
+            if (daysRented > 3) {
+               price += (daysRented - 3) * 1.5;
+            }
+            return price;
+         }
+      },
+		NEW_RELEASE {
+         @Override
+         public double computePrice(int daysRented) {
+            return daysRented * 3;
+         }
+      }
+      ;
+
+      public abstract double computePrice(int daysRented);
 	}
    private final String title;
    private final Category category;
+
+
 
    public Movie(String title, Category category) {
       this.title = title;
--- src/main/java/videostore/horror/Rental.java
+++ src/main/java/videostore/horror/Rental.java
@@ -1,7 +1,6 @@
 package videostore.horror;
 
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NonNls;
+import org.junit.Test;
 import videostore.horror.Movie.Category;
 
 public class Rental {
@@ -26,26 +25,48 @@ public class Rental {
    }
 
    public double computePrice() {
-      double price = 0;
-      switch (getMovie().getCategory()) {
-         case REGULAR:
-            price += 2;
-            if (daysRented > 2) {
-               price += (daysRented - 2) * 1.5;
-            }
-            break;
-         case NEW_RELEASE:
-            price += daysRented * 3;
-            break;
-         case CHILDRENS:
-            price += 1.5;
-            if (daysRented > 3) {
-               price += (daysRented - 3) * 1.5;
-            }
-            break;
-      }
-      return price;
+      return movie.getCategory().computePrice(daysRented);
+//      switch (getMovie().getCategory()) {
+//         case REGULAR:
+//            return computeRegularPrice();
+//         case NEW_RELEASE:
+//            return computeNewReleasePrice();
+//         case CHILDRENS:
+//            return computeChildrenPrice();
+//         default:
+//            throw new IllegalStateException("Unexpected value: " + getMovie().getCategory());
+//      }
    }
+
+   // imagine a unit test doing Movie.Category.values()
+
+   @Test
+   public void test() {
+
+      for (Category value : Category.values()) {
+         // TODO call the method computePrice for this type of movie: expect non zero, no exception.
+      }
+   }
+
+//   private double computeChildrenPrice() {
+//      double price = 1.5;
+//      if (daysRented > 3) {
+//         price += (daysRented - 3) * 1.5;
+//      }
+//      return price;
+//   }
+//
+//   private int computeNewReleasePrice() {
+//      return daysRented * 3;
+//   }
+//
+//   private double computeRegularPrice() {
+//      double price = 2;
+//      if (daysRented > 2) {
+//         price += (daysRented - 2) * 1.5;
+//      }
+//      return price;
+//   }
 
    public int computeRenterPoints() {
       int frequentRenterPoints = 1;
