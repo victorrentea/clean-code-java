18d85e0818e58b6c582a7496813297e4bec0ac17
Victor Rentea <victorrentea@gmail.com>
2021-04-19 11:41:57 +0300

    We extracted a Value Object from a large signature --> pushed logic and constraints in that new object + we used it to simplify methods and reduce the number of fields of persistend entities ! OMG !!!

--- src/main/java/victor/training/cleancode/UtilsVsVO.java
+++ src/main/java/victor/training/cleancode/UtilsVsVO.java
@@ -1,8 +1,8 @@
 package victor.training.cleancode;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UtilsVsVO {
    // Ford Focus:     [2012 ---- 2016]
@@ -10,7 +10,7 @@ public class UtilsVsVO {
    public static void main(String[] args) {
       // can't afford a 2021 car
       CarSearchCriteria criteria = new CarSearchCriteria(2014, 2018, "Ford");
-      CarModel fordFocusMk2 = new CarModel("Ford", "Focus", 2012, 2016);
+      CarModel fordFocusMk2 = new CarModel("Ford", "Focus", new Interval(2012, 2016));
       List<CarModel> models = new SearchEngine().filterCarModels(criteria, Arrays.asList(fordFocusMk2));
       System.out.println(models);
    }
@@ -19,32 +19,59 @@ public class UtilsVsVO {
 
 class SearchEngine {
 
+
+   // in 2010 i search for a car made between 2005 -    2008
+   // Ford Focus Mk2 was made between 2002 -          2007
+
    public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> models) {
-      List<CarModel> results = new ArrayList<>(models);
-      results.removeIf(model -> !MathUtil.intervalsIntersect(
-          model.getStartYear(), model.getEndYear(),
-          criteria.getStartYear(), criteria.getEndYear()));
+
+      List<CarModel> results = models.stream()
+          .filter(model -> criteria.matchesProductionYears(model.getYearInterval()))
+          .collect(Collectors.toList());
+
       System.out.println("More filtering logic");
       return results;
    }
 
    private void applyCapacityFilter() {
-      System.out.println(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+      System.out.println(new Interval(1000, 1600).intersects(new Interval(1250, 2000)));
    }
 
 }
 class Alta {
    private void applyCapacityFilter() {
-      System.out.println(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+      System.out.println(new Interval(1000, 1600).intersects(new Interval(1250, 2000)));
    }
 
 }
 
+class Interval {
+   private final int start;
+   private final int end;
+
+   public Interval(int start, int end) {
+      if (start > end) {
+         throw new IllegalArgumentException("start > end");
+      }
+      this.start = start;
+      this.end = end;
+   }
+
+   public boolean intersects(Interval other) {
+      return start <= other.end && other.start <= end;
+   }
+
+   public int getStart() {
+      return start;
+   }
+
+   public int getEnd() {
+      return end;
+   }
+}
+
 class MathUtil {
 
-   public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
-      return start1 <= end2 && start2 <= end1;
-   }
 }
 
 
@@ -79,29 +106,34 @@ class CarSearchCriteria {
    public String getMake() {
       return make;
    }
+
+   public Interval getYearInterval() {
+      return new Interval(startYear, endYear);
+   }
+
+   public boolean matchesProductionYears(Interval yearInterval) {
+      return getYearInterval().intersects(yearInterval);
+   }
 }
 
+// THe HOLY ENTITY MODEL !!!!!!!
 class CarModel {
    private final String make;
    private final String model;
-   private final int startYear;
-   private final int endYear;
 
-   public CarModel(String make, String model, int startYear, int endYear) {
+   private final Interval yearInterval;
+
+   public CarModel(String make, String model, Interval yearInterval) {
       this.make = make;
       this.model = model;
-      if (startYear > endYear) throw new IllegalArgumentException("start larger than end");
-      this.startYear = startYear;
-      this.endYear = endYear;
+      this.yearInterval = yearInterval;
    }
 
-   public int getEndYear() {
-      return endYear;
+
+   public Interval getYearInterval() {
+      return yearInterval;
    }
 
-   public int getStartYear() {
-      return startYear;
-   }
 
    public String getMake() {
       return make;
