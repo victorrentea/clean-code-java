da8a0907d09a3af6ff43e943455f96b4b5fdbce1
Victor Rentea <victorrentea@gmail.com>
2021-04-20 10:28:18 +0300

    Immutable Object with Java < 17

--- src/main/java/victor/training/refactoring/ImmutablePlay.java
+++ src/main/java/victor/training/refactoring/ImmutablePlay.java
@@ -1,42 +1,75 @@
 package victor.training.refactoring;
 
+import com.google.common.collect.ImmutableList;
+
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
 public class ImmutablePlay {
    public static void main(String[] args) {
-      List<Integer> numbers = Stream.of(1, 2, 3, 4, 5).collect(toList());
+//      List<Integer> numbers = Stream.of(1, 2, 3, 4, 5).collect(toList());
+      ImmutableList<Integer> numbers = ImmutableList.of(1, 2, 3, 4, 5);
 
-      Immutable immutable = new Immutable();
+      Other other = new Other(13);
 
+      Immutable immutable = new Immutable(2, numbers, other);
+//      numbers.clear();
 
-      immutable.x = 2;
-      immutable.numbers = numbers;
-      immutable.other = new Other(13);
+      System.out.println(immutable);
 
-      // LOTS OF BUSINESS LOGIC HERE
+//      immutable.getNumbers().add(-1);
 
+      for (Integer number : immutable.getNumbers()) {
+         System.out.println("El: " + number);
+      }
       System.out.println(immutable);
    }
 }
 
 class Immutable {
-   public int x;
-   public List<Integer> numbers;
-   public Other other;
-
+   private final int x;
+   private final ImmutableList<Integer> numbers;
+   private final Other other;
+   public Immutable(int x, ImmutableList<Integer> numbers, Other other) {
+      this.x = x;
+      this.numbers = numbers;
+      this.other = other;
+   }
    public String toString() {
       return String.format("Immutable{x=%d, numbers=%s, other=%s}", x, numbers, other);
+   }
+
+   public int getX() {
+      return x;
+   }
+
+   // most usual
+//   public List<Integer> getNumbers() {
+//      return Collections.unmodifiableList(numbers);
+//   }
+
+   // a bit too geek
+//   public Iterable<Integer> getNumbers() {
+//      return numbers;
+//   }
+
+   public ImmutableList<Integer> getNumbers() {
+      return numbers;
+   }
+
+
+
+   public Other getOther() {
+      return other;
    }
 }
 
 class Other {
-   private int a;
+   private final int a;
 
    public Other(int a) {
       this.a = a;
@@ -44,9 +77,5 @@ class Other {
 
    public int getA() {
       return a;
-   }
-
-   public void setA(int a) {
-      this.a = a;
    }
 }
