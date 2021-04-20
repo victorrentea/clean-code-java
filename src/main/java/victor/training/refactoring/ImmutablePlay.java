bcb324a79512cb621cf3308c504b3cc992584e51
Victor Rentea <victorrentea@gmail.com>
2021-04-20 10:55:10 +0300

    correct POM

--- src/main/java/victor/training/refactoring/ImmutablePlay.java
+++ src/main/java/victor/training/refactoring/ImmutablePlay.java
@@ -2,6 +2,8 @@ package victor.training.refactoring;
 
 import com.google.common.collect.ImmutableList;
 
+import java.util.Objects;
+
 public class ImmutablePlay {
    public static void main(String[] args) {
 //      List<Integer> numbers = Stream.of(1, 2, 3, 4, 5).collect(toList());
@@ -9,7 +11,7 @@ public class ImmutablePlay {
 
       Other other = new Other(13);
 
-      Immutable immutable = new Immutable(2, numbers, other);
+      final Immutable immutable = new Immutable(2, numbers, other);
 //      numbers.clear();
 
       System.out.println(immutable);
@@ -19,11 +21,57 @@ public class ImmutablePlay {
       for (Integer number : immutable.numbers()) {
          System.out.println("El: " + number);
       }
+
+      Immutable immutableChanged = f(immutable);
+
       System.out.println(immutable);
+   }
+
+   private static Immutable f(Immutable immutable) {
+      return immutable.withX(-7);
    }
 }
 
-record Immutable(int x, ImmutableList<Integer> numbers, Other other) {
+class Immutable {
+   private final int x;
+   private final ImmutableList<Integer> numbers;
+   private final Other other;
+   public Immutable(int x, ImmutableList<Integer> numbers, Other other) {
+      this.x = x;
+      this.numbers = numbers;
+      this.other = Objects.requireNonNull(other);
+   }
+   public String toString() {
+      return String.format("Immutable{x=%d, numbers=%s, other=%s}", x, numbers, other);
+   }
+   // gtters/ no setter but withers
+   public Immutable withX(int newX) {
+      return new Immutable(newX, numbers, other);
+   }
+
+   public int x() {
+      return x;
+   }
+
+   // most usual
+//   public List<Integer> numbers() {
+//      return Collections.unmodifiableList(numbers);
+//   }
+
+   // a bit too geek
+//   public Iterable<Integer> numbers() {
+//      return numbers;
+//   }
+
+   public ImmutableList<Integer> numbers() {
+      return numbers;
+   }
+
+
+
+   public Other other() {
+      return other;
+   }
 }
 
 class Other {
