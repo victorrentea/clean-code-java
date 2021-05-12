cfaf8a16748b722018c498cde422ddafaeee32f0
Victor Rentea <victorrentea@gmail.com>
2021-05-12 05:39:00 +0300

    Advanced FP : changes + cleanup unused files

--- src/main/java/victor/training/java8/Loan_Pattern.java
+++ src/main/java/victor/training/java8/Loan_Pattern.java
@@ -1,34 +1,77 @@
 package victor.training.java8;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.jooq.lambda.Unchecked;
 import victor.training.cleancode.pretend.JpaRepository;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
+
+
 
 // export all orders to a file
 
 interface OrderRepo extends JpaRepository<Order, Long> { // Spring Data FanClub
 	Stream<Order> findByActiveTrue(); // 1 Mln orders ;)
+	List<Order> findAllToExport();
+}
+
+
+class StartingPoint {
+	FileExporter fileExporter;
+	OrderExportWriter orderExportWriter;
+	UserExportWriter userExportWriter;
+
+	public void play() throws IOException {
+		fileExporter.exportFile("orders.csv", writer -> orderExportWriter.orderWriteContent(writer));
+		fileExporter.exportFile("users.csv", userExportWriter::writeUsers);
+	}
+}
+
+
+class UserExportWriter {
+	private UserRepo userRepo;
+	@SneakyThrows
+	public void writeUsers(Writer writer) {
+		writer.write("username;lastname\n");
+		userRepo.findAll()
+			.stream()
+			.map(user -> user.getUsername() + ";" + user.getLastName())
+			.forEach(Unchecked.consumer(writer::write));
+	}
+}
+class OrderExportWriter {
+	private OrderRepo repo;
+	@SneakyThrows
+	public void orderWriteContent(Writer writer) {
+		writer.write("OrderID;Date\n");
+		repo.findAllToExport()
+			.stream()
+			.map(o -> o.getId() + ";" + o.getCreationDate()+"\n")
+			.forEach(Unchecked.consumer(writer::write));
+	}
 }
 
 @Slf4j
-class OrderExporter {
-	
-	private OrderRepo repo;
-			
-//	public File exportFile(String fileName) {
-//		File file = new File("export/" + fileName);
-//		try (Writer writer = new FileWriter(file)) {
-//			writer.write("OrderID;Date\n");
-//			repo.findByActiveTrue()
-//				.map(o -> o.getId() + ";" + o.getCreationDate())
-//				.forEach(writer::write);
-//			return file;
-//		} catch (Exception e) {
-//			// TODO send email notification
-//			log.debug("Gotcha!", e); // TERROR-Driven Development
-//			throw e;
-//		}
-//	}
+class FileExporter {
+
+	public File exportFile(String fileName, Consumer<Writer> contentWriterFunc) throws IOException {
+		File file = new File("export/" + fileName);
+		try (Writer writer = new FileWriter(file)) {
+			contentWriterFunc.accept(writer);
+			return file;
+		} catch (Exception e) {
+			// TODO send email notification
+			log.debug("Gotcha!", e); // TERROR-Driven Development
+			throw e;
+		}
+	}
+
 }
 
--- src/main/java/victor/training/java8/Stream_Wrecks.java
+++ src/main/java/victor/training/java8/Stream_Wrecks.java
@@ -1,9 +1,12 @@
 package victor.training.java8;
 
+import io.vavr.control.Either;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import static java.util.stream.Collectors.*;
@@ -14,18 +17,29 @@ import static java.util.stream.Collectors.*;
 class ProductService {
 	private ProductRepo productRepo;
 
+//	public Either<String, Integer> method() {
+//
+//	}
 	public List<Product> getFrequentOrderedProducts(List<Order> orders) {
+		Map<Product, Integer> productOrderCounts = countProductsOverLastYear(orders);
+
+		List<Product> frequentProducts = productOrderCounts.entrySet().stream()
+			.filter(e -> e.getValue() >= 10)
+			.map(Entry::getKey)
+			.collect(toList());
+
+		List<Long> hiddenProductIds = productRepo.getHiddenProductIds();
+		return frequentProducts.stream()
+			.filter(Product::isNotDeleted)
+			.filter(p -> !hiddenProductIds.contains(p.getId()))
+			.collect(toList());
+	}
+
+	private Map<Product, Integer> countProductsOverLastYear(List<Order> orders) {
 		return orders.stream()
-				.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
-				.flatMap(o -> o.getOrderLines().stream())
-				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)))
-				.entrySet()
-				.stream()
-				.filter(e -> e.getValue() >= 10)
-				.map(Entry::getKey)
-				.filter(p -> !p.isDeleted())
-				.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
-				.collect(toList());
+			.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
+			.flatMap(o -> o.getOrderLines().stream())
+			.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)));
 	}
 }
 
@@ -35,6 +49,10 @@ class ProductService {
 class Product {
 	private Long id;
 	private boolean deleted;
+
+	public boolean isNotDeleted() {
+		return !deleted;
+	}
 }
 
 @Data
