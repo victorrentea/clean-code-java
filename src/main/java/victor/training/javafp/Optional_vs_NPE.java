74ecbd8da02fe90ca53cfa8c7a0e3be2fe121464
Victor Rentea <victorrentea@gmail.com>
2021-06-09 17:23:06 +0300

    Null-safe with Optionals

--- src/main/java/victor/training/javafp/Optional_vs_NPE.java
+++ src/main/java/victor/training/javafp/Optional_vs_NPE.java
@@ -2,6 +2,8 @@ package victor.training.javafp;
 
 import lombok.Data;
 
+import java.util.Optional;
+
 /* "I call it my billion-dollar mistake. 
  * It was the invention of the null reference in 1965..."
  *  -- Sir Charles Antony Richard  */
@@ -10,22 +12,35 @@ import lombok.Data;
 
 class DiscountService {
 	public String getDiscountLine(Customer customer) {
-		return "Discount%: " + getApplicableDiscountPercentage(customer.getMemberCard());
+
+//		if (!customer.getMemberCard().isPresent()) {
+//			return "";
+//		Stream<Stream<Integer>>
+//		}
+		return customer.getMemberCard()
+			.flatMap(card -> getApplicableDiscountPercentage(card))
+			.map(p -> "Discount%: " + p)
+			.orElse("");
 	}
 		
-	private Integer getApplicableDiscountPercentage(MemberCard card) { 
+	private Optional<Integer> getApplicableDiscountPercentage(MemberCard card) {
+//		if (card == null) {
+//			return Optional.empty();
+//		}
 		if (card.getFidelityPoints() >= 100) {
-			return 5;
+			return Optional.of(5);
 		}
 		if (card.getFidelityPoints() >= 50) {
-			return 3;
+			return Optional.of(3);
 		}
-		return null;
+		return Optional.empty();
 	}
 		
 	// test: 60, 10, no MemberCard
 	public static void main(String[] args) {
-		
+		System.out.println(new DiscountService().getDiscountLine(new Customer(new MemberCard(60))));
+		System.out.println(new DiscountService().getDiscountLine(new Customer(new MemberCard(10))));
+		System.out.println(new DiscountService().getDiscountLine(new Customer()));
 	}
 }
 
@@ -38,8 +53,14 @@ class Customer {
 	public Customer(MemberCard profile) {
 		this.memberCard = profile;
 	}
-	public MemberCard getMemberCard() {
-		return memberCard;
+
+	public Customer setMemberCard(MemberCard memberCard) {
+		this.memberCard = memberCard;
+		return this;
+	}
+
+	public Optional<MemberCard> getMemberCard() {
+		return Optional.ofNullable(memberCard);
 	}
 }
 
