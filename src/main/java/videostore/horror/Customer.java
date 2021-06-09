e9bb6fb87583224c997c08c650f70c338a5bf467
Victor Rentea <victorrentea@gmail.com>
2021-06-09 13:33:12 +0300

    pauza de masa !

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -1,7 +1,6 @@
 package videostore.horror;
 
 import java.util.*;
-import java.util.Map.Entry;
 
 class Customer {
 	private final String name;
@@ -22,51 +21,28 @@ class Customer {
 	public String statement() {
 		double totalPrice = 0;
 		int frequentRenterPoints = 0;
-
-		String result = "Rental Record for " + getName() + "\n";
-
-
+		String result = formatHeader();
 		for (Rental rental : rentals) {
-			Movie movie = rental.getMovie();
-			int daysRented = rental.getDaysRented();
-
-			double price = calculatePrice(movie, daysRented);
-
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((movie.getCategory() == Movie.Category.NEW_RELEASE) && daysRented >= 2) {
-				frequentRenterPoints++;
-			}
-			// show figures line for this rental
-			result += "\t" + movie.getTitle() + "\t" + price + "\n";
-			totalPrice += price;
+			frequentRenterPoints += rental.calculateBonus();
+			result += formatBodyLine(rental);
+			totalPrice += rental.calculatePrice();
 		}
-		// add footer lines
-		result += "Amount owed is " + totalPrice + "\n";
-		result += "You earned " + frequentRenterPoints + " frequent renter points";
+		result += formatFooter(totalPrice, frequentRenterPoints);
 		return result;
 	}
 
-	private double calculatePrice(Movie movie, int daysRented) {
-		double price;
-		switch (movie.getCategory()) {
-			case REGULAR:
-				price = 2;
-				if (daysRented > 2)
-					price += (daysRented - 2) * 1.5;
-				break;
-			case NEW_RELEASE:
-				price = daysRented * 3;
-				break;
-			case CHILDREN:
-				price = 1.5;
-				if (daysRented > 3)
-					price += (daysRented - 3) * 1.5;
-				break;
-			default:
-				throw new IllegalStateException("Unexpected value: " + movie.getCategory());
-		}
-		return price;
+	private String formatBodyLine(Rental rental) {
+		return "\t" + rental.getMovie().getTitle() + "\t" + rental.calculatePrice() + "\n";
 	}
+
+	private String formatHeader() {
+		return "Rental Record for " + name + "\n";
+	}
+
+	private String formatFooter(double totalPrice, int frequentRenterPoints) {
+		return "Amount owed is " + totalPrice + "\n"
+				 + "You earned " + frequentRenterPoints + " frequent renter points";
+	}
+
+
 }
--- src/main/java/videostore/horror/Rental.java
+++ src/main/java/videostore/horror/Rental.java
@@ -18,4 +18,35 @@ public class Rental {
    public Movie getMovie() {
       return movie;
    }
+
+   public double calculatePrice() {
+      double price;
+      switch (movie.getCategory()) {
+         case REGULAR:
+            price = 2;
+            if (daysRented > 2)
+               price += (daysRented - 2) * 1.5;
+            break;
+         case NEW_RELEASE:
+            price = daysRented * 3;
+            break;
+         case CHILDREN:
+            price = 1.5;
+            if (daysRented > 3)
+               price += (daysRented - 3) * 1.5;
+            break;
+         default:
+            throw new IllegalStateException("Unexpected value: " + movie.getCategory());
+      }
+      return price;
+   }
+
+   public int calculateBonus() {
+      int frequentRenterPoints = 1;
+      // add bonus for a two day new release rental
+      if ((getMovie().getCategory() == Movie.Category.NEW_RELEASE) && getDaysRented() >= 2) {
+         frequentRenterPoints++;
+      }
+      return frequentRenterPoints;
+   }
 }
