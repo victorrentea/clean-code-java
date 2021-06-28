97ed7326fae7aeaa3b36e39d4abbce3a5c47c4f7
Victor Rentea <victorrentea@gmail.com>
2021-06-28 12:59:39 +0300

    Manual Refactoring (dear reviewer: i hope i didnt f***ed up anythng in her) thank you.

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -2,6 +2,10 @@ package victor.training.cleancode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class BooleanParameters {
    public static void main(String[] args) {
@@ -13,50 +17,80 @@ public class BooleanParameters {
       bigUglyMethod(5, 1);
 
       // TODO From my use-case #323, I call it too, to do more within:
-      bigUglyMethod(2, 1);
+      bigUglyMethod323(2, 1);
 
    }
 
    static void bigUglyMethod(int b, int a) {
+      before(b, a);
+
+      after(b);
+   }
+
+   static void bigUglyMethod323(int b, int a) {
+      before(b, a);
+
+      // HERE!!! I need to add something to happen , but only when I want that.
+      System.out.println("THIS only for cr 323");
+
+      after(b);
+   }
+
+   private static void after(int b) {
+      System.out.println("More Complex Logic " + b);
+      System.out.println("More Complex Logic " + b);
+      System.out.println("More Complex Logic " + b);
+   }
+
+   private static void before(int b, int a) {
       System.out.println("Complex Logic 1 " + a + " and " + b);
       System.out.println("Complex Logic 2 " + a);
       System.out.println("Complex Logic 3 " + a);
-
-      System.out.println("More Complex Logic " + b);
-      System.out.println("More Complex Logic " + b);
-      System.out.println("More Complex Logic " + b);
    }
 
 
    // ============== "BOSS" LEVEL: Deeply nested functions are a lot harder to break down =================
 
-   public void bossLevel(boolean stuff, boolean fluff, List<Task> tasks) {
-      int index = 0; // TODO move closer to usages
-      int j = tasks.size();
+   public void bossLevelStuffFluff(List<Task> tasks) {
       System.out.println("Logic1");
-      List<Long> taskIds = new ArrayList<>();
-      if (stuff) {
-         System.out.println("Logic2");
-         if (fluff) {
-            System.out.println("Logic3");
-            for (Task task : tasks) {
-               System.out.println("Logic4: Validate " + task);
-               task.setRunning();
+      System.out.println("Logic2");
+      System.out.println("Logic3");
 
-               taskIds.add(task.getId());
 
-               // TODO When **I** call this method, I want this to run HERE, too:
-               // System.out.println("My Logic: " + task);
+      List<Long> taskIds = tasks.stream().map(Task::getId).collect(toList());
 
-               index++;
-               System.out.println("Logic5 " + index + " on " + task.isRunning());
-            }
-            System.out.println("Logic6 " + j);
-            System.out.println("Task Ids: " + taskIds);
-         } else {
-            System.out.println("Logic7 " + tasks);
-         }
+      for (Task task : tasks) {
+         System.out.println("Logic4: Validate " + task);
+         task.setRunning(); // mutation of the loop item
       }
+      int index = 0;
+      for (Task task : tasks) {
+
+         // TODO When **I** call this method, I want this to run HERE, too:
+//         if (cr323) {
+            System.out.println("My Logic: " + task);
+//         }
+
+         index++; // mutation outside of the loop
+         System.out.println("Logic5 " + index + " on " + task.isRunning());
+      }
+      System.out.println("Logic6 " + tasks.size());
+      System.out.println("Task Ids: " + taskIds);
+      System.out.println("Logic7");
+   }
+
+
+
+
+   public void bossLevelStuffNoFluff(List<Task> tasks) {
+      System.out.println("Logic1");
+      System.out.println("Logic2");
+      System.out.println("Logic7 " + tasks);
+      System.out.println("Logic7");
+   }
+
+   public void bossLevelNoStuff() {
+      System.out.println("Logic1");
       System.out.println("Logic7");
    }
 
