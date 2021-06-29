e65c2d814eed87e2317a993fcfaf5fe1957fe0c9
Victor Rentea <victorrentea@gmail.com>
2021-06-29 11:14:43 +0300

    yolo

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -21,44 +21,40 @@ class Customer {
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
-		Iterator<Movie> rentals = this.rentals.keySet().iterator();
 		String result = "Rental Record for " + getName() + "\n";
-		while (rentals.hasNext()) {
+		for (Movie each : rentals.keySet()) {
 			double thisAmount = 0;
-			Movie each = (Movie) rentals.next();
 			// determine amounts for each line
-			int dr = this.rentals.get(each);
+			int dr = rentals.get(each);
 			switch (each.getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (dr > 2)
-					thisAmount += (dr - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += dr * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (dr > 3)
-					thisAmount += (dr - 3) * 1.5;
-				break;
+				case Movie.REGULAR:
+					thisAmount += 2;
+					if (dr > 2)
+						thisAmount += (dr - 2) * 1.5;
+					break;
+				case Movie.NEW_RELEASE:
+					thisAmount += dr * 3;
+					break;
+				case Movie.CHILDRENS:
+					thisAmount += 1.5;
+					if (dr > 3)
+						thisAmount += (dr - 3) * 1.5;
+					break;
 			}
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
 			if (each.getPriceCode() != null &&
-					(each.getPriceCode() == Movie.NEW_RELEASE)
-					&& dr > 1)
+				 (each.getPriceCode() == Movie.NEW_RELEASE)
+				 && dr > 1)
 				frequentRenterPoints++;
 			// show figures line for this rental
-			result += "\t" + each.getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
+			result += "\t" + each.getTitle() + "\t" + thisAmount + "\n";
 			totalAmount += thisAmount;
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
-				+ " frequent renter points";
+		result += "Amount owed is " + totalAmount + "\n";
+		result += "You earned " + frequentRenterPoints + " frequent renter points";
 		return result;
 	}
 }
