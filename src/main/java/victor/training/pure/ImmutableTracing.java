cf5e46a816913476703632f911050ab063a4c04a
Victor Rentea <victorrentea@gmail.com>
2021-07-02 10:22:44 +0300

    Refactor to immutables

--- src/main/java/victor/training/pure/ImmutableTracing.java
+++ src/main/java/victor/training/pure/ImmutableTracing.java
@@ -1,5 +1,7 @@
 package victor.training.pure;
 
+import lombok.With;
+
 public class ImmutableTracing {
 
    public static void main(String[] args) {
@@ -9,39 +11,64 @@ public class ImmutableTracing {
 
 
 class MutableHell {
-   class Mutable {
-      private int x;
+   static class Mutable {
+      @With
+      private final int x;
+      @With
+      private final int y;
+      private final int z;
+
+      Mutable(int x, int y, int z) {
+         this.x = x;
+         this.y = y;
+         this.z = z;
+      }
 
       public int getX() {
          return x;
       }
 
-      public void setX(int x) {
-         this.x = x;
+      public int getY() {
+         return y;
       }
+
+      public int getZ() {
+         return z;
+      }
+
+//      public Mutable withX(int x) {
+//         return new Mutable(x, getY(), getZ());
+//      }
    }
 
    public void h() {
-      Mutable data = new Mutable();
-      data.setX(1);
+      Mutable data = new Mutable(1, 10, 20);
       // more code
       g(data);
    }
 
    public void g(Mutable data) {
-      data.setX(2);
+      data = data.withX(2);
       // more code
-      evil(data);
+//      data = innocent(data); // teoretic in innocent se poate modifica orice din obietul Data.
+
+      XY xy = innocent(data);
+      data = data.withX(xy.getX()).withY(xy.getY());
+//      data = data.withXY(xy);
       // more code
       f(data);
    }
 
-   private void evil(Mutable data) {
-      deeperEvil(data);
+   private XY innocent(Mutable data) {
+      return new XY(evil(data), 77);
    }
 
-   private void deeperEvil(Mutable data) {
-      data.setX(3);
+   private int evil(Mutable data) {
+      return deeperEvil(data);
+   }
+
+   private int deeperEvil(Mutable data) {
+      return 3;
    }
 
    public void f(Mutable data) {
---
+++ src/main/java/victor/training/pure/XY.java
@@ -0,0 +1,9 @@
+package victor.training.pure;
+
+import lombok.Data;
+
+@Data
+public class XY {
+   private final int x;
+   private final int y;
+}
