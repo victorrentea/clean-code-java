91113690f3bc131a8c4ca5bbdfe032022d548743
Victor Rentea <victorrentea@gmail.com>
2021-07-02 09:54:03 +0300

    immutables java SE

--- src/main/java/victor/training/pure/ImmutableAdvanced.java
+++ src/main/java/victor/training/pure/ImmutableAdvanced.java
@@ -1,29 +1,34 @@
 package victor.training.pure;
 
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static java.util.Collections.*;
 import static java.util.stream.Collectors.toList;
 
 public class ImmutableAdvanced {
    public static void main(String[] args) {
-      List<Integer> numbers = Stream.of(1, 2, 3).collect(toList());
+      ImmutableList<Integer> numbers =ImmutableList.of(1,2,3);
 
-      Immutable immutable = null;//new Immutable(1, numbers, new Other(15));
+      Immutable immutable = new Immutable(1, numbers, new Other(15));
       System.out.println(immutable);
 
+      numbers.add(1);
       // wilderness
-
       System.out.println(immutable);
    }
 }
 
 class Immutable {
    private final int x;
-   private final List<Integer> numbers;
+   private final ImmutableList<Integer> numbers;
    private final Other other;
 
-   Immutable(int x, List<Integer> numbers, Other other) {
+   Immutable(int x, ImmutableList<Integer> numbers, Other other) {
       this.x = x;
       this.numbers = numbers;
       this.other = other;
@@ -45,7 +50,7 @@ class Immutable {
 }
 
 class Other {
-   private int a;
+   private final int a;
 
    public Other(int a) {
       this.a = a;
@@ -55,7 +60,4 @@ class Other {
       return a;
    }
 
-   public void setA(int a) {
-      this.a = a;
-   }
 }
