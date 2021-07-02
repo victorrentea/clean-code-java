7b81dd739131c75bb1b0e2d09b2c4526dec5ea83
Victor Rentea <victorrentea@gmail.com>
2021-07-02 15:44:01 +0300

    jool library to avoid checked exceptions in lambdas

--- src/main/java/victor/training/fp/Loan_Pattern.java
+++ src/main/java/victor/training/fp/Loan_Pattern.java
@@ -1,8 +1,16 @@
 package victor.training.fp;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jooq.lambda.Unchecked;
+import org.jooq.lambda.fi.util.function.CheckedConsumer;
+import org.jooq.lambda.fi.util.function.CheckedFunction;
 import victor.training.cleancode.pretend.JpaRepository;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 // export all orders to a file
@@ -13,22 +21,22 @@ interface OrderRepo extends JpaRepository<Order, Long> { // Spring Data FanClub
 
 @Slf4j
 class OrderExporter {
-	
+
 	private OrderRepo repo;
 			
-//	public File exportFile(String fileName) {
-//		File file = new File("export/" + fileName);
-//		try (Writer writer = new FileWriter(file)) {
-//			writer.write("OrderID;Date\n");
-//			repo.findByActiveTrue()
-//				.map(o -> o.getId() + ";" + o.getCreationDate())
-//				.forEach(writer::write);
-//			return file;
-//		} catch (Exception e) {
-//			// TODO send email notification
-//			log.debug("Gotcha!", e); // TERROR-Driven Development
-//			throw e;
-//		}
-//	}
+	public File exportFile(String fileName) throws IOException {
+		File file = new File("export/" + fileName);
+		try (Writer writer = new FileWriter(file)) {
+			writer.write("OrderID;Date\n");
+			repo.findByActiveTrue()
+				.map(o -> o.getId() + ";" + o.getCreationDate())
+				.forEach(Unchecked.consumer(writer::write));
+			return file;
+		} catch (Exception e) {
+			// TODO send email notification
+			log.debug("Gotcha!", e); // TERROR-Driven Development
+			throw e;
+		}
+	}
 }
 
