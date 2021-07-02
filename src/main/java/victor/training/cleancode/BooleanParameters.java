e85570278e1ad490468a689e23d81c2afabc5ea5
Victor Rentea <victorrentea@gmail.com>
2021-07-02 12:26:17 +0300

    Guard clauses

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -6,33 +6,48 @@ import java.util.List;
 public class BooleanParameters {
    public static void main(String[] args) {
       // The big method is called from various foreign places in the codebase
-      bigUglyMethod(1, 5, false);
-      bigUglyMethod(2, 4, false);
-      bigUglyMethod(3, 3, false);
-      bigUglyMethod(4, 2, false);
-      bigUglyMethod(5, 1, false);
+      bigUglyMethod(1, 5);
+      bigUglyMethod(2, 4);
+      bigUglyMethod(3, 3);
+      bigUglyMethod(4, 2);
+      bigUglyMethod(5, 1);
 
       // TODO From my use-case #323, I call it too, to do more within:
-      bigUglyMethod(2, 1, true);
+      bigUglyMethod(2, 1);
+
+
+      // TODO From my use-case #325, I call it too, to do more within:
+      bigUglyMethod323(2, 1);
 
    }
 
-   static void bigUglyMethod(int b, int a, boolean cr323) {
+   static void bigUglyMethod(int b, int a) {
+      beforeLogic(b, a);
+      after(b);
+   }
+   static void bigUglyMethod323(int b, int a) {
+      beforeLogic(b, a);
+      System.out.println("#sieu pt cr323"); // doar cand o chem eu
+      after(b);
+   }
+
+   private static void after(int b) {
+      System.out.println("More Complex Logic " + b);
+      System.out.println("More Complex Logic " + b);
+      System.out.println("More Complex Logic " + b);
+   }
+
+   private static void beforeLogic(int b, int a) {
       System.out.println("Complex Logic 1 " + a + " and " + b);
       System.out.println("Complex Logic 2 " + a);
       System.out.println("Complex Logic 3 " + a);
-
-      if (cr323) {
-         System.out.println("#sieu pt cr323"); // doar cand o chem eu
-      }
-
-      System.out.println("More Complex Logic " + b);
-      System.out.println("More Complex Logic " + b);
-      System.out.println("More Complex Logic " + b);
    }
 
 
    // ============== "BOSS" LEVEL: Deeply nested functions are a lot harder to break down =================
+   public void clientuSaracu() {
+      bossLevel(true, false, new ArrayList<>());
+   }
 
    public void bossLevel(boolean stuff, boolean fluff, List<Task> tasks) {
       int index = 0; // TODO move closer to usages
@@ -50,7 +65,7 @@ public class BooleanParameters {
                taskIds.add(task.getId());
 
                // TODO When **I** call this method, I want this to run HERE, too:
-               // System.out.println("My Logic: " + task);
+               System.out.println("My Logic: " + task);
 
                index++;
                System.out.println("Logic5 " + index + " on " + task.isRunning());
