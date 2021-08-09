d106c85804c5d0ee7858ea78aad80d74097f7f5b
Victor Rentea <victorrentea@gmail.com>
2021-08-09 12:34:52 +0300

    Split loop + bug

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -1,6 +1,7 @@
 package victor.training.cleancode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BooleanParameters {
@@ -13,15 +14,28 @@ public class BooleanParameters {
       bigUglyMethod(5, 1);
 
       // TODO From my use-case #323, I call it too, to do more within:
-      bigUglyMethod(2, 1);
+      bigUglyMethod323(2, 1);
 
    }
 
    static void bigUglyMethod(int b, int a) {
+      bigStart(b, a);
+      bigEnd(b);
+   }
+
+   static void bigUglyMethod323(int b, int a) {
+      bigStart(b, a);
+      System.out.println("CR323 specific");
+      bigEnd(b);
+   }
+
+   private static void bigStart(int b, int a) {
       System.out.println("Complex Logic 1 " + a + " and " + b);
       System.out.println("Complex Logic 2 " + a);
       System.out.println("Complex Logic 3 " + a);
+   }
 
+   private static void bigEnd(int b) {
       System.out.println("More Complex Logic " + b);
       System.out.println("More Complex Logic " + b);
       System.out.println("More Complex Logic " + b);
@@ -30,34 +44,64 @@ public class BooleanParameters {
 
    // ============== "BOSS" LEVEL: Deeply nested functions are a lot harder to break down =================
 
-   public void bossLevel(boolean stuff, boolean fluff, List<Task> tasks) {
-      int index = 0; // TODO move closer to usages
-      int j = tasks.size();
+   {
+      bossLevelStuffFluff(Collections.emptyList(), false);
+      bossLevelStuffNoFluff(Collections.emptyList());
+      bossLevelNoStuff(Collections.emptyList());
+      bossLevelNoStuff(Collections.emptyList());
+
+      //cr323
+      bossLevelStuffFluff(Collections.emptyList(), true);
+
+   }
+
+   public void bossLevelStuffFluff(List<Task> tasks, boolean cr323) {
       System.out.println("Logic1");
       List<Long> taskIds = new ArrayList<>();
-      if (stuff) {
-         System.out.println("Logic2");
-         if (fluff) {
-            System.out.println("Logic3");
-            for (Task task : tasks) {
-               System.out.println("Logic4: Validate " + task);
-               task.setRunning();
+      System.out.println("Logic2");
+      System.out.println("Logic3");
+      int index = 0;
+      for (Task task : tasks) {
+         System.out.println("Logic4: Validate " + task);
+         task.setRunning();
+         index++;
+      }
 
-               taskIds.add(task.getId());
-
-               // TODO When **I** call this method, I want this to run HERE, too:
-               // System.out.println("My Logic: " + task);
-
-               index++;
-               System.out.println("Logic5 " + index + " on " + task.isRunning());
-            }
-            System.out.println("Logic6 " + j);
-            System.out.println("Task Ids: " + taskIds);
-         } else {
-            System.out.println("Logic7 " + tasks);
+      for (Task task : tasks) {
+         taskIds.add(task.getId());
+      }
+      for (Task task : tasks) {
+         // TODO When **I** call this method, I want this to run HERE, too:
+         if (cr323) {
+            System.out.println("My Logic: " + task);
          }
       }
-      System.out.println("Logic7");
+      for (Task task : tasks) {
+//         index++;
+         System.out.println("Logic5 " + index + " on " + task.isRunning());
+      }
+      System.out.println("Logic6 " + tasks.size());
+      System.out.println("Task Ids: " + taskIds);
+      System.out.println("Logic7 " + tasks.size());
+   }
+
+
+
+   public void bossLevelStuffNoFluff(List<Task> tasks) {
+      System.out.println("Logic1");
+      System.out.println("Logic2");
+      System.out.println("Logic7 " + tasks);
+      System.out.println("Logic7 " + tasks.size());
+   }
+   public void bossLevelNoStuff(List<Task> tasks) {
+      System.out.println("Logic1");
+      System.out.println("Logic7 " + tasks.size());
+   }
+
+
+   private void innocent(List<Task> tasks) {
+
+      tasks.clear();
    }
 
 }
