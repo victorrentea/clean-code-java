1e36042fd537e9046d0415a938d5811a07e48fe0
Victor Rentea <victorrentea@gmail.com>
2021-08-10 13:36:42 +0300

    abstract enum to protect against forgetting to add behavior for a new added enum value

--- src/main/java/videostore/horror/Movie.java
+++ src/main/java/videostore/horror/Movie.java
@@ -1,14 +1,32 @@
 package videostore.horror;
 
-import java.util.Objects;
-
 import static java.util.Objects.requireNonNull;
 
 public class Movie {
 	enum Category  {
-   	CHILDRENS,
-   	REGULAR,
-   	NEW_RELEASE
+   	CHILDRENS {
+			public double computePrice(int daysRented) {
+				double price = 1.5;
+				if (daysRented > 3)
+					price += (daysRented - 3) * 1.5;
+				return price;
+			}
+		},
+   	REGULAR {
+			public double computePrice(int daysRented) {
+				double price = 2;
+				if (daysRented > 2)
+					price += (daysRented - 2) * 1.5;
+				return price;
+			}
+		},
+   	NEW_RELEASE {
+			public double computePrice(int daysRented) {
+				return daysRented * 3;
+			}
+		};
+
+   	public abstract double computePrice(int daysRented);
 	}
 
    private final String title;
--- src/main/java/videostore/horror/Rental.java
+++ src/main/java/videostore/horror/Rental.java
@@ -19,25 +19,11 @@ public class Rental {
       return movie;
    }
 
+//   move compute price into movie and then replace switch with polymorphism
    public double computePrice() {
-      double price = 0;
-      switch (getMovie().getCategory()) {
-         case REGULAR:
-            price += 2;
-            if (getDaysRented() > 2)
-               price += (getDaysRented() - 2) * 1.5;
-            break;
-         case NEW_RELEASE:
-            price += getDaysRented() * 3;
-            break;
-         case CHILDRENS:
-            price += 1.5;
-            if (getDaysRented() > 3)
-               price += (getDaysRented() - 3) * 1.5;
-            break;
-      }
-      return price;
+      return movie.getCategory().computePrice(daysRented);
    }
+
 
    public boolean isBonusApplicable() {
       return (movie.getCategory() == Category.NEW_RELEASE)
