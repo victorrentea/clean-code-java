2053458af92692cba45171f3de59d0a7fd14aeab
Victor Rentea <victorrentea@gmail.com>
2021-09-17 14:51:12 +0300

    Java 17 switch :) love asta nu compileaza !!

--- src/main/java/victor/training/cleancode/ReturnSwitchWTF.java
+++ src/main/java/victor/training/cleancode/ReturnSwitchWTF.java
@@ -1,24 +1,25 @@
-package victor.training.cleancode;
-
-public class ReturnSwitchWTF {
-   public int getHeight(TypeOfStuf stuf) {
-      return switch (stuf) {
-         case INALT -> 250;
-         case JOS -> 80;
-         case VAMA_VECHE -> 0;
-      };
-
-   }
-
-   public void test() {
-      System.out.println(getHeight(TypeOfStuf.INALT));
-   }
-}
-
-
-enum TypeOfStuf {
-   INALT,
-   JOS,
-   VAMA_VECHE,
-   COSTINESTI
-}
+//package victor.training.cleancode;
+//
+//public class ReturnSwitchWTF {
+//   // nu compileaza pe java 17
+//   public int getHeight(TypeOfStuf stuf) {
+//      return switch (stuf) {
+//         case INALT -> 250;
+//         case JOS -> 80;
+//         case VAMA_VECHE -> 0;
+//      };
+//
+//   }
+//
+//   public void test() {
+//      System.out.println(getHeight(TypeOfStuf.INALT));
+//   }
+//}
+//
+//
+//enum TypeOfStuf {
+//   INALT,
+//   JOS,
+//   VAMA_VECHE,
+//   COSTINESTI
+//}
