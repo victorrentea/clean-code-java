9e2559baf5d0fdfe2afac6f0e677a6f93486173d
Victor Rentea <victorrentea@gmail.com>
2021-09-27 17:13:17 +0300

    Am extras partea de presentation (formatting) din Model (Customer)

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -21,36 +21,7 @@ class Customer {
 		return name;
 	}
 
-	public String generateStatement() {
-		return formatHeader()
-				 + formatBody()
-				 + formatFooter();
+	public List<Rental> getRentals() {
+		return rentals;
 	}
-
-	private String formatBody() {
-		return rentals.stream().map(this::formatBodyLine).collect(joining());
-	}
-
-	private String formatHeader() {
-		return "Rental Record for " + getName() + "\n";
-	}
-
-	private String formatBodyLine(Rental rental) {
-		return "\t" + rental.getMovie().getTitle() + "\t" + rental.computePrice() + "\n";
-	}
-
-	private String formatFooter() {
-		return "Amount owed is " + computeTotalPrice() + "\n"
-				 + "You earned " + computeTotalPoints() + " frequent renter points";
-	}
-
-	private double computeTotalPrice() {
-		return rentals.stream().mapToDouble(Rental::computePrice).sum();
-	}
-
-	private int computeTotalPoints() {
-		return rentals.stream().mapToInt(Rental::computePoints).sum();
-	}
-	// "separation by layers of abstraction" -- codul dintr-o metoda trebuie sa fie aprox la acelasi nivel de detaliu
-
 }
---
+++ src/main/java/videostore/horror/StatementGenerator.java
@@ -0,0 +1,48 @@
+package videostore.horror;
+
+import lombok.Value;
+import victor.training.cleancode.pretend.Autowired;
+import victor.training.cleancode.pretend.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.joining;
+
+@Service
+public class StatementGenerator {
+
+
+   public String generateStatement(String customerName, List<Rental> rentals) {
+      return formatHeader(customerName)
+             + formatBody(rentals)
+             + formatFooter(rentals);
+   }
+
+   private String formatHeader(String customerName) {
+      return "Rental Record for " + customerName + "\n";
+   }
+
+   private String formatBody(List<Rental> rentals) {
+      return rentals.stream().map(this::formatBodyLine).collect(joining());
+   }
+
+   private String formatBodyLine(Rental rental) {
+      return "\t" + rental.getMovie().getTitle() + "\t" + rental.computePrice() + "\n";
+   }
+
+   private String formatFooter(List<Rental> rentals) {
+      return "Amount owed is " + computeTotalPrice(rentals) + "\n"
+             + "You earned " + computeTotalPoints(rentals) + " frequent renter points";
+   }
+
+   private double computeTotalPrice(List<Rental> rentals) {
+      return rentals.stream().mapToDouble(Rental::computePrice).sum();
+   }
+
+   private int computeTotalPoints(List<Rental> rentals) {
+      return rentals.stream().mapToInt(Rental::computePoints).sum();
+   }
+   // "separation by layers of abstraction" -- codul dintr-o metoda trebuie sa fie aprox la acelasi nivel de detaliu
+
+}
