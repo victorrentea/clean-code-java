8f3e931eafe84ec025c0157bc7bdeb08cb8264e3
Victor Rentea <victorrentea@gmail.com>
2021-09-28 15:02:31 +0300

    Deep immutable objects cu Java Standard

--- src/main/java/victor/training/pure/basic/ImmutableBasic.java
+++ src/main/java/victor/training/pure/basic/ImmutableBasic.java
@@ -1,5 +1,9 @@
 package victor.training.pure.basic;
 
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -9,33 +13,49 @@ public class ImmutableBasic {
    public static void main(String[] args) {
       List<Integer> numbers = Stream.of(1, 2, 3, 4, 5).collect(toList());
 
-      Immutable immutable = new Immutable();
-
-      immutable.x = 2;
-      immutable.numbers = numbers;
-      immutable.other = new Other(13);
+      Immutable immutable = new Immutable(2, numbers, new Other(13));
 
       System.out.println(immutable);
 
       // LOTS OF BUSINESS LOGIC HERE
+      numbers.clear(); // rar in practica
 
-      System.out.println(immutable.numbers);
+      System.out.println(immutable.getNumbers());
       System.out.println(immutable);
    }
 }
 
 class Immutable {
-   public int x;
-   public List<Integer> numbers;
-   public Other other;
+   private final int x;
+   private final List<Integer> numbers;
+   private final Other other;
+
+   Immutable(int x, List<Integer> numbers, Other other) {
+      this.x = x;
+      this.numbers = new ArrayList<>(numbers);// waste de memorie.
+      this.other = other;
+   }
+   public int getX() {
+      return x;
+   }
+//   public List<Integer> getNumbers() {
+//      return new ArrayList<>(numbers); // waste de memorie
+//   }
+   public List<Integer> getNumbers() {
+      return Collections.unmodifiableList(numbers); // cea mai raspandita solutie
+   }
+   public Other getOther() {
+      return other;
+   }
 
    public String toString() {
       return String.format("Immutable{x=%d, numbers=%s, other=%s}", x, numbers, other);
    }
 }
 
+@ToString
 class Other {
-   private int a;
+   private final int a;
 
    public Other(int a) {
       this.a = a;
@@ -45,7 +65,4 @@ class Other {
       return a;
    }
 
-   public void setA(int a) {
-      this.a = a;
-   }
 }
