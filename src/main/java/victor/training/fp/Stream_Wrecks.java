0190d8e707ba5bcd1692161a8419e88e78f790f0
Victor Rentea <victorrentea@gmail.com>
2021-11-15 16:14:17 +0200

    FP clean code rules

--- src/main/java/victor/training/fp/Stream_Wrecks.java
+++ src/main/java/victor/training/fp/Stream_Wrecks.java
@@ -4,8 +4,12 @@ import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
+import static java.util.function.Predicate.not;
 import static java.util.stream.Collectors.*;
 
 // get the products frequently ordered during the past year
@@ -15,18 +19,37 @@ class ProductService {
 	private ProductRepo productRepo;
 
 	public List<Product> getFrequentOrderedProducts(List<Order> orders) {
-		return orders.stream()
-				.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
-				.flatMap(o -> o.getOrderLines().stream())
-				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)))
-				.entrySet()
-				.stream()
-				.filter(e -> e.getValue() >= 10)
-				.map(Entry::getKey)
-				.filter(p -> !p.isDeleted())
-				.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
+		Map<Product, Integer> productCounts = orders.stream()
+			.filter(this::isRecent) // use-case specific logic. //Only I consider orders to be recent if < 1 year old
+			.flatMap(o -> o.getOrderLines().stream())
+			.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)));
+
+		List<Product> popularProducts = productCounts.entrySet().stream()
+			.filter(e -> e.getValue() >= 10)
+			.map(Entry::getKey)
+			.toList();
+
+		if (popularProducts.size() > 100) {
+			System.out.println("I have many prop");
+		}
+
+		List<Long> hiddenProductIds = productRepo.getHiddenProductIds(); // 1 single call do DB
+
+		return popularProducts.stream()
+
+
+
+			.filter(p -> !p.isDeleted())
+			.filter(not(Product::isDeleted))
+
+				.filter(p -> !hiddenProductIds.contains(p.getId()))
 				.collect(toList());
 	}
+
+	private boolean isRecent(Order o) {
+		return o.getCreationDate().isAfter(LocalDate.now().minusYears(1));
+	}
+
 }
 
 
