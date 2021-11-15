5a209e7b84a61b6b39cbac906a47785cd924392e
Victor Rentea <victorrentea@gmail.com>
2021-11-15 10:10:26 +0200

    Pure functions recap

--- src/main/java/victor/training/cleancode/ParameterObjects.java
+++ src/main/java/victor/training/cleancode/ParameterObjects.java
@@ -5,8 +5,8 @@ public class ParameterObjects {
       new ParameterObjects().placeOrder("John", "Doe", "St. Albergue", "Paris", 99);
    }
 
-   public void placeOrder(String fName, String lName, String city, String streetName, Integer streetNumber) {
-      if (fName == null || lName == null) throw new IllegalArgumentException();
+   public void placeOrder(String firstName, String lName, String city, String streetName, Integer streetNumber) {
+      if (firstName == null || lName == null) throw new IllegalArgumentException();
 
       System.out.println("Some Logic");
       System.out.println("Shipping to " + city + " on St. " + streetName + " " + streetNumber);
@@ -53,8 +53,27 @@ class PersonService {
    public void f(Person person) {
       System.out.println("Hi there, " + person.getFirstName());
 
-      String fullNameStr = person.getFirstName() + " " + person.getLastName().toUpperCase();
-      System.out.println("Record for " + fullNameStr);
+      String fullName = formatFullName(person);
+      // pure function:  also typically very fast since they do no network.
+      // 1  NO side effects, eg>
+      //   a) File read/write, DB, HTTP call (any network);
+      //    b) modifying some state (eg fields in Person class)
+      // 2   Same output for same inputs:: eg DONT:
+      //    a) if the return of formatFullName depends on some state besides the Person param
+               //
+      //    b) current time/random
+      System.out.println("Record for " + fullName);
+      System.out.println("Record for " + fullName);
+      System.out.println("Record for " + fullName);
+   }
+
+int n; // state outside of the params involved in the result
+   private String formatFullName(Person person) {
+      //prefix = run("SELECT aaa FROM ..."); // NOT pure because a COMMIT might occur in between 2 calls of tihs function
+
+      // anotherImpureFunction=()   BAD
+      //
+      return  /*n + *//*prefix + */person.getFirstName() + " " + person.getLastName().toUpperCase();
    }
 
    public void p(String streetName, String city, Integer streetNumber) {
