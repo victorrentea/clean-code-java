27a4fcde64fa62b56e00f8cf9eb23348e1c0f24d
Victor Rentea <victorrentea@gmail.com>
2021-11-16 12:31:58 +0200

    guard clauses

--- src/main/java/victor/training/cleancode/GuardClauses.java
+++ src/main/java/victor/training/cleancode/GuardClauses.java
@@ -4,75 +4,75 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GuardClauses {
-   public int getPayAmount(Marine marine) {
-      int result;
-      if (marine != null) {
-         if (!isDead(marine)) { // network call
-            if (!marine.isRetired()) {
-               if (marine.getYearsService() != null) {
-                  result = marine.getYearsService() * 100;
-                  if (!marine.getAwards().isEmpty()) {
-                     result += 1000;
-                  }
-                  if (marine.getAwards().size() >= 3) {
-                     result += 2000;
-                  }
-                  // HEAVY logic here...
-               } else {
-                  throw new IllegalArgumentException("Any marine should have the years of service set");
-               }
-            } else result = retiredAmount();
+	public int getPayAmount(Marine marine) {
+		int result;
+		if (marine == null) {
+			throw new RuntimeException("Marine is null");
+		}
+		if (isDead(marine)) {
+			return deadAmount();
+		} // network call
+      if (!marine.isRetired()) {
+         if (marine.getYearsService() != null) {
+            result = marine.getYearsService() * 100;
+            if (!marine.getAwards().isEmpty()) {
+               result += 1000;
+            }
+            if (marine.getAwards().size() >= 3) {
+               result += 2000;
+            }
+            // HEAVY logic here...
          } else {
-            result = deadAmount();
+            throw new IllegalArgumentException("Any marine should have the years of service set");
          }
-      } else {
-         throw new RuntimeException("Marine is null");
-      }
-      return result; // TODO ALT-ENTER move return closer
-   }
+      } else
+         result = retiredAmount();
 
-   private boolean isDead(Marine marine) {
-      // after 500 millis
-      return false;
-   }
+      return result;
+	}
 
-   private int deadAmount() {
-      return 1;
-   }
+	private boolean isDead(Marine marine) {
+		// after 500 millis
+		return false;
+	}
 
-   private int retiredAmount() {
-      return 2;
-   }
+	private int deadAmount() {
+		return 1;
+	}
+
+	private int retiredAmount() {
+		return 2;
+	}
 
 }
 
 class Marine {
-   private final boolean dead;
-   private final boolean retired;
-   private final Integer yearsService;
-   private final List<Award> awards = new ArrayList<>();
+	private final boolean dead;
+	private final boolean retired;
+	private final Integer yearsService;
+	private final List<Award> awards = new ArrayList<>();
 
-   Marine(boolean dead, boolean retired, Integer yearsService) {
-      this.dead = dead;
-      this.retired = retired;
-      this.yearsService = yearsService;
-   }
+	Marine(boolean dead, boolean retired, Integer yearsService) {
+		this.dead = dead;
+		this.retired = retired;
+		this.yearsService = yearsService;
+	}
 
-   public List<Award> getAwards() {
-      return awards;
-   }
+	public List<Award> getAwards() {
+		return awards;
+	}
 
-   public Integer getYearsService() {
-      return yearsService;
-   }
+	public Integer getYearsService() {
+		return yearsService;
+	}
 
-   public boolean isRetired() {
-      return retired;
-   }
+	public boolean isRetired() {
+		return retired;
+	}
 
-   public boolean isDead() {
-      return dead;
-   }
+	public boolean isDead() {
+		return dead;
+	}
 }
 
 class Award {
