68ca6be9c312c1aba081e78557e0e9651a9487b4
Victor Rentea <victorrentea@gmail.com>
2021-11-17 17:12:17 +0200

    Extrating impurity out

--- src/main/java/victor/training/pure/purity/PriceService.java
+++ src/main/java/victor/training/pure/purity/PriceService.java
@@ -1,6 +1,7 @@
 package victor.training.pure.purity;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,13 +19,20 @@ public class PriceService {
       Customer customer = customerRepo.findById(customerId);
       List<Product> products = productRepo.findAllById(productIds);
 
+      Map<Long, Double> resolvedPrices = resolvePrices(internalPrices, products);
+
+      PriceComputationResult result = computePrice(customer, products, resolvedPrices);
+
+      couponRepo.markUsedCoupons(customerId, result.getUsedCoupons());
+      return result.getFinalPrices();
+   }
+
+   // PURE FUNCTION: best part: testable wihtout MOCKS
+   private PriceComputationResult computePrice(Customer customer, List<Product> products, Map<Long, Double> resolvedPrices) {
       List<Coupon> usedCoupons = new ArrayList<>();
       Map<Long, Double> finalPrices = new HashMap<>();
       for (Product product : products) {
-         Double price = internalPrices.get(product.getId());
-         if (price == null) {
-            price = thirdPartyPrices.retrievePrice(product.getId());
-         }
+         Double price = resolvedPrices.get(product.getId());
          for (Coupon coupon : customer.getCoupons()) {
             if (coupon.autoApply() && coupon.isApplicableFor(product) && !usedCoupons.contains(coupon)) {
                price = coupon.apply(product, price);
@@ -33,9 +41,25 @@ public class PriceService {
          }
          finalPrices.put(product.getId(), price);
       }
+      return new PriceComputationResult(finalPrices, usedCoupons);
+   }
 
-      couponRepo.markUsedCoupons(customerId, usedCoupons);
-      return finalPrices;
+   @Value
+   static class PriceComputationResult {
+      Map<Long, Double> finalPrices;
+      List<Coupon> usedCoupons;
+   }
+
+   private Map<Long, Double> resolvePrices(Map<Long, Double> internalPrices, List<Product> products) {
+      Map<Long, Double> resolvedPrices = new HashMap<>();
+      for (Product product : products) {
+         Double price = internalPrices.get(product.getId());
+         if (price == null) {
+            price = thirdPartyPrices.retrievePrice(product.getId());
+         }
+         resolvedPrices.put(product.getId(), price);
+      }
+      return resolvedPrices;
    }
 
 }
