5d4c598ed5faafe3b12e08f7b58ec67465f27af7
Victor Rentea <victorrentea@gmail.com>
2021-11-18 17:38:08 +0200

    END

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -22,24 +22,28 @@ class Rental {
 	}
 
 	public double computePrice() {
-		double price = 0;
-		switch (getMovie().getPriceCode()) {
-			case REGULAR:
-				price += 2;
-				if (getDaysRented() > 2)
-					price += (getDaysRented() - 2) * 1.5;
-				break;
-			case NEW_RELEASE:
-				price += getDaysRented() * 3;
-				break;
-			case CHILDRENS:
-				price += 1.5;
-				if (getDaysRented() > 3)
-					price += (getDaysRented() - 3) * 1.5;
-				break;
-			default: //always
-				throw new IllegalStateException("Unexpected value: " + getMovie().getPriceCode());
-		}
+		return switch (getMovie().getPriceCode()) {
+			case REGULAR -> computeRegularPrice();
+			case NEW_RELEASE -> computeNewReleasePrice();
+			case CHILDRENS -> computeChildrenPrice();
+		};
+	}
+
+	private int computeNewReleasePrice() {
+		return getDaysRented() * 3;
+	}
+
+	private double computeChildrenPrice() {
+		double price = 1.5;
+		if (getDaysRented() > 3)
+			price += (getDaysRented() - 3) * 1.5;
+		return price;
+	}
+
+	private double computeRegularPrice() {
+		double price = 2;
+		if (getDaysRented() > 2)
+			price += (getDaysRented() - 2) * 1.5;
 		return price;
 	}
 
