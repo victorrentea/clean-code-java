cde5e9862c558d586672fca9ccfed8a3c0b22aa8
Victor Rentea <victorrentea@gmail.com>
2022-01-20 11:08:14 +0200

    Code polishing

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -3,12 +3,12 @@ package videostore.horror;
 import java.util.*;
 
 class Customer {
-	private String name;
-	private Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order
+	private final String name;
+	private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order
 
 	public Customer(String name) {
 		this.name = name;
-	};
+	}
 
 	public void addRental(Movie m, int d) {
 		rentals.put(m, d);
@@ -22,38 +22,45 @@ class Customer {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
-		for (Movie each : rentals.keySet()) {
-			double thisAmount = 0;
-			// determine amounts for each line
-			int dr = rentals.get(each);
-			switch (each.getCategory()) {
-				case REGULAR:
-					thisAmount += 2;
-					if (dr > 2)
-						thisAmount += (dr - 2) * 1.5;
-					break;
-				case NEW_RELEASE:
-					thisAmount += dr * 3;
-					break;
-				case CHILDREN:
-					thisAmount += 1.5;
-					if (dr > 3)
-						thisAmount += (dr - 3) * 1.5;
-					break;
-			}
+		for (Movie movie : rentals.keySet()) {
+			int dr = rentals.get(movie);
+			double price = computePrice(movie, dr);
+
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if ((each.getCategory() == Movie.Category.NEW_RELEASE)
+			if ((movie.getCategory() == Movie.Category.NEW_RELEASE)
 				 && dr > 1)
 				frequentRenterPoints++;
+
 			// show figures line for this rental
-			result += "\t" + each.getTitle() + "\t" + thisAmount + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + movie.getTitle() + "\t" + price + "\n";
+
+			totalAmount += price;
 		}
 		// add footer lines
 		result += "Amount owed is " + totalAmount + "\n";
 		result += "You earned " + frequentRenterPoints + " frequent renter points";
 		return result;
+	}
+
+	private double computePrice(Movie each, int dr) {
+		double thisAmount = 0;
+		switch (each.getCategory()) {
+			case REGULAR:
+				thisAmount += 2;
+				if (dr > 2)
+					thisAmount += (dr - 2) * 1.5;
+				break;
+			case NEW_RELEASE:
+				thisAmount += dr * 3;
+				break;
+			case CHILDREN:
+				thisAmount += 1.5;
+				if (dr > 3)
+					thisAmount += (dr - 3) * 1.5;
+				break;
+		}
+		return thisAmount;
 	}
 }
