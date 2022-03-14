f2790d6e430bcc26727a0db609a46745a94b5704
Victor Rentea <victorrentea@gmail.com>
2022-03-14 15:51:00 +0200

    WIP

---
+++ src/main/java/victor/training/fp/Optionals.java
@@ -0,0 +1,34 @@
+
+package victor.training.fp;
+
+import lombok.Data;
+import victor.training.exception.model.Customer;
+import victor.training.exception.model.MemberCard;
+
+@SuppressWarnings("ConstantConditions")
+public class Optionals {
+	public static void main(String[] args) {
+		// test: 60, 10, no MemberCard
+		System.out.println(getDiscountLine(new Customer(new MemberCard(60))));
+	}
+
+	public static String getDiscountLine(Customer customer) {
+		return "Discount: " + getApplicableDiscountPercentage(customer.getMemberCard()).getGlobalPercentage();
+	}
+
+	private static Discount getApplicableDiscountPercentage(MemberCard card) {
+		if (card.getFidelityPoints() >= 100) {
+			return new Discount(5);
+		}
+		if (card.getFidelityPoints() >= 50) {
+			return new Discount(3);
+		}
+		return null;
+	}
+}
+
+@Data
+class Discount {
+	private final int globalPercentage;
+	private Map<String, Integer> categoryDiscounts = new HashMap<>();
+}
