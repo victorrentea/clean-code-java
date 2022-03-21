a65d71b326de0a20cddf4adef28aade289b2a4e3
Victor Rentea <victorrentea@gmail.com>
2022-03-21 15:10:12 +0200

    null-safe model cu optionale

--- src/main/java/victor/training/fp/Optionals.java
+++ src/main/java/victor/training/fp/Optionals.java
@@ -7,31 +7,44 @@ import victor.training.exception.model.MemberCard;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @SuppressWarnings("ConstantConditions")
 public class Optionals {
-	public static void main(String[] args) {
-		// test: 60, 10, no MemberCard
-		System.out.println(getDiscountLine(new Customer(new MemberCard(60))));
-	}
+   public static void main(String[] args) {
+      // test: 60, 10, no MemberCard
+      System.out.println(getDiscountLine(new Customer(new MemberCard(60))));
+      System.out.println(getDiscountLine(new Customer(new MemberCard(10))));
+      System.out.println(getDiscountLine(new Customer()));
+   }
 
-	public static String getDiscountLine(Customer customer) {
-		return "Discount: " + getApplicableDiscountPercentage(customer.getMemberCard()).getGlobalPercentage();
-	}
+   public static String getDiscountLine(Customer customer) {
+		return
+			customer.getMemberCard()
+				.flatMap(card -> getApplicableDiscountPercentage(card))
+				.map(discount -> "Discount%: " + discount.getGlobalPercentage())
+				.orElse("");
+   }
 
-	private static Discount getApplicableDiscountPercentage(MemberCard card) {
+   private static Optional<Discount> getApplicableDiscountPercentage(MemberCard card) {
+
+		if (card == null) {
+			return Optional.empty();
+		}
+
 		if (card.getFidelityPoints() >= 100) {
-			return new Discount(5);
-		}
-		if (card.getFidelityPoints() >= 50) {
-			return new Discount(3);
-		}
-		return null;
-	}
+         return Optional.of(new Discount(5));
+      }
+      if (card.getFidelityPoints() >= 50) {
+         return Optional.of(new Discount(3));
+      }
+//      return new Discount(0); // NULL OBJECT PATTERN = un obiect care reprezinta prin valorile lui "ABSENTA"
+		return Optional.empty();
+   }
 }
 
 @Data
 class Discount {
-	private final int globalPercentage;
-	private Map<String, Integer> categoryDiscounts = new HashMap<>();
+   private final int globalPercentage;
+   private Map<String, Integer> categoryDiscounts = new HashMap<>();
 }
