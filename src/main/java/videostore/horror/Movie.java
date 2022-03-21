159c95c9b5acb977d199dd4caded29c3dbf76957
Victor Rentea <victorrentea@gmail.com>
2022-03-21 15:53:56 +0200

    priceCode not null

--- src/main/java/videostore/horror/Movie.java
+++ src/main/java/videostore/horror/Movie.java
@@ -3,23 +3,20 @@ public class Movie {
 	public static final int CHILDRENS = 2;
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
-	private String _title;
-	private Integer _priceCode;
 
-	public Movie(String title, Integer priceCode) {
-		_title = title;
-		_priceCode = priceCode;
+	private final String title;
+	private final int priceCode;
+
+	public Movie(String title, int priceCode) {
+		this.title = title;
+		this.priceCode = priceCode;
 	}
 
-	public Integer getPriceCode() {
-		return _priceCode;
-	}
-
-	public void setPriceCode(Integer arg) {
-		_priceCode = arg;
+	public int getPriceCode() {
+		return priceCode;
 	}
 
 	public String getTitle() {
-		return _title;
+		return title;
 	};
 }
