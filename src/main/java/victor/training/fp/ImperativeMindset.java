e5ca1ca5a998bf203d505e4d70b016ac54ff3055
Victor Rentea <victorrentea@gmail.com>
2022-03-22 19:02:20 +0200

    imperative mindset

---
+++ src/main/java/victor/training/fp/ImperativeMindset.java
@@ -0,0 +1,15 @@
+package victor.training.fp;
+
+import java.util.List;
+
+public class ImperativeMindset {
+   public void useStreams(List<Order> orders) {
+      int sum = 0;
+      orders.stream()
+          .filter(order -> order.isActive())
+          .forEach(order -> {
+             // imperative mindset: let's *add* to ...
+             // sum += order.getPrice();
+          });
+   }
+}
