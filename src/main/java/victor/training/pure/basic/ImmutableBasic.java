bdb504ae670bdc7b25bde689bf5e01bcf3425ca3
Victor Rentea <victorrentea@gmail.com>
2022-04-11 17:34:49 +0300

    END of day1

--- src/main/java/victor/training/pure/basic/ImmutableBasic.java
+++ src/main/java/victor/training/pure/basic/ImmutableBasic.java
@@ -1,15 +1,15 @@
 package victor.training.pure.basic;
 
 import com.google.common.collect.ImmutableList;
+import lombok.Value;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
+//@Slf4j
 public class ImmutableBasic {
+//   private static final Logger log = LoggerFactory.getLogger(ImmutableBasic.class);
    public static void main(String[] args) {
       ImmutableList<Integer> numbers = ImmutableList.copyOf(Stream.of(1, 2, 3, 4, 5).collect(toList()));
 
@@ -18,46 +18,63 @@ public class ImmutableBasic {
 
       System.out.println(immutable);
 //numbers.clear();
-      undevaAdanc(immutable);
+      immutable = undevaAdanc(immutable);
 
       System.out.println(immutable.getNumbers());
       System.out.println(immutable);
    }
 
-   private static void undevaAdanc(Immutable immutable) {
+   private static Immutable undevaAdanc(Immutable immutable) {
 //      immutable.getNumbers().add(-1);
+      return immutable.withX(immutable.getX() + 1);
    }
+
 }
 
-class Immutable { // deeply immutable
-   private final int x;
-   private final ImmutableList<Integer> numbers; // NU MERGE CU HIBERNATE. merge cu Cassandra, Mongo, jooq pe relational DB
-   private final Other other;
+  // love 💘  obiect imutabil nativ in limbaj in Java 17
+//record Immutable(int x, ImmutableList<Integer> numbers, Other other) {}
 
-   Immutable(int x, ImmutableList<Integer> numbers, Other other) {
-      this.x = x;
-      this.numbers = numbers;
-      this.other = other;
-   }
-   public String toString() {
-      return String.format("Immutable{x=%d, numbers=%s, other=%s}", getX(), getNumbers(), getOther());
-   }
-   public int getX() {
-      return x;
-   }
+//@Data // hate
+@Value // love 💘
+class Immutable {
+//   @With
+   int x;
+   ImmutableList<Integer> numbers; // NU MERGE CU HIBERNATE. merge cu Cassandra, Mongo, jooq pe relational DB
+   Other other;
 
-   public ImmutableList<Integer> getNumbers() {
-      return numbers;
+   public Immutable withX(int newX) {
+      return new Immutable(newX, numbers, other);
    }
-
-   //   public List<Integer> getNumbers() {
-////      return new ArrayList<>(numbers);// Solutia 1: waste of mem + surpriza la caller ca-l lasa sa puna add
-//      return Collections.unmodifiableList(numbers); // Solutia 2: merge pe Hibernate entity; nu face malloc (), crapa cu exceptie
+}
+//class Immutable { // deeply immutable
+//   private final int x;
+//   private final ImmutableList<Integer> numbers; // NU MERGE CU HIBERNATE. merge cu Cassandra, Mongo, jooq pe relational DB
+//   private final Other other;
+//
+//   Immutable(int x, ImmutableList<Integer> numbers, Other other) {
+//      this.x = x;
+//      this.numbers = numbers;
+//      this.other = other;
 //   }
-   public Other getOther() {
-      return other;
-   }
-}
+//   public String toString() {
+//      return String.format("Immutable{x=%d, numbers=%s, other=%s}", getX(), getNumbers(), getOther());
+//   }
+//   public int getX() {
+//      return x;
+//   }
+//
+//   public ImmutableList<Integer> getNumbers() {
+//      return numbers;
+//   }
+//
+//   //   public List<Integer> getNumbers() {
+//////      return new ArrayList<>(numbers);// Solutia 1: waste of mem + surpriza la caller ca-l lasa sa puna add
+////      return Collections.unmodifiableList(numbers); // Solutia 2: merge pe Hibernate entity; nu face malloc (), crapa cu exceptie
+////   }
+//   public Other getOther() {
+//      return other;
+//   }
+//}
 
 class Other {
    private final int a;
