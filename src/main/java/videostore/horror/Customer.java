054a01879b550f279e2a7e04d8b43b8d5c26abbc
Victor Rentea <victorrentea@gmail.com>
2022-04-12 15:33:19 +0300

    de ce iubim fct pure

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -2,16 +2,66 @@ package videostore.horror;
 
 import java.util.*;
 
+class Rental {
+	private final Movie movie;
+	private final int daysRented;
+
+	Rental(Movie movie, int daysRented) {
+		this.movie = Objects.requireNonNull(movie);
+		this.daysRented = daysRented;
+	}
+
+	public int getDaysRented() {
+		return daysRented;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public int computeFrequentRenterPoints() {
+		int points = 1;
+		boolean isNewRelease = getMovie().getCategory() == MovieCategory.NEW_RELEASE;
+		if (isNewRelease && getDaysRented() >= 2) {
+			points++;
+		}
+		return points;
+	}
+
+	public double computePrice() {
+		double price = 0;
+		switch (getMovie().getCategory()) {
+			case REGULAR:
+				price += 2;
+				if (getDaysRented() > 2)
+					price += (getDaysRented() - 2) * 1.5;
+				break;
+			case NEW_RELEASE:
+				price += getDaysRented() * 3;
+				break;
+			case CHILDRENS:
+				price += 1.5;
+				if (getDaysRented() > 3)
+					price += (getDaysRented() - 3) * 1.5;
+				break;
+		}
+		return price;
+	}
+}
+
 class Customer {
 	private final String name;
-	private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order
+	// Map.get(key) // +O(1)
+	private final Map<Movie, Integer> rentalsMap = new LinkedHashMap<>(); // preserves order
+	private final List<Rental> rentals = new ArrayList<>();
 
 	public Customer(String name) {
 		this.name = name;
 	}
 
 	public void addRental(Movie movie, int daysRented) {
-		rentals.put(movie, daysRented);
+		rentalsMap.put(movie, daysRented);
+		rentals.add(new Rental(movie, daysRented));
 	}
 
 	public String getName() {
@@ -19,45 +69,39 @@ class Customer {
 	}
 
 	public String statement() {
-		double totalAmount = 0;
+		double totalPrice = 0;
 		int frequentRenterPoints = 0; // code smells: acumulatori
-		String result = "Rental Record for " + getName() + "\n";
-		for (Movie movie : rentals.keySet()) {
-			double thisAmount = 0;
-			// determine amounts for each line
-			int daysRented = rentals.get(movie);
-			switch (movie.getCategory()) {
-				case REGULAR:
-					thisAmount += 2;
-					if (daysRented > 2)
-						thisAmount += (daysRented - 2) * 1.5;
-					break;
-				case NEW_RELEASE:
-					thisAmount += daysRented * 3;
-					break;
-				case CHILDRENS:
-					thisAmount += 1.5;
-					if (daysRented > 3)
-						thisAmount += (daysRented - 3) * 1.5;
-					break;
-			}
-			frequentRenterPoints += addFrequentRenterPoints(movie);
+		String result = "Rental Record for " + name + "\n";
+
+		for (Rental rental : rentals) {
+			Movie movie = rental.getMovie();
+
+			double price = rental.computePrice();
+			frequentRenterPoints += rental.computeFrequentRenterPoints();
+
 			// show figures line for this rental
-			result += "\t" + movie.getTitle() + "\t" + thisAmount + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + movie.getTitle() + "\t" + price + "\n";
+		}
+		for (Rental rental : rentals) {
+			double price = rental.computePrice();
+			// pentru a putea sparge forul in 2, va trebui sa repet un apel de functie (rental.computePrice()).
+			// cand e asta riscant?
+			// 1) daca functia nu e pura => buguri
+				// PURE =
+				// 	a) da acelasi rezultat daca e chemata de mai mute ori cu acelasi input
+				// 	b) nu modifica starea din jur (nu are side effect)
+			// 2) durata de executie sa fie mica. (in 99% din cazuri o fct pura e super fast).
+			// pt ca de obicei intarzierile sunt cauzate de DB, alte APIuri, WS...
+				// evaluateChessBoard(board) sau validate dig signature (message)
+
+			// Concluzie. Noi iubim fct pure pt ca sunt safe de chemat de cate ori avem nevoie!
+
+			totalPrice += price;
 		}
 		// add footer lines
-		result += "Amount owed is " + totalAmount + "\n";
+		result += "Amount owed is " + totalPrice + "\n";
 		result += "You earned " + frequentRenterPoints + " frequent renter points";
 		return result;
 	}
 
-	private int addFrequentRenterPoints(Movie movie) {
-		int daysRented = rentals.get(movie);
-		int points = 1;
-		if (movie.getCategory() == MovieCategory.NEW_RELEASE && daysRented >= 2) {
-			points++;
-		}
-		return points;
-	}
 }
