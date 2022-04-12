ca4cd1e0f3914d998bf31f47bb3ea118e4cc3097
Victor Rentea <victorrentea@gmail.com>
2022-04-12 11:20:56 +0300

    Shawarma pattern: ai inghitit cu de toate. Buguri.

--- src/main/java/victor/training/exception/Biz.java
+++ src/main/java/victor/training/exception/Biz.java
@@ -4,20 +4,32 @@ import victor.training.exception.model.Customer;
 import victor.training.exception.model.MemberCard;
 import victor.training.exception.model.Order;
 
+import java.io.IOException;
+import java.text.ParseException;
+
 //@Service
 public class Biz {
+   public static void main(String[] args) {
+      new Biz().applyDiscount(null, null);
+   }
 
-   public void applyDiscount(Order order, Customer customer) {
+   public void applyDiscount(Order order, Customer customer)  {
       System.out.println("START");
-      if (order.getOfferDate().before(Config.getLastPromoDate())) { // TODO inside
-         System.out.println("APPLYING DISCOUNT");
-         Integer points = customer.getMemberCard()
-             .map(MemberCard::getFidelityPoints)
-             .orElse(0);
+      try {
+         if (order.getOfferDate().before(Config.getLastPromoDate())) {
+            //  Checked exceptions sunt "Abstraction Leak": tradeaza implementarea
+            System.out.println("APPLYING DISCOUNT");
+            Integer points = customer.getMemberCard()
+                .map(MemberCard::getFidelityPoints)
+                .orElse(0);
 
-         order.setPrice(order.getPrice() * (100 - 2 * points) / 100);
-      } else {
-         System.out.println("NO DISCOUNT");
+            order.setPrice(order.getPrice() * (100 - 2 * points) / 100);
+         } else {
+            System.out.println("NO DISCOUNT");
+         }
+         System.out.println("GATA");
+      } catch (Exception e) { // cand prinzi pe toate NU CUMVA sa o inghiti > inghiti si NPE
+         // TODO ma dcs sa intreb.
       }
 
    }
--- src/main/java/victor/training/exception/Config.java
+++ src/main/java/victor/training/exception/Config.java
@@ -14,16 +14,21 @@ import java.util.Properties;
  */
 public class Config {
 
-   public static Date getLastPromoDate() {
-//      File file = new File("config.properties");
-//      SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-//      Properties properties = new Properties();
-//      try (FileReader reader = new FileReader(file)) {
-//         properties.load(reader);
-//      }
-//      return format.parse(properties.getProperty("last.promo.date"));
-      return new Date();
+   public static Date getLastPromoDate() throws IOException, ParseException {
+      File file = new File("config.properties");
+      SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+//      String dinBaza = "2022-0131";
+      Properties properties = new Properties();
+      try (FileReader reader = new FileReader(file)) {
+         properties.load(reader);
+      }
+      String dinBaza = properties.getProperty("last.promo.date");
+      return format.parse(dinBaza);
+//      return new Date();
    }
+
+   // Maria: le ridici pana intr-un handler de exceptii care le gestioneaza cumva uniform
+   // ! CE FACE acel handler ? > se uita la tipul exceptiei si da mesaje customizate userilor / notificari.
 
 
 }
