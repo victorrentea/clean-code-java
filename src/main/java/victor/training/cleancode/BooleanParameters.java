79bc812995b9c621f951b77fe4690c814ccc232d
victorrentea <victorrentea@gmail.com>
2022-06-20 15:22:36 +0300

    DO NOT MUTATE PARAMS

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -18,25 +18,16 @@ public class BooleanParameters {
 
       // TODO From my use-case #323, I call it too, to do more within:
       Task task = new Task(1);
-      bigUglyMethod323(2, task, true);
+      bigUglyMethod323(2, task);
    }
 
    static void bigUglyMethod(int b, Task task) {
       bigStart(b, task);
-
-      if (false) {
-         System.out.println("Logic just for CR323 : " + task);
-      }
-
       bigEnd(b);
    }
-   static void bigUglyMethod323(int b, Task task, boolean cr323) {
+   static void bigUglyMethod323(int b, Task task) {
       bigStart(b, task);
-
-      if (cr323) {
-         System.out.println("Logic just for CR323 : " + task);
-      }
-
+      System.out.println("Logic just for CR323 : " + task);
       bigEnd(b);
    }
 
@@ -57,13 +48,14 @@ public class BooleanParameters {
 
    // Lord gave us tests!
    public void bossLevel(boolean fluff, List<Task> tasks, boolean cr323) {
-      int index = 0; // TODO move closer to usages
-      int j = tasks.size();
+//      int j = tasks.size();
       System.out.println("Logic1");
       List<Integer> taskIds = new ArrayList<>();
       System.out.println("Logic2");
       if (fluff) {
+         innocent(tasks); // java sucks, 26 years old.
          System.out.println("Logic3");
+         int index = 0;
          for (Task task : tasks) {
             System.out.println("Logic4: Validate " + task);
             task.setRunning();
@@ -77,12 +69,17 @@ public class BooleanParameters {
             index++;
             System.out.println("Logic5 index=" + index + " on running=" + task.isRunning());
          }
+         int j = tasks.size();
          System.out.println("Logic6 " + j);
          System.out.println("Task Ids: " + taskIds);
       } else {
          System.out.println("Logic7 " + tasks);
       }
       System.out.println("Logic7");
+   }
+
+   private void innocent(List<Task> tasks) {
+      tasks.clear(); // you are reassigned to a lovely cobol project
    }
 
 }
