9f6c1ef4ebd19f3bda679d9ddb05256a4bbc4989
victorrentea <victorrentea@gmail.com>
2022-06-20 17:45:25 +0300

    i'm safe.

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -2,9 +2,11 @@ package videostore.horror;
 
 import java.util.*;
 
+
 class Customer {
 	private final String name;
 	private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order (someone was sorry here, felt the need to drop a comment because the code was not explciti)
+
 
 	public Customer(String name) {
 		this.name = name;
@@ -22,20 +24,19 @@ class Customer {
 		double totalPrice = 0;
 		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
-		for (Movie each : rentals.keySet()) {
-			double price = 0;
-			// determine amounts for each line
-			int daysRented = rentals.get(each);
-			price = computePrice(each, daysRented);
+
+//		for (Movie movie : rentals.keySet()) {
+//			int daysRented = rentals.get(movie);
+
+		for (Map.Entry<Movie, Integer> entry : rentals.entrySet()) {
+			Movie movie = entry.getKey();
+			Integer daysRented = entry.getValue();
+
+			double price = computePrice(movie, daysRented);
 			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if (each.getPriceCode() != null &&
-				 (each.getPriceCode() == Movie.PriceCode.NEW_RELEASE)
-				 && daysRented > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints += computeRenterPoints(movie.getPriceCode(), daysRented);
 			// show figures line for this rental
-			result += "\t" + each.getTitle() + "\t" + price + "\n";
+			result += "\t" + movie.getTitle() + "\t" + price + "\n";
 			totalPrice += price;
 		}
 		// add footer lines
@@ -44,24 +45,35 @@ class Customer {
 		return result;
 	}
 
-	private double computePrice(Movie each, int daysRented) {
-		double price = 0;
-		switch (each.getPriceCode()) {
-			case REGULAR:
-				price = computeRegularPrice(daysRented, price);
-				break;
-			case NEW_RELEASE:
-				price += computeNewReleasePrice(daysRented);
-				break;
-			case CHILDREN:
-				price = computeChildrenPrice(daysRented, price);
-				break;
-		}
-		return price;
+	private int computeRenterPoints(Movie.PriceCode priceCode, int daysRented) {
+		int frequentRenterPoints = 1;
+		if (priceCode == Movie.PriceCode.NEW_RELEASE && daysRented >= 2)
+			frequentRenterPoints++;
+		return frequentRenterPoints;
 	}
 
-	private double computeChildrenPrice(int daysRented, double price) {
-		price += 1.5;
+	private double computePrice(Movie movie, int daysRented) {
+		switch (movie.getPriceCode()) {
+			case REGULAR:
+				return computeRegularPrice(daysRented);
+			case NEW_RELEASE:
+				return computeNewReleasePrice(daysRented);
+			case CHILDREN:
+				return computeChildrenPrice(daysRented);
+			default:
+				throw new IllegalStateException("Unexpected value: " + movie.getPriceCode());
+		}
+		// day-dream: Java 17
+//		return switch (each.getPriceCode()) {
+//			case REGULAR -> computeRegularPrice(daysRented);
+//			case NEW_RELEASE -> computeNewReleasePrice(daysRented);
+//			case CHILDREN -> computeChildrenPrice(daysRented);
+//			default -> throw new IllegalStateException("Unexpected value: " + each.getPriceCode());
+//		};
+	}
+
+	private double computeChildrenPrice(int daysRented) {
+		double price =  1.5;
 		if (daysRented > 3) {
 			price += (daysRented - 3) * 1.5;
 		}
@@ -72,11 +84,11 @@ class Customer {
 		return daysRented * 3;
 	}
 
-	private double computeRegularPrice(int daysRented, double thisAmount) {
-		thisAmount += 2;
+	private double computeRegularPrice(int daysRented) {
+		double price =  2;
 		if (daysRented > 2) {
-			thisAmount += (daysRented - 2) * 1.5;
+			price += (daysRented - 2) * 1.5;
 		}
-		return thisAmount;
+		return price;
 	}
 }
--- src/main/java/videostore/horror/Movie.java
+++ src/main/java/videostore/horror/Movie.java
@@ -1,18 +1,21 @@
 package videostore.horror;
 
+import java.util.Objects;
+
 public class Movie {
     enum PriceCode {
         CHILDREN,
         REGULAR,
         NEW_RELEASE,
+        ELDER
     }
 
     private final String title;
     private final PriceCode priceCode;
 
     public Movie(String title, PriceCode priceCode) {
-        this.title = title;
-        this.priceCode = priceCode;
+        this.title = Objects.requireNonNull(title);
+        this.priceCode = Objects.requireNonNull(priceCode);
     }
 
     public PriceCode getPriceCode() {
