e9d2670296e9629513ef7dbbd4cef76786b17f5c
victorrentea <victorrentea@gmail.com>
2022-06-21 14:07:00 +0300

    end of day2

--- src/main/java/victor/training/fp/Loan_Pattern.java
+++ src/main/java/victor/training/fp/Loan_Pattern.java
@@ -4,57 +4,84 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.jooq.lambda.Unchecked;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
+
+
+interface MyConsumer<T> {
+
+
+    void accept(T t) throws Exception;
+}
+
+interface OrderRepo extends JpaRepository<Order, Long> {
+    Stream<Order> findByActiveTrue(); // Streaming query over 1 million orders
+}
 
 @Slf4j
 @RequiredArgsConstructor
 class FileExporter {
-   private final OrderRepo orderRepo;
+    private final OrderRepo orderRepo;
 
-   public void exportOrders() throws IOException {
-      File file = new File("target/orders.csv");
-      log.info("Starting export into {} ...", file.getAbsolutePath());
-      long t0 = System.currentTimeMillis();
-      try (Writer writer = new FileWriter(file)) {
+    public static <T> Consumer<T> convert(MyConsumer<T> consumerThrows) {
+        return s -> {
+            try {
+                consumerThrows.accept(s);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
 
-         writer.write("order_id;date\n");
-         orderRepo.findByActiveTrue()
-             .map(o -> o.getId() + ";" + o.getCreationDate() + "\n")
-             .forEach(Unchecked.consumer(writer::write));
+    public void exportOrders(Consumer<Writer> contentWriterFunction) throws IOException {
+        File file = new File("target/orders.csv");
+        log.info("Starting export into {} ...", file.getAbsolutePath());
+        long t0 = System.currentTimeMillis();
+        try (Writer writer = new FileWriter(file)) {
+            contentWriterFunction.accept(writer); // DO NOT ABUSE only use for
+            // cases in which you HAVE to be in a try { or for {
 
-         log.info("Export DONE");
-      } catch (Exception e) {
-         log.error("Export FAILED!", e); // TERROR-Driven Development
-         // imagine... sendErrorEmail(e);
-         throw e;
-      } finally {
-         long t1 = System.currentTimeMillis();
-         log.info("Export completed in {} seconds ", (t1 - t0) / 1000);
-      }
-   }
+            log.info("Export DONE");
+        } catch (Exception e) {
+            log.error("Export FAILED!", e); // TERROR-Driven Development
+            // imagine... sendErrorEmail(e);
+            throw e;
+        } finally {
+            long t1 = System.currentTimeMillis();
+            log.info("Export completed in {} seconds ", (t1 - t0) / 1000);
+            // TODO victorrentea 21.06.2022:
+        }
+    }
 }
-
+//Stephan
 @RequiredArgsConstructor
 class ExportService {
-   private final FileExporter fileExporter;
+    private final FileExporter fileExporter;
+    @Autowired
+    private OrderRepo orderRepo;
 
-   @SneakyThrows
-   public void exportOrders() {
-      fileExporter.exportOrders();
-   }
 
-   @SneakyThrows
-   public void exportUsers() {
-      // TODO implement the export of users using *the same workflow* as for orders
-   }
-}
 
-interface OrderRepo extends JpaRepository<Order, Long> {
-   Stream<Order> findByActiveTrue(); // Streaming query over 1 million orders
+    @SneakyThrows
+    public void exportOrders() {
+        fileExporter.exportOrders(Unchecked.consumer(writer -> {
+            writer.write("order_id;date\n");
+
+            orderRepo.findByActiveTrue()
+                    .map(o -> o.getId() + ";" + o.getCreationDate() + "\n")
+                    .forEach(Unchecked.consumer(writer::write));
+        }));
+    }
+
+    @SneakyThrows
+    public void exportUsers() {
+        // TODO implement the export of users using *the same workflow* as for orders
+    }
 }
--- src/main/java/victor/training/fp/StreamWrecks.java
+++ src/main/java/victor/training/fp/StreamWrecks.java
@@ -4,25 +4,39 @@ import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Stream;
 
+import static java.util.function.Predicate.not;
 import static java.util.stream.Collectors.*;
 
 public class StreamWrecks {
 	private ProductRepo productRepo;
 
 	public List<Product> getFrequentOrderedProducts(List<Order> orders) {
-		return orders.stream()
-				.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
-				.flatMap(o -> o.getOrderLines().stream())
-				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)))
-				.entrySet()
-				.stream()
+		Map<Product, Integer> recentProductCounts = getRecentProductCounts(orders);
+		// DO NOT Keep Mono,Flux,Stream in Variable
+		List<Product> frequentProducts = recentProductCounts.entrySet().stream()
 				.filter(e -> e.getValue() >= 10)
 				.map(Entry::getKey)
-				.filter(p -> !p.isDeleted())
-				.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
+				.toList();
+		List<Long> hiddenProductIds = productRepo.getHiddenProductIds();
+		return frequentProducts.stream()
+				.filter(not(Product::isDeleted))
+				.filter(p -> !hiddenProductIds.contains(p.getId()))
 				.collect(toList());
+	}
+
+	private Map<Product, Integer> getRecentProductCounts(List<Order> orders) {
+		return orders.stream()
+				.filter(this::isRecent)
+				.flatMap(o -> o.getOrderLines().stream())
+				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)));
+	}
+
+	private boolean isRecent(Order order) {
+		return order.getCreationDate().isAfter(LocalDate.now().minusYears(1));
 	}
 }
 
