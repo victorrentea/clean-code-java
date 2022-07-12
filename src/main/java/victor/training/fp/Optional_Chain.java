bc9647a05047347d3abc4c07b63db9e912c32138
victorrentea <victorrentea@gmail.com>
2022-07-12 13:10:50 +0300

    Optionals agains NPE

--- src/main/java/victor/training/fp/Optional_Chain.java
+++ src/main/java/victor/training/fp/Optional_Chain.java
@@ -1,5 +1,8 @@
 package victor.training.fp;
 
+import java.util.Objects;
+import java.util.Optional;
+
 public class Optional_Chain {
 	private static final MyMapper mapper = new MyMapper();
    public static void main(String[] args) {
@@ -14,7 +17,17 @@ public class Optional_Chain {
 class MyMapper {
    public DeliveryDto convert(Parcel parcel) {
       DeliveryDto dto = new DeliveryDto();
-      dto.recipientPerson = parcel.getDelivery().getAddress().getContactPerson().getName().toUpperCase();
+//      if (
+//              parcel != null &&
+//              parcel.getDelivery() != null &&
+//              parcel.getDelivery().getAddress() != null &&
+//              parcel.getDelivery().getAddress().getContactPerson() != null &&
+//              parcel.getDelivery().getAddress().getContactPerson().getName() != null
+//      )
+      dto.recipientPerson = parcel.getDelivery()
+              .flatMap(d -> d.getAddress().getContactPerson())
+              .map(cp -> cp.getName().toUpperCase())
+              .orElse(null);
       return dto;
    }
 }
@@ -25,8 +38,8 @@ class DeliveryDto {
 class Parcel {
    private Delivery delivery; // NULL until a delivery is scheduled
 
-   public Delivery getDelivery() {
-      return delivery;
+   public Optional<Delivery> getDelivery() {
+      return Optional.ofNullable(delivery);
    }
 	public void setDelivery(Delivery delivery) {
       this.delivery = delivery;
@@ -38,11 +51,11 @@ class Delivery {
    private Address address; // NOT NULL IN DB
 
    public Delivery(Address address) {
-      this.address = address;
+       this.address = Objects.requireNonNull(address);
    }
 
 	public void setAddress(Address address) {
-		this.address = address; // TODO null safe
+        this.address = Objects.requireNonNull(address); // TODO null safe
 	}
 
 	public Address getAddress() {
@@ -57,8 +70,8 @@ class Address {
       this.contactPerson = contactPerson;
    } // TODO allow not setting
 
-   public ContactPerson getContactPerson() {
-      return contactPerson;
+   public Optional<ContactPerson> getContactPerson() {
+      return Optional.ofNullable(contactPerson);
    }
 }
 
