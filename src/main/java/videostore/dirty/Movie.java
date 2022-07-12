8ce2d83e262ddd6b736e7a79a301c702523156e7
victorrentea <victorrentea@gmail.com>
2022-07-12 15:33:38 +0300

    Rental curatat

--- src/main/java/videostore/dirty/Movie.java
+++ src/main/java/videostore/dirty/Movie.java
@@ -1,12 +1,17 @@
 package videostore.dirty;
 
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+
 public final class Movie {
     private final String title;
     private final MovieCategory category;
 
     public Movie(String title, MovieCategory category) {
-        this.title = title;
-        this.category = category;
+        // reflex: daca ai vreun obiect fara setteri (imutabil) incerci sa-i pui validari prin ctor
+        this.title = requireNonNull(title);
+        this.category = requireNonNull(category);
     }
 
     public MovieCategory getCategory() {
--- src/main/java/videostore/dirty/Rental.java
+++ src/main/java/videostore/dirty/Rental.java
@@ -1,18 +1,26 @@
 package videostore.dirty;
+
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+
 class Rental {
-	private Movie _movie;
-	private int _daysRented;
+	private final Movie movie;
+	private final int daysRented;
 
 	public Rental(Movie movie, int daysRented) {
-		_movie = movie;
-		_daysRented = daysRented;
+		this.movie = requireNonNull(movie);
+		if (daysRented <= 0) {
+			throw new IllegalArgumentException("Negative days");
+		}
+		this.daysRented = daysRented;
 	}
 
 	public int getDaysRented() {
-		return _daysRented;
+		return daysRented;
 	}
 
 	public Movie getMovie() {
-		return _movie;
+		return movie;
 	}
 }
