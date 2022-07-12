9ca2511728e930fa74ced8001b6d3eec8be710e0
victorrentea <victorrentea@gmail.com>
2022-07-12 15:18:57 +0300

    incep

--- src/main/java/victor/training/cleancode/MicroTypes.java
+++ src/main/java/victor/training/cleancode/MicroTypes.java
@@ -1,38 +1,52 @@
 package victor.training.cleancode;
 
-import org.jooq.lambda.tuple.Tuple;
-import org.jooq.lambda.tuple.Tuple2;
+import lombok.Value;
 import org.junit.jupiter.api.Test;
 
+import javax.persistence.Id;
 import java.util.List;
 import java.util.Map;
 
 import static java.util.stream.Collectors.joining;
+    @Value // getter, ctor, hash/eq, tostring
+    class ProductCount {
+        String productName;
+        int count;
+    }
+
+    @Value
+    class CustomerId {
+        long id;
+    }
 
 public class MicroTypes {
 
+
     //<editor-fold desc="Unknown source of data">
-    public Map<Long, List<Tuple2<String, Integer>>> extremeFP() {
+    public Map<CustomerId, List<ProductCount>> extremeFP() {
         Long customerId = 1L;
         Integer product1Count = 2;
         Integer product2Count = 4;
-        return Map.of(customerId, List.of(
-                Tuple.tuple("Table", product1Count),
-                Tuple.tuple("Chair", product2Count)
+        return Map.of(new CustomerId(customerId), List.of(
+                new ProductCount("Table", product1Count),
+                new ProductCount("Chair", product2Count)
         ));
     }
     //</editor-fold>
 
+//    @Id
+//    CustomerId id;
+
     @Test
     void lackOfAbstractions() {
-        Map<Long, List<Tuple2<String, Integer>>> map = extremeFP();
+        Map<CustomerId, List<ProductCount>> map = extremeFP();
         // Joke: try "var" above :)
 
-        for (Long cid : map.keySet()) {
+        for (CustomerId cid : map.keySet()) {
             String pl = map.get(cid).stream()
-                    .map(t -> t.v2 + " of " + t.v1)
+                    .map(pc -> pc.getCount() + " of " + pc.getProductName())
                     .collect(joining(", "));
-            System.out.println("cid=" + cid + " got " + pl);
+            System.out.println("cid=" + cid.getId() + " got " + pl);
         }
     }
 }
