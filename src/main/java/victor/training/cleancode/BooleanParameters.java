e13a462a4cbb1d604a844f34a990e06e4cce45c3
victorrentea <victorrentea@gmail.com>
2022-07-25 10:48:59 +0300

    Am sopart functia mare in bucati mici. Dar m-am lovit de-un for. pe care l-am spart si pe el >>> Stream💪

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -11,110 +11,122 @@ import static java.util.stream.Collectors.toList;
  */
 public class BooleanParameters {
 
-   public static void main(String[] args) {
-      // The big method is called from various foreign places in the codebase
-      bigUglyMethod(1, new Task(5));
-      bigUglyMethod(2, new Task(4));
-      bigUglyMethod(3, new Task(3));
-      bigUglyMethod(4, new Task(2));
-      bigUglyMethod(5, new Task(1));
+    public static void main(String[] args) {
+        // The big method is called from various foreign places in the codebase
+        bigUglyMethod(1, new Task(5));
+        bigUglyMethod(2, new Task(4));
+        bigUglyMethod(3, new Task(3));
+        bigUglyMethod(4, new Task(2));
+        bigUglyMethod(5, new Task(1));
 
-      // TODO From my use-case CR#323, I call it too, to do more within:
-      Task task = new Task(1);
-      bigUglyMethod323(2, task);
+        // TODO From my use-case CR#323, I call it too, to do more within:
+        Task task = new Task(1);
+        bigUglyMethod323(2, task);
 
-   }
+    }
 
-   static void bigUglyMethod323(int b, Task task) {
-      startUgly(b, task);
-      System.out.println("Logic just for CR323 : " + task);
-      endUgly(b);
-   }
+    static void bigUglyMethod323(int b, Task task) {
+        startUgly(b, task);
+        System.out.println("Logic just for CR323 : " + task);
+        endUgly(b);
+    }
 
-   static void bigUglyMethod(int b, Task task) {
-      startUgly(b, task);
-      endUgly(b);
-   }
+    static void bigUglyMethod(int b, Task task) {
+        startUgly(b, task);
+        endUgly(b);
+    }
 
-   private static void endUgly(int b) {
-      System.out.println("More Complex Logic " + b);
-      System.out.println("More Complex Logic " + b);
-      System.out.println("More Complex Logic " + b);
-   }
+    private static void endUgly(int b) {
+        System.out.println("More Complex Logic " + b);
+        System.out.println("More Complex Logic " + b);
+        System.out.println("More Complex Logic " + b);
+    }
 
-   private static void startUgly(int b, Task task) {
-      System.out.println("Complex Logic 1 " + task + " and " + b);
-      System.out.println("Complex Logic 2 " + task);
-      System.out.println("Complex Logic 3 " + task);
-   }
+    private static void startUgly(int b, Task task) {
+        System.out.println("Complex Logic 1 " + task + " and " + b);
+        System.out.println("Complex Logic 2 " + task);
+        System.out.println("Complex Logic 3 " + task);
+    }
 
 
-   // ============== "BOSS" LEVEL: Deeply nested functions are a lot harder to break down =================
+    // ============== "BOSS" LEVEL: Deeply nested functions are a lot harder to break down =================
+    // 1 for --> 4 foruri:
+    // performanta nu doare. O(N)
+    // dar poate sa faca buguri daca:
+    // mutezi date din afara forului pe care un akl doilea for le citeste.
+    // break; continue; return; throw
 
-   // Lord gave us tests!
-   public void bossLevelFluff(List<Task> tasks, boolean cr323) {
-      int j = tasks.size();
-      System.out.println("Logic1");
-      System.out.println("Logic2");
-      System.out.println("Logic3");
-      // 1 for --> 4 foruri:
-      // performanta nu doare. O(N)
-      // dar poate sa faca buguri daca:
-         // mutezi date din afara forului pe care un akl doilea for le citeste.
-         // break; continue; return; throw
-      int index = 0;
-      for (Task task : tasks) {
-         System.out.println("Logic4: Validate " + task);
-         task.setRunning();
-      }
-
-      List<Integer> taskIds = tasks.stream().map(Task::getId).collect(toList());
-
-      for (Task task : tasks) {
-         if (cr323) { // TODO remove the boolean
+    // Lord gave us tests!
+    public void bossLevelFluff(List<Task> tasks) {
+        bossStart(tasks);
+        bossEnd(tasks);
+    }
+    public void bossLevelFluff323(List<Task> tasks) {
+        bossStart(tasks);
+        for (Task task : tasks) {
             System.out.println("My Logic: " + task);
-         }
-      }
-      for (Task task : tasks) {
-         index++;
-         System.out.println("Logic5 index=" + index + " on running=" + task.isRunning());
-      }
-      System.out.println("Logic6 " + j);
-      System.out.println("Task Ids: " + taskIds);
-      System.out.println("Logic7");
-   }
-   public void bossLevelNoFluff(List<Task> tasks) {
-      System.out.println("Logic1");
-      System.out.println("Logic2");
-      System.out.println("Logic7 " + tasks);
-      System.out.println("Logic7");
-   }
+        }
+        bossEnd(tasks);
+    }
+
+    private void bossEnd(List<Task> tasks) {
+        int index = 0;
+        for (Task task : tasks) {
+            index++;
+            System.out.println("Logic5 index=" + index + " on running=" + task.isRunning());
+        }
+        System.out.println("Logic6 " + tasks.size());
+        List<Integer> taskIds = tasks.stream().map(Task::getId).collect(toList());
+        System.out.println("Task Ids: " + taskIds);
+        System.out.println("Logic7");
+    }
+
+    private void bossStart(List<Task> tasks) {
+        System.out.println("Logic1");
+        System.out.println("Logic2");
+        System.out.println("Logic3");
+        for (Task task : tasks) {
+            System.out.println("Logic4: Validate " + task);
+            task.setRunning();
+        }
+    }
+
+    private void inocenta(List<Task> tasks) {
+        tasks.clear();
+    }
+
+    public void bossLevelNoFluff(List<Task> tasks) {
+        System.out.println("Logic1");
+        System.out.println("Logic2");
+        System.out.println("Logic7 " + tasks);
+        System.out.println("Logic7");
+    }
 
 }
 
 
 class Task {
-   private final int id;
-   private boolean running;
+    private final int id;
+    private boolean running;
 
-   Task(int id) {
-      this.id = id;
-   }
+    Task(int id) {
+        this.id = id;
+    }
 
-   public void setRunning() {
-      running = true;
-   }
+    public void setRunning() {
+        running = true;
+    }
 
-   public boolean isRunning() {
-      return running;
-   }
+    public boolean isRunning() {
+        return running;
+    }
 
-   public int getId() {
-      return id;
-   }
+    public int getId() {
+        return id;
+    }
 
-   @Override
-   public String toString() {
-      return "Task{" + "id=" + id + ", running=" + running + '}';
-   }
+    @Override
+    public String toString() {
+        return "Task{" + "id=" + id + ", running=" + running + '}';
+    }
 }
