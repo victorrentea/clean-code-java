60faf373ae0827b67ebe8226ceb7990dde2a929c
victorrentea <victorrentea@gmail.com>
2022-07-25 16:16:18 +0300

    SURPRIZA : in lista aia de fapt n-ai voie sa pui >>> RUNTIME EXCEPTON

--- src/main/java/victor/training/cleancode/immutables/basic/ImmutableBasic.java
+++ src/main/java/victor/training/cleancode/immutables/basic/ImmutableBasic.java
@@ -1,5 +1,7 @@
 package victor.training.cleancode.immutables.basic;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -9,30 +11,68 @@ public class ImmutableBasic {
    public static void main(String[] args) {
       List<Integer> numbers = Stream.of(1, 2, 3, 4, 5).collect(toList());
 
-      Immutable immutable = new Immutable();
+      Immutable immutable = new Immutable(2, numbers, new Other(13));
 
-      immutable.x = 2;
-      immutable.numbers = numbers;
-      immutable.other = new Other(13);
 
       System.out.println(immutable);
 
+      adanc(immutable);
       // LOTS OF BUSINESS LOGIC HERE
 
-      System.out.println(immutable.numbers);
+      System.out.println(immutable.getNumbers());
       System.out.println(immutable);
+   }
+
+   private static void adanc(Immutable immutable) {
+      horror(immutable);
+      if (immutable.getNumbers().size() == 5) {
+         System.out.println("ceva cod important");
+      }
+   }
+
+   private static void horror(Immutable immutable) {
+      simaiAdanc(immutable);
+   }
+
+   private static void simaiAdanc(Immutable immutable) {
+      // chestt
+      // e tarziu Vinerim =de Neversea, si ... am un bug. ce ma fac? Orice pot
+      immutable.getNumbers().add(-1); // nici nu banuiesti ca te arzi
    }
 }
 
 class Immutable {
-   public int x;
-   public List<Integer> numbers;
-   public Other other;
+   private final int x;
+   private final List<Integer> numbers;
+   private final Other other;
+
+   public Immutable(int x, List<Integer> numbers, Other other) {
+      this.x = x;
+      this.numbers = numbers;
+      this.other = other;
+   }
 
    public String toString() {
       return String.format("Immutable{x=%d, numbers=%s, other=%s}", x, numbers, other);
    }
+
+   public int getX() {
+      return x;
+   }
+
+   public List<Integer> getNumbers() {
+//      return new ArrayList<>(numbers); // malloc
+      return Collections.unmodifiableList(numbers);
+   }
+
+   public Other getOther() {
+      return other;
+   }
+
 }
+
+
+
 
 class Other {
    private int a;
