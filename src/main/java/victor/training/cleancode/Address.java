8bc3983b4f773a6e65d63a98811f2d55ad06dda9
victorrentea <victorrentea@gmail.com>
2022-07-25 14:19:56 +0300

    Pranz

---
+++ src/main/java/victor/training/cleancode/Address.java
@@ -0,0 +1,25 @@
+package victor.training.cleancode;
+
+public class Address {
+    private final String city;
+    private final String streetName;
+    private final Integer streetNumber;
+
+    public Address(String city, String streetName, Integer streetNumber) {
+        this.city = city;
+        this.streetName = streetName;
+        this.streetNumber = streetNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public Integer getStreetNumber() {
+        return streetNumber;
+    }
+}
--- src/main/java/victor/training/cleancode/ExtractValueObjects.java
+++ src/main/java/victor/training/cleancode/ExtractValueObjects.java
@@ -6,158 +6,153 @@ import static java.util.stream.Collectors.toList;
 
 class ExtractValueObjects {
 
-   // see tests
-   public List<CarModel> filterCarModels(CarSearchCriteria criteria,
-                                         List<CarModel> models) {
-      List<CarModel> results = models.stream()
-              .filter(model -> yearIntervalsIntersect(criteria, model))
-              .collect(toList());
-      System.out.println("More filtering logic");
-      return results;
-   }
+    // see tests
+    public List<CarModel> filterCarModels(CarSearchCriteria criteria,
+                                          List<CarModel> models) {
+        Interval criteriaInterval = new Interval(criteria.getStartYear(), criteria.getEndYear());
+        List<CarModel> results = models.stream()
+                .filter(model -> criteriaInterval.intersects(model.getYearInterval()))
+                .collect(toList());
+        System.out.println("More filtering logic");
+        return results;
+    }
 
-   private boolean yearIntervalsIntersect(CarSearchCriteria criteria, CarModel model) {
-      return MathUtil.intervalsIntersect2(new Interval(criteria.getStartYear(), criteria.getEndYear()), new Interval(model.getStartYear(), model.getEndYear()));
-   }
-
-   private void applyCapacityFilter() {
-      System.out.println(MathUtil.intervalsIntersect2(new Interval(1000, 1600), new Interval(1250, 2000)));
-   }
+    private void applyCapacityFilter() {
+        System.out.println(new Interval(1000, 1600).intersects(new Interval(1250, 2000)));
+    }
 
 }
+
 class Alta {
-   private void applyCapacityFilter() {
-      System.out.println(MathUtil.intervalsIntersect2(new Interval(1000, 1600), new Interval(1250, 2000)));
-   }
+    private void applyCapacityFilter() {
+        System.out.println(new Interval(1000, 1600).intersects(new Interval(1250, 2000)));
+    }
 
 }
 
 class MathUtil {
-
-   public static boolean intervalsIntersect2(
-          Interval interval1, Interval interval2) {
-      return interval1.getStart() <= interval2.getEnd() &&
-             interval2.getStart() <= interval1.getEnd();
-   }
 }
 
 class Interval {
-   private final int start;
-   private final int end;
+    private final int start;
+    private final int end;
 
-   Interval(int start, int end) {
-      this.start = start;
-      this.end = end;
-   }
+    Interval(int start, int end) {
+        if (start > end) {
+            throw new IllegalArgumentException("start larger than end");
+        }
+        this.start = start;
+        this.end = end;
+    }
 
-   public int getStart() {
-      return start;
-   }
+    public boolean intersects(Interval other) {
+        return start <= other.end && other.start <= end;
+    }
 
-   public int getEnd() {
-      return end;
-   }
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
 
 }
 
 
-
-
-
-
-
-
-
 class CarSearchCriteria {
-   private final int startYear;
-   private final int endYear;
-   private final String make;
+    private final int startYear;
+    private final int endYear;
+    private final String make;
 
-   public CarSearchCriteria(int startYear, int endYear, String make) {
-      this.make = make;
-      if (startYear > endYear) throw new IllegalArgumentException("start larger than end");
-      this.startYear = startYear;
-      this.endYear = endYear;
-   }
+    public CarSearchCriteria(int startYear, int endYear, String make) {
+        this.make = make;
+        if (startYear > endYear) throw new IllegalArgumentException("start larger than end");
+        this.startYear = startYear;
+        this.endYear = endYear;
+    }
 
-   public int getStartYear() {
-      return startYear;
-   }
+    public int getStartYear() {
+        return startYear;
+    }
 
-   public int getEndYear() {
-      return endYear;
-   }
+    public int getEndYear() {
+        return endYear;
+    }
 
-   public String getMake() {
-      return make;
-   }
+    public String getMake() {
+        return make;
+    }
 }
 
 //@Entity
 class CarModel {
-//   @Id
-   private Long id;
-   private String make;
-   private String model;
-   private int startYear;
-   private int endYear;
+    //   @Id
+    private Long id;
+    private String make;
+    private String model;
+    private Interval yearInterval;
 
-   private CarModel() {} // for Hibernate
-   public CarModel(String make, String model, int startYear, int endYear) {
-      this.make = make;
-      this.model = model;
-      if (startYear > endYear) throw new IllegalArgumentException("start larger than end");
-      this.startYear = startYear;
-      this.endYear = endYear;
-   }
+    private CarModel() {
+    } // for Hibernate
 
-   public Long getId() {
-      return id;
-   }
+    public CarModel(String make, String model, Interval yearInterval1) {
+        this.make = make;
+        this.model = model;
+        yearInterval = yearInterval1;
+    }
 
-   public int getEndYear() {
-      return endYear;
-   }
+    public Interval getYearInterval() {
+        return yearInterval;
+    }
 
-   public int getStartYear() {
-      return startYear;
-   }
+    public Long getId() {
+        return id;
+    }
 
-   public String getMake() {
-      return make;
-   }
+    public String getMake() {
+        return make;
+    }
 
-   public String getModel() {
-      return model;
-   }
+    public String getModel() {
+        return model;
+    }
 
 
-   @Override
-   public String toString() {
-      return "CarModel{" +
-             "make='" + make + '\'' +
-             ", model='" + model + '\'' +
-             '}';
-   }
+    @Override
+    public String toString() {
+        return "CarModel{" +
+               "make='" + make + '\'' +
+               ", model='" + model + '\'' +
+               '}';
+    }
 }
 
 
 class CarModelMapper {
-   public CarModelDto toDto(CarModel carModel) {
-      CarModelDto dto = new CarModelDto();
-      dto.make = carModel.getMake();
-      dto.model = carModel.getModel();
-      dto.startYear = carModel.getStartYear();
-      dto.endYear = carModel.getEndYear();
-      return dto;
-   }
-   public CarModel fromDto(CarModelDto dto) {
-      return new CarModel(dto.make, dto.model, dto.startYear, dto.endYear);
-   }
+    public CarModelDto toDto(CarModel carModel) {
+        CarModelDto dto = new CarModelDto();
+        dto.make = carModel.getMake();
+        dto.model = carModel.getModel();
+        dto.startYear = carModel.getYearInterval().getStart();
+        dto.startYear = carModel.getYearInterval().getStart();
+        dto.startYear = carModel.getYearInterval().getStart();
+        dto.startYear = carModel.getYearInterval().getStart();
+        dto.startYear = carModel.getYearInterval().getStart();
+        dto.startYear = carModel.getYearInterval().getStart();
+        dto.startYear = carModel.getYearInterval().getStart();
+        dto.endYear = carModel.getYearInterval().getEnd();
+        return dto;
+    }
+
+    public CarModel fromDto(CarModelDto dto) {
+        return new CarModel(dto.make, dto.model, new Interval(dto.startYear, dto.endYear));
+    }
 }
+
 class CarModelDto {
-   public String make;
-   public String model;
-   public int startYear;
-   public int endYear;
+    public String make;
+    public String model;
+    public int startYear;
+    public int endYear;
 }
