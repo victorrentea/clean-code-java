23993f41ea875408384f01e150fed869916fde51
victorrentea <victorrentea@gmail.com>
2022-07-26 09:56:24 +0300

    cioaca. am fugit ingrozit de NPE. NPE imi ghieeaza viata si designul

--- src/main/java/victor/training/fp/Optional_Intro.java
+++ src/main/java/victor/training/fp/Optional_Intro.java
@@ -7,26 +7,35 @@ import victor.training.cleancode.exception.model.MemberCard;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @SuppressWarnings("ConstantConditions")
 public class Optional_Intro {
 	public static void main(String[] args) {
 		// test: 60, 10, no MemberCard
 		System.out.println(getDiscountLine(new Customer(new MemberCard(60))));
+		System.out.println(getDiscountLine(new Customer(new MemberCard(10))));
+		System.out.println(getDiscountLine(new Customer()));
 	}
 
 	public static String getDiscountLine(Customer customer) {
-		return "Discount: " + getApplicableDiscountPercentage(customer.getMemberCard()).getGlobalPercentage();
+		return getApplicableDiscountPercentage(customer.getMemberCard())
+				.map(discount -> "Discount: " + discount.getGlobalPercentage())
+				.orElse("No discount");
 	}
+	// un api pe care-l chemmi =-----------------------
 
-	private static Discount getApplicableDiscountPercentage(MemberCard card) {
+	private static Optional<Discount> getApplicableDiscountPercentage(MemberCard card) {
+		if (card == null) {
+			return Optional.empty();
+		}
 		if (card.getFidelityPoints() >= 100) {
-			return new Discount(5);
+			return Optional.of(new Discount(5));
 		}
 		if (card.getFidelityPoints() >= 50) {
-			return new Discount(3);
+			return Optional.of(new Discount(3));
 		}
-		return null;
+		return Optional.empty();
 	}
 }
 
