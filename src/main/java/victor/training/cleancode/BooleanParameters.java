d8ba2bb39b45b9bbd125caf0546ffece7911f44f
victorrentea <victorrentea@gmail.com>
2022-09-19 10:53:04 +0300

    BOULEan parameter : fixed. Fri evening syndrome;

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -10,24 +10,26 @@ public class BooleanParameters {
 
    public static void main(String[] args) {
       // The big method is called from various foreign places in the codebase
-      bigUglyMethod(1, new Task(5));
-      bigUglyMethod(2, new Task(4));
-      bigUglyMethod(3, new Task(3));
-      bigUglyMethod(4, new Task(2));
-      bigUglyMethod(5, new Task(1));
+      bigUglyMethod(new Task(5), 1, false);
+      bigUglyMethod(new Task(4), 2, false);
+      bigUglyMethod(new Task(3), 3, false);
+      bigUglyMethod(new Task(2), 4, false);
+      bigUglyMethod(new Task(1), 5, false);
 
       // TODO From my use-case #323, I call it too, to do more within:
       Task task = new Task(1);
-      bigUglyMethod(2, task);
+      bigUglyMethod(task, 2, true);
 
    }
 
-   static void bigUglyMethod(int b, Task task) {
+   static void bigUglyMethod(Task task, int b, boolean cr323) {
       System.out.println("Complex Logic 1 " + task + " and " + b);
       System.out.println("Complex Logic 2 " + task);
       System.out.println("Complex Logic 3 " + task);
 
-      // System.out.println("Logic just for CR323 : " + task);
+      if (cr323) {
+         System.out.println("Logic just for CR323 : " + task);
+      }
 
       System.out.println("More Complex Logic " + b);
       System.out.println("More Complex Logic " + b);
@@ -38,13 +40,13 @@ public class BooleanParameters {
    // ============== "BOSS" LEVEL: Deeply nested functions are a lot harder to break down =================
 
    // Lord gave us tests!
-   public void bossLevel(boolean fluff, List<Task> tasks, boolean cr323) {
+   public void bossLevel(boolean fluff2, List<Task> tasks, boolean cr323) {
       int index = 0; // TODO move closer to usages
-      int j = tasks.size();
+      int taskCount = tasks.size();
       System.out.println("Logic1");
       List<Integer> taskIds = new ArrayList<>();
       System.out.println("Logic2");
-      if (fluff) {
+      if (fluff2) {
          System.out.println("Logic3");
          for (Task task : tasks) {
             System.out.println("Logic4: Validate " + task);
@@ -59,7 +61,7 @@ public class BooleanParameters {
             index++;
             System.out.println("Logic5 index=" + index + " on running=" + task.isRunning());
          }
-         System.out.println("Logic6 " + j);
+         System.out.println("Logic6 " + taskCount);
          System.out.println("Task Ids: " + taskIds);
       } else {
          System.out.println("Logic7 " + tasks);
