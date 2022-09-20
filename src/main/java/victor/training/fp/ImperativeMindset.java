4be40ac3b53c6a3f7683befd7df99b0904e91149
victorrentea <victorrentea@gmail.com>
2022-09-20 13:26:10 +0300

    cleaning up a stream

--- src/main/java/victor/training/fp/ImperativeMindset.java
+++ src/main/java/victor/training/fp/ImperativeMindset.java
@@ -2,15 +2,16 @@ package victor.training.fp;
 
 import java.util.List;
 
-public class ImperativeMindset {
+public class ImperativeMindset { // stateful
+
    public void useStreams(List<Order> orders) {
-      int sum = 0;
-      orders.stream()
-          .filter(order -> order.isActive())
-          .forEach(order -> {
-             // imperative mindset: let's *add* to ...
-             // sum += order.getPrice();
-          });
-      System.out.println("Total price of active orders: " + sum);
+      int sum = orders.stream()
+              .filter(Order::isActive)
+              .mapToInt(Order::getPrice)
+              .sum();
+       //              .map(Order::getPrice)
+       //              .reduce(0, (a, b) -> a + b)
+       // imperative mindset: let's *add* to ...
+       System.out.println("Total price of active orders: " + sum);
    }
 }
--- src/main/java/victor/training/fp/StreamWrecks.java
+++ src/main/java/victor/training/fp/StreamWrecks.java
@@ -3,8 +3,12 @@ package victor.training.fp;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.*;
 
@@ -12,17 +16,39 @@ public class StreamWrecks {
 	private ProductRepo productRepo;
 
 	public List<Product> getFrequentOrderedProducts(List<Order> orders) {
-		return orders.stream()
-				.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
-				.flatMap(o -> o.getOrderLines().stream())
-				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)))
-				.entrySet()
-				.stream()
+		Map<Product, Integer> numberOfItemsPerProduct = getNumberOfItemsPerProduct(orders);
+
+		// avoid keeping variables of type Stream because they can be mistakenly consumed
+		List<Product> frequentProducts = numberOfItemsPerProduct.entrySet().stream()
 				.filter(e -> e.getValue() >= 10)
 				.map(Entry::getKey)
-				.filter(p -> !p.isDeleted())
-				.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
 				.collect(toList());
+
+		// tomorrow...
+//		if (frequentProducts.count() > 10) {
+//			System.out.println("The client is regular");
+//		}
+
+		//careful with functions in variables!
+//		Predicate<Product> done = p -> !productRepo.getHiddenProductIds().contains(p.getId());
+		List<Long> hiddenProductIds = productRepo.getHiddenProductIds(); // only 1 DB call.
+		return frequentProducts.stream()
+				.filter(p -> !p.isDeleted())
+				.filter(p -> !hiddenProductIds.contains(p.getId()))
+				.collect(toList());
+	}
+
+	private static Map<Product, Integer> getNumberOfItemsPerProduct(List<Order> orders) {
+		return orders.stream()
+				.filter(StreamWrecks::isRecent)
+				.flatMap(o -> o.getOrderLines().stream())
+				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)));
+	}
+
+	private static boolean isRecent(Order o) { // not reusable at all. it's just my perspective on this use case
+		// NOT move it in the Order
+//		String str = o.getCreationDate().format(DateTimeFormatter.ofPattern("yyyy-MMM-dd"));
+		return o.getCreationDate().isAfter(LocalDate.now().minusYears(1));
 	}
 }
 
