43c7275d83aadb77d8a5f765f584a3a2debc09a6
victorrentea <victorrentea@gmail.com>
2022-09-20 23:54:30 +0300

    use the FORCE, Luke (IntelliJ) to find identical syntax, THEN extract common parts

--- src/main/java/victor/training/cleancode/ParameterizeAndExtract.kt
+++ src/main/java/victor/training/cleancode/ParameterizeAndExtract.kt
@@ -2,26 +2,24 @@ package victor.training.cleancode
 
 fun f(n: Int) {
     println("Logic F")
+    common(n)
+}
+
+fun g(n: Int) {
+    println("Logic G")
+    try {
+        common(n)
+    } catch (e: Exception) {
+        throw RuntimeException("Rethrow", e)
+    }
+}
+
+private fun common(n: Int) {
     for (i in 0..3) {
         if (n + i < 0) {
             println("Code $i")
         } else {
             throw IllegalArgumentException()
         }
-    }
-}
-
-fun g(n: Int) {
-    println("Logic G")
-    try {
-        for (j in 0..3) {
-            if (n + j < 0) {
-                println("Code$j")
-            } else {
-                throw IllegalArgumentException()
-            }
-        }
-    } catch (e: Exception) {
-        throw RuntimeException("Rethrow", e)
     }
 }
