9bdf5b42a3a454fce1e04388ee6cf9a55618c820
victorrentea <victorrentea@gmail.com>
2022-09-20 11:45:01 +0300

    arraty clone on getter - no

--- src/main/java/victor/training/cleancode/immutables/basic/ImmutableBasic.java
+++ src/main/java/victor/training/cleancode/immutables/basic/ImmutableBasic.java
@@ -1,5 +1,6 @@
 package victor.training.cleancode.immutables.basic;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -9,28 +10,55 @@ public class ImmutableBasic {
    public static void main(String[] args) {
       List<Integer> numbers = Stream.of(1, 2, 3, 4, 5).collect(toList());
 
-      Immutable immutable = new Immutable();
-
-      immutable.x = 2;
-      immutable.numbers = numbers;
-      immutable.other = new Other(13);
+      Immutable immutable = new Immutable(2, numbers, new Other(13));
 
       System.out.println(immutable);
 
+      dark1000KbizLogic(immutable);
       // LOTS OF BUSINESS LOGIC HERE
 
-      System.out.println(immutable.numbers);
+      System.out.println(immutable.getNumbers());
       System.out.println(immutable);
+   }
+
+   private static void dark1000KbizLogic(Immutable immutable) {
+      Immutable changedCopy = layersBelow(immutable);
+      // later
+      System.out.println("Some logic that is fixed is x++ " + changedCopy);
+   }
+
+   private static Immutable layersBelow(Immutable immutable) {
+      // bug fix
+      immutable.getNumbers().add(-1); // no error, nothing. just silence. but your change is discarded.
+      Immutable changedCopy = new Immutable(immutable.getX() + 1, immutable.getNumbers(), immutable.getOther());
+      return changedCopy;
    }
 }
 
+// "immutable" = you can't change after you instantiated it
 class Immutable {
-   public int x;
-   public List<Integer> numbers;
-   public Other other;
+   private final int x;
+   private final List<Integer> numbers;
+   private final Other other;
+
+   public Immutable(int x, List<Integer> numbers, Other other) {
+      this.x = x;
+      this.numbers = numbers;
+      this.other = other;
+   }
+
+   public int getX() {
+      return x;
+   }
+   public List<Integer> getNumbers() {
+      return new ArrayList<>(numbers); // malloc / free  inefficient + lying: the client adding to this list MIGHT HAVE the impressiion that it's changing my list. no exeption.
+   }
+   public Other getOther() {
+      return other;
+   }
 
    public String toString() {
-      return String.format("Immutable{x=%d, numbers=%s, other=%s}", x, numbers, other);
+      return String.format("Immutable{x=%d, numbers=%s, other=%s}", getX(), getNumbers(), getOther());
    }
 }
 
