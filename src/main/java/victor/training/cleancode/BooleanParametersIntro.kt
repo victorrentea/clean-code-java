cfbc19b22d51997a6c656b54f6e6239e9883499f
Victor Rentea <victorrentea@gmail.com>
2022-09-26 15:04:21 +0300

    kt❤️

---
+++ src/main/java/victor/training/cleancode/BooleanParametersIntro.kt
@@ -0,0 +1,25 @@
+package victor.training.cleancode
+
+fun main() {
+    // The big method is called from various foreign places in the codebase
+    bigUglyMethod(1, Task(5))
+    bigUglyMethod(2, Task(4))
+    bigUglyMethod(3, Task(3))
+    bigUglyMethod(4, Task(2))
+    bigUglyMethod(5, Task(1))
+
+    // TODO From my use-case #323, I call it too, to do more within:
+    val task = Task(1)
+    bigUglyMethod(2, task)
+}
+
+fun bigUglyMethod(b2: Int, task: Task) {
+    println("Complex Logic 1 $task and $b2")
+    println("Complex Logic 2 $task")
+    println("Complex Logic 3 $task")
+
+    // System.out.println("Logic just for CR#323 : " + task);
+    println("More Complex Logic $b2")
+    println("More Complex Logic $b2")
+    println("More Complex Logic $b2")
+}
