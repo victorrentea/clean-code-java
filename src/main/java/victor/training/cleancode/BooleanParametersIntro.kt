112a9a42b1ea238476b01c5699edf29a10bc3bf9
Victor Rentea <victorrentea@gmail.com>
2022-09-26 15:29:03 +0300

    Boolean params (flag arguments) can be removed by breakign the method.

--- src/main/java/victor/training/cleancode/BooleanParametersIntro.kt
+++ src/main/java/victor/training/cleancode/BooleanParametersIntro.kt
@@ -8,18 +8,44 @@ fun main() {
     bigUglyMethod(4, Task(2))
     bigUglyMethod(5, Task(1))
 
-    // TODO From my use-case #323, I call it too, to do more within:
+
+
+    // TODO From my CR #323, I call it too, to do more within:
     val task = Task(1)
-    bigUglyMethod(2, task)
+    bigUglyMethod323(2, task)
+
+
+    // TODO From my CR #370, I call it too, to do more within:
+//    val task = Task(1)
+//    bigUglyMethod(2, task, true)
 }
 
 fun bigUglyMethod(b2: Int, task: Task) {
+    bigUglyStart(task, b2)
+    bigUglyEnd(b2)
+}
+fun bigUglyMethod323(b2: Int, task: Task) {
+    bigUglyStart(task, b2)
+    System.out.println("Logic just for CR#323 : " + task);
+    System.out.println("Logic just for CR#323 : " + task);
+    System.out.println("Logic just for CR#323 : " + task);
+    bigUglyEnd(b2)
+}
+
+
+
+
+private fun bigUglyEnd(b2: Int) {
+    println("More Complex Logic $b2")
+    println("More Complex Logic $b2")
+    println("More Complex Logic $b2")
+    println("More Complex Logic $b2")
+    println("More Complex Logic $b2")
+}
+
+private fun bigUglyStart(task: Task, b2: Int) {
     println("Complex Logic 1 $task and $b2")
     println("Complex Logic 2 $task")
     println("Complex Logic 3 $task")
-
-    // System.out.println("Logic just for CR#323 : " + task);
-    println("More Complex Logic $b2")
-    println("More Complex Logic $b2")
-    println("More Complex Logic $b2")
+    println("Complex Logic 3 $task")
 }
