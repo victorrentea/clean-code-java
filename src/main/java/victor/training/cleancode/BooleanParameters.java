cfe26e6cad875073b9bd96f631255061032ecd14
Victor Rentea <victorrentea@gmail.com>
2022-12-07 11:17:42 +0200

    cum distrugi booleanu: creand functii specializate pt fiecare caz.
    => SRP ++

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -10,44 +10,46 @@ public class BooleanParameters {
 
    public static void main(String[] args) {
       // The big method is called from various foreign places in the codebase
-      bigUglyMethod(1, new Task(5), false);
-      bigUglyMethod(2, new Task(4), false);
-      bigUglyMethod(3, new Task(3), false);
-      bigUglyMethod(4, new Task(2), false);
-      bigUglyMethod(5, new Task(1), false);
+      bigUglyMethod(1, new Task(5));
+      bigUglyMethod(2, new Task(4));
+      bigUglyMethod(3, new Task(3));
+      bigUglyMethod(4, new Task(2));
+      bigUglyMethod(5, new Task(1));
 
       // TODO From my use-case #323, I call it too, to do more within:
       Task task = new Task(1);
-      bigUglyMethod(2, task, true);
+      bigUglyMethod323(2, task);
 
    }
 
+      //      if (cr435) {
+//         System.out.println("Doar pentur uc maria cr#435");
+//      }
 
 
+   static void bigUglyMethod(int b, Task task) {
+      bigStart(b, task);
+      sendNotificationTo3rdParties(b);
+   }
+   static void bigUglyMethod323(int b, Task task) {
+      bigStart(b, task);
 
+      System.out.println("Logic just for CR#323 : " + task);
 
+      sendNotificationTo3rdParties(b);
+   }
 
-   static void bigUglyMethod(int b, Task task, boolean cr323) {
+   private static void sendNotificationTo3rdParties(int b) {
+      System.out.println("More Complex Logic " + b);
+      System.out.println("More Complex Logic " + b);
+      System.out.println("More Complex Logic " + b);
+   }
+
+   private static void bigStart(int b, Task task) {
       System.out.println("Complex Logic 1 " + task + " and " + b);
       System.out.println("Complex Logic 2 " + task);
       System.out.println("Complex Logic 3 " + task);
-
-      if (cr323) {
-         System.out.println("Logic just for CR#323 : " + task);
-      }
-
-      System.out.println("More Complex Logic " + b);
-      System.out.println("More Complex Logic " + b);
-      System.out.println("More Complex Logic " + b);
    }
-
-
-
-
-
-
-
-
 
 
    // ============== "BOSS" LEVEL: Deeply nested functions are a lot harder to break down =================
