76f410412f3d611984f0498a73f169da77fa097a
Victor Rentea <victorrentea@gmail.com>
2022-12-08 10:20:25 +0200

    Opt<> param antipattern

--- src/main/java/victor/training/fp/Optional_Intro.java
+++ src/main/java/victor/training/fp/Optional_Intro.java
@@ -25,15 +25,17 @@ public class Optional_Intro {
 						.orElse("");
 	}
 
+	// antipattern: sa iei opt ca param pt ca te obliga sa il verifici inauntru => SRP violation.
+	// cum fix: NICI nu chemi functia daca n-ai card
 	// in domain logic
-	private static Optional<Discount> getApplicableDiscountPercentage(MemberCard card) {
-		if (card == null) {
+	private static Optional<Discount> getApplicableDiscountPercentage(Optional<MemberCard> card) {
+		if (card.isEmpty()) {
 			return Optional.empty();
 		}
-		if (card.getFidelityPoints() >= 100) {
+		if (card.get().getFidelityPoints() >= 100) {
 			return Optional.of(new Discount(5));
 		}
-		if (card.getFidelityPoints() >= 50) {
+		if (card.get().getFidelityPoints() >= 50) {
 			return Optional.of(new Discount(3));
 		}
 //		return new Discount(0); // null object design pattern - un ob normal doar ca are date ce reprezinta NIMICUL ==> aici da BAD UX
