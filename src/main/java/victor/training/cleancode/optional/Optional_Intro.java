921182de5477fbb0d460bd74b74d1dbea182ca78
Victor Rentea <victorrentea@gmail.com>
2022-12-19 15:59:37 +0200

    intro

--- src/main/java/victor/training/cleancode/optional/Optional_Intro.java
+++ src/main/java/victor/training/cleancode/optional/Optional_Intro.java
@@ -7,31 +7,37 @@ import victor.training.cleancode.exception.model.MemberCard;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @SuppressWarnings("ConstantConditions")
 public class Optional_Intro {
-	public static void main(String[] args) {
-		// test: 60, 10, no MemberCard
-		System.out.println(getDiscountLine(new Customer(new MemberCard(60))));
-	}
+  public static void main(String[] args) {
+    // test: 60, 10, no MemberCard
+    System.out.println(getDiscountLine(new Customer(new MemberCard(60))));
+    System.out.println(getDiscountLine(new Customer(new MemberCard(10))));
+  }
 
-	public static String getDiscountLine(Customer customer) {
-		return "You got a discount of %" + getApplicableDiscountPercentage(customer.getMemberCard()).getGlobalPercentage();
-	}
+  public static String getDiscountLine(Customer customer) {
+    return getApplicableDiscountPercentage(customer.getMemberCard())
+            .map(discount -> "You got a discount of %" + discount.getGlobalPercentage())
+            .orElse("");
+  }
 
-	private static Discount getApplicableDiscountPercentage(MemberCard card) {
-		if (card.getFidelityPoints() >= 100) {
-			return new Discount(5);
-		}
-		if (card.getFidelityPoints() >= 50) {
-			return new Discount(3);
-		}
-		return null;
-	}
-	@Data
-	public static class Discount {
-		private final int globalPercentage;
-		private Map<String, Integer> categoryDiscounts = new HashMap<>();
-	}
+
+  private static Optional<Discount> getApplicableDiscountPercentage(MemberCard card) {
+    if (card.getFidelityPoints() >= 100) {
+      return Optional.of(new Discount(5));
+    }
+    if (card.getFidelityPoints() >= 50) {
+      return Optional.of(new Discount(3));
+    }
+    return Optional.empty();
+  }
+
+  @Data
+  public static class Discount {
+    private final int globalPercentage;
+    private Map<String, Integer> categoryDiscounts = new HashMap<>();
+  }
 }
 
