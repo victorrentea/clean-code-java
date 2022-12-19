ac6d6ffbf038db847157838a654ca107c50fce83
Victor Rentea <victorrentea@gmail.com>
2022-12-19 14:42:04 +0200

    validari in obiectele cu date care se plimba prin logica centrala, ca sa nu-ti mai tremure lenjeria pe tine cand lucrezi cu ele.!!

--- src/main/java/victor/training/cleancode/ExtractValueObjects.java
+++ src/main/java/victor/training/cleancode/ExtractValueObjects.java
@@ -42,7 +42,10 @@ class Interval {
   private final int start;
   private final int end;
 
-  Interval(int start, int end) {
+  public Interval(int start, int end) {
+    if (start > end) {
+      throw new IllegalArgumentException("Nu e bun");
+    }
     this.start = start;
     this.end = end;
   }
