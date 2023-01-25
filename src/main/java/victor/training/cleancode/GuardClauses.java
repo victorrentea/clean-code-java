41354188fc7920c16ed3ba11d1e38897df1a973d
Victor Rentea <victorrentea@gmail.com>
2023-01-25 12:52:36 +0200

    Guard clauses : dai peste cap un if ca sa incepi metoda cu situatii pe care return/throw imediat (cazuri banale sau eronate).
    > cod mai putin indentat, mai usor pt creier.

--- src/main/java/victor/training/cleancode/GuardClauses.java
+++ src/main/java/victor/training/cleancode/GuardClauses.java
@@ -8,30 +8,34 @@ public class GuardClauses {
    public static final int DEAD_PAY_AMOUNT = 1;
 
    public int getPayAmount(Marine marine) {
-      int result;
-      if (marine != null) {
-         if (!isDead(marine)) {
-            if (!marine.isRetired()) {
-               if (marine.getYearsService() != null) {
-                  result = marine.getYearsService() * 100;
-                  if (!marine.getAwards().isEmpty()) {
-                     result += 1000;
-                  }
-                  if (marine.getAwards().size() >= 3) {
-                     result += 2000;
-                  }
-                  // HEAVY logic here...
-               } else {
-                  throw new IllegalArgumentException("Any marine should have the years of service set");
-               }
-            } else result = retiredAmount();
-         } else {
-            result = DEAD_PAY_AMOUNT;
-         }
-      } else {
+      if (marine == null) {
          throw new RuntimeException("Marine is null");
       }
-      return result; // TODO ALT-ENTER move return closer
+      if (isDead(marine)) {
+         return DEAD_PAY_AMOUNT;
+      }
+      if (marine.isRetired()) {
+         return retiredAmount();
+      }
+      if (marine.getYearsService() == null) {
+         throw new IllegalArgumentException("Any marine should have the years of service set");
+      }
+      if (marine.getAwards() == null) {
+         throw new RuntimeException("WHAT?!! colectie nula la mine in cod !?!?!");
+      }
+      return computePayAmount(marine);
+   }
+
+   private static int computePayAmount(Marine marine) {
+      int result = marine.getYearsService() * 100;
+      if (!marine.getAwards().isEmpty()) {
+         result += 1000;
+      }
+      if (marine.getAwards().size() >= 3) {
+         result += 2000;
+      }
+      // HEAVY logic here...
+      return result;
    }
 
    private boolean isDead(Marine marine) {
@@ -48,7 +52,12 @@ class Marine {
    private final boolean dead;
    private final boolean retired;
    private final Integer yearsService;
-   private final List<Award> awards = new ArrayList<>();
+   private final List<Award> awards = new ArrayList<>(); // daca vreodata prind colectii nulle FAC SCANDAL!
+   //vin la tine. Niciodata nu ai voie sa primesti ca List<> un null in codul central. NICIODATA> doar colectii goale.
+
+   // motiv de scandal:
+   // JSF,Jackson, JAXB -> asjustezi frameworkuri
+   //
 
    Marine(boolean dead, boolean retired, Integer yearsService) {
       this.dead = dead;
