1fe1db9a9ebd861a987126d5abf63a4dab37d772
Victor Rentea <victorrentea@gmail.com>
2023-01-25 16:30:30 +0200

    microtypes over primitive obsessions

--- src/main/java/victor/training/cleancode/MicroTypes.java
+++ src/main/java/victor/training/cleancode/MicroTypes.java
@@ -1,5 +1,6 @@
 package victor.training.cleancode;
 
+import lombok.Value;
 import org.jooq.lambda.tuple.Tuple;
 import org.jooq.lambda.tuple.Tuple2;
 import org.junit.jupiter.api.Test;
@@ -14,27 +15,39 @@ import static java.util.stream.Collectors.joining;
 public class MicroTypes {
 
     //<editor-fold desc="Unknown source of data">
-    public Map<Long, List<Tuple2<String, Integer>>> extremeFP() {
+    public Map<CustomerId, List<ProductCount>> extremeFP() {
         Long customerId = 1L;
         Integer product1Count = 2;
         Integer product2Count = 4;
-        Map<Long, List<Tuple2<String, Integer>>> results = new HashMap<>();
-        results.put(customerId, Arrays.asList(Tuple.tuple("Table", product1Count),
-                Tuple.tuple("Chair", product2Count)));
+        Map<CustomerId, List<ProductCount>> results = new HashMap<>();
+        results.put(new CustomerId(customerId), Arrays.asList(
+                new ProductCount("Table", product1Count),
+                new ProductCount("Chair", product2Count)));
         return results;
     }
     //</editor-fold>
 
+
+
     @Test
     void lackOfAbstractions() {
-        Map<Long, List<Tuple2<String, Integer>>> map = extremeFP();
-        // Joke: try "var" above :)
+        Map<CustomerId, List<ProductCount>> map = extremeFP();
 
-        for (Long cid : map.keySet()) {
+        for (CustomerId cid : map.keySet()) {
             String pl = map.get(cid).stream()
-                    .map(t -> t.v2 + " pcs. of " + t.v1)
+                    .map(t -> t.getCount() + " pcs. of " + t.getProductName())
                     .collect(joining(", "));
             System.out.println("cid=" + cid + " got " + pl);
         }
     }
+}
+@Value
+class CustomerId{
+    long id;
+}
+
+@Value
+class ProductCount {
+    String productName;
+    int count;
 }
