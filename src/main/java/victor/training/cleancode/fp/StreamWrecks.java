19ff952aaf558d425a559e4c8cb79a206817b235
Victor Rentea <victorrentea@gmail.com>
2023-01-26 12:16:12 +0200

    Explicam streamurile dupa 2-3-4 operatori: stop si explici : variabile sau extragand metode;

--- src/main/java/victor/training/cleancode/fp/StreamWrecks.java
+++ src/main/java/victor/training/cleancode/fp/StreamWrecks.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import static java.util.stream.Collectors.*;
@@ -12,17 +13,35 @@ public class StreamWrecks {
 	private ProductRepo productRepo;
 
 	public List<Product> getFrequentOrderedProducts(List<Order> orders) {
-		return orders.stream()
-				.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
-				.flatMap(o -> o.getOrderLines().stream())
-				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)))
-				.entrySet()
-				.stream()
-				.filter(e -> e.getValue() >= 10)
-				.map(Entry::getKey)
-				.filter(p -> !p.isDeleted())
-				.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
+		Map<Product, Integer> recentlyOrderedProductCounts = orders.stream()
+						.filter(StreamWrecks::orderIsRecent)
+						.flatMap(o -> o.getOrderLines().stream())
+						.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)));
+
+		List<Product> frequentProducts = recentlyOrderedProductCounts.entrySet().stream()
+						.filter(e -> e.getValue() >= 10)
+						.map(Entry::getKey)
+						.collect(toList());
+
+		// tinere, tu tine in variable Stream<> ca-o sa te arzi, ci List
+
+//		if (frequentProducts.size() > 100) {
+//			System.out.println("AM MULTE FRECVENTE!");
+//		}
+
+		List<Long> hiddenProductIds = productRepo.getHiddenProductIds(); // 1 single DB query => -1 sec runtime
+		return frequentProducts.stream()
+				// A mai secvential de citit
+				.filter(p -> p.isActive())
+				.filter(p -> !hiddenProductIds.contains(p.getId()))
+
+				// B mai 'new-age', o tzara mai eficient >> Masoar-o
+//				.filter(p -> p.isActive() && !productRepo.getHiddenProductIds().contains(p.getId()))
 				.collect(toList());
+	}
+
+	private static boolean orderIsRecent(Order o) {
+		return o.getCreationDate().isAfter(LocalDate.now().minusYears(1));
 	}
 }
 
@@ -32,6 +51,10 @@ public class StreamWrecks {
 class Product {
 	private Long id;
 	private boolean deleted;
+
+	public boolean isActive() {
+		return !deleted;
+	}
 }
 
 @Data
