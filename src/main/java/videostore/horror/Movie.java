d051fcf788a22db6a166ed412732d7def68bb012
Victor Rentea <victorrentea@gmail.com>
2023-01-26 14:29:01 +0200

    r migrated price code to int

--- src/main/java/videostore/horror/Movie.java
+++ src/main/java/videostore/horror/Movie.java
@@ -1,22 +1,23 @@
 package videostore.horror;
 
+// what could someone say about this code ?
 public class Movie {
   public static final int CHILDRENS = 2;
   public static final int REGULAR = 0;
   public static final int NEW_RELEASE = 1;
   private String _title;
-  private Integer _priceCode;
+  private int _priceCode;
 
-  public Movie(String title, Integer priceCode) {
+  public Movie(String title, int priceCode) {
     _title = title;
     _priceCode = priceCode;
   }
 
-  public Integer getPriceCode() {
+  public int getPriceCode() {
     return _priceCode;
   }
 
-  public void setPriceCode(Integer arg) {
+  public void setPriceCode(int arg) {
     _priceCode = arg;
   }
 
