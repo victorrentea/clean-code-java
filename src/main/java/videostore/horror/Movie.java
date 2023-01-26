3ed1bf72163845e9ccfc6aa73c42be661c75d347
Victor Rentea <victorrentea@gmail.com>
2023-01-26 14:38:35 +0200

    R am trecut de la int la enum PriceCode

--- src/main/java/videostore/horror/Movie.java
+++ src/main/java/videostore/horror/Movie.java
@@ -5,19 +5,32 @@ public class Movie {
   public static final int CHILDRENS = 2;
   public static final int REGULAR = 0;
   public static final int NEW_RELEASE = 1;
-  private String _title;
-  private int _priceCode;
 
-  public Movie(String title, int priceCode) {
+  enum MovieType {
+    REGULAR(0),
+    NEW_RELEASE(1),
+    CHILDRENS(2);
+
+    private final int code;
+
+    MovieType(int code) {
+      this.code = code;
+    }
+  }
+
+  private String _title;
+  private MovieType _priceCode;
+
+  public Movie(String title, MovieType priceCode) {
     _title = title;
     _priceCode = priceCode;
   }
 
-  public int getPriceCode() {
+  public MovieType getPriceCode() {
     return _priceCode;
   }
 
-  public void setPriceCode(int arg) {
+  public void setPriceCode(MovieType arg) {
     _priceCode = arg;
   }
 
