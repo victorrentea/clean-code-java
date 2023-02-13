0214d0c31404a6ce67e3bad6c329473da3b44027
Victor Rentea <victorrentea@gmail.com>
2023-02-13 14:50:58 +0200

    BOULEan parameters: when you want a method do do +1 thing-> add a boolean without thinking
    WRONG

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -19,43 +19,44 @@ class SomeController {
 
 class SomeService {
   public void blueMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task);
+    BooleanParameters.bigUglyMethod(id, task, false);
   }
 
   public void greenMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task);
+    BooleanParameters.bigUglyMethod(id, task, false);
   }
 
   public void yellowMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task);
+    BooleanParameters.bigUglyMethod(id, task, false);
   }
 
   public void redMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task);
+    BooleanParameters.bigUglyMethod(id, task, false);
   }
 }
-
 class MyService {
   public void useCase323(int id, Task task) {
     // TODO From my use-case #323, I call the method too, but have it do more within:
-    BooleanParameters.bigUglyMethod(2, task);
+    BooleanParameters.bigUglyMethod(2, task, true);
   }
 }
 
 public class BooleanParameters {
-
   // Note: this method might be called from multiple places in the codebase ...
-  static void bigUglyMethod(int storeId, Task task) {
+  static void bigUglyMethod(int storeId, Task task, boolean cr323) {
     System.out.println("Cow Logic 1 " + task + " and " + storeId);
     System.out.println("Cow Logic 2 " + task);
     System.out.println("Cow Logic 3 " + task);
 
-    // System.out.println("Logic just for CR#323 : " + task);
+    if (cr323) {
+      System.out.println("Logic just for CR#323 : " + task);
+    }
 
     System.out.println("Donkey Logic 1 " + storeId);
     System.out.println("Donkey Logic 2 " + storeId);
     System.out.println("Donkey Logic 3 " + storeId);
   }
+
 
 
   // ============== "BOSS" LEVEL: Deeply nested functions are a lot harder to break down =================
