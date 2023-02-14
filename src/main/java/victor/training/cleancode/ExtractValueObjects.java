8e77f78b948d35375b21e657e8a9f8d4f1626db2
Victor Rentea <victorrentea@gmail.com>
2023-02-14 15:13:50 +0200

    Value Objects

--- src/main/java/victor/training/cleancode/ExtractValueObjects.java
+++ src/main/java/victor/training/cleancode/ExtractValueObjects.java
@@ -1,7 +1,6 @@
 package victor.training.cleancode;
 
 
-import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.List;
@@ -11,8 +10,10 @@ class ExtractValueObjects {
 
     // see tests
     public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> models) {
+        Interval yearInterval = new Interval(criteria.getStartYear(), criteria.getEndYear());
         List<CarModel> results = models.stream()
-                .filter(model -> new Interval(criteria.getStartYear(), criteria.getEndYear()).intersects(new Interval(model.getStartYear(), model.getEndYear())))
+//                .filter(model -> model.matchesYears(criteria)) // violates arch rules: couples Domain Model👑 to the API
+                .filter(model -> yearInterval.intersects(model.getYearInterval()))
                 .collect(Collectors.toList());
         System.out.println("More filtering logic");
         return results;
@@ -47,11 +48,15 @@ class Interval {
     private final int end;
 
     Interval(int start, int end) {
+        // #kudos for this:
+        if (start > end) throw new IllegalArgumentException("start larger than end");
+        // model becomes more strict with conssitency (but sometimes makes this class less reusable)
+
         this.start = start;
         this.end = end;
     }
 
-    public boolean intersects(Interval other) {
+    public boolean intersects(Interval other) { // OOP teacher= proud
         return start <= other.end && other.start <= end;
     }
 
@@ -90,36 +95,34 @@ class CarSearchCriteria { // smells like JSON ...
 }
 
 @Entity
-class CarModel { // the holy Entity Model
+class CarModel {
     @Id
     private Long id;
     private String make;
     private String model;
-    private int startYear;
-    private int endYear;
+    private Interval yearInterval;
 
     protected CarModel() {
     } // for Hibernate
 
-    public CarModel(String make, String model, int startYear, int endYear) {
+    public CarModel(String make, String model, Interval yearInterval) {
         this.make = make;
         this.model = model;
-        if (startYear > endYear) throw new IllegalArgumentException("start larger than end");
-        this.startYear = startYear;
-        this.endYear = endYear;
+        this.yearInterval = yearInterval;
+    }
+
+    public Interval getYearInterval() {
+        return yearInterval;
     }
 
     public Long getId() {
         return id;
     }
 
-    public int getEndYear() {
-        return endYear;
-    }
-
-    public int getStartYear() {
-        return startYear;
-    }
+    // code smell : Middle Man : stupid method
+    //    public int getEndYear() {
+    //        return yearInterval.getEnd();
+    //    }
 
     public String getMake() {
         return make;
@@ -145,13 +148,18 @@ class CarModelMapper {
         CarModelDto dto = new CarModelDto();
         dto.make = carModel.getMake();
         dto.model = carModel.getModel();
-        dto.startYear = carModel.getStartYear();
-        dto.endYear = carModel.getEndYear();
+        dto.startYear = carModel.getYearInterval().getStart();
+        dto.endYear = carModel.getYearInterval().getEnd();
+        dto.endYear = carModel.getYearInterval().getEnd();
+        dto.endYear = carModel.getYearInterval().getEnd();
+        dto.endYear = carModel.getYearInterval().getEnd();
+        dto.endYear = carModel.getYearInterval().getEnd();
+        dto.endYear = carModel.getYearInterval().getEnd();
         return dto;
     }
 
     public CarModel fromDto(CarModelDto dto) {
-        return new CarModel(dto.make, dto.model, dto.startYear, dto.endYear);
+        return new CarModel(dto.make, dto.model, new Interval(dto.startYear, dto.endYear));
     }
 }
 
@@ -161,3 +169,26 @@ class CarModelDto {
     public int startYear;
     public int endYear;
 }
+
+
+//@Service
+//class ServiceA {
+//    @Autowired
+//    private ServiceB b;
+//
+//    public void method() {
+//       getB(1);
+//    }
+//
+//    public void getB(int param) {
+//        b.stuff(param);
+//    }
+//}
+//
+//@Service
+//class ServiceB {
+//
+//    public void stuff(int param) {
+//
+//    }
+//}
