601d24e1544a004e0608f14828c468efbdfc7fc1
Victor Rentea <victorrentea@gmail.com>
2023-02-15 14:21:06 +0200

    IF like in the good ol' days of highschool

--- src/main/java/victor/training/cleancode/optional/Optional_Intro.java
+++ src/main/java/victor/training/cleancode/optional/Optional_Intro.java
@@ -7,6 +7,7 @@ import victor.training.cleancode.exception.model.MemberCard;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @SuppressWarnings("ConstantConditions")
 public class Optional_Intro {
@@ -17,20 +18,20 @@ public class Optional_Intro {
 	}
 
 	public static String getDiscountLine(Customer customer) {
-		Discount discount = computeDiscount(customer.getMemberCard());
-		if (discount.getGlobalPercentage() != 0) // worse< here you might not have thought of checking for zero
-			return "You got a discount of %" + discount.getGlobalPercentage();
+		Optional<Discount> discount = computeDiscount(customer.getMemberCard());
+		if (discount.isPresent())
+			return "You got a discount of %" + discount.get().getGlobalPercentage();
 		return "";
 	}
 
-	private static Discount computeDiscount(MemberCard card) {
+	private static Optional<Discount> computeDiscount(MemberCard card) {
 		if (card.getFidelityPoints() >= 100) {
-			return new Discount(5);
+			return Optional.of(new Discount(5));
 		}
 		if (card.getFidelityPoints() >= 50) {
-			return new Discount(3);
+			return Optional.of(new Discount(3));
 		}
-		return new Discount(0); // Null-Object pattern = you create a dummy instnace instead of passing a nul laround
+		return Optional.empty();
 	}
 
 	@Data
