3d439832f3c07caba9364159708e7dc23b8aad32
Victor Rentea <victorrentea@gmail.com>
2023-02-16 17:00:35 +0200

    R from Map rentals to List<Rental>

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -2,9 +2,7 @@ package videostore.horror;
 
 import videostore.horror.Movie.PriceCode;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 import static java.util.Objects.requireNonNull;
 
@@ -31,14 +29,14 @@ class Rental {
 
 class Customer {
   private final String name;
-  private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order
+  private final List<Rental> rentals = new ArrayList<>();
 
   public Customer(String name) {
     this.name = name;
   }
 
   public void addRental(Movie movie, int daysRented) {
-    rentals.put(movie, daysRented);
+    rentals.add(new Rental(movie, daysRented));
   }
 
   public String getName() {
@@ -49,18 +47,19 @@ class Customer {
     double totalAmount = 0;
     int frequentRenterPoints = 0;
     String result = "Rental Record for " + name + "\n";
-    for (Movie each : rentals.keySet()) {
-      int daysRented = rentals.get(each);
-      double thisAmount = computeAmountForMovie(each, daysRented);
+    for (Rental rental : rentals) {
+      Movie movie = rental.getMovie();
+      int daysRented = rental.getDaysRented();
+      double thisAmount = computeAmountForMovie(movie, daysRented);
       // add frequent renter points
       frequentRenterPoints++;
       // add bonus for a two day new release rental
-      if (each.getPriceCode() != null &&
-          (each.getPriceCode() == PriceCode.NEW_RELEASE)
+      if (movie.getPriceCode() != null &&
+          (movie.getPriceCode() == PriceCode.NEW_RELEASE)
           && daysRented >= 2)
         frequentRenterPoints++;
       // show figures line for this rental
-      result += "\t" + each.getTitle() + "\t" + thisAmount + "\n";
+      result += "\t" + movie.getTitle() + "\t" + thisAmount + "\n";
       totalAmount += thisAmount;
     }
     // add footer lines
