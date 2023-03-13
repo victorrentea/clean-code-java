13240c1d058aa7da5c4420fa3923334643c87713
Victor Rentea <victorrentea@gmail.com>
2023-03-13 16:51:23 +0200

    Immutable pe collections

--- src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
+++ src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
@@ -1,5 +1,10 @@
 package victor.training.cleancode.immutable.advanced;
 
+import lombok.Data;
+import lombok.Value;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -11,6 +16,7 @@ public class ImmutableAdvanced {
 
       Immutable immutable = new Immutable(1, numbers, new Other(15));
       System.out.println("Before: " + immutable);
+      numbers.add(-1);
 
       wilderness(immutable);
       // reasons to use immutable objects:
@@ -22,6 +28,7 @@ public class ImmutableAdvanced {
 
    private static void wilderness(Immutable immutable) {
       // dark deep logic
+      //      immutable.getNumbers().add(-1);
    }
 }
 
@@ -32,37 +39,41 @@ class Immutable {
 
    Immutable(int x, List<Integer> numbers, Other other) {
       this.x = x;
-      this.numbers = numbers;
+      this.numbers = List.copyOf(numbers); // opt3
       this.other = other;
    }
+   //   public List<Integer> getNumbers() {
+   //      return new ArrayList<>(numbers); // +1 malloc , si misleading
+   //   }
+   //   public List<Integer> getNumbers() { // opt2
+   //      return Collections.unmodifiableList(numbers); // un decorator peste lista originala
+   //      // care blocheaza (arunca ex) la orice incercare de a modifica
+   //      // eficient cu mem
+   //   }
+
+
    public List<Integer> getNumbers() {
-      return numbers;
+      return numbers;// opt 3
    }
+
    public int getX() {
       return x;
    }
+
    public Other getOther() {
       return other;
    }
 
-   @Override
    public String toString() {
       return String.format("Immutable{x=%d, numbers=%s, other=%s}", x, numbers, other);
    }
 }
 
+// PE  @Entity : @Getter
+//@Data // niciodata
+
+// pe non-@Entity: toate asa:
+@Value // @Data + toate campurile finale = getter ctor tostring hash/eq
 class Other {
-   private int a;
-
-   public Other(int a) {
-      this.a = a;
-   }
-
-   public int getA() {
-      return a;
-   }
-
-   public void setA(int a) {
-      this.a = a;
-   }
+   int a;
 }
