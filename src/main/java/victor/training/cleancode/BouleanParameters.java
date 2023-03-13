43d495c6b57e846b59bbdbdbd1cd737a0803e722
Victor Rentea <victorrentea@gmail.com>
2023-03-13 12:13:36 +0200

    gata functii mici scurte

--- src/main/java/victor/training/cleancode/BouleanParameters.java
+++ src/main/java/victor/training/cleancode/BouleanParameters.java
@@ -5,9 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 class SomeController {
   SomeService someService;
@@ -78,24 +76,9 @@ public class BouleanParameters {
 
   // ============== "BOSS" LEVEL: Deeply nested functions are a lot harder to break down =================
 
-  // Lord gave us tests! 👌 TODO run tests
-  public void bossLevel(List<Task> tasks, boolean cr323) {
-    System.out.println("Logic1");
-    System.out.println("Logic3");
-    int index = 0;
-    for (Task task : tasks) {
-      System.out.println("Validate " + task);
-      task.setStarted(true);
-    }
-
-    //    List<Integer> taskIds = tasks.map(t=>t.id); filter find any
+  private static void bossEnd(List<Task> tasks) {
     List<Integer> taskIds = tasks.stream().map(Task::getId).toList();
-
-    for (Task task : tasks) {
-      if (cr323) { // TODO remove the boolean
-        System.out.println("My Logic: " + task);
-      }
-    }
+    int index = 0;
     for (Task task : tasks) {
       index++;
       System.out.println("Audit task #" + index + ": " + task);
@@ -103,6 +86,32 @@ public class BouleanParameters {
     System.out.println("Logic6 " + tasks.size());
     System.out.println("Task Ids: " + taskIds);
     System.out.println("Logic8");
+  }
+
+  private static void bossStart(List<Task> tasks) {
+    System.out.println("Logic1");
+    System.out.println("Logic3");
+    for (Task task : tasks) {
+      System.out.println("Validate " + task);
+      task.setStarted(true);
+    }
+  }
+
+  // Lord gave us tests! 👌 TODO run tests
+  public void bossLevel(List<Task> tasks) {
+    bossStart(tasks);
+
+    bossEnd(tasks);
+  }
+
+  public void bossLevel323(List<Task> tasks) {
+    bossStart(tasks);
+
+    for (Task task : tasks) {
+      System.out.println("My Logic: " + task);
+    }
+
+    bossEnd(tasks);
   }
 
   public void bossLevelNoFluff(List<Task> tasks) {
