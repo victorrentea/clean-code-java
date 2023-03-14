fe0579a76f653fcfd3a35246402f48ed73a0e271
Victor Rentea <victorrentea@gmail.com>
2023-03-14 16:32:52 +0200

    m-am spart in figuri
    PR comment: get a life! mai lasa-l incolo de calculator.

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -4,37 +4,42 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
+import static java.util.stream.Collectors.joining;
+
 class Customer {
     private final String name;
-    private final List<Rental> rentalList = new ArrayList<>(); // preserves order
+    private final List<Rental> rentals = new ArrayList<>(); // preserves order
 
     public Customer(String name) {
         this.name = name;
     }
 
     public String composeStatement() {
-        String statement = composeHeader();
+        return composeHeader() + composeBody() + composeFooter();
+    }
 
-        int frequentRenterPoints = rentalList.stream().mapToInt(Rental::getFrequentRenterPoints).sum();
-        for (Rental rental : rentalList) {
-            statement += composeLine(rental);
-        }
-        double totalPrice = rentalList.stream().mapToDouble(Rental::getPrice).sum();
-        statement += composeFooter(totalPrice, frequentRenterPoints);
-        return statement;
+    private String composeBody() {
+        return rentals.stream().map(Customer::composeLine).collect(joining());
     }
 
     @NotNull
+    private String composeFooter() {
+        return "Amount owed is " + totalPrice() + "\n" +
+               "You earned " + totalPoints() + " frequent renter points";
+    }
+
+    private int totalPoints() {
+        return rentals.stream().mapToInt(Rental::getFrequentRenterPoints).sum();
+    }
+
+    private double totalPrice() {
+        return rentals.stream().mapToDouble(Rental::getPrice).sum();
+    }
+
     private String composeHeader() {
         return "Rental Record for " + getName() + "\n";
     }
 
-    @NotNull
-    private static String composeFooter(double totalPrice, int frequentRenterPoints) {
-        return "Amount owed is " + totalPrice + "\n" + "You earned " + frequentRenterPoints + " frequent renter points";
-    }
-
-    @NotNull
     private static String composeLine(Rental rental) {
         return "\t" + rental.getMovie().getTitle() + "\t" + rental.getPrice() + "\n";
     }
@@ -44,7 +49,7 @@ class Customer {
     }
 
     public void addRental(Movie movie, int daysRented) {
-        rentalList.add(new Rental(movie, daysRented));
+        rentals.add(new Rental(movie, daysRented));
     }
 
 
