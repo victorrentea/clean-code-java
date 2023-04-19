152c3f7a9e31423c4d2b955d0f92685354d33c4f
Victor Rentea <victorrentea@gmail.com>
2023-04-19 22:27:03 +0300

    Fix it on monday

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -24,38 +24,40 @@ class SomeController {
 
 class SomeService {
   public void blueMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task);
+    BooleanParameters.bigUglyMethod(id, task, false);
   }
 
   public void greenMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task);
+    BooleanParameters.bigUglyMethod(id, task, false);
   }
 
   public void yellowMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task);
+    BooleanParameters.bigUglyMethod(id, task, false);
   }
 
   public void redMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task);
+    BooleanParameters.bigUglyMethod(id, task, false);
   }
 }
 
 class MyService {
-  public void useCase323(int id, Task task) {
+  public void useCase(int id, Task task) {
     // TODO The shared called method must execute logic specific for my use-case #323
-    BooleanParameters.bigUglyMethod(id, task);
+    BooleanParameters.bigUglyMethod(id, task, true);
   }
 }
 
 public class BooleanParameters {
 
   // Note: this method might be called from multiple places in the codebase ...
-  static void bigUglyMethod(int storeId, Task task) {
+  static void bigUglyMethod(int storeId, Task task, boolean cr323) {
     System.out.println("Cow Logic 1 " + task + " and " + storeId);
     System.out.println("Cow Logic 2 " + task);
     System.out.println("Cow Logic 3 " + task);
 
-    // System.out.println("Logic just for CR#323 : " + task);
+    if (cr323) {
+      System.out.println("Logic just for CR#323 : " + task);
+    }
 
     System.out.println("Donkey Logic 1 " + storeId);
     System.out.println("Donkey Logic 2 " + storeId);
@@ -68,28 +70,38 @@ public class BooleanParameters {
   // Lord gave us tests! 👌 TODO run tests
   public void bossLevel(boolean fluff, List<Task> tasks, boolean cr323) {
     int index = 0; // TODO move closer to usages in a safe way
-    int j = tasks.size();
+    int taskCount = tasks.size();
     System.out.println("Logic1");
     List<Integer> taskIds = new ArrayList<>();
     if (fluff) {
-      System.out.println("Logic3");
-      for (Task task : tasks) {
-        System.out.println("Validate " + task);
-        task.setStarted(true);
+      if (fluff) {
+        if (fluff) {
+          if (fluff) {
+            if (fluff) {
+              if (fluff) {
+                System.out.println("Logic3");
+                for (Task task : tasks) {
+                  System.out.println("Validate " + task);
+                  task.setStarted(true);
 
-        taskIds.add(task.getId());
+                  taskIds.add(task.getId());
 
-        if (cr323) { // TODO remove the boolean
-          System.out.println("My Logic: " + task);
+                  if (cr323) { // TODO remove the boolean
+                    System.out.println("My Logic: " + task);
+                  }
+
+                  index++;
+                  System.out.println("Audit task #" + index + ": " + task);
+                }
+                System.out.println("Logic6 " + taskCount);
+                System.out.println("Task Ids: " + taskIds);
+              } else {
+                System.out.println("Logic7 on fluff=false " + tasks);
+              }
+            }
+          }
         }
-
-        index++;
-        System.out.println("Audit task #" + index + ": " + task);
       }
-      System.out.println("Logic6 " + j);
-      System.out.println("Task Ids: " + taskIds);
-    } else {
-      System.out.println("Logic7 on fluff=false " + tasks);
     }
     System.out.println("Logic8");
   }
--- src/main/java/victor/training/cleancode/fp/StreamWrecks.java
+++ src/main/java/victor/training/cleancode/fp/StreamWrecks.java
@@ -6,6 +6,7 @@ import victor.training.cleancode.fp.support.Product;
 import victor.training.cleancode.fp.support.ProductRepo;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map.Entry;
 
@@ -16,16 +17,21 @@ public class StreamWrecks {
 
 	public List<Product> getFrequentOrderedProducts(List<Order> orders) {
 		return orders.stream()
-				.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
-				.flatMap(o -> o.getOrderLines().stream())
-				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)))
-				.entrySet()
-				.stream()
-				.filter(e -> e.getValue() >= 10)
-				.map(Entry::getKey)
-				.filter(p -> !p.isDeleted())
-				.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
-				.collect(toList());
+						.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
+
+						.flatMap(o -> o.getOrderLines().stream())
+
+						//				.map(Order::getOrderLines)
+						//				.flatMap(Collection::stream)
+
+						.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)))
+						.entrySet()
+						.stream()
+						.filter(e -> e.getValue() >= 10)
+						.map(Entry::getKey)
+						.filter(p -> !p.isDeleted())
+						.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
+						.collect(toList());
 	}
 }
 
