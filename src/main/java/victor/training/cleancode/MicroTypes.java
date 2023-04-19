8d3b8db05e23c5dac80f2174ef290259e126ff56
Victor Rentea <victorrentea@gmail.com>
2023-04-20 00:10:13 +0300

    microtypes to enrich semantics of the code

--- src/main/java/victor/training/cleancode/MicroTypes.java
+++ src/main/java/victor/training/cleancode/MicroTypes.java
@@ -2,6 +2,7 @@ package victor.training.cleancode;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.BiFunction;
@@ -12,32 +13,32 @@ import static java.util.stream.Collectors.joining;
 public class MicroTypes {
 
     //<editor-fold desc="Unknown source of data">
-    public Map<Long, Map<String, Integer>> extremeFP() {
+    public Map<CustomerId, List<ProductCount>> extremeFP() {
         Long customerId = 1L;
         Integer product1Count = 2;
         Integer product2Count = 4;
-        return Map.of(customerId, Map.of(
-                "Table", product1Count,
-                "Chair", product2Count
+        return Map.of(new CustomerId(customerId), List.of(
+                new ProductCount("Table", product1Count),
+                new ProductCount("Chair", product2Count)
         ));
-    }
-    //</editor-fold>
-
-    public static <K, V, R> Function<Entry<K, V>, R> entry(BiFunction<K, V, R> f) {
-        return e -> f.apply(e.getKey(), e.getValue());
     }
 
     @Test
     void lackOfAbstractions() {
-        Map<Long, Map<String, Integer>> map = extremeFP();
-        // Joke: try "var" above :)
+        Map<CustomerId, List<ProductCount>> map = extremeFP();
 
-        for (Long cid : map.keySet()) {
-            String pl = map.get(cid).entrySet().stream()
-                    .map(entry -> entry.getValue() + " pcs. of " + entry.getKey())
-                    //              .map(entry((k,v) -> ...))
+        for (CustomerId cid : map.keySet()) {
+            String pl = map.get(cid).stream()
+                    .map(productCount -> productCount.count() + " pcs. of " + productCount.name())
                     .collect(joining(", "));
             System.out.println("cid=" + cid + " got " + pl);
         }
+    }
+
+    //</editor-fold>
+    record CustomerId(Long id) {
+    }
+
+    record ProductCount(String name, Integer count) {
     }
 }
