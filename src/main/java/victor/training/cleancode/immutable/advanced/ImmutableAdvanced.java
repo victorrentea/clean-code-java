725386ede3b0e587b3a210195adc0559724456e8
Victor Rentea <victorrentea@gmail.com>
2023-04-20 17:35:39 +0300

    Java-only solutions for immutable collections

--- src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
+++ src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
@@ -1,5 +1,9 @@
 package victor.training.cleancode.immutable.advanced;
 
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -16,13 +20,14 @@ public class ImmutableAdvanced {
       // reasons to use immutable objects:
       // -
       // -
+      //      numbers.add(4); unmodifiableList is vulnerable to this mutation
 
       System.out.println("After: " + immutable);
    }
 
    private static void wilderness(Immutable immutable) {
       // dark deep logic
-      immutable.getNumbers().add(4);
+      //      immutable.getNumbers().add(4);
    }
 }
 
@@ -35,12 +40,19 @@ class Immutable {
 
    Immutable(int x, List<Integer> numbers, Other other) {
       this.x = x;
-      this.numbers = numbers;
+      this.numbers = numbers; // List.copyOf(numbers); // java 10, malloc 😞
       this.other = other;
    }
 
+   //   public List<Integer> getNumbers() {
+   //      return new ArrayList<>(numbers);
+   //      // 1) malloc
+   //      // 2) misleading to clients (an exception would be better)
+   //   }
+
    public List<Integer> getNumbers() {
-      return numbers;
+      return Collections.unmodifiableList(numbers); // a decorator over the original list that blocks any mutation
+      // without allocating any memory
    }
 
    public int getX() {
@@ -50,25 +62,11 @@ class Immutable {
    public Other getOther() {
       return other;
    }
-
    @Override
    public String toString() {
       return String.format("Immutable{x=%d, numbers=%s, other=%s}", x, numbers, other);
    }
 }
 
-class Other {
-   private int a;
-
-   public Other(int a) {
-      this.a = a;
-   }
-
-   public int getA() {
-      return a;
-   }
-
-   public void setA(int a) {
-      this.a = a;
-   }
+record Other(int a) {
 }
