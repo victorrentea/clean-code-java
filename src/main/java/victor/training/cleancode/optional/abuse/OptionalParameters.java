604e5360b65e8a2f23b7f914040722f804c5ab82
Victor Rentea <victorrentea@gmail.com>
2023-04-20 23:41:07 +0300

    Optional abuses?

--- src/main/java/victor/training/cleancode/optional/abuse/OptionalParameters.java
+++ src/main/java/victor/training/cleancode/optional/abuse/OptionalParameters.java
@@ -2,11 +2,6 @@ package victor.training.cleancode.optional.abuse;
 
 public class OptionalParameters {
 
-  private static void sendMessage(String recipient, String message) {
-    System.out.println("Resolve phone number for " + recipient);
-    System.out.println("Send message " + message);
-  }
-
   public void callers() {
     // without
     sendMessage("jdoe", "message");
@@ -17,9 +12,15 @@ public class OptionalParameters {
   }
 
   // ⬇⬇⬇⬇⬇⬇ utility / library code ⬇⬇⬇⬇⬇⬇
+
   public void sendTrackedMessage(String recipient, String message, String trackingRegistry) {
     sendMessage(recipient, message);
 
     System.out.println("Also notify the tracking registry : " + trackingRegistry);
+  }
+
+  private static void sendMessage(String recipient, String message) {
+    System.out.println("Resolve phone number for " + recipient);
+    System.out.println("Send message " + message);
   }
 }
--- src/main/java/victor/training/cleancode/optional/abuse/TrappedOptional.java
+++ src/main/java/victor/training/cleancode/optional/abuse/TrappedOptional.java
@@ -17,18 +17,32 @@ public class TrappedOptional {
 
   public void trappedOptional(MyDto dto) {
     MyEntity entity = new MyEntity();
-    Optional.ofNullable(dto.recipientPerson)
-            .map(String::toUpperCase)
-            .ifPresent(name -> {
-              entity.setRecipient(name);
-            });
+    // you start a chain of calls with Optional.ofNullable but you never return /pass out htat Optional
+    //    Optional.ofNullable(dto.recipientPerson)
+    //            .map(String::toUpperCase)
+    //            .ifPresent(name -> {
+    //              entity.setRecipient(name);
+    //            });
+
+    // better:
+    if (dto.recipientPerson != null) {
+      entity.setRecipient(dto.recipientPerson.toUpperCase());
+    }
+    // also: if the dto.recipientPerson would be Optional, back on first form
+
+    //    entity.setRecipient(Optional.ofNullable(dto.recipientPerson)
+    //            .map(String::toUpperCase)
+    //            .orElse(null));
   }
+
 
   public void trappedOptionalWithExternalSideEffect(MyDto dto) {
     Optional.ofNullable(dto.recipientPerson)
             .map(String::toUpperCase)
-            .ifPresent(name -> {
-              kafkaSend(name);
-            });
+            .ifPresent(this::kafkaSend);
+
+    if (dto.recipientPerson != null) {
+      kafkaSend(dto.recipientPerson.toUpperCase());
+    }
   }
 }
