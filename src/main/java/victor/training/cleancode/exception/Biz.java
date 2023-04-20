f4dfade1cc357c4e783dfff5d374236a4de10012
Victor Rentea <victorrentea@gmail.com>
2023-04-20 23:49:45 +0300

    diaper pattern

--- src/main/java/victor/training/cleancode/exception/Biz.java
+++ src/main/java/victor/training/cleancode/exception/Biz.java
@@ -6,6 +6,9 @@ import victor.training.cleancode.exception.model.Customer;
 import victor.training.cleancode.exception.model.MemberCard;
 import victor.training.cleancode.exception.model.Order;
 
+import java.io.IOException;
+import java.text.ParseException;
+
 @Service
 @RequiredArgsConstructor
 public class Biz {
@@ -13,12 +16,17 @@ public class Biz {
 
    public void applyDiscount(Order order, Customer customer) {
       System.out.println("START");
-      if (order.getOfferDate().before(config.getLastPromoDate())) { // TODO inside
-         System.out.println("APPLYING DISCOUNT");
-         Integer points = customer.getMemberCard().map(MemberCard::getFidelityPoints).orElse(0);
-         order.setPrice(order.getPrice() * (100 - 2 * points) / 100);
-      } else {
-         System.out.println("NO DISCOUNT");
+      try {
+         if (order.getOfferDate().before(config.getLastPromoDate())) { // TODO inside
+            System.out.println("APPLYING DISCOUNT");
+            Integer points = customer.getMemberCard().map(MemberCard::getFidelityPoints).orElse(0);
+            order.setPrice(order.getPrice() * (100 - 2 * points) / 100);
+         } else {
+            System.out.println("NO DISCOUNT");
+         }
+      } catch (Exception e) {
+         //
+         // TODO i sent an email to biz. they should answer...
       }
    }
 }
--- src/main/java/victor/training/cleancode/exception/Config.java
+++ src/main/java/victor/training/cleancode/exception/Config.java
@@ -15,15 +15,15 @@ import java.util.Properties;
 @Component
 public class Config {
 
-   public Date getLastPromoDate() {
-//      File file = new File("config.properties");
-//      SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-//      Properties properties = new Properties();
-//      try (FileReader reader = new FileReader(file)) {
-//         properties.load(reader);
-//      }
-//      return format.parse(properties.getProperty("last.promo.date"));
-      return new Date();
+   public Date getLastPromoDate() throws IOException, ParseException {
+      File file = new File("config.properties");
+      SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+      Properties properties = new Properties();
+      try (FileReader reader = new FileReader(file)) {
+         properties.load(reader);
+      }
+      return format.parse(properties.getProperty("last.promo.date"));
+      //      return new Date();
    }
 
 
