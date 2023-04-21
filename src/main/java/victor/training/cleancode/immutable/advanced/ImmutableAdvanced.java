2de61b52cf36d0e4080152a62d47fc37f7b1080a
Victor Rentea <victorrentea@gmail.com>
2023-04-21 21:50:20 +0300

    ImmutableList

--- src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
+++ src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
@@ -1,5 +1,7 @@
 package victor.training.cleancode.immutable.advanced;
 
+import com.google.common.collect.ImmutableList;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
@@ -10,7 +12,7 @@ public class ImmutableAdvanced {
    public static void main(String[] args) {
       List<Integer> numbers = Stream.of(1, 2, 3).collect(toList());
 
-      Immutable immutable = new Immutable(1, numbers, new Other(15));
+      Immutable immutable = new Immutable(1, ImmutableList.copyOf(numbers), new Other(15));
       System.out.println("Before: " + immutable);
 
 //      numbers.clear();
@@ -21,25 +23,30 @@ public class ImmutableAdvanced {
 
    private static void wilderness(Immutable immutable) {
       // dark deep logic
-//      immutable.getNumbers().add(1);
+      immutable.getNumbers().add(1);
    }
 }
 
 class Immutable { // SHALLOW IMMUTABLE
    private final int x;
-   private final List<Integer> numbers;
+   private final ImmutableList<Integer> numbers;
+   // #5
    private final Other other;
 
-   Immutable(int x, List<Integer> numbers, Other other) {
+   Immutable(int x, ImmutableList<Integer> numbers, Other other) {
       this.x = x;
       this.numbers = numbers; // List.copyOf(numbers); // #1 Java 11 - malloc
       this.other = other;
    }
-   public List<Integer> getNumbers() {
-//      return new ArrayList<>(numbers); // #2 copy on getter: - malloc - misleading
-      return Collections.unmodifiableList(numbers); // #3 immutable decorate: - changing the original list passed to ctor -> mutation
+
+   public ImmutableList<Integer> getNumbers() {
+      return numbers;
    }
 
+   //   public List<Integer> getNumbers() {
+////      return new ArrayList<>(numbers); // #2 copy on getter: - malloc - misleading
+//      return Collections.unmodifiableList(numbers); // #3 immutable decorate: - changing the original list passed to ctor -> mutation
+//   }
 //   public Stream<Integer> numbers() {
 //      return numbers.stream(); // #4 strange? - no .contains()
 //   }
