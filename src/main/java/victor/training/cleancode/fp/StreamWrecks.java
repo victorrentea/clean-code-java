bf23a35cd985f2c422927e68957e40fdd060c553
Victor Rentea <victorrentea@gmail.com>
2023-04-21 16:00:12 +0300

    Optional Intro DONE!

--- src/main/java/victor/training/cleancode/fp/StreamWrecks.java
+++ src/main/java/victor/training/cleancode/fp/StreamWrecks.java
@@ -16,16 +16,16 @@ public class StreamWrecks {
 
 	public List<Product> getFrequentOrderedProducts(List<Order> orders) {
 		return orders.stream()
-				.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
-				.flatMap(o -> o.getOrderLines().stream())
-				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)))
-				.entrySet()
-				.stream()
-				.filter(e -> e.getValue() >= 10)
-				.map(Entry::getKey)
-				.filter(p -> !p.isDeleted())
-				.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
-				.collect(toList());
+						.filter(o -> o.isActive() && o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
+						.flatMap(o -> o.getOrderLines().stream())
+						.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)))
+						.entrySet()
+						.stream()
+						.filter(e -> e.getValue() >= 10)
+						.map(Entry::getKey)
+						.filter(p -> !p.isDeleted())
+						.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
+						.collect(toList());
 	}
 }
 
--- src/main/java/victor/training/cleancode/optional/Optional_Intro.java
+++ src/main/java/victor/training/cleancode/optional/Optional_Intro.java
@@ -7,6 +7,7 @@ import victor.training.cleancode.exception.model.MemberCard;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @SuppressWarnings("ConstantConditions")
 public class Optional_Intro {
@@ -14,24 +15,27 @@ public class Optional_Intro {
 		// test: 60, 10, no MemberCard
 		System.out.println(getDiscountLine(new Customer(new MemberCard(60))));
 		System.out.println(getDiscountLine(new Customer(new MemberCard(1))));
+		System.out.println(getDiscountLine(new Customer()));
 	}
 
 	public static String getDiscountLine(Customer customer) {
-		Discount discount = computeDiscount(customer.getMemberCard());
-		if (discount != null)
-			return "You got a discount of %" +
-				   discount.getGlobalPercentage();
-		else return "";
+		return customer.getMemberCard()
+						.flatMap(Optional_Intro::computeDiscount)
+						.map(Discount::getGlobalPercentage)
+						.map(per -> "You got a discount of %" + per)
+						.orElse("");
 	}
 
-	private static Discount computeDiscount(MemberCard card) {
+	// Optional in Java was added to let the callers know that might receive nothing (empty()) nstead of a NULL.
+	// and avoid NPE
+	private static Optional<Discount> computeDiscount(MemberCard card) {
 		if (card.getFidelityPoints() >= 100) {
-			return new Discount(5);
+			return Optional.of(new Discount(5));
 		}
 		if (card.getFidelityPoints() >= 50) {
-			return new Discount(3);
+			return Optional.of(new Discount(3));
 		}
-		return null;
+		return Optional.empty();
 	}
 
 	@Data
