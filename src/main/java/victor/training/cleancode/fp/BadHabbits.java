811ca2f3719cdf3e959864e21e30e197442f7b5c
Victor Rentea <victorrentea@gmail.com>
2023-04-24 11:02:23 +0300

    yolo

---
+++ src/main/java/victor/training/cleancode/fp/BadHabbits.java
@@ -0,0 +1,20 @@
+package victor.training.cleancode.fp;
+
+import lombok.RequiredArgsConstructor;
+
+public class BadHabbits {
+
+  private static final String SPACE = " ";
+
+}
+
+
+
+@RequiredArgsConstructor
+class X {
+  private final int i;
+
+//  X(int i, int i,int i,int i,int i,int i,int i) { generat ctor pt orice camp final
+//    this.i = i;
+//  }
+}
