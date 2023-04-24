20e18cb56277567743c15aa5acfce234299a7f6b
Kristjan <arnasonkris@gmail.com>
2023-04-24 13:07:23 -0600

    Extract method

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -18,43 +18,49 @@ class Customer {
 		return name;
 	}
 
-	public String statement() {
+	public String getReceipt() {
 		double totalPrice = 0;
 		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
-		for (Movie each : rentals.keySet()) {
-			double price = 0;
-			// determine amounts for each line
-			int daysRented = rentals.get(each);
-			switch (each.category()) {
-				case REGULAR:
-					price += 2;
-					if (daysRented > 2)
-						price += (daysRented - 2) * 1.5;
-					break;
-				case NEW_RELEASE:
-					price += daysRented * 3;
-					break;
-				case CHILDRENS:
-					price += 1.5;
-					if (daysRented > 3)
-						price += (daysRented - 3) * 1.5;
-					break;
-			}
+		for (Movie movie : rentals.keySet()) {
+			// determine amounts for movie line
+			int daysRented = rentals.get(movie);
+			double price = calculatePrice(movie, daysRented);
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if (each.category() != null &&
-				 (each.category() == MovieCategory.NEW_RELEASE)
+			if ((movie.category() == MovieCategory.NEW_RELEASE)
 				 && daysRented > 1)
 				frequentRenterPoints++;
 			// show figures line for this rental
-			result += "\t" + each.title() + "\t" + price + "\n";
+			result += "\t" + movie.title() + "\t" + price + "\n";
 			totalPrice += price;
 		}
 		// add footer lines
 		result += "Amount owed is " + totalPrice + "\n";
 		result += "You earned " + frequentRenterPoints + " frequent renter points";
 		return result;
+	}
+
+	private double calculatePrice(Movie movie, int daysRented) {
+		double price;
+		switch (movie.category()) {
+			case REGULAR:
+				price = 2;
+				if (daysRented > 2)
+					price += (daysRented - 2) * 1.5;
+				break;
+			case NEW_RELEASE:
+				price = daysRented * 3;
+				break;
+			case CHILDRENS:
+				price = 1.5;
+				if (daysRented > 3)
+					price += (daysRented - 3) * 1.5;
+				break;
+			default:
+				throw new IllegalStateException("Unexpected value: " + movie.category());
+		}
+		return price;
 	}
 }
