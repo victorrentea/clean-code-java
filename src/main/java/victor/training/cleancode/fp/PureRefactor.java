470192445c570667037b53190b07ea21be57869f
Victor Rentea <victorrentea@gmail.com>
2023-04-25 10:32:53 +0300

    refactor to pure functions

--- src/main/java/victor/training/cleancode/fp/PureRefactor.java
+++ src/main/java/victor/training/cleancode/fp/PureRefactor.java
@@ -1,14 +1,14 @@
 package victor.training.cleancode.fp;
 
+import com.google.common.annotations.VisibleForTesting;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import victor.training.cleancode.fp.support.*;
 import victor.training.cleancode.fp.support.Product;
 import victor.training.cleancode.fp.support.ProductRepo;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RequiredArgsConstructor
 class PureRefactor {
@@ -18,28 +18,54 @@ class PureRefactor {
   private final ProductRepo productRepo;
 
   // TODO extract a pure function with as much logic possible
-  public Map<Long, Double> computePrices(long customerId, List<Long> productIds, Map<Long, Double> internalPrices) {
+  public Map<Long, Double> computePrices(long customerId, List<Long> productIds,
+                                         Map<Long, Double> internalPrices) {
     Customer customer = customerRepo.findById(customerId);
     List<Product> products = productRepo.findAllById(productIds);
+    Map<Long, Double> allPrices = resolveAllPrices(internalPrices, products);
 
-    List<Coupon> usedCoupons = new ArrayList<>();
-    Map<Long, Double> finalPrices = new HashMap<>();
+    PriceCalculationResult result = doComputePrices(customer, products, allPrices);
+
+    couponRepo.markUsedCoupons(customerId, result.usedCoupons());
+    return result.finalPrices();
+  }
+
+  private Map<Long, Double> resolveAllPrices(Map<Long, Double> internalPrices, List<Product> products) {
+    Map<Long, Double> allPrices = new HashMap<>();
     for (Product product : products) {
       Double price = internalPrices.get(product.getId());
       if (price == null) {
         price = thirdPartyPrices.fetchPrice(product.getId());
       }
+      allPrices.put(product.getId(), price);
+    }
+    return allPrices;
+  }
+
+  @NotNull
+  @VisibleForTesting // daca sonaru te prinde ca o chemi din afara clasei asteia in prod
+  // TZIPA SONARU
+   static PriceCalculationResult doComputePrices(Customer customer,
+                                                        List<Product> products,
+                                                        Map<Long, Double> allPrices) {
+    Map<Long, Double> finalPrices = new HashMap<>();
+    List<Coupon> usedCoupons = new ArrayList<>();
+    for (Product product : products) {
+      Double price  = allPrices.get(product.getId());
       for (Coupon coupon : customer.getCoupons()) {
-        if (coupon.autoApply() && coupon.isApplicableFor(product) && !usedCoupons.contains(coupon)) {
+        if (coupon.autoApply()
+            && coupon.isApplicableFor(product)
+            && !usedCoupons.contains(coupon)) {
           price = coupon.apply(product, price);
           usedCoupons.add(coupon);
         }
       }
       finalPrices.put(product.getId(), price);
     }
+    return new PriceCalculationResult(usedCoupons, finalPrices);
+  }
 
-    couponRepo.markUsedCoupons(customerId, usedCoupons);
-    return finalPrices;
+  private record PriceCalculationResult(List<Coupon> usedCoupons, Map<Long, Double> finalPrices) {
   }
 }
 
