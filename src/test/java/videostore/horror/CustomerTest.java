8291e5a239533e0ee75826a31a691e205d66b435
Victor Rentea <victorrentea@gmail.com>
2023-04-28 18:26:08 +0300

    END

--- src/test/java/videostore/horror/CustomerTest.java
+++ src/test/java/videostore/horror/CustomerTest.java
@@ -14,15 +14,13 @@ public class CustomerTest {
         customer.addRental(new Movie("Star Wars", PriceFactor.NEW_RELEASE), 6);
         customer.addRental(new Movie("Sofia", PriceFactor.CHILDREN), 7);
         customer.addRental(new Movie("Inception", PriceFactor.REGULAR), 5);
-        
-        String expected = """
+
+        assertThat(customer.statement()).isEqualToIgnoringNewLines("""
                 Rental Record for John Doe
                 	Star Wars	18.0
                 	Sofia	7.5
                 	Inception	6.5
                 Amount owed is 32.0
-                You earned 4 frequent renter points""";
-        
-        assertThat(customer.statement()).isEqualToIgnoringNewLines(expected);
+                You earned 4 frequent renter points""");
     }
 }
