8f50ad8cbbe40cbb23edece928a40df92cfcc55a
Victor Rentea <victorrentea@gmail.com>
2023-04-28 17:53:15 +0300

    text blocks ftw in annotations @Query, tests for JSONs..

--- src/test/java/videostore/horror/CustomerTest.java
+++ src/test/java/videostore/horror/CustomerTest.java
@@ -15,12 +15,13 @@ public class CustomerTest {
         customer.addRental(new Movie("Sofia", PriceFactor.CHILDREN), 7);
         customer.addRental(new Movie("Inception", PriceFactor.REGULAR), 5);
         
-        String expected = "Rental Record for John Doe\n"
-                + "	Star Wars	18.0\n"
-                + "	Sofia	7.5\n"
-                + "	Inception	6.5\n"
-                + "Amount owed is 32.0\n"
-                + "You earned 4 frequent renter points";
+        String expected = """
+                Rental Record for John Doe
+                	Star Wars	18.0
+                	Sofia	7.5
+                	Inception	6.5
+                Amount owed is 32.0
+                You earned 4 frequent renter points""";
         
         assertThat(customer.statement()).isEqualToIgnoringNewLines(expected);
     }
