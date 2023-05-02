9f32e5e70679b41dbfc11bd1edefb769b2e1b508
Victor Rentea <victorrentea@gmail.com>
2023-05-02 17:19:42 +0300

    acum o instanta de Immutable este DEEP immutable pe bune, nu doar SHALLOW immutable

--- src/main/java/victor/training/cleancode/immutable/basic/ImmutableBasic.java
+++ src/main/java/victor/training/cleancode/immutable/basic/ImmutableBasic.java
@@ -1,5 +1,6 @@
 package victor.training.cleancode.immutable.basic;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -14,18 +15,30 @@ public class ImmutableBasic {
       System.out.println("Before:" + immutable);
 
       // LOTS OF BUSINESS LOGIC HERE
+      imagineDragons(immutable);
 
       System.out.println("After: " + immutable);
    }
 
    private static void imagineDragons(Immutable immutable) {
       // 1000 loc de biz logic horror. d-ala de intrebi bizu si el zice je ne sais pas
-      immutable.getNumbers().add(-1);
+
+      immutable.getNumbers().add(-1); // face degeaba add. isi ia tzeapa
+      System.out.println("ma astept ca intoarce: " +
+                         immutable.getNumbers().contains(-1));
+
+      int xuMeu = 7;
+      //      immutable.getNumbers().contains(xuMeu);
+      //      immutable.getNumbers().stream();
+      //      for (int i = 0; i < immutable.getNumbersCount(); i++) {
+      //      }
+      //      immutable.
    }
 }
 
 // unei clase imutabile nu ii poti schimba starea dupa ce ai instantiat-o
-final class Immutable {
+
+class Immutable {
    private final int x;
    private final List<Integer> numbers;
    private final Other other;
@@ -40,8 +53,29 @@ final class Immutable {
       return x;
    }
 
-   public List<Integer> getNumbers() {
-      return numbers;
+   // 1) prea mult?
+   //   public Integer getNumber(int index) {
+   //      return numbers.get(index);
+   //   }
+   //   public int getNumbersCount() {
+   //      return numbers.size();
+   //   }
+
+   // 2) ii dam o copie
+   // - misleading pt client: de ce il lasi sa creada ca poate modifica?
+   // - malloc
+   //   public List<Integer> getNumbers() {
+   //      return new ArrayList<>(numbers);
+   //   }
+
+
+   // 3) Decorator Pattern (GoF): o instanta care imbraca lista originala,
+   // delegand toate apelurile de citire la lista originala,
+   // dar blocand apelurile de scriere
+   // + nu aloca
+   // + crapa clientul, nu-l lasi sa creada ca poate modifica
+   public List<Integer> getNumbers() { // pick this for normal cases + @Entity de Hibernate
+      return Collections.unmodifiableList(numbers);
    }
 
    public Other getOther() {
@@ -54,7 +88,7 @@ final class Immutable {
 }
 
 class Other {
-   private int a;
+   private final int a;
 
    public Other(int a) {
       this.a = a;
@@ -63,8 +97,24 @@ class Other {
    public int getA() {
       return a;
    }
-
-   public void setA(int a) {
-      this.a = a;
-   }
+   //
+   //   public void setA(int a) {
+   //      this.a = a;
+   //   }
 }
+
+
+// de ce clasa sa fie finala
+//class DusmanDeClasa extends Immutable {
+//
+//   private final List<Integer> copiaMea;
+//
+//   DusmanDeClasa(int x, List<Integer> numbers, Other other) {
+//      super(x, numbers, other);
+//      this.copiaMea = numbers;
+//   }
+//
+//   public void tioArd(Integer vreauEu) {
+//      copiaMea.add(vreauEu);
+//   }
+//}
