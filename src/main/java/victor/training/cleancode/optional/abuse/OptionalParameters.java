6ee05a592dacd474b9f64ec4c10702f5d349a192
Victor Rentea <victorrentea@gmail.com>
2023-05-03 12:41:33 +0300

    opt<> param -> split function

--- src/main/java/victor/training/cleancode/optional/abuse/OptionalParameters.java
+++ src/main/java/victor/training/cleancode/optional/abuse/OptionalParameters.java
@@ -1,26 +1,24 @@
 package victor.training.cleancode.optional.abuse;
 
-import victor.training.cleancode.exception.model.MemberCard;
-import victor.training.cleancode.optional.Optional_Intro.Discount;
-
-import java.util.Optional;
-
 public class OptionalParameters {
 
   public void callers() {
     // without
-    sendMessage("jdoe", "message", Optional.empty());
+    sendMessage("jdoe", "message");
 
     // with
-    sendMessage("jdoe", "message", Optional.of("REGLISS"));
+    sendTrackedMessage("jdoe", "message", "REGLISS");
 
   }
 
   // ⬇⬇⬇⬇⬇⬇ utility / library code ⬇⬇⬇⬇⬇⬇
-  public void sendMessage(String recipient, String message, Optional<String> trackingRegistry) {
+  public void sendTrackedMessage(String recipient, String message, String trackingRegistry) {
+    sendMessage(recipient, message);
+    System.out.println("Also notify the tracking registry : " + trackingRegistry);
+  }
+
+  public void sendMessage(String recipient, String message) {
     System.out.println("Resolve phone number for " + recipient);
     System.out.println("Send message " + message);
-
-    trackingRegistry.ifPresent(reg -> System.out.println("Also notify the tracking registry : " + reg));
   }
 }
