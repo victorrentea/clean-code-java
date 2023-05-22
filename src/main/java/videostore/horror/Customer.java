0f54e6ce0d9ee0d17a6db7765e59e717cdfd886d
Corneliu-Iosif Voiculescu <Corneliu-Iosif.Voiculescu@lseg.com>
2023-05-22 16:08:45 +0300

    added renter points method

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -1,71 +1,55 @@
 package videostore.horror;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import static videostore.horror.Movie.PriceCode.NEW_RELEASE;
 
 
 class Customer {
-	private final String name;
-	private final List<MovieRental> rentals = new ArrayList<>(); // preserves order
+    private final String name;
+    private final List<MovieRental> rentals = new ArrayList<>(); // preserves order
 
-	public Customer(String name) {
-		this.name = name;
-	};
+    public Customer(String name) {
+        this.name = name;
+    }
 
-	public void addRental(Movie movie, int daysRented) {
-		rentals.add(new MovieRental(movie, daysRented));
-	}
+    public void addRental(Movie movie, int daysRented) {
+        rentals.add(new MovieRental(movie, daysRented));
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		String result = "Rental Record for " + getName() + "\n";
-		for (MovieRental rental : rentals) {
-// determine amounts for every line
-			int daysRented = rental.daysRented();
-			double moviePrice = calculateMoviePrice(rental.movie(), daysRented);
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        String result = "Rental Record for " + getName() + "\n";
+        for (MovieRental rental : rentals) {
+
+            double moviePrice = rental.calculateMoviePrice();
 // add frequent renter points
-			frequentRenterPoints++;
-
-			if ((rental.movie().priceCode() == NEW_RELEASE)
-					&& daysRented >= 2) {
-				frequentRenterPoints++;
-			}
+            var movieRenterPoints = calculateRenterPoints(rental);
+            frequentRenterPoints += movieRenterPoints;
 // show figures line for this rental
-			result += "\t" + rental.movie().title() + "\t" + moviePrice + "\n";
-			totalAmount += moviePrice;
-		}
+            result += "\t" + rental.movie().title() + "\t" + moviePrice + "\n";
+            totalAmount += moviePrice;
+        }
 // add footer lines
-		result += "Amount owed is " + totalAmount + "\n";
-		result += "You earned " + frequentRenterPoints + " frequent renter points";
-		return result;
-	}
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        return result;
+    }
 
-	private static double calculateMoviePrice(Movie each, int dr) {
-		double result = 0.0;
-		switch (each.priceCode()) {
-			case REGULAR:
-				result += 2;
-				if (dr > 2)
-					result += (dr - 2) * 1.5;
-				break;
-			case NEW_RELEASE:
-				result += dr * 3;
-				break;
-			case CHILDREN:
-				result += 1.5;
-				if (dr > 3)
-					result += (dr - 3) * 1.5;
-				break;
-		}
-		return result;
-	}
+    private static int calculateRenterPoints(MovieRental rental) {
+        int result = 1;
+
+        if ((rental.movie().priceCode() == NEW_RELEASE)
+                && rental.daysRented() >= 2) {
+            result++;
+        }
+        return result;
+    }
+
 }
