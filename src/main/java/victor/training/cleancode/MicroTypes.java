387942dfb9ce8af1abcb659c24adc2893cd7fc7e
Victor Rentea <victorrentea@gmail.com>
2023-05-22 10:17:50 +0300

    microtypes

--- src/main/java/victor/training/cleancode/MicroTypes.java
+++ src/main/java/victor/training/cleancode/MicroTypes.java
@@ -1,7 +1,9 @@
 package victor.training.cleancode;
 
+import lombok.Value;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static java.util.stream.Collectors.joining;
@@ -9,27 +11,38 @@ import static java.util.stream.Collectors.joining;
 public class MicroTypes {
 
   //<editor-fold desc="fetchData()">
-  public Map<Long, Map<String, Integer>> fetchData() {
+  public Map<CustomerId, List<ProductCount>> fetchData() {
     Long customerId = 1L;
     Integer product1Count = 2;
     Integer product2Count = 4;
-    return Map.of(customerId, Map.of(
-            "Table", product1Count,
-            "Chair", product2Count
+    return Map.of(new CustomerId(customerId), List.of(
+            new ProductCount("Table", product1Count),
+            new ProductCount("Chair", product2Count)
     ));
   }
   //</editor-fold>
+  @Value
+  class CustomerId {
+    long id;
+  }
+
+  @Value
+  class ProductCount {
+    String productName;
+    int count;
+  }
+
 
   @Test
   void lackOfAbstractions() {
-    Map<Long, Map<String, Integer>> map = fetchData();
+    Map<CustomerId, List<ProductCount>> customerToProductCounts = fetchData();
     // Joke: try "var" above 🤪
 
-    for (Long cid : map.keySet()) {
-      String pl = map.get(cid).entrySet().stream()
-              .map(entry -> entry.getValue() + " pcs. of " + entry.getKey())
+    for (CustomerId cid : customerToProductCounts.keySet()) {
+      String pl = customerToProductCounts.get(cid).stream()
+              .map(entry -> entry.getCount() + " pcs. of " + entry.getProductName())
               .collect(joining(", "));
-      System.out.println("cid=" + cid + " got " + pl);
+      System.out.println("cid=" + cid.id + " got " + pl);
     }
   }
 }
