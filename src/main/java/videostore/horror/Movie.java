5789762b502e234d3439a5128c2f19f2a9aa49d8
Corneliu-Iosif Voiculescu <Corneliu-Iosif.Voiculescu@lseg.com>
2023-05-22 15:28:14 +0300

    Migrated to enum

--- src/main/java/videostore/horror/Movie.java
+++ src/main/java/videostore/horror/Movie.java
@@ -1,35 +1,28 @@
 package videostore.horror;
+
 public class Movie {
 
-	enum PriceCode {
-		CHILDREN (2),
-		REGULAR (0),
-		NEW_RELEASE (1);
+    enum PriceCode {
+        CHILDREN,
+        REGULAR,
+        NEW_RELEASE;
 
-		private final int index;
+    }
 
-		PriceCode(int i) {
-			index = i;
-		}
-	}
+    private final String title;
+    private final PriceCode priceCode;
 
-	public static final int CHILDREN = 2;
-	public static final int REGULAR = 0;
-	public static final int NEW_RELEASE = 1;
-	private final String title;
-	private final int priceCode;
+    public Movie(String title, PriceCode priceCode) {
+        this.title = title;
+        this.priceCode = priceCode;
+    }
 
-	public Movie(String title, PriceCode priceCode) {
-		this.title = title;
-		this.priceCode = priceCode;
-	}
-
-	public int getPriceCode() {
-		return priceCode;
-	}
+    public PriceCode getPriceCode() {
+        return priceCode;
+    }
 
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 }
--- src/test/java/videostore/horror/CustomerTest.java
+++ src/test/java/videostore/horror/CustomerTest.java
@@ -3,6 +3,7 @@ package videostore.horror;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static videostore.horror.Movie.PriceCode.*;
 
 
 public class CustomerTest {
@@ -10,17 +11,17 @@ public class CustomerTest {
     @Test
     public void characterizationTest() {
         Customer customer = new Customer("John Doe");
-        customer.addRental(new Movie("Star Wars", Movie.NEW_RELEASE), 6);
-        customer.addRental(new Movie("Sofia", Movie.CHILDREN), 7);
-        customer.addRental(new Movie("Inception", Movie.REGULAR), 5);
-        
+        customer.addRental(new Movie("Star Wars", NEW_RELEASE), 6);
+        customer.addRental(new Movie("Sofia", CHILDREN), 7);
+        customer.addRental(new Movie("Inception", REGULAR), 5);
+
         String expected = "Rental Record for John Doe\n"
                 + "	Star Wars	18.0\n"
                 + "	Sofia	7.5\n"
                 + "	Inception	6.5\n"
                 + "Amount owed is 32.0\n"
                 + "You earned 4 frequent renter points";
-        
+
         assertEquals(expected, customer.statement());
     }
 }
