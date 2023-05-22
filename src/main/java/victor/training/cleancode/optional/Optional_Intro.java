0bd8b11618bebf722a3e180f6137fb24b1ded9a6
Victor Rentea <victorrentea@gmail.com>
2023-05-22 10:44:14 +0300

    fixed with Optional like pro

--- src/main/java/victor/training/cleancode/optional/Optional_Intro.java
+++ src/main/java/victor/training/cleancode/optional/Optional_Intro.java
@@ -11,36 +11,43 @@ import java.util.Optional;
 
 @SuppressWarnings("ConstantConditions")
 public class Optional_Intro {
-	public static void main(String[] args) {
-		// test: 60, 10, no MemberCard
-		System.out.println(getDiscountLine(new Customer(new MemberCard(60))));
-		System.out.println(getDiscountLine(new Customer(new MemberCard(1))));
-		System.out.println(getDiscountLine(new Customer(null)));
-	}
+  public static void main(String[] args) {
+    // test: 60, 10, no MemberCard
+    System.out.println(getDiscountLine(new Customer(new MemberCard(60))));
+    System.out.println(getDiscountLine(new Customer(new MemberCard(1))));
+    System.out.println(getDiscountLine(new Customer(null)));
+  }
 
-	public static String getDiscountLine(Customer customer) {
-//		if (opt.isPresent()) de evitat
-		return computeDiscount(customer.getMemberCard())
-				.map(d -> "You got a discount of %" + d.getGlobalPercentage())
+  public static String getDiscountLine(Customer customer) {
+//		return computeDiscount(customer.getMemberCard())
+//				.map(d -> "You got a discount of %" + d.getGlobalPercentage())
+//				.orElse("");
+
+    return customer.getMemberCard()
+        .flatMap(Optional_Intro::computeDiscount)
+
+//				.map(d -> "You got a discount of %" + d.getGlobalPercentage())
+
+				.map(Discount::getGlobalPercentage)
+				.map(p -> "You got a discount of %" + p)
+
 				.orElse("");
-	}
+  }
 
-	private static Optional<Discount> computeDiscount(MemberCard card) {
-		if (card == null) {
-			return Optional.empty();
-		}
-		if (card.getFidelityPoints() >= 100) {
-			return Optional.of(new Discount(5));
-		}
-		if (card.getFidelityPoints() >= 50) {
-			return Optional.of(new Discount(3));
-		}
-		return Optional.empty();
-	}
-	@Data
-	public static class Discount {
-		private final int globalPercentage;
-		private Map<String, Integer> categoryDiscounts = new HashMap<>();
-	}
+  private static Optional<Discount> computeDiscount(MemberCard card) {
+    if (card.getFidelityPoints() >= 100) {
+      return Optional.of(new Discount(5));
+    }
+    if (card.getFidelityPoints() >= 50) {
+      return Optional.of(new Discount(3));
+    }
+    return Optional.empty();
+  }
+
+  @Data
+  public static class Discount {
+    private final int globalPercentage;
+    private Map<String, Integer> categoryDiscounts = new HashMap<>();
+  }
 }
 
