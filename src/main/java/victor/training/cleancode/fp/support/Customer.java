b9e92f6f958166722b367ce03ff26a3b0d06d689
Victor Rentea <victorrentea@gmail.com>
2023-09-04 09:13:04 +0300

    cleanup

--- src/main/java/victor/training/cleancode/fp/support/Customer.java
+++ src/main/java/victor/training/cleancode/fp/support/Customer.java
@@ -1,7 +1,6 @@
 package victor.training.cleancode.fp.support;
 
 import lombok.Data;
-import org.apache.tomcat.jni.Address;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -14,7 +13,6 @@ class Customer {
   private String name;
   private String email;
   private List<String> labels = new ArrayList<>();
-  private Address address = new Address();
   private Date createDate;
   private List<Coupon> coupons = new ArrayList<>();
 }
