443bafd5812714c4a940d7eb50aba73018b1c0f7
Victor Rentea <victorrentea@gmail.com>
2023-09-05 11:35:54 +0300

    carpit!

--- src/main/java/victor/training/cleancode/optional/Optional_Intro.java
+++ src/main/java/victor/training/cleancode/optional/Optional_Intro.java
@@ -7,26 +7,34 @@ import victor.training.cleancode.exception.model.MemberCard;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @SuppressWarnings("ConstantConditions")
 public class Optional_Intro {
 	public static void main(String[] args) {
 		// test: 60, 10, no MemberCard
 		System.out.println(getDiscountLine(new Customer(new MemberCard(60))));
+		System.out.println(getDiscountLine(new Customer(new MemberCard(1))));
+		System.out.println(getDiscountLine(new Customer()));
 	}
 
 	public static String getDiscountLine(Customer customer) {
-		return "You got a discount of %" + computeDiscount(customer.getMemberCard()).getGlobalPercentage();
+		return computeDiscount(customer.getMemberCard())
+				.map(discount -> "You got a discount of %" + discount.getGlobalPercentage())
+				.orElse("Daca ai fi avut pct de fidelitate, iti putea lua discount :)");
 	}
 
-	private static Discount computeDiscount(MemberCard card) {
+	private static Optional<Discount> computeDiscount(MemberCard card) {
+		if (card == null) {
+			return Optional.empty();
+		}
 		if (card.getFidelityPoints() >= 100) {
-			return new Discount(5);
+			return Optional.of(new Discount(5));
 		}
 		if (card.getFidelityPoints() >= 50) {
-			return new Discount(3);
+			return Optional.of(new Discount(3));
 		}
-		return null;
+		return Optional.empty();
 	}
 	@Data
 	public static class Discount {
