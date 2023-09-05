3b3f1a679a8e95bc0bcb99e92f567a0d92b2e2ce
Sorin Fagateanu <sorin-mihai.fagateanu@ing.com>
2023-09-05 14:41:24 +0300

    - first push

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -4,7 +4,27 @@ package videostore.horror;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import static victor.training.cleancode.Movie.Category.*;
+
+enum X {
+	CHILDREN(2),
+	REGULAR(0),
+	NEW_RELEASE(1);
+
+
+	private final int i;
+
+	X(int i) {
+		this.i = i;
+	}
+
+	public int getI() {
+		return i;
+	}
+}
+
 class Customer {
+
 	private String name;
 
 	private Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order
@@ -30,15 +50,15 @@ class Customer {
 			// determine amounts for every line
 			int dr = rentals.get(each);
 			switch (each.getPriceCode()) {
-				case Movie.REGULAR:
+				case REGULAR:
 					thisAmount += 2;
 					if (dr > 2)
 						thisAmount += (dr - 2) * 1.5;
 					break;
-				case Movie.NEW_RELEASE:
+				case NEW_RELEASE:
 					thisAmount += dr * 3;
 					break;
-				case Movie.CHILDRENS:
+				case CHILDREN:
 					thisAmount += 1.5;
 					if (dr > 3)
 						thisAmount += (dr - 3) * 1.5;
@@ -48,7 +68,7 @@ class Customer {
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
 			if (each.getPriceCode() != null &&
-				 (each.getPriceCode() == Movie.NEW_RELEASE)
+				 (each.getPriceCode() == X.NEW_RELEASE.getI())
 				 && dr > 1)
 				frequentRenterPoints++;
 			// show figures line for this rental
--- src/main/java/videostore/horror/Movie.java
+++ src/main/java/videostore/horror/Movie.java
@@ -1,25 +1,21 @@
 package videostore.horror;
+
+
 public class Movie {
-	public static final int CHILDRENS = 2;
-	public static final int REGULAR = 0;
-	public static final int NEW_RELEASE = 1;
-	private String _title;
-	private Integer _priceCode;
+
+	private final String title;
+	private final Integer priceCode;
 
 	public Movie(String title, Integer priceCode) {
-		_title = title;
-		_priceCode = priceCode;
+		this.title = title;
+		this.priceCode = priceCode;
 	}
 
 	public Integer getPriceCode() {
-		return _priceCode;
-	}
-
-	public void setPriceCode(Integer arg) {
-		_priceCode = arg;
+		return priceCode;
 	}
 
 	public String getTitle() {
-		return _title;
+		return title;
 	};
 }
