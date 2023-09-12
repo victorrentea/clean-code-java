4453a698fee3841adfbd731eebde34cdc39507a1
Victor Rentea <victorrentea@gmail.com>
2023-09-12 12:01:57 +0300

    am redus adancimea functie cu 1 distrugand un if top-level

--- src/test/java/victor/training/cleancode/BooleanParametersTest.java
+++ src/test/java/victor/training/cleancode/BooleanParametersTest.java
@@ -17,7 +17,7 @@ class BooleanParametersTest {
    @Test
    @CaptureSystemOutput
    void bossLevelFluff(OutputCapture outputCapture) {
-      target.bossLevel(true, List.of(new Task(5)), false);
+      target.bossLevelFluff(List.of(new Task(5)), false);
 
       assertThat(outputCapture.toString())
           .isEqualToIgnoringNewLines("Logic1\n" +
@@ -31,7 +31,7 @@ class BooleanParametersTest {
    @Test
    @CaptureSystemOutput
    void bossLevelFluff_c323(OutputCapture outputCapture) {
-      target.bossLevel(true, List.of(new Task(5)), true);
+      target.bossLevelFluff(List.of(new Task(5)), true);
 
       assertThat(outputCapture.toString())
           .isEqualToIgnoringNewLines("Logic1\n" +
@@ -47,7 +47,7 @@ class BooleanParametersTest {
    @Test
    @CaptureSystemOutput
    void bossLevelFluff_emptyList(OutputCapture outputCapture) {
-      target.bossLevel(true, Collections.emptyList(), false);
+      target.bossLevelFluff(Collections.emptyList(), false);
 
       assertThat(outputCapture.toString())
           .isEqualToIgnoringNewLines("Logic1\n" +
@@ -59,7 +59,7 @@ class BooleanParametersTest {
    @Test
    @CaptureSystemOutput
    void bossLevelFalse(OutputCapture outputCapture) {
-      target.bossLevel(false, List.of(new Task(5)), false);
+      target.bossLevelNoFluff(List.of(new Task(5)));
 
       assertThat(outputCapture.toString())
           .isEqualToIgnoringNewLines("Logic1\n" +
@@ -70,7 +70,7 @@ class BooleanParametersTest {
    @Test
    @CaptureSystemOutput
    void stuffForEachElement_inWateverOrder(OutputCapture outputCapture) {
-      target.bossLevel(true, List.of(new Task(5),new Task(6)), false);
+      target.bossLevelFluff(List.of(new Task(5),new Task(6)), false);
 
       // we don't care in what order we validate or audit tasks
       assertThat(outputCapture.toString())
