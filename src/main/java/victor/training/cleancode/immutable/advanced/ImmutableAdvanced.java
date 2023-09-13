0f186031a9576c2dc7ee843cf710bd5a41c0f897
Victor Rentea <victorrentea@gmail.com>
2023-09-13 13:27:33 +0300

    Revert "FP inseamna obiecte full deep immutable, collectii imutabile si var/param FINALE (ne-reatribuite)"
    This reverts commit 44a2fe5a718426b05bb3ef088966c50334e0f225.

--- src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
+++ src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
@@ -1,80 +1,66 @@
 package victor.training.cleancode.immutable.advanced;
 
-import com.google.common.collect.ImmutableList;
+import lombok.Data;
 import lombok.Value;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
 
-import static com.google.common.collect.ImmutableList.toImmutableList;
 import static java.util.stream.Collectors.toList;
 
 public class ImmutableAdvanced {
    public static void main(String[] args) {
-      ImmutableList<Integer> numbers = Stream.of(1, 2, 3).collect(toImmutableList());
+      List<Integer> numbers = Stream.of(1, 2, 3).collect(toList());
 
       Immutable immutable = new Immutable(1, numbers, new Other(15));
       System.out.println("Before: " + immutable);
 
-//      numbers.clear(); // in practica e fff mic riscul asta.
+      numbers.clear(); // in practica e fff mic riscul asta.
+      wilderness(immutable);
+      // reasons to use immutable objects:
+      // -
+      // -
 
-      Immutable updated = wilderness(immutable); // temporal coupled to next line
-
-      System.out.println("After:  " + updated); // folsind var diferite (nu reciclam var) te ajuta compilerul sa te prinda
+      System.out.println("After:  " + immutable);
    }
 
-   private static Immutable wilderness(Immutable immutable) {
+   private static void wilderness(Immutable immutable) {
       // dark deep logic sute de linii de cod
-      // callerul se va prinde doar la runtime de eroare
-      // acum pe ImmutableList : deprecated
-//      immutable.numbers().add(-1); // hackuri POC
+//      immutable.getNumbers().add(-1); // hackuri POC
 //      immutable.getOther().setA()
-      if (immutable.numbers().size() > 2) {
-         return immutable.withX(7);
-      } else {
-         return immutable;
-      }
    }
 }
 // imutabil = obiect a carui stare nu o poti modifica dupa instantiere.
 // "shallow (pe primul nivel) vs deep(in jos pe toate altele ob referite)" clone, immutable, equals
+class Immutable {
+   private final Integer x;
+   private final List<Integer> numbers;
+   private final Other other;
 
-record Immutable(Integer x, ImmutableList<Integer> numbers, Other other) {
-   public Immutable withX(int newX) {
-      return new Immutable(newX, numbers, other);
+   Immutable(Integer x, List<Integer> numbers, Other other) {
+      this.x = x;
+      this.numbers = new ArrayList<>(numbers); // #3 te protejezi si de dusmani care tin pointer la colectia pe care ti-o dau
+      this.other = other;
    }
-   // +din semnatura declari ca vrei un subtip de List care nu poate fi mutat
-   // +deprecheaza .add .remove (mutatorii)
-   // -nu merge cu Hibernate. Dar DA cu Mongo,Cassandra,Jackson
+   public List<Integer> getNumbers() {
+//      return new ArrayList<>(numbers); // #1 -malloc -misleading ca nu crapa in client
+      return Collections.unmodifiableList(numbers); // #2 wrapper peste lista initiala care blocheaza orice modificare "decorator pattern"
+      // +nu malloc mult, +CRAPA
+   }
+   public Integer getX() {
+      return x;
+   }
+   public Other getOther() {
+      return other;
+   }
+
+   public String toString() {
+      return String.format("Immutable{x=%d, numbers=%s, other=%s}", x, numbers, other);
+   }
 }
-
-
-//class Immutable {
-//   private final Integer x;
-//   private final List<Integer> numbers;
-//   private final Other other;
-//
-//   Immutable(Integer x, ImmutableList<Integer> numbers, Other other) {
-//      this.x = x;
-//      this.numbers = new ArrayList<>(numbers); // #3 te protejezi si de dusmani care tin pointer la colectia pe care ti-o dau
-//      this.other = other;
-//   }
-//   public List<Integer> getNumbers() {
-////      return new ArrayList<>(numbers); // #1 -malloc -misleading ca nu crapa in client
-//      return Collections.unmodifiableList(numbers); // #2 wrapper peste lista initiala care blocheaza orice modificare "decorator pattern"
-//      // +nu malloc mult, +CRAPA
-//   }
-//   public Integer getX() {
-//      return x;
-//   }
-//   public Other getOther() {
-//      return other;
-//   }
-//
-//   public String toString() {
-//      return String.format("Immutable{x=%d, numbers=%s, other=%s}", x, numbers, other);
-//   }
-//}
 
 //record Other(int a) {}
 
