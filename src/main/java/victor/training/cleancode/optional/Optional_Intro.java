4ffa8c9280b994cd3eb43e25fcb09d09900ddb6c
Victor Rentea <victorrentea@gmail.com>
2023-09-28 01:18:39 +0300

    line breaks

--- src/main/java/victor/training/cleancode/optional/Optional_Intro.java
+++ src/main/java/victor/training/cleancode/optional/Optional_Intro.java
@@ -12,11 +12,13 @@ import java.util.Map;
 public class Optional_Intro {
 	public static void main(String[] args) {
 		// test: 60, 10, no MemberCard
-		System.out.println(getDiscountLine(new Customer(new MemberCard("bar", 60))));
+		System.out.println(getDiscountLine(new Customer(
+				new MemberCard("bar", 60))));
 	}
 
 	public static String getDiscountLine(Customer customer) {
-		return "You got a discount of %" + computeDiscount(customer.getMemberCard()).getGlobalPercentage();
+		Discount discount = computeDiscount(customer.getMemberCard());
+		return "You got a discount of %" + discount.getGlobalPercentage();
 	}
 
 	private static Discount computeDiscount(MemberCard card) {
