d8f31e0854ed45909b020051441124cb57ddb5cc
Victor Rentea <victorrentea@gmail.com>
2023-11-24 22:36:32 +0100

    reorganize

--- src/main/java/victor/training/cleancode/MicroTypes.java
+++ src/main/java/victor/training/cleancode/MicroTypes.java
@@ -1,9 +1,12 @@
 package victor.training.cleancode;
 
 import com.google.common.collect.Maps;
+import org.jooq.lambda.tuple.Tuple;
+import org.jooq.lambda.tuple.Tuple2;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import static java.util.stream.Collectors.joining;
@@ -34,15 +37,33 @@ public class MicroTypes {
     }
   }
 
-//  @Test
-//  void tuples() {
-//    Map<Long, Map<String, Integer>> map = fetchData().;
-//
-//    for (var e : map.entrySet()) {
-//      String pl = e.getValue().entrySet().stream()
-//          .map(entry -> entry.getValue() + " pcs. of " + entry.getKey())
-//          .collect(joining(", "));
-//      System.out.println("cid=" + e.getKey() + " got " + pl);
-//    }
-//  }
+
+
+
+
+
+  //<editor-fold desc="Tuple source of data">
+  public Map<Long, List<Tuple2<String, Integer>>> extremeFP() {
+    Long customerId = 1L;
+    Integer product1Count = 2;
+    Integer product2Count = 4;
+    return Map.of(customerId, List.of(
+        org.jooq.lambda.tuple.Tuple.tuple("Table", product1Count),
+        Tuple.tuple("Chair", product2Count)
+    ));
+  }
+  //</editor-fold>
+
+  @Test
+  void lackOfAbstractions() {
+    Map<Long, List<Tuple2<String, Integer>>> map = extremeFP();
+    // Joke: try "var" above :)
+
+    for (Long cid : map.keySet()) {
+      String pl = map.get(cid).stream()
+          .map(t -> t.v2 + " pcs. of " + t.v1)
+          .collect(joining(", "));
+      System.out.println("cid=" + cid + " got " + pl);
+    }
+  }
 }
