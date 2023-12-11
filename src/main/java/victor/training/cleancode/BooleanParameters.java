938dcc0446aa9a7d4f73938c8d15df7ec0d97134
Victor Rentea <victorrentea@gmail.com>
2023-12-11 13:37:16 +0200

    names matter

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -51,15 +51,15 @@ public class BooleanParameters {
 
   // Warning⚠️: this method might be called from multiple places in the codebase ...
   public static void bigUglyMethod(int storeId, Task task) {
-    System.out.println("Cow Logic 1 " + task + " and " + storeId);
+    System.out.println("Donkey Logic 1 " + task + " and " + storeId);
     System.out.println(task);
-    System.out.println("Cow Logic 3 " + task);
+    System.out.println("Donkey Logic 3 " + task);
 
     // System.out.println("Logic just for CR#323 : " + task);
 
-    System.out.println("Donkey Logic 1 " + storeId);
-    System.out.println("Donkey Logic 2 " + storeId);
-    System.out.println("Donkey Logic 3 " + storeId);
+    System.out.println("Sheep Logic 1 " + storeId);
+    System.out.println("Sheep Logic 2 " + storeId);
+    System.out.println("Sheep Logic 3 " + storeId);
   }
 }
 
