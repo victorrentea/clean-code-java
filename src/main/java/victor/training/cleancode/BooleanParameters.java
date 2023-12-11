1b59bbe8d0fbb059ae585db916fe84edf91c810f
Victor Rentea <victorrentea@gmail.com>
2023-12-11 11:48:09 +0200

    am curatat chestii evidente si am spart functia mare in 2

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -20,44 +20,52 @@ class SomeController {
 
 class SomeService {
   public void blueMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task, false);
+    BooleanParameters.bigUglyMethod(id, task);
   }
 
   public void greenMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task, false);
+    BooleanParameters.bigUglyMethod(id, task);
   }
 
   public void yellowMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task, false);
+    BooleanParameters.bigUglyMethod(id, task);
   }
 
   public void redMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task, false);
+    BooleanParameters.bigUglyMethod(id, task);
   }
 }
 
 class MyService {
   public void useCase323(int id, Task task) {
     // TODO The shared called method must execute logic specific for my use-case #323
-    BooleanParameters.bigUglyMethod(id, task, true);
+    BooleanParameters.bigUglyMethod323(id, task);
   }
 }
 
 public class BooleanParameters {
 
   // Warning⚠️: this method might be called from multiple places in the codebase ...
-  public static void bigUglyMethod(int storeId, Task task, boolean isUseCase323) {
-    System.out.println("Cow Logic 1 " + task + " and " + storeId);
-    System.out.println(task);
-    System.out.println("Cow Logic 3 " + task);
+  public static void bigUglyMethod(int storeId, Task task) {
+    cow(storeId, task);
+    donkey(storeId);
+  }
+  public static void bigUglyMethod323(int storeId, Task task) {
+    cow(storeId, task);
+    System.out.println("Logic just for CR#323 : " + task);
+    donkey(storeId);
+  }
 
-    if (isUseCase323) {
-      System.out.println("Logic just for CR#323 : " + task);
-    }
-
+  private static void donkey(int storeId) {
     System.out.println("Donkey Logic 1 " + storeId);
     System.out.println("Donkey Logic 2 " + storeId);
     System.out.println("Donkey Logic 3 " + storeId);
+  }
+
+  private static void cow(int storeId, Task task) {
+    System.out.println("Cow Logic 1 " + task + " and " + storeId);
+    System.out.println(task);
+    System.out.println("Cow Logic 3 " + task);
   }
 }
 
--- src/main/java/victor/training/cleancode/Boss.java
+++ src/main/java/victor/training/cleancode/Boss.java
@@ -1,7 +1,5 @@
 package victor.training.cleancode;
 
-import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,32 +7,42 @@ import java.util.List;
 public class Boss {
 
   // Lord gave us tests! 👌 TODO run them
-  public void bossLevel(boolean fluff, List<Task> tasks, boolean cr323) {
-    int index = 0;
-    int taskCount = tasks.size();
+  public void bossLevel(List<Task> tasks, boolean cr323) {
+//    int taskCount = tasks.size();
     System.out.println("Logic1");
+//    check(tasks); // side effect on tasks
+//    filteredTasks = filter(tasks); ca sa evite sa modifice lista
+    // daca te prin in 2023, anul Domnului, ca modific COLECTII IN JAVA, te bat cu biciul [AI inserted]
+    // nu e bine sa faci list.add list.remove, eviti map.put., daca poti sa
+    //    creezi o noua colectie cu .stream() ... etc
+    System.out.println("Logic3");
+    int index = 0;
     List<Integer> taskIds = new ArrayList<>();
-    if (fluff) {
-      System.out.println("Logic3");
-      for (Task task : tasks) {
-        System.out.println("Starting " + task);
-        task.setStarted(true);
+    for (Task task : tasks) {
+      System.out.println("Starting " + task);
+      task.setStarted(true);
 
-        taskIds.add(task.getId());
+      taskIds.add(task.getId());
 
-        if (cr323) { // TODO task = remove the boolean
-          System.out.println("My Logic: " + task);
-        }
-
-        index++;
-        System.out.println("Audit task #" + index + ": " + task);
+      if (cr323) { // TODO task = remove the boolean
+        System.out.println("My Logic: " + task);
       }
-      System.out.println("Logic6 " + taskCount);
-      System.out.println("Task Ids: " + taskIds);
-    } else {
-      System.out.println("Logic7 on fluff=false " + tasks);
+
+      index++;
+      System.out.println("Audit task #" + index + ": " + task);
     }
+    System.out.println("Logic6 " + tasks.size());
+    System.out.println("Task Ids: " + taskIds);
     System.out.println("Logic8");
+  }
+  public void bossLevelNoFluff(List<Task> tasks) {
+    System.out.println("Logic1");
+    System.out.println("Logic7 on fluff=false " + tasks);
+    System.out.println("Logic8");
+  }
+
+  private void check(List<Task> tasks) {
+    tasks.clear();// hehe!! 😈
   }
 
 }
