26339b0da129467086d4f4eb241b20badd4a26a4
Victor Rentea <victorrentea@gmail.com>
2023-12-12 11:58:28 +0200

    shallow immutable: ai campuri finale dar obiectele referite de tine prin campuri sunt mutabile: Other.set, List.add

--- src/main/java/victor/training/cleancode/immutable/basic/ImmutableBasic.java
+++ src/main/java/victor/training/cleancode/immutable/basic/ImmutableBasic.java
@@ -1,5 +1,8 @@
 package victor.training.cleancode.immutable.basic;
 
+import lombok.Data;
+import lombok.Value;
+
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -9,25 +12,29 @@ public class ImmutableBasic {
    public static void main(String[] args) {
       List<Integer> numbers = Stream.of(1, 2, 3, 4, 5).collect(toList());
 
-      Immutable immutable = new Immutable();
-
-      immutable.x = 2;
-      immutable.numbers = numbers;
-      immutable.other = new Other(13);
+      Immutable immutable = new Immutable(2, numbers, new Other(13));
 
       System.out.println(immutable);
 
       // LOTS OF BUSINESS LOGIC HERE
+      darkBizRules(immutable);
 
-      System.out.println(immutable.numbers);
+      System.out.println(immutable.getNumbers());
       System.out.println(immutable);
+   }
+
+   private static void darkBizRules(Immutable immutable) {
+      // hack de 21:00 Vineri
+      immutable.getOther().setA(-9);
    }
 }
 
-class Immutable {
-   public Integer x;
-   public List<Integer> numbers;
-   public Other other;
+// The state of an immutable object cannot be changed after it is created.
+@Value //?? = 'private final' fields + getters + equals/hashCode + toString
+class Immutable { // acum acest obiect este doar "shallow immutable"
+   Integer x;
+   List<Integer> numbers;
+   Other other;
 
    public String toString() {
       return String.format("Immutable{x=%d, numbers=%s, other=%s}", x, numbers, other);
