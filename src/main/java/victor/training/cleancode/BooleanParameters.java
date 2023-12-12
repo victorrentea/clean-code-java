375a62d0fc9260cf057ef5ee1bd88c70f754758b
c329245 <c329245>
2023-12-12 14:22:21 +0200

    remove test

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-//Bianca test
 class SomeController {
   SomeService someService;
 
