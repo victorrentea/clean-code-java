6dcb70cbd9a9b3a00180412ab3ebb4329fbb7037
Victor Rentea <victorrentea@gmail.com>
2023-12-12 10:17:42 +0200

    Optional folosit imperativ (gresit)

--- src/main/java/victor/training/cleancode/optional/Optional_Intro.java
+++ src/main/java/victor/training/cleancode/optional/Optional_Intro.java
@@ -7,29 +7,35 @@ import victor.training.cleancode.exception.model.MemberCard;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @SuppressWarnings("ConstantConditions")
 public class Optional_Intro {
 	public static void main(String[] args) {
 		// test: 60, 10, no MemberCard
-		System.out.println(getDiscountLine(new Customer(
-				new MemberCard("bar", 60))));
+		System.out.println(getDiscountLine(new Customer(new MemberCard("bar", 60))));
+		System.out.println(getDiscountLine(new Customer(new MemberCard("bar", 10))));
 	}
 
 	public static String getDiscountLine(Customer customer) {
-		Discount discount = computeDiscount(customer.getMemberCard());
-		return "You got a discount of %" + discount.getGlobalPercentage();
+		Optional<Discount> discount = computeDiscount(customer.getMemberCard());
+		if (discount.isEmpty()) {
+			return "Earn more fidelity points to benefit from a discount";
+		}
+		return "You got a discount of %" + discount.get().getGlobalPercentage();
 	}
 
-	private static Discount computeDiscount(MemberCard card) {
+	private static Optional<Discount> computeDiscount(MemberCard card) {
 		if (card.getFidelityPoints() >= 100) {
-			return new Discount(5);
+			return Optional.of(new Discount(5));
 		}
 		if (card.getFidelityPoints() >= 50) {
-			return new Discount(3);
+			return Optional.of(new Discount(3));
 		}
-		return null;
+		return Optional.empty();
 	}
+
+
 	@Data
 	public static class Discount {
 		private final int globalPercentage;
