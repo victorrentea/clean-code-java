a999781645f8a711d912a2503038ad789d6670c0
Victor Rentea <victorrentea@gmail.com>
2024-01-29 14:13:53 +0200

    reformat

--- src/main/java/sample/FunctionalCodeExample.java
+++ src/main/java/sample/FunctionalCodeExample.java
@@ -16,143 +16,143 @@ import java.util.stream.Stream;
 
 public class FunctionalCodeExample {
 
-    private final FooService fooService;
-    private final FooDao fooDao;
+  private final FooService fooService;
+  private final FooDao fooDao;
 
-    private final ExecutorService executorService;
+  private final ExecutorService executorService;
 
-    public FunctionalCodeExample(FooService fooService, FooDao fooDao, ExecutorService executorService) {
-        this.fooService = fooService;
-        this.fooDao = fooDao;
-        this.executorService = executorService;
-    }
+  public FunctionalCodeExample(FooService fooService, FooDao fooDao, ExecutorService executorService) {
+    this.fooService = fooService;
+    this.fooDao = fooDao;
+    this.executorService = executorService;
+  }
 
 
-    public void processAll() {
-        fooService.fooStream()
-//                .flatMap(t -> streamTryResult(t, throwable ->
-//                    logError("Streaming foos failed", throwable)))
-                .flatMap(t -> logAndIgnoreFailure(t, "Streaming foos failed"))
-//                .logFailure()
-                .map(foo -> Try.of(() -> wrap(foo)))
-                .flatMap(t -> logAndIgnoreFailure(t, "Failed wrapping foo"))
-                .map(wrapped -> Tuple.of(wrapped, process(wrapped)))
-                .filter(tuple -> tuple._2().isFailure())
-                .forEach(tuple -> logError("Couldn't process foo that has id %s".formatted(tuple._1.foo().uuid()), tuple._2.getCause()));
-    }
+  public void processAll() {
+    fooService.fooStream()
+        .flatMap(t -> logAndIgnoreFailure(t, "Streaming foos failed"))
+//                .logFailure() // life with extension function (kt, scala, ...)
+        .map(foo -> Try.of(() -> wrap(foo)))
+        .flatMap(t -> logAndIgnoreFailure(t, "Failed wrapping foo"))
+        .map(wrapped -> Tuple.of(wrapped, process(wrapped)))
+        .filter(tuple -> tuple._2().isFailure())
+        .forEach(tuple -> logError("Couldn't process foo that has id %s".formatted(tuple._1.foo().uuid()), tuple._2.getCause()));
+  }
 
-    private  <T> Stream<T> streamTryResult(Try<T> t, Consumer<Throwable> failure) {
-        return t.map(Stream::of).getOrElseGet(throwable -> {
-            failure.accept(throwable);
-            return Stream.empty();
-        });
-    }
-    private  <T> Stream<T> logAndIgnoreFailure(Try<T> t, String errorLogMessage) {
+  // could be overengineering to pass a function instead of creating 3-4 variants of it
+  private <T> Stream<T> streamTryResult(Try<T> t, Consumer<Throwable> failure) {
+    return t.map(Stream::of).getOrElseGet(throwable -> {
+      failure.accept(throwable);
+      return Stream.empty();
+    });
+  }
+
+  private <T> Stream<T> logAndIgnoreFailure(Try<T> t, String errorLogMessage) {
 //        return t.map(Stream::of).getOrElseGet(throwable -> {
 //           logError("Streaming foos failed", throwable);
 //            return Stream.empty();
 //        });
-        if (t.isFailure()) {
-            logError(errorLogMessage, t.getCause());
-            return Stream.empty();
-        }
-        return Stream.of(t.get());
+    if (t.isFailure()) {
+      logError(errorLogMessage, t.getCause());
+      return Stream.empty();
+    }
+    return Stream.of(t.get());
+  }
+
+  private Try<UUID> process(final FooWrapper wrapped) {
+    final var foo = wrapped.foo();
+    return Try.of(() -> {
+      fooDao.insert(foo);
+      final Set<Result> results = executorService.invokeAll(toCallable(wrapped)).stream()
+          .map(tFuture -> Try.of(tFuture::get).onFailure(throwable -> logError("Couldn't get from future", throwable)).toJavaOptional())
+          .flatMap(Optional::stream)
+          .collect(Collectors.toSet());
+
+      results.forEach(Result::onComplete);
+
+      if (results.stream().allMatch(Result::success)) {
+        //other code doing stuff like sending an event
+      }
+      return foo.uuid();
+    });
+  }
+
+  private Set<Callable<Result>> toCallable(final FooWrapper wrapped) {
+    return null;
+  }
+
+  private FooWrapper wrap(Foo foo) {
+    return new FooWrapper(foo);
+  }
+
+  private void logError(final String someErrorMessage, final Throwable throwable) {
+
+  }
+
+
+  static class FooService {
+    public Stream<Try<Foo>> fooStream() {
+      return fooNames().get().stream().map(name -> readFoos(name).of(is -> {
+            //do some more stuff here
+            //
+            //
+            return inputStreamToStorage(is).recoverWith(ex -> Try.failure(new RuntimeException("Throw some runtime exception here")));
+          })
+          .flatMap(Function.identity()));
     }
 
-    private Try<UUID> process(final FooWrapper wrapped) {
-        final var foo = wrapped.foo();
-        return Try.of(() -> {
-            fooDao.insert(foo);
-            final Set<Result> results = executorService.invokeAll(toCallable(wrapped)).stream()
-                    .map(tFuture -> Try.of(tFuture::get).onFailure(throwable -> logError("Couldn't get from future", throwable)).toJavaOptional())
-                    .flatMap(Optional::stream)
-                    .collect(Collectors.toSet());
-
-            results.forEach(Result::onComplete);
-
-            if(results.stream().allMatch(Result::success)){
-                //other code doing stuff like sending an event
-            }
-            return foo.uuid();
-        });
+    private Try<Foo> inputStreamToStorage(InputStream inputStream) {
+      //do some stuff and return the result
+      return Try.success(new Foo());
     }
 
-    private Set<Callable<Result>> toCallable(final FooWrapper wrapped) {
-        return null;
+    private Try.WithResources1<InputStream> readFoos(final String name) {
+      return null;
     }
 
-    private FooWrapper wrap(Foo foo) {
-        return new FooWrapper(foo);
+    private Try<Set<String>> fooNames() {
+      return null;
+    }
+  }
+
+
+  static class Foo {
+    public UUID uuid() {
+      return null;
+    }
+  }
+
+  //assume this is doing more stuff when it wraps foo - maybe can throw validation exception
+  record FooWrapper(Foo foo) {
+  }
+
+  static class FooDao {
+    public void insert(final Foo foo) {
+
+    }
+  }
+
+  static class Result {
+    private final boolean success;
+    private final Runnable runOnSuccess;
+    private final Runnable runOnFailure;
+
+    Result(boolean success, Runnable runOnSuccess, Runnable runOnFailure) {
+      this.success = success;
+      this.runOnSuccess = runOnSuccess;
+      this.runOnFailure = runOnFailure;
     }
 
-    private void logError(final String someErrorMessage, final Throwable throwable) {
-
+    public void onComplete() {
+      if (success) {
+        runOnSuccess.run();
+      } else {
+        runOnFailure.run();
+      }
     }
 
-
-    static class FooService {
-        public Stream<Try<Foo>> fooStream() {
-            return fooNames().get().stream().map(name -> readFoos(name).of(is -> {
-                        //do some more stuff here
-                        //
-                        //
-                        return  inputStreamToStorage(is).recoverWith(ex -> Try.failure(new RuntimeException("Throw some runtime exception here")));
-                    })
-                    .flatMap(Function.identity()));
-        }
-
-        private Try<Foo> inputStreamToStorage(InputStream inputStream) {
-            //do some stuff and return the result
-            return Try.success(new Foo());
-        }
-
-        private Try.WithResources1<InputStream> readFoos(final String name) {
-            return null;
-        }
-
-        private Try<Set<String>> fooNames() {
-            return null;
-        }
+    public boolean success() {
+      return success;
     }
-
-
-    static class Foo {
-        public UUID uuid() {
-            return null;
-        }
-    }
-
-    //assume this is doing more stuff when it wraps foo - maybe can throw validation exception
-    record FooWrapper(Foo foo) {
-    }
-
-    static class FooDao {
-        public void insert(final Foo foo) {
-
-        }
-    }
-
-    static class Result {
-        private final boolean success;
-        private final Runnable runOnSuccess;
-        private final Runnable runOnFailure;
-
-        Result(boolean success, Runnable runOnSuccess, Runnable runOnFailure) {
-            this.success = success;
-            this.runOnSuccess = runOnSuccess;
-            this.runOnFailure = runOnFailure;
-        }
-
-        public void onComplete() {
-            if (success) {
-                runOnSuccess.run();
-            } else {
-                runOnFailure.run();
-            }
-        }
-
-        public boolean success() {
-            return success;
-        }
-    }
+  }
 }
