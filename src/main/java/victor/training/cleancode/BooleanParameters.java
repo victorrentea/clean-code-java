309fb566c0485c9c78d385c7d09d0afbab98ebac
Victor Rentea <victorrentea@gmail.com>
2024-01-29 12:59:12 +0200

    back on simplicity (KISS principle)

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -1,13 +1,8 @@
 package victor.training.cleancode;
 
-import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
 
 class SomeController {
   SomeService someService;
@@ -25,44 +20,55 @@ class SomeController {
 
 class SomeService {
   public void blueMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task, t->{});
+    BooleanParameters.bigUglyMethod(id, task);
   }
 
   public void greenMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task, t->{});
+    BooleanParameters.bigUglyMethod(id, task);
   }
 
   public void yellowMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id,task, t->{});
+    BooleanParameters.bigUglyMethod(id, task);
   }
 
   public void redMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id,task, t->{});
+    BooleanParameters.bigUglyMethod(id, task);
   }
 }
 
 class MyService {
   public void useCase323(int id, Task task) {
     // TODO The shared called method must execute logic specific for my use-case #323
-    BooleanParameters.bigUglyMethod(id, task, t -> System.out.println("Logic just for CR#323 : " + t));
+    BooleanParameters.bigUglyMethod323(id, task);
   }
 }
 
 public class BooleanParameters {
-
   // Warning⚠️: this method might be called from multiple places in the codebase ...
-  public static void bigUglyMethod(int storeId, Task task, Consumer<Task> f) {
-    System.out.println("Cow Logic 1 " + task + " and " + storeId);
-    System.out.println(task);
-    System.out.println("Cow Logic 3 " + task);
-
-    f.accept(task);
-    // System.out.println("Logic just for CR#323 : " + task);
-
-    System.out.println("Donkey Logic 1 " + storeId);
-    System.out.println("Donkey Logic 2 " + storeId);
-    System.out.println("Donkey Logic 3 " + storeId);
+  public static void bigUglyMethod(int storeId, Task task) {
+    donkey(storeId, task);
+    sheep(storeId);
   }
+  public static void bigUglyMethod323(int storeId, Task task) {
+    donkey(storeId, task);
+    System.out.println("Logic just for CR#323 : " + task);
+    sheep(storeId);
+  }
+
+  private static void donkey(int storeId, Task task) {
+    System.out.println("Donkey Logic 1 " + task + " and " + storeId);
+    System.out.println(task);
+    System.out.println("Donkey Logic 3 " + task);
+    System.out.println("Donkey Logic 5 " + task);
+    System.out.println("Donkey Logic 4 " + task);
+  }
+
+  private static void sheep(int storeId) {
+    System.out.println("Sheep Logic 1 " + storeId);
+    System.out.println("Sheep Logic 2 " + storeId);
+    System.out.println("Sheep Logic 3 " + storeId);
+  }
+
 }
 
 
