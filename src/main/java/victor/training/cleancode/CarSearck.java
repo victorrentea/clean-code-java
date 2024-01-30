31d92f22e236fd6685696e8e13e3b625506c6448
Victor Rentea <victorrentea@gmail.com>
2024-01-30 12:04:17 +0200

    taking safe tiny babysteps to refactor a method signature. using INLINE.

--- src/main/java/victor/training/cleancode/CarSearck.java
+++ src/main/java/victor/training/cleancode/CarSearck.java
@@ -17,29 +17,29 @@ class CarSearck {
   }
 
   private boolean matchesYears(CarSearchCriteria criteria, CarModel carModel) {
-    return MathUtil.intervalsIntersect(
-        criteria.getStartYear(), criteria.getEndYear(),
-        carModel.getStartYear(), carModel.getEndYear());
+    int start1 = criteria.getStartYear();
+    int end1 = criteria.getEndYear();
+    int start2 = carModel.getStartYear();
+    int end2 = carModel.getEndYear();
+    return MathUtil.intervalsIntersect(new Interval(start1, end1), new Interval(start2, end2));
   }
 
   private void applyCapacityFilter() {
-    System.out.println(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+    System.out.println(MathUtil.intervalsIntersect(new Interval(1000, 1600), new Interval(1250, 2000)));
   }
-
 }
-
 class Alta {
   private void applyCapacityFilter() {
-    System.out.println(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+    System.out.println(MathUtil.intervalsIntersect(new Interval(1000, 1600), new Interval(1250, 2000)));
   }
-
 }
-
 class MathUtil {
-
-  public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
-    return start1 <= end2 && start2 <= end1;
+  // dream method:
+  public static boolean intervalsIntersect(Interval interval1, Interval interval2) {
+    return interval1.start() <= interval2.end() && interval2.start() <= interval1.end();
   }
+}
+record Interval(int start, int end) {
 }
 
 
