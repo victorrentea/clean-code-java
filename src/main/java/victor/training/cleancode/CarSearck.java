7eb370949715445bc799c43e41fee91886a9bd31
Victor Rentea <victorrentea@gmail.com>
2024-01-30 12:08:19 +0200

    move logic from static method into the data structure taken as param (if suitable)

--- src/main/java/victor/training/cleancode/CarSearck.java
+++ src/main/java/victor/training/cleancode/CarSearck.java
@@ -17,29 +17,25 @@ class CarSearck {
   }
 
   private boolean matchesYears(CarSearchCriteria criteria, CarModel carModel) {
-    int start1 = criteria.getStartYear();
-    int end1 = criteria.getEndYear();
-    int start2 = carModel.getStartYear();
-    int end2 = carModel.getEndYear();
-    return MathUtil.intervalsIntersect(new Interval(start1, end1), new Interval(start2, end2));
+    return new Interval(criteria.getStartYear(), criteria.getEndYear()).intervalsIntersect(new Interval(carModel.getStartYear(), carModel.getEndYear()));
   }
 
   private void applyCapacityFilter() {
-    System.out.println(MathUtil.intervalsIntersect(new Interval(1000, 1600), new Interval(1250, 2000)));
+    System.out.println(new Interval(1000, 1600).intervalsIntersect(new Interval(1250, 2000)));
   }
 }
 class Alta {
   private void applyCapacityFilter() {
-    System.out.println(MathUtil.intervalsIntersect(new Interval(1000, 1600), new Interval(1250, 2000)));
+    System.out.println(new Interval(1000, 1600).intervalsIntersect(new Interval(1250, 2000)));
   }
 }
 class MathUtil {
-  // dream method:
-  public static boolean intervalsIntersect(Interval interval1, Interval interval2) {
-    return interval1.start() <= interval2.end() && interval2.start() <= interval1.end();
-  }
 }
 record Interval(int start, int end) {
+  // dream method:
+  public boolean intervalsIntersect(Interval interval2) {
+    return start() <= interval2.end() && interval2.start() <= end();
+  }
 }
 
 
