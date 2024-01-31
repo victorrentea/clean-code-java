1aef2a38d0c50c747cee674965b3859f323996cd
Victor Rentea <victorrentea@gmail.com>
2024-01-31 14:38:19 +0200

    never Optional<List>
    Never List<> Set<> Map<> = null;

--- src/main/java/victor/training/cleancode/fp/ImperativeMindset.java
+++ src/main/java/victor/training/cleancode/fp/ImperativeMindset.java
@@ -4,27 +4,36 @@ import victor.training.cleancode.fp.support.Order;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ImperativeMindset {
+//  int sumField = 0;// on the heap.
   public int totalOrderPrice(List<Order> orders) {
-    int sum = 0;
-    orders.stream()
-        .filter(order -> order.isActive())
-        .forEach(order -> {
-          // imperative: let's *add* to the sum
-          // sum += order.getPrice();
-        });
+    // NEVER DO THIS:
+//    var ref = new Object() { // WTF is this !?!?!
+//      // anonymous class scoped to SINGLE METHOD!!
+//      int sum = 0;
+//    };
+//    final int[] sum = {0};
+//    AtomicInteger sum = new AtomicInteger();
+    int sum = orders.stream() // parallelStream would cause multithreadd access on stack frame
+        .filter(Order::isActive)
+        .mapToInt(Order::getPrice)
+        .sum(); // this lives on Stack Frame of this thread running the method
+    // parallelStream would cause multithreadd access on stack frame
     return sum;
   }
 
 
   public List<Integer> getOrderPrices(List<Order> orders) {
-    List<Integer> prices = new ArrayList<>();
-    orders.stream()
+    List<Integer> prices = orders.stream()
         .filter(order -> order.isActive())
-        .forEach(order -> {
-          prices.add(order.getPrice());
-        });
+//        .forEach(order -> { // for each used to accumulate data is a code smell.
+//          // worse than a good ol' for()
+//          prices.add(order.getPrice());
+//        });
+        .map(Order::getPrice)
+        .toList();
     return prices;
   }
 }
--- src/main/java/victor/training/cleancode/fp/StreamWreck.java
+++ src/main/java/victor/training/cleancode/fp/StreamWreck.java
@@ -7,6 +7,7 @@ import victor.training.cleancode.fp.support.ProductRepo;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import static java.util.stream.Collectors.*;
@@ -15,11 +16,12 @@ public class StreamWreck {
 	private ProductRepo productRepo;
 
 	public List<Product> getFrequentOrderedProducts(List<Order> orders) {
-		return orders.stream()
+		Map<Product, Integer> productToItemCount = orders.stream()
 				.filter(Order::isActive)
-				.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
+				.filter(this::lessThanAYearAgo)
 				.flatMap(o -> o.getOrderLines().stream())
-				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)))
+				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)));
+		return productToItemCount
 				.entrySet()
 				.stream()
 				.filter(e -> e.getValue() >= 10)
@@ -27,6 +29,11 @@ public class StreamWreck {
 				.filter(p -> !p.isDeleted())
 				.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
 				.collect(toList());
+	}
+
+	// better name, shielding my eyes from LDate api
+	private boolean lessThanAYearAgo(Order o) {
+		return o.getCreationDate().isAfter(LocalDate.now().minusYears(1));
 	}
 }
 
