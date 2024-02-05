80cd5f9362faaf14a049e8fe96b785557f61024d
Victor Rentea <victorrentea@gmail.com>
2024-02-05 10:15:48 +0200

    better names

--- src/main/java/victor/training/cleancode/Guards.java
+++ src/main/java/victor/training/cleancode/Guards.java
@@ -8,6 +8,7 @@ public class Guards {
 
   public static final int DEAD_PAY_AMOUNT = 1;
 
+
   public int getPayAmount(Marine marine, BonusPackage bonusPackage) {
     int result;
     if (marine != null && (bonusPackage.value() > 100 || bonusPackage.value() < 10)) {
