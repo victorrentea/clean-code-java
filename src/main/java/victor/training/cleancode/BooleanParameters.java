1d0299fbae4d1cf557b917f0825374dcf1973c75
Victor Rentea <victorrentea@gmail.com>
2024-02-05 11:22:21 +0200

    separate functions (SRP), extracting complex logic in separate methods to DRY

--- src/main/java/victor/training/cleancode/BooleanParameters.java
+++ src/main/java/victor/training/cleancode/BooleanParameters.java
@@ -16,43 +16,52 @@ class SomeController {
 
 class SomeService {
   public void blueMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task, false);
+    BooleanParameters.bigUglyMethod(id, task);
   }
 
   public void greenMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task, false);
+    BooleanParameters.bigUglyMethod(id, task);
   }
 
   public void yellowMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task, false);
+    BooleanParameters.bigUglyMethod(id, task);
   }
 
   public void redMethod(int id, Task task) {
-    BooleanParameters.bigUglyMethod(id, task, false);
+    BooleanParameters.bigUglyMethod(id, task);
   }
 }
 
 class MyService {
   public void useCase323(int id, Task task) {
     // TODO The shared called method must execute logic specific for my use-case #323
-    BooleanParameters.bigUglyMethod(id, task, true);
+    BooleanParameters.bigUglyMethod323(id, task);
   }
 }
 
 public class BooleanParameters {
 
   // Warning⚠️: this method might be called from multiple places in the codebase ...
-  public static void bigUglyMethod(int storeId, Task task, boolean cr323) {
-    System.out.println("Donkey Logic 1 " + task + " and " + storeId);
-    System.out.println(task);
-    System.out.println("Donkey Logic 3 " + task);
+  public static void bigUglyMethod(int storeId, Task task) {
+    donkey(storeId, task);
+    sheep(storeId);
+  }
+  public static void bigUglyMethod323(int storeId, Task task) {
+    donkey(storeId, task);
+    System.out.println("Logic just for CR#323 : " + task);
+    sheep(storeId);
+  }
 
-    if (cr323)
-      System.out.println("Logic just for CR#323 : " + task);
-
+  private static void sheep(int storeId) {
     System.out.println("Sheep Logic 1 " + storeId);
     System.out.println("Sheep Logic 2 " + storeId);
     System.out.println("Sheep Logic 3 " + storeId);
+  }
+
+  private static void donkey(int storeId, Task task) {
+    System.out.println("Donkey Logic 1 " + task + " and " + storeId);
+    System.out.println(task);
+    System.out.println("Donkey Logic 3 " + task);
   }
 }
 
