815849d6f8225890562ca178b1d717ce72d670fb
Victor Rentea <victorrentea@gmail.com>
2024-02-05 14:36:13 +0200

    we grouped to int together into a new Value Object (Interval): immutable, small, lacking PK (identity). -> simpler signatures. Fixed: "Missing Abstraction" code smell

--- src/main/java/victor/training/cleancode/CarSearch.java
+++ src/main/java/victor/training/cleancode/CarSearch.java
@@ -1,39 +1,52 @@
 package victor.training.cleancode;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-class CarSearck {
+class CarSearch {
 
     // see tests
     public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> carModels) {
         List<CarModel> results = carModels.stream()
-                .filter(carModel -> MathUtil.intervalsIntersect(
-                        criteria.getStartYear(), criteria.getEndYear(),
-                        carModel.getStartYear(), carModel.getEndYear()))
-                .collect(Collectors.toList());
+                .filter(carModel -> matchesYears(criteria, carModel))
+                .toList(); // hope the caller doesn't do .add .remove
         System.out.println("More filtering logic ...");
         return results;
     }
 
+    private boolean matchesYears(CarSearchCriteria criteria, CarModel carModel) {
+        int start1 = criteria.getStartYear();
+        int end1 = criteria.getEndYear();
+        int start2 = carModel.getStartYear();
+        int end2 = carModel.getEndYear();
+        return MathUtil.intervalsIntersect(new Interval(start1, end1), new Interval(start2, end2));
+    }
+
     private void applyCapacityFilter() {
-        System.out.println(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+        System.out.println(MathUtil.intervalsIntersect(new Interval(1000, 1600), new Interval(1250, 2000)));
+        System.out.println(MathUtil.intervalsIntersect(new Interval(1000, 1600), new Interval(1250, 2000)));
+        System.out.println(MathUtil.intervalsIntersect(new Interval(1000, 1600), new Interval(1250, 2000)));
+        System.out.println(MathUtil.intervalsIntersect(new Interval(1000, 1600), new Interval(1250, 2000)));
     }
 
 }
 
 class Alta {
     private void applyCapacityFilter() {
-        System.out.println(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+        System.out.println(MathUtil.intervalsIntersect(new Interval(1000, 1600), new Interval(1250, 2000)));
     }
 
 }
 
 class MathUtil {
-
-    public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
-        return start1 <= end2 && start2 <= end1;
+    // dream:
+    public static boolean intervalsIntersect(Interval interval1, Interval interval2) {
+        return interval1.end() >= interval2.start() && interval1.start() <= interval2.end();
     }
+    // present:
+}
+
+// Value Object: grouping of data, immutable small
+record Interval(int start, int end) {
 }
 
 
