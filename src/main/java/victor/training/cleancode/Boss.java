9213222767a486a1b0ff325393eed72d9c757815
Victor Rentea <victorrentea@gmail.com>
2024-02-15 10:14:06 +0200

    100% safe refactorings rename de chestii dintr-o functie sau extract meyhods

--- src/main/java/victor/training/cleancode/Boss.java
+++ src/main/java/victor/training/cleancode/Boss.java
@@ -9,13 +9,13 @@ import java.util.List;
 public class Boss {
 
   // 👌 TODO run tests
-  public void bossLevel(boolean b, List<Task> tasks, boolean cr323) {
+  public void bossLevel(boolean bulgaria, List<Task> tasks, boolean cr323) {
     int index = 0;
-    int j = tasks.size();
+    int taskCount = tasks.size();
     if (tasks.size() == 0) return;
     System.out.println("Logic1");
     List<Integer> taskIds = new ArrayList<>();
-    if (b) {
+    if (bulgaria) {
       System.out.println("Logic3");
       check(tasks);
       for (Task task : tasks) {
@@ -31,7 +31,7 @@ public class Boss {
         index++;
         System.out.println("Audit task #" + index + ": " + task);
       }
-      System.out.println("Logic6 " + j);
+      System.out.println("Logic6 " + taskCount);
       System.out.println("Task Ids: " + taskIds);
     } else {
       System.out.println("Logic7 on fluff=false " + tasks);
