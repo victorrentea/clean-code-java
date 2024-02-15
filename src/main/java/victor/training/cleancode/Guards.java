06d8f864064933e6f5f784f79beb445e379c77e2
Victor Rentea <victorrentea@gmail.com>
2024-02-15 12:00:44 +0200

    guard clauses / early return sa aplatizezi codul

--- src/main/java/victor/training/cleancode/Guards.java
+++ src/main/java/victor/training/cleancode/Guards.java
@@ -9,31 +9,28 @@ public class Guards {
   public static final int DEAD_PAY_AMOUNT = 1;
 
   public int getPayAmount(Marine marine, BonusPackage bonusPackage) {
-    int result;
-    if (marine != null && (bonusPackage.value() > 100 || bonusPackage.value() < 10)) {
-      if (!isDead(marine)) {
-        if (!marine.retired()) {
-          if (marine.yearsService() != null) {
-            result = marine.yearsService() * 100 + bonusPackage.value();
-            if (!marine.awards().isEmpty()) {
-              result += 1000;
-
-            }
-            if (marine.awards().size() >= 3) {
-              result += 2000;
-            }
-            // HEAVY core logic here, business-rules ...
-          } else {
-            throw new IllegalArgumentException("Any marine should have the years of service set");
-          }
-        } else result = retiredAmount();
-      } else {
-        result = DEAD_PAY_AMOUNT;
-      }
-    } else{
+    if (marine == null || (bonusPackage.value() <= 100 && bonusPackage.value() >= 10)) {
       throw new IllegalArgumentException("Not applicable!");
     }
-    return result; // TODO ALT-ENTER move return closer
+    if (isDead(marine)) {
+      return DEAD_PAY_AMOUNT;
+    }
+    if (marine.retired()) {
+      return retiredAmount();
+    }
+    if (marine.yearsService() == null) {
+      throw new IllegalArgumentException("Any marine should have the years of service set");
+    }
+
+    int result = marine.yearsService() * 100 + bonusPackage.value();
+    if (!marine.awards().isEmpty()) {
+      result += 1000;
+    }
+    if (marine.awards().size() >= 3) {
+      result += 2000;
+    }
+    // HEAVY core logic here, business-rules ...
+    return result;
   }
 
   private boolean isDead(Marine marine) {
