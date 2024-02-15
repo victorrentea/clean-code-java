b76f7ec76115b81dc38f009e8fd91c31803ec732
Victor Rentea <victorrentea@gmail.com>
2024-02-15 11:14:48 +0200

    better names bis

--- src/main/java/victor/training/cleancode/Boss.java
+++ src/main/java/victor/training/cleancode/Boss.java
@@ -12,7 +12,8 @@ public class Boss {
   public void bossLevel(boolean bulgaria, List<Task> tasks, boolean cr323) {
     int index = 0;
     int taskCount = tasks.size();
-    if (tasks.size() == 0) return;
+//    if (tasks.size() == 0) return;
+    if (tasks.isEmpty()) return;
     System.out.println("Logic1");
     List<Integer> taskIds = new ArrayList<>();
     if (bulgaria) {
