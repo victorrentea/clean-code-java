ada5332d8779921be866d1cd95c59ca4cefa6e3d
Victor Rentea <victorrentea@gmail.com>
2024-02-16 11:55:27 +0200

    immutabile facute fitza

--- src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
+++ src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
@@ -3,6 +3,7 @@ package victor.training.cleancode.immutable.advanced;
 import lombok.With;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -11,7 +12,7 @@ public class ImmutableAdvanced {
   public static void main(String[] args) {
     List<Integer> numbers = Stream.of(1, 2, 3).collect(toList()); // ArrayList
 
-    Immutable immutable = new Immutable(1, 1, numbers, new Other(15));
+    Immutable immutable = new Immutable(new Point(1, 1, Optional.empty()), numbers, new Other(15));
     System.out.println("Before: " + immutable);
 
     Immutable changed = wilderness(immutable);
@@ -21,26 +22,26 @@ public class ImmutableAdvanced {
 
   private static Immutable wilderness(Immutable immutable) {
     // dark, deep logic
-    return immutable.withXY(2, 2);
+    return immutable.withPoint(new Point(2, 2, Optional.empty()));
   }
   private static Immutable colasa(Immutable immutable) {
     // dark, deep logic
-    return immutable.withXY(2, 2);
+    return immutable.withPoint(new Point(2, 2, Optional.empty()));
   }
   private static Immutable colea(Immutable immutable) {
     // dark, deep logic
-    return immutable.withXY(2, 2);
+    return immutable.muta(1,1);
   }
 }
 
+record Point(int x, int y, Optional<Integer> z) {}
+
 //@With
-record Immutable(Integer x,
-                 Integer y,
+record Immutable(@With Point point,
                  List<Integer> numbers,
                  Other other) {
-
-  public Immutable withXY(int x, int y) {
-    return new Immutable(x, y, numbers, other);
+  public Immutable muta(int dx, int dy) {
+    return withPoint(new Point(point.x() + dx, point.y() + dy, point.z()));
   }
 }
 
