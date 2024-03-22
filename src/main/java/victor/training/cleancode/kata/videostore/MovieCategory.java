3177d28d9982045fcf4ea76ebb7c8e987632a65d
Bharat.Santani <bharat.santani@rabobank.nl>
2024-03-22 14:58:59 +0100

    Extract Movie to a Record

---
+++ src/main/java/victor/training/cleancode/kata/videostore/MovieCategory.java
@@ -0,0 +1,7 @@
+package victor.training.cleancode.kata.videostore;
+
+public class MovieCategory {
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+}
