794e8d8db71124bee9139e218308bc44e96c6a22
Akshay Bapusaheb Kulkarni <akshay.bapusaheb.kulkarni@rabobank.nl>
2024-03-22 15:03:12 +0100

    Company switch case has been updated

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -1,8 +1,11 @@
 package victor.training.cleancode.kata.videostore;
 
+import lombok.Getter;
+
 import java.util.*;
 
 class Customer {
+	@Getter
 	private final String name;
 	private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order
 
@@ -14,34 +17,28 @@ class Customer {
 		rentals.put(m, d);
 	}
 
-	public String getName() {
-		return name;
-	}
-
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
-		String result = "Rental Record for " + getName() + "\n";
+		String result = "Rental Record for %s \n".formatted(getName());
 		// iterate each rental
 		for (Movie each : rentals.keySet()) {
 			double thisAmount = 0;
 			// determine amounts for every line
 			int dr = rentals.get(each);
-			switch (each.priceCode()) {
-				case Movie.REGULAR:
-					thisAmount += 2;
-					if (dr > 2)
-						thisAmount += (dr - 2) * 1.5;
-					break;
-				case Movie.NEW_RELEASE:
-					thisAmount += dr * 3;
-					break;
-				case Movie.CHILDRENS:
-					thisAmount += 1.5;
-					if (dr > 3)
-						thisAmount += (dr - 3) * 1.5;
-					break;
-			}
+            switch (each.priceCode()) {
+                case Movie.REGULAR -> {
+                    thisAmount += 2;
+                    if (dr > 2)
+                        thisAmount += (dr - 2) * 1.5;
+                }
+                case Movie.NEW_RELEASE -> thisAmount += dr * 3;
+                case Movie.CHILDRENS -> {
+                    thisAmount += 1.5;
+                    if (dr > 3)
+                        thisAmount += (dr - 3) * 1.5;
+                }
+            }
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two-day new release rental
