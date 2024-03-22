d3afe99f0223f286167fddb3afdf00ec6b6f4ed2
Sathish Ranganathan <sathish.ranganathan@rabobank.nl>
2024-03-22 15:43:58 +0100

    commit the changes so far

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -1,21 +1,18 @@
 package victor.training.cleancode.kata.videostore;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.*;
 
+@RequiredArgsConstructor
 class Customer {
-	private String name;
-	private Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order
+	@Getter
+	private final String name;
+	private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order
 
-	public Customer(String name) {
-		this.name = name;
-	};
-
-	public void addRental(Movie m, int d) {
-		rentals.put(m, d);
-	}
-
-	public String getName() {
-		return name;
+    public void addRental(Movie movie, int rental) {
+		rentals.put(movie, rental);
 	}
 
 	public String statement() {
@@ -26,22 +23,18 @@ class Customer {
 		for (Movie each : rentals.keySet()) {
 			double price = 0;
 			// determine amounts for every line
-			int dr = rentals.get(each);
+			int rentalValue = rentals.get(each);
 			switch (each.getPriceType()) {
-				case REGULAR -> price = getRegularPrice(dr);
-				case NEW_RELEASE -> price += dr * 3;
-				case CHILDRENS -> {
-					price += 1.5;
-					if (dr > 3)
-						price += (dr - 3) * 1.5;
-				}
+				case REGULAR -> price = getRegularPrice(rentalValue);
+				case NEW_RELEASE -> price += rentalValue * 3;
+				case CHILDRENS -> price = getChildrenPrice(rentalValue);
 			}
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
 			if (each.getPriceType() != null &&
 				 (each.getPriceType() == PriceType.NEW_RELEASE)
-				 && dr > 1)
+				 && rentalValue > 1)
 				frequentRenterPoints++;
 			// show figures line for this rental
 			result += "\t" + each.getTitle() + "\t" + price + "\n";
@@ -53,10 +46,11 @@ class Customer {
 		return result;
 	}
 
-	private static double getRegularPrice(int dr) {
-		double thisAmount = 2;
-		if (dr > 2)
-			thisAmount += (dr - 2) * 1.5;
-		return thisAmount;
+	private static double getChildrenPrice(int rentalValue) {
+		return (rentalValue > 3) ? (1.5d + (rentalValue - 3) * 1.5) : 1.5d;
+	}
+
+	private static double getRegularPrice(int rentalValue) {
+		return (rentalValue > 2) ? (2 + (rentalValue - 2) * 1.5) : 2d;
 	}
 }
