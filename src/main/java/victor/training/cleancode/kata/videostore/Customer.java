dbcdece4acd560205a7a46f92b973c9adeaba9f5
Early Charlemagne <early.charlemagne@rabobank.nl>
2024-03-22 15:27:34 +0100

    moved calculation of costs to Rental class

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -13,7 +13,7 @@ class Customer {
 
 	public Customer(String name) {
 		this.name = name;
-	};
+	}
 
 	public void addRental(Rental rental) {
 		rentals.add(rental);
@@ -25,24 +25,8 @@ class Customer {
 		String result = "Rental Record for " + getName() + "\n";
 		// iterate each rental
 		for (Rental rental : rentals) {
-			double thisAmount = 0;
-			// determine amounts for every line
 			int rentalDays = rental.rentalDays();
-			switch (rental.movie().category()) {
-				case REGULAR:
-					thisAmount += 2;
-					if (rentalDays > 2)
-						thisAmount += (rentalDays - 2) * 1.5;
-					break;
-				case NEW_RELEASE:
-					thisAmount += rentalDays * 3;
-					break;
-				case CHILDRENS:
-					thisAmount += 1.5;
-					if (rentalDays > 3)
-						thisAmount += (rentalDays - 3) * 1.5;
-					break;
-			}
+			double thisAmount = rental.calculateCosts(rentalDays);
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
@@ -57,4 +41,5 @@ class Customer {
 		result += "You earned " + frequentRenterPoints + " frequent renter points";
 		return result;
 	}
+
 }
--- src/main/java/victor/training/cleancode/kata/videostore/Rental.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Rental.java
@@ -1,4 +1,24 @@
 package victor.training.cleancode.kata.videostore;
 
 public record Rental(Movie movie, int rentalDays) {
+    double calculateCosts(int rentalDays) {
+        double thisAmount = 0;
+        // determine amounts for every line
+        switch (movie.category()) {
+            case REGULAR:
+                thisAmount += 2;
+                if (rentalDays > 2)
+                    thisAmount += (rentalDays - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                thisAmount += rentalDays * 3;
+                break;
+            case CHILDRENS:
+                thisAmount += 1.5;
+                if (rentalDays > 3)
+                    thisAmount += (rentalDays - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
 }
