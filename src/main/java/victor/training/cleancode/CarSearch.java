6e20f952a11f9aad32e330711a00763d44961791
Victor Rentea <victorrentea@gmail.com>
2024-04-01 15:58:26 +0300

    refactor: ma jur DOAR AM INLINAT metoda intersect. JUR DOAR IDE am bagat! nu merita atentia ta, dreaga reviewer

--- src/main/java/victor/training/cleancode/CarSearch.java
+++ src/main/java/victor/training/cleancode/CarSearch.java
@@ -8,31 +8,41 @@ class CarSearch {
   // run tests
   public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> carModels) {
     List<CarModel> results = carModels.stream()
-        .filter(carModel -> MathUtil.intervalsIntersect(
-            criteria.getStartYear(), criteria.getEndYear(),
-            carModel.getStartYear(), carModel.getEndYear()))
+        .filter(carModel -> doi(criteria, carModel))
         .collect(Collectors.toList());
     System.out.println("More filtering logic ...");
     return results;
+  }
+
+  private boolean doi(CarSearchCriteria criteria, CarModel carModel) {
+    int start1 = criteria.getStartYear();
+    int end1 = criteria.getEndYear();
+    int start2 = carModel.getStartYear();
+    int end2 = carModel.getEndYear();
+    return MathUtil.intervalsIntersect(new Interval(start1, end1), new Interval(start2, end2));
   }
 }
 
 class SomeOtherClientCode {
   private void applyLengthFilter() { // pretend
-    System.out.println(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+    System.out.println(MathUtil.intervalsIntersect(new Interval(1000, 1600), new Interval(1250, 2000)));
   }
+
   private void applyCapacityFilter() { // pretend
-    System.out.println(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+    System.out.println(MathUtil.intervalsIntersect(new Interval(1000, 1600), new Interval(1250, 2000)));
   }
 }
 
 class MathUtil {
 
-  public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
-    return start1 <= end2 && start2 <= end1;
+  // REA, VECHE:
+
+  // BUNA:
+  public static boolean intervalsIntersect(Interval interval1, Interval interval2) {
+    return interval1.start() <= interval2.end() && interval2.start() <= interval1.end();
   }
 }
-
+record Interval(int start, int end) {}
 
 class CarSearchCriteria { // a DTO received from JSON
   private final int startYear;
