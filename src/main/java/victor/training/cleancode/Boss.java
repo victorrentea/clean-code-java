ee592efba9e115200f5f1588e6587a20fa23f4bf
Victor Rentea <victorrentea@gmail.com>
2024-04-01 15:35:25 +0300

    turbo

--- src/main/java/victor/training/cleancode/Boss.java
+++ src/main/java/victor/training/cleancode/Boss.java
@@ -4,37 +4,46 @@ import com.google.common.collect.ImmutableList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 // Deep nested functions are harder to break down
 public class Boss {
 
   // 👌 TODO run tests
-  public void bossLevel(boolean bg, ImmutableList<Task> tasks, boolean cr323) {
+  public void bossLevel(ImmutableList<Task> tasks, boolean cr323) {
     if (tasks.isEmpty()) return;
     System.out.println("Logic1");
-    if (bg) {
-      System.out.println("Logic3");
-      check(tasks);
-      int i = 0;
-      List<Integer> taskIds = new ArrayList<>();
-      for (Task task : tasks) {
-        System.out.println("Starting " + task);
-        task.setStarted(true);
+    System.out.println("Logic3");
+    check(tasks);
+    int i = 0;
 
-        taskIds.add(task.getId());
-
-        if (cr323) { // TODO remove the boolean
-          System.out.println("My Logic: " + task);
-        }
-
-        i++;
-        System.out.println("Audit task #" + i + ": " + task);
-      }
-      System.out.println("Logic6 " + tasks.size());
-      System.out.println("Task Ids: " + taskIds);
-    } else {
-      System.out.println("Logic7 on fluff=false " + tasks);
+    for (Task task : tasks) {
+      System.out.println("Starting " + task);
+      task.setStarted(true);
     }
+
+    List<Integer> taskIds = tasks.stream()
+        .map(Task::getId)
+        .toList();
+
+    for (Task task : tasks) {
+      if (cr323) { // TODO remove the boolean
+        System.out.println("My Logic: " + task);
+      }
+}
+    for (Task task : tasks) {
+      i++;
+      System.out.println("Audit task #" + i + ": " + task);
+    }
+    System.out.println("Logic6 " + tasks.size());
+    System.out.println("Task Ids: " + taskIds);
+    System.out.println("Logic8");
+  }
+
+  public void bossLevelNonBg(ImmutableList<Task> tasks) {
+    if (tasks.isEmpty()) return;
+    System.out.println("Logic1");
+    System.out.println("Logic7 on fluff=false " + tasks);
     System.out.println("Logic8");
   }
 
