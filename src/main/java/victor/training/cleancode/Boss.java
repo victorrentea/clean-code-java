756e7a24d3c805258e7878277c445d0489fe6f4b
Victor Rentea <victorrentea@gmail.com>
2024-04-01 12:41:20 +0300

    si acum, prapadu'

--- src/main/java/victor/training/cleancode/Boss.java
+++ src/main/java/victor/training/cleancode/Boss.java
@@ -10,14 +10,13 @@ public class Boss {
 
   // 👌 TODO run tests
   public void bossLevel(boolean bg, ImmutableList<Task> tasks, boolean cr323) {
-    int taskCount = tasks.size();
     if (tasks.isEmpty()) return;
     System.out.println("Logic1");
-    List<Integer> taskIds = new ArrayList<>();
     if (bg) {
       System.out.println("Logic3");
       check(tasks);
       int i = 0;
+      List<Integer> taskIds = new ArrayList<>();
       for (Task task : tasks) {
         System.out.println("Starting " + task);
         task.setStarted(true);
@@ -31,8 +30,7 @@ public class Boss {
         i++;
         System.out.println("Audit task #" + i + ": " + task);
       }
-//      int taskCount = tasks.size();
-      System.out.println("Logic6 " + taskCount);
+      System.out.println("Logic6 " + tasks.size());
       System.out.println("Task Ids: " + taskIds);
     } else {
       System.out.println("Logic7 on fluff=false " + tasks);
