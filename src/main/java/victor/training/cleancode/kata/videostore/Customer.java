53210ee84694abefbac4dfaa3dafe2ff4942ba58
Theodor Samson <Theodor.Samson@orange.com>
2024-04-02 15:35:57 +0300

    refactor: change Movie model

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -1,61 +1,57 @@
 package victor.training.cleancode.kata.videostore;
 
-import java.util.*;
+import lombok.Value;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Value
 class Customer {
-	private String name;
-	private Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order
+    String name;
+    Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order
 
-	public Customer(String name) {
-		this.name = name;
-	};
+    public void addRental(Movie movie, int price) {
+        rentals.put(movie, price);
+    }
 
-	public void addRental(Movie m, int d) {
-		rentals.put(m, d);
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		String result = "Rental Record for " + getName() + "\n";
-		// iterate each rental
-		for (Movie each : rentals.keySet()) {
-			double thisAmount = 0;
-			// determine amounts for every line
-			int dr = rentals.get(each);
-			switch (each.getPriceCode()) {
-				case Movie.REGULAR:
-					thisAmount += 2;
-					if (dr > 2)
-						thisAmount += (dr - 2) * 1.5;
-					break;
-				case Movie.NEW_RELEASE:
-					thisAmount += dr * 3;
-					break;
-				case Movie.CHILDRENS:
-					thisAmount += 1.5;
-					if (dr > 3)
-						thisAmount += (dr - 3) * 1.5;
-					break;
-			}
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if (each.getPriceCode() != null &&
-				 (each.getPriceCode() == Movie.NEW_RELEASE)
-				 && dr > 1)
-				frequentRenterPoints++;
-			// show figures line for this rental
-			result += "\t" + each.getTitle() + "\t" + thisAmount + "\n";
-			totalAmount += thisAmount;
-		}
-		// add footer lines
-		result += "Amount owed is " + totalAmount + "\n";
-		result += "You earned " + frequentRenterPoints + " frequent renter points";
-		return result;
-	}
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        String result = "Rental Record for " + getName() + "\n";
+        // iterate each rental
+        for (Movie each : rentals.keySet()) {
+            double thisAmount = 0;
+            // determine amounts for every line
+            int dr = rentals.get(each);
+            switch (each.movieType()) {
+                case MovieType.REGULAR:
+                    thisAmount += 2;
+                    if (dr > 2)
+                        thisAmount += (dr - 2) * 1.5;
+                    break;
+                case MovieType.NEW_RELEASE:
+                    thisAmount += dr * 3;
+                    break;
+                case MovieType.CHILDREN:
+                    thisAmount += 1.5;
+                    if (dr > 3)
+                        thisAmount += (dr - 3) * 1.5;
+                    break;
+            }
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two day new release rental
+            if (each.movieType() != null &&
+                    (each.movieType() == MovieType.NEW_RELEASE)
+                    && dr > 1)
+                frequentRenterPoints++;
+            // show figures line for this rental
+            result += "\t" + each.title() + "\t" + thisAmount + "\n";
+            totalAmount += thisAmount;
+        }
+        // add footer lines
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        return result;
+    }
 }
---
+++ src/main/java/victor/training/cleancode/kata/videostore/MovieType.java
@@ -0,0 +1,14 @@
+package victor.training.cleancode.kata.videostore;
+
+public enum MovieType {
+    REGULAR(0), NEW_RELEASE(1), CHILDREN(2);
+    private final int value;
+
+    MovieType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
