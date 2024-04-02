3401b8515a01809303531788f0c2c4fc15456400
Theodor Samson <Theodor.Samson@orange.com>
2024-04-02 15:00:15 +0300

    test commit Theo

--- src/main/java/victor/training/cleancode/kata/videostore/Movie.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Movie.java
@@ -1,6 +1,7 @@
 package victor.training.cleancode.kata.videostore;
 public class Movie {
 	public static final int CHILDRENS = 2;
+
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
 	private String _title;
