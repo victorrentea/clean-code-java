23fa9ab5c637ebc3a209860fe625bb5e566458bc
Victor Rentea <victorrentea@gmail.com>
2024-04-02 12:41:56 +0300

    ia uite

--- src/main/java/victor/training/cleancode/MicroTypes.java
+++ src/main/java/victor/training/cleancode/MicroTypes.java
@@ -12,10 +12,9 @@ public class MicroTypes {
 
   //<editor-fold desc="fetchData()">
   public Map<CustomerId, List<ProductCount>> fetchData() {
-    Long customerId = 1L;
-    Integer product1Count = 2;
-
-    Integer product2Count = 4;
+    final Long customerId = 1L;
+    final Integer product1Count = 2;
+    final Integer product2Count = 4;
     return Map.of(new CustomerId(customerId), List.of(
         new ProductCount("Table", product1Count),
         new ProductCount("Chair", product2Count)
@@ -24,10 +23,10 @@ public class MicroTypes {
   //</editor-fold>
 
   void primitiveObsession() {
-    Map<CustomerId, List<ProductCount>> customerOrderedProducts = fetchData();
+    final Map<CustomerId, List<ProductCount>> customerOrderedProducts = fetchData();
 
-    for (var e : customerOrderedProducts.entrySet()) { // iterating customerOrderedProducts entries 🤢
-      String pl = e.getValue().stream()
+    for (final var e : customerOrderedProducts.entrySet()) { // iterating customerOrderedProducts entries 🤢
+      final String pl = e.getValue().stream()
           .map(entry -> entry.count() + " pcs. of " + entry.name())
               .collect(joining(", "));
       System.out.println("cid=" + e.getKey().id() + " got " + pl);
@@ -36,9 +35,9 @@ public class MicroTypes {
 
   //<editor-fold desc="Tuple source of data">
   public Map<Long, List<Tuple2<String, Integer>>> extremeFP() {
-    Long customerId = 1L;
-    Integer product1Count = 2;
-    Integer product2Count = 4;
+    final Long customerId = 1L;
+    final Integer product1Count = 2;
+    final Integer product2Count = 4;
     return Map.of(customerId, List.of(
         org.jooq.lambda.tuple.Tuple.tuple("Table", product1Count),
         Tuple.tuple("Chair", product2Count)
@@ -46,11 +45,11 @@ public class MicroTypes {
   }
 
   void lackOfAbstractions() {
-    Map<Long, List<Tuple2<String, Integer>>> map = extremeFP();
+    final Map<Long, List<Tuple2<String, Integer>>> map = extremeFP();
     // Joke: try "var" above :)
 
-    for (Long cid : map.keySet()) {
-      String pl = map.get(cid).stream()
+    for (final Long cid : map.keySet()) {
+      final String pl = map.get(cid).stream()
           .map(t -> t.v2 + " pcs. of " + t.v1)
           .collect(joining(", "));
       System.out.println("cid=" + cid + " got " + pl);
