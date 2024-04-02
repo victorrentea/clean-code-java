88eeeeb1e196b02ca445e3453e060465c8487892
Victor Rentea <victorrentea@gmail.com>
2024-04-02 12:41:14 +0300

    ia uite

--- src/main/java/victor/training/cleancode/MicroTypes.java
+++ src/main/java/victor/training/cleancode/MicroTypes.java
@@ -1,10 +1,8 @@
 package victor.training.cleancode;
 
-import com.google.common.collect.Maps;
 import org.jooq.lambda.tuple.Tuple;
 import org.jooq.lambda.tuple.Tuple2;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -13,35 +11,7 @@ import static java.util.stream.Collectors.joining;
 public class MicroTypes {
 
   //<editor-fold desc="fetchData()">
-  public Map<Long, Map<String, Integer>> fetchData() {
-    Long customerId = 1L;
-    Integer product1Count = 2;
-    Integer product2Count = 4;
-    return Map.of(customerId, Map.of(
-            "Table", product1Count,
-            "Chair", product2Count
-    ));
-  }
-  //</editor-fold>
-
-  void primitiveObsession() {
-    Map<Long, Map<String, Integer>> map = fetchData();
-
-    for (var e : map.entrySet()) { // iterating map entries 🤢
-      String pl = e.getValue().entrySet().stream()
-              .map(entry -> entry.getValue() + " pcs. of " + entry.getKey())
-              .collect(joining(", "));
-      System.out.println("cid=" + e.getKey() + " got " + pl);
-    }
-  }
-
-
-
-
-
-
-  //<editor-fold desc="Tuple source of data">
-  public Map<CustomerId, List<ProductCount>> extremeFP() {
+  public Map<CustomerId, List<ProductCount>> fetchData() {
     Long customerId = 1L;
     Integer product1Count = 2;
     Integer product2Count = 4;
@@ -52,24 +22,44 @@ public class MicroTypes {
   }
   //</editor-fold>
 
-  record CustomerId(Long id) {}
-  // other examples:
-  // OK: IBAN(String), SatelliteId(Long), ProductCode(String), OrderId(Long), etc.
-  // NOK: TransactionId(String), 20GB/day of XML 1M VISA transactions today
-  // PERFORMANCE HIT: memory waste until Project Valhalla (Java ????)
-  // Kotlin: inline class
-  record ProductCount(String product, Integer count) {}
+  void primitiveObsession() {
+    Map<CustomerId, List<ProductCount>> customerOrderedProducts = fetchData();
+
+    for (var e : customerOrderedProducts.entrySet()) { // iterating customerOrderedProducts entries 🤢
+      String pl = e.getValue().stream()
+          .map(entry -> entry.count() + " pcs. of " + entry.name())
+              .collect(joining(", "));
+      System.out.println("cid=" + e.getKey().id() + " got " + pl);
+    }
+  }
+
+  //<editor-fold desc="Tuple source of data">
+  public Map<Long, List<Tuple2<String, Integer>>> extremeFP() {
+    Long customerId = 1L;
+    Integer product1Count = 2;
+    Integer product2Count = 4;
+    return Map.of(customerId, List.of(
+        org.jooq.lambda.tuple.Tuple.tuple("Table", product1Count),
+        Tuple.tuple("Chair", product2Count)
+    ));
+  }
 
   void lackOfAbstractions() {
-//    var map = extremeFP();// BAD!!
-    Map<CustomerId, List<ProductCount>> customerIdToProductCounts = extremeFP();
+    Map<Long, List<Tuple2<String, Integer>>> map = extremeFP();
     // Joke: try "var" above :)
 
-    for (CustomerId cid : customerIdToProductCounts.keySet()) {
-      String pl = customerIdToProductCounts.get(cid).stream()
-          .map(productCount -> productCount.count() + " pcs. of " + productCount.product())
+    for (Long cid : map.keySet()) {
+      String pl = map.get(cid).stream()
+          .map(t -> t.v2 + " pcs. of " + t.v1)
           .collect(joining(", "));
-      System.out.println("cid=" + cid.id() + " got " + pl);
+      System.out.println("cid=" + cid + " got " + pl);
     }
+  }
+
+  record CustomerId(Long id) {
+  }
+  //</editor-fold>
+
+  record ProductCount(String name, Integer count) {
   }
 }
