7b76da95321673028079e5fd669b81422199fba1
Victor Rentea <victorrentea@gmail.com>
2024-04-02 12:56:49 +0300

    comit

--- src/main/java/victor/training/cleancode/fp/MutantPipeline.java
+++ src/main/java/victor/training/cleancode/fp/MutantPipeline.java
@@ -3,28 +3,28 @@ package victor.training.cleancode.fp;
 import victor.training.cleancode.fp.support.Order;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class MutantPipeline {
-  public int totalActiveOrderPrice(List<Order> orders) {
-    int sum = 0;
-    orders.stream()
-        .filter(order -> order.isActive())
-        .forEach(order -> {
-//           sum += order.getPrice(); // let's add to sum
-        });
+  public int totalActiveOrderPrice(final List<Order> orders) {
+    // NU cu:
+    //    var ref = new Object() {int sum = 0;};
+    //    final int[] sum = {0};
+    //    AtomicInteger sum = new AtomicInteger();
+    final int sum = orders.stream()
+        .filter(Order::isActive)
+        .mapToInt(Order::price)
+        .sum();
     return sum;
   }
 
 
-  public List<LocalDate> getShipDates(List<Order> orders) {
-    List<LocalDate> shipDates = new ArrayList<>();
-    orders.stream()
-        .filter(order -> order.isActive())
-        .forEach(order -> {
-          order.shipDate().ifPresent(date -> shipDates.add(date));
-        });
-    return shipDates;
+  public List<LocalDate> getShipDates(final List<Order> orders) {
+    return orders.stream()
+        .filter(Order::isActive)
+        .map(Order::shipDate)
+        .flatMap(Optional::stream)
+        .toList();
   }
 }
