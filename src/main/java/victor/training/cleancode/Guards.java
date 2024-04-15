41c95865784448b98e869df4a44b9272abef26d5
Victor Rentea <victorrentea@gmail.com>
2024-04-15 15:07:33 +0300

    guard conditions

--- src/main/java/victor/training/cleancode/Guards.java
+++ src/main/java/victor/training/cleancode/Guards.java
@@ -1,7 +1,5 @@
 package victor.training.cleancode;
 
-import lombok.Value;
-
 import java.util.List;
 
 public class Guards {
@@ -9,31 +7,28 @@ public class Guards {
   public static final int DEAD_PAY_AMOUNT = 1;
 
   public int getPayAmount(Marine marine, BonusPackage bonusPackage) {
-    int result;
-    if (marine != null && (bonusPackage.value() > 100 || bonusPackage.value() < 10)) {
-      if (!isDead(marine)) {
-        if (!marine.retired()) {
-          if (marine.yearsService() != null) {
-            result = marine.yearsService() * 100 + bonusPackage.value();
-            if (!marine.awards().isEmpty()) {
-              result += 1000;
-
-            }
-            if (marine.awards().size() >= 3) {
-              result += 2000;
-            }
-            // HEAVY core logic here, business-rules ...
-          } else {
-            throw new IllegalArgumentException("Any marine should have the years of service set");
-          }
-        } else result = retiredAmount();
-      } else {
-        result = DEAD_PAY_AMOUNT;
-      }
-    } else{
-      throw new IllegalArgumentException("Not applicable!");
+    if (marine == null || (bonusPackage.value() <= 100 && bonusPackage.value() >= 10)) {
+      throw new IllegalArgumentException("Not applicable!"); // early throw
     }
-    return result; // TODO ALT-ENTER move return closer
+    if (isDead(marine)) {
+      return DEAD_PAY_AMOUNT; // DUUUUH!!
+    }
+    if (marine.retired()) { // guard conditions
+      return retiredAmount(); // early return
+    }
+    if (marine.yearsService() == null) {
+      throw new IllegalArgumentException("Any marine should have the years of service set");
+    }
+    int result = marine.yearsService() * 100 + bonusPackage.value();
+    if (!marine.awards().isEmpty()) {
+      result += 1000;
+
+    }
+    if (marine.awards().size() >= 3) {
+      result += 2000;
+    }
+    // HEAVY core logic here, business-rules ...
+    return result;
   }
 
   private boolean isDead(Marine marine) {
