c2a54262ed82ad0c3f8554f7ae84ceae67c9fad2
Victor Rentea <victorrentea@gmail.com>
2024-04-16 15:38:21 +0300

    correct

--- src/main/java/victor/training/cleancode/MicroTypes.java
+++ src/main/java/victor/training/cleancode/MicroTypes.java
@@ -37,38 +37,4 @@ public class MicroTypes {
 
 
 
-
-
-  //<editor-fold desc="Tuple source of data">
-  public Map<CustomerId, List<ProductCount>> extremeFP() {
-    Long customerId = 1L;
-    Integer product1Count = 2;
-    Integer product2Count = 4;
-    return Map.of(new CustomerId(customerId), List.of(
-        new ProductCount("Table", product1Count),
-        new ProductCount("Chair", product2Count)
-    ));
-  }
-  //</editor-fold>
-
-  record CustomerId(Long id) {}
-  // other examples:
-  // OK: IBAN(String), SatelliteId(Long), ProductCode(String), OrderId(Long), etc.
-  // NOK: TransactionId(String), 20GB/day of XML 1M VISA transactions today
-  // PERFORMANCE HIT: memory waste until Project Valhalla (Java ????)
-  // Kotlin: inline class
-  record ProductCount(String product, Integer count) {}
-
-  void lackOfAbstractions() {
-//    var map = extremeFP();// BAD!!
-    Map<CustomerId, List<ProductCount>> customerIdToProductCounts = extremeFP();
-    // Joke: try "var" above :)
-
-    for (CustomerId cid : customerIdToProductCounts.keySet()) {
-      String pl = customerIdToProductCounts.get(cid).stream()
-          .map(productCount -> productCount.count() + " pcs. of " + productCount.product())
-          .collect(joining(", "));
-      System.out.println("cid=" + cid.id() + " got " + pl);
-    }
-  }
 }
