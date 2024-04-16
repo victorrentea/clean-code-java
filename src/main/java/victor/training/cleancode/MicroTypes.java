66a3598e9438f918031f60f86045df588b291f5f
Victor Rentea <victorrentea@gmail.com>
2024-04-16 15:46:36 +0300

    microtypes to never forget what that Long meant

--- src/main/java/victor/training/cleancode/MicroTypes.java
+++ src/main/java/victor/training/cleancode/MicroTypes.java
@@ -8,8 +8,8 @@ import static java.util.stream.Collectors.joining;
 public class MicroTypes {
 
   //<editor-fold desc="fetchData()">
-  public Map<Long, List<ProductCount>> fetchData() {
-    Long customerId = 1L;
+  public Map<CustomerId, List<ProductCount>> fetchData() {
+    CustomerId customerId = new CustomerId(1L);
     Integer product1Count = 2;
     Integer product2Count = 4;
     return Map.of(customerId, List.of(
@@ -21,14 +21,17 @@ public class MicroTypes {
 
   void primitiveObsession() {
 //    Map<Long, Map<String, Integer>> customerIdToProductCounts = fetchData();
-    Map<Long, List<ProductCount>> customerIdToProductCounts = fetchData();
+    Map<CustomerId, List<ProductCount>> customerIdToProductCounts = fetchData();
 
     for (var e : customerIdToProductCounts.entrySet()) {
       String pl = e.getValue().stream()
           .map(entry -> entry.product() + " pcs. of " + entry.count())
               .collect(joining(", "));
-      System.out.println("cid=" + e.getKey() + " got " + pl);
+      System.out.println("cid=" + e.getKey().id() + " got " + pl);
     }
+  }
+
+  record CustomerId(Long id) {
   }
 
   record ProductCount(String product, Integer count) {
