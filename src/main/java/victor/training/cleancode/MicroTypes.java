a070860021fec11318c7579e0fd2110ecc9d9370
Victor Rentea <victorrentea@gmail.com>
2024-04-16 15:38:06 +0300

    grouped together the String + Integer

--- src/main/java/victor/training/cleancode/MicroTypes.java
+++ src/main/java/victor/training/cleancode/MicroTypes.java
@@ -1,10 +1,5 @@
 package victor.training.cleancode;
 
-import com.google.common.collect.Maps;
-import org.jooq.lambda.tuple.Tuple;
-import org.jooq.lambda.tuple.Tuple2;
-
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -13,26 +8,30 @@ import static java.util.stream.Collectors.joining;
 public class MicroTypes {
 
   //<editor-fold desc="fetchData()">
-  public Map<Long, Map<String, Integer>> fetchData() {
+  public Map<Long, List<ProductCount>> fetchData() {
     Long customerId = 1L;
     Integer product1Count = 2;
     Integer product2Count = 4;
-    return Map.of(customerId, Map.of(
-            "Table", product1Count,
-            "Chair", product2Count
+    return Map.of(customerId, List.of(
+        new ProductCount("Table", product1Count),
+        new ProductCount("Chair", product2Count)
     ));
   }
   //</editor-fold>
 
   void primitiveObsession() {
-    Map<Long, Map<String, Integer>> map = fetchData();
+//    Map<Long, Map<String, Integer>> customerIdToProductCounts = fetchData();
+    Map<Long, List<ProductCount>> customerIdToProductCounts = fetchData();
 
-    for (var e : map.entrySet()) { // iterating map entries 🤢
-      String pl = e.getValue().entrySet().stream()
-              .map(entry -> entry.getValue() + " pcs. of " + entry.getKey())
+    for (var e : customerIdToProductCounts.entrySet()) {
+      String pl = e.getValue().stream()
+          .map(entry -> entry.product() + " pcs. of " + entry.count())
               .collect(joining(", "));
       System.out.println("cid=" + e.getKey() + " got " + pl);
     }
+  }
+
+  record ProductCount(String product, Integer count) {
   }
 
 
