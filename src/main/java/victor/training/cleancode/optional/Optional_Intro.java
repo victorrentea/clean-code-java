b9d82a2ab2eccc5cc8cab09a062e076576dab09f
Victor Rentea <victorrentea@gmail.com>
2024-04-17 14:43:55 +0300

    null object pattern is a great idea. but requires caller to be aware of it

--- src/main/java/victor/training/cleancode/optional/Optional_Intro.java
+++ src/main/java/victor/training/cleancode/optional/Optional_Intro.java
@@ -1,4 +1,3 @@
-
 package victor.training.cleancode.optional;
 
 import victor.training.cleancode.exception.model.Customer;
@@ -10,12 +9,13 @@ import java.util.Map;
 public class Optional_Intro {
   public static void main(String[] args) {
     // test: 60, 10, no MemberCard
-    System.out.println(getDiscountLine(new Customer(
-        new MemberCard("bar", 60))));
+    System.out.println(getDiscountLine(new Customer(new MemberCard("bar", 60))));
+    System.out.println(getDiscountLine(new Customer(new MemberCard("bar", 10))));
   }
 
   public static String getDiscountLine(Customer customer) {
     Discount discount = computeDiscount(customer.getMemberCard());
+
     return "You got a discount of %" + discount.globalPercentage();
   }
 
@@ -26,7 +26,8 @@ public class Optional_Intro {
     if (card.getFidelityPoints() >= 50) {
       return new Discount(3, Map.of());
     }
-    return null;
+    return new Discount(0, Map.of()); // null object pattern= a non-null value that MEANS nothing
+    // REQUIRES THE CALLER TO BE AWARE OF THIS CONVENTION
   }
 
   public record Discount(int globalPercentage, Map<String, Integer> categoryDiscounts) {
