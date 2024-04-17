4f4446a1a4a59a4f75a6976b4bb4c545ffefb0a8
Victor Rentea <victorrentea@gmail.com>
2024-04-17 12:50:15 +0300

    return changed copy of immutable object confused variables

--- src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
+++ src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
@@ -10,21 +10,28 @@ public class ImmutableAdvanced {
   public static void main(String[] args) {
     ImmutableList<Integer> numbers = Stream.of(1, 2, 3).collect(toImmutableList()); // ArrayList
 
-    Immutable immutable = new Immutable(1, 2, numbers, new Other(15));
-    System.out.println("Before: " + immutable);
+    Immutable blue = new Immutable(1, 2, numbers, new Other(15));
+    System.out.println("Before: " + blue);
 
-    wilderness(immutable);
+    blue = wilderness(blue); // Confused Variable code smell
+    // this practice, of reassigning the variable can lead to temporal coupling and confusion in the reader
 
-    System.out.println("After:  " + immutable);
+    System.out.println("Moved object:  " + blue);
   }
 
-  private static void wilderness(Immutable immutable) {
+  private static Immutable wilderness(Immutable immutable) {
     // imagine 1500 lines of code working with immutable object
 
     // dark, deep logic not expected to change the immutable object x,y
 
     // quickfix BUG-123124  Fri evening hack
 //    immutable.getNumbers().clear();
+
+    // CR: in this wilderness, we need to 'relocate the position of the object on the screen'  like this:
+//    immutable.setX(immutable.getX()+1);
+//    immutable.setY(immutable.getY()+1);
+
+    return new Immutable(immutable.getX() + 1, immutable.getY() + 1, immutable.getNumbers(), immutable.getOther());
   }
 }
 
