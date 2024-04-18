ee1df250e6ebfd831cd4ed5fadbb02dedd4467e2
Yossi.Duani <Yossi.Duani@finastra.com>
2024-04-18 15:07:57 +0300

    Yossi Commit #4

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -1,9 +1,7 @@
 package victor.training.cleancode.kata.videostore;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 class Customer {
     private final String name;
@@ -26,19 +24,16 @@ class Customer {
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + name + "\n";
         for (Rental rental : rentals) {
-            Movie movie = rental.movie();
-            int daysRented = rental.daysRented();
-
-            double rentalPrice = calculatePrice(movie, daysRented);
+            double rentalPrice = calculatePrice(rental);
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((movie.priceCode() == PriceCode.NEW_RELEASE)
-                    && daysRented > 1) {
+            if ((rental.movie().priceCode() == PriceCode.NEW_RELEASE)
+                    && rental.daysRented() > 1) {
                 frequentRenterPoints++;
             }
             // show figures line for this rental
-            result += "\t" + movie.title() + "\t" + rentalPrice + "\n";
+            result += "\t" + rental.movie().title() + "\t" + rentalPrice + "\n";
             totalAmount += rentalPrice;
         }
         // add footer lines
@@ -47,30 +42,5 @@ class Customer {
         return result;
     }
 
-    private static double calculatePrice(Movie movie, int daysRented) {
-      return switch (movie.priceCode()) {
-        case REGULAR -> calculateRegularPrice(daysRented);
-        case NEW_RELEASE -> daysRented * 3;
-        case CHILDREN -> calculateChildrenPrice(daysRented);
-        //case BLOCKBUSTER -> 0.0;
-      };
-    }
 
-    private static double calculateChildrenPrice(int daysRented) {
-        final int MIN_DAYS_FOR_EXTRA_PRICE = 3;
-        double thisAmount = 1.5;
-        if (daysRented > MIN_DAYS_FOR_EXTRA_PRICE) {
-            thisAmount += (daysRented - MIN_DAYS_FOR_EXTRA_PRICE) * 1.5;
-        }
-        return thisAmount;
-    }
-
-    private static double calculateRegularPrice(int daysRented) {
-        final int MIN_DAYS_FOR_EXTRA_PRICE = 2;
-        double thisAmount = 2;
-        if (daysRented > MIN_DAYS_FOR_EXTRA_PRICE) {
-            thisAmount += (daysRented - MIN_DAYS_FOR_EXTRA_PRICE) * 1.5;
-        }
-        return thisAmount;
-    }
 }
--- src/main/java/victor/training/cleancode/kata/videostore/Rental.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Rental.java
@@ -3,4 +3,31 @@ package victor.training.cleancode.kata.videostore;
 public record Rental(
     Movie movie,
     int daysRented) {
+    private static double calculatePrice(Rental rental) {
+        int daysRented = rental.daysRented();
+        return switch (rental.movie().priceCode()) {
+            case REGULAR -> calculateRegularPrice(daysRented);
+            case NEW_RELEASE -> daysRented * 3;
+            case CHILDREN -> calculateChildrenPrice(daysRented);
+            //case BLOCKBUSTER -> 0.0;
+        };
+    }
+
+    private static double calculateChildrenPrice(int daysRented) {
+        final int MIN_DAYS_FOR_EXTRA_PRICE = 3;
+        double thisAmount = 1.5;
+        if (daysRented > MIN_DAYS_FOR_EXTRA_PRICE) {
+            thisAmount += (daysRented - MIN_DAYS_FOR_EXTRA_PRICE) * 1.5;
+        }
+        return thisAmount;
+    }
+
+    private static double calculateRegularPrice(int daysRented) {
+        final int MIN_DAYS_FOR_EXTRA_PRICE = 2;
+        double thisAmount = 2;
+        if (daysRented > MIN_DAYS_FOR_EXTRA_PRICE) {
+            thisAmount += (daysRented - MIN_DAYS_FOR_EXTRA_PRICE) * 1.5;
+        }
+        return thisAmount;
+    }
 }
