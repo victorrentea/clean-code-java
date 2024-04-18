637212f92eac8f4732eb7902d5f33275dfc16a70
Agarwal, Shruthi <shruthi703@gmail.com>
2024-04-18 18:12:08 +0530

    Convert the for loop to streams

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -2,6 +2,7 @@ package victor.training.cleancode.kata.videostore;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Customer {
   private final String name;
@@ -20,22 +21,25 @@ class Customer {
   }
 
   public String statement() {
-    double totalPrice = 0;
-    int frequentRenterPoints = 0;
+    
+	double totalPrice = rentals.stream().mapToDouble(Rental::calculatePrice).sum();
+    
+    int frequentRenterPoints = rentals.stream().mapToInt(Rental::calculateFrequentRenterPoints).sum();
+    
     String result = "Rental Record for " + name + "\n";
-    for (Rental rental : rentals) {
-      frequentRenterPoints += rental.calculateFrequentRenterPoints();
-
-      result += "\t" + rental.movie().title() + "\t" + rental.calculatePrice() + "\n";
-
-      totalPrice += rental.calculatePrice();
-    }
+    
+    result += rentals.stream().map(this::statementLine).collect(Collectors.joining());
+    
     //  IT IS OK TO REPEAT A PURE FUNCTION (no side effects, same result, usually insanely fast)
     // add footer lines
     result += "Amount owed is " + totalPrice + "\n";
     result += "You earned " + frequentRenterPoints + " frequent renter points";
     return result;
   }
+
+	private String statementLine(Rental rental) {
+		return "\t" + rental.movie().title() + "\t" + rental.calculatePrice() + "\n";
+	}
 
 
 }
