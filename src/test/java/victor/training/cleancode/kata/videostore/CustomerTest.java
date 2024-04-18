4d01fb144017ecb2b9de6fb8f113edc0763b8c06
Agarwal, Shruthi <shruthi703@gmail.com>
2024-04-18 15:50:24 +0530

    Added New line

--- src/test/java/victor/training/cleancode/kata/videostore/CustomerTest.java
+++ src/test/java/victor/training/cleancode/kata/videostore/CustomerTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomerTest {
 
+	
     @Test
     public void characterizationTest() {
         Customer customer = new Customer("John Doe");
