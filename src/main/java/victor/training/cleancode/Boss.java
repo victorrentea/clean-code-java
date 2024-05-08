3f5d35be4a7c2eaf938d34a89fb965fac31030de
Victor Rentea <victorrentea@gmail.com>
2024-05-08 12:45:27 +0300

    split loop refactoring: spargi foru daca schimbarea ORDINEI operatiilor nu e o problema

--- src/main/java/victor/training/cleancode/Boss.java
+++ src/main/java/victor/training/cleancode/Boss.java
@@ -9,34 +9,42 @@ import java.util.List;
 public class Boss {
 
   // 👌 TODO run tests
-  public void bossLevel(boolean forBulgaria, final ImmutableList<Task> tasks, boolean cr323) {
+  public void bossLevel(ImmutableList<Task> tasks) {
     if (tasks.isEmpty()) {
       return;
     }
     System.out.println("Logic1");
-    List<Integer> taskIds = new ArrayList<>();
-    if (forBulgaria) {
-      System.out.println("Logic3");
-      check(tasks); // nu-mi da nimic inapoi = COMMAND method care arunca exceptie sau  face side-effects (modifica parametrii, INSERT, SEND, POST)
-      int index = 0;
-      for (Task task : tasks) {
-        System.out.println("Starting " + task);
-        task.setStarted(true);
+    System.out.println("Logic7 on fluff=false " + tasks);
+    System.out.println("Logic8");
+  }
 
-        taskIds.add(task.getId());
-
-        if (cr323) { // TODO remove the boolean
-          System.out.println("My Logic: " + task);
-        }
-        index++;
-        System.out.println("Audit task #" + index + ": " + task);
-      }
-      int count = tasks.size();
-      System.out.println("Logic6 " + count);
-      System.out.println("Task Ids: " + taskIds);
-    } else {
-      System.out.println("Logic7 on fluff=false " + tasks);
+  public void bossLevelBulgaria(ImmutableList<Task> tasks, boolean cr323) {
+    if (tasks.isEmpty()) {
+      return;
     }
+    System.out.println("Logic1");
+    System.out.println("Logic3");
+    check(tasks); // nu-mi da nimic inapoi = COMMAND method care arunca exceptie sau  face side-effects (modifica parametrii, INSERT, SEND, POST)
+    int index = 0;
+    List<Integer> taskIds = new ArrayList<>();
+    for (Task task : tasks) {
+      System.out.println("Starting " + task);
+      task.setStarted(true);
+    }
+    for (Task task : tasks) {
+      taskIds.add(task.getId());
+    }
+    for (Task task : tasks) {
+      if (cr323) { // TODO remove the boolean
+        System.out.println("My Logic: " + task);
+      }
+    }
+    for (Task task : tasks) {
+      index++;
+      System.out.println("Audit task #" + index + ": " + task);
+    }
+    System.out.println("Logic6 " + tasks.size());
+    System.out.println("Task Ids: " + taskIds);
     System.out.println("Logic8");
   }
 
