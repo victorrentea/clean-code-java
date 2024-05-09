39bd2631482a81da51826cc0a57eff0fd216c6bc
Victor Rentea <victorrentea@gmail.com>
2024-05-09 12:13:16 +0300

    nu modificam date dintr-un pipe FP (Stream chain..)

--- src/main/java/victor/training/cleancode/fp/MutantPipeline.java
+++ src/main/java/victor/training/cleancode/fp/MutantPipeline.java
@@ -8,23 +8,37 @@ import java.util.List;
 
 public class MutantPipeline {
   public int totalActiveOrderPrice(List<Order> orders) {
-    int sum = 0;
-    orders.stream()
-        .filter(order -> order.isActive())
-        .forEach(order -> {
-//           sum += order.getPrice(); // let's add to sum
-        });
+    //     var ref = new Object() {int sum = 0;};
+    //    final int[] sum = {0};
+    //    AtomicInteger sum = new AtomicInteger();
+    int sum = orders.stream()
+        .filter(Order::isActive)
+        .mapToInt(Order::price)
+        .sum();// Mantra FP: return, don't change!
     return sum;
   }
 
 
+
+
+
+
+
+
   public List<LocalDate> getShipDates(List<Order> orders) {
-    List<LocalDate> shipDates = new ArrayList<>();
-    orders.stream()
-        .filter(order -> order.isActive())
-        .forEach(order -> {
-          order.shipDate().ifPresent(date -> shipDates.add(date));
-        });
+    var shipDates = orders.stream()
+        .filter(Order::isActive)
+        .flatMap(order->order.shipDate().stream())
+        .toList();
+
     return shipDates;
   }
+
+//  public List<LocalDate> getShipDates(List<Order> orders) {
+//    List<LocalDate> shipDates = new ArrayList<>();
+//    orders.stream()
+//        .filter(Order::isActive)
+//        .forEach(order -> order.shipDate().ifPresent(date -> shipDates.add(date)));
+//    return shipDates;
+//  }
 }
