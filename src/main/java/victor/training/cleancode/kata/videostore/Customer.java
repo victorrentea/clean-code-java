d6975994031247d9b2a8815245337c03f14c056e
Victor Rentea <victorrentea@gmail.com>
2024-05-09 16:55:50 +0300

    wip

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -23,14 +23,22 @@ class Customer {
     public String statement() {
         int frequentRenterPoints = rentals.stream().filter(Rental::eligibleForBonus).toList().size() + rentals.size();
         double totalAmount = rentals.stream().mapToDouble(Rental::calculateAmount).sum();
-        return getRentalDetails(frequentRenterPoints, totalAmount);
+        return header() + getBody() + footer(frequentRenterPoints, totalAmount);
     }
 
-    private String getRentalDetails(int frequentRenterPoints, double totalAmount) {
-        String prefix = "Rental Record for " + getName() + "\n";
-        String suffix = "Amount owed is " + totalAmount + "\n" + "You earned "
-                + frequentRenterPoints + " frequent renter points";
-        return rentals.stream().map(Rental::getText).collect(Collectors.joining("", prefix, suffix));
+    private String getBody() {
+        return rentals.stream()
+            .map(Rental::getText)
+            .collect(Collectors.joining(""));
+    }
+
+    private String header() {
+        return "Rental Record for " + getName() + "\n";
+    }
+
+    private String footer(int frequentRenterPoints, double totalAmount) {
+        return "Amount owed is " + totalAmount + "\n" + "You earned "
+               + frequentRenterPoints + " frequent renter points";
     }
 
 }
