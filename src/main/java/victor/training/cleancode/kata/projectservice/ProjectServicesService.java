4d8f77f995eca28326963ec9617d11fa83a380a6
Victor Rentea <victorrentea@gmail.com>
2024-05-21 09:38:47 +0300

    Revert "refactor exercise"
    This reverts commit e06eac510ad3561ad9a3484f7232ae7000d35a19.

--- src/main/java/victor/training/cleancode/kata/projectservice/ProjectServicesService.java
+++ src/main/java/victor/training/cleancode/kata/projectservice/ProjectServicesService.java
@@ -1,9 +1,8 @@
 package victor.training.cleancode.kata.projectservice;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProjectServicesService {
    List<ProjectServices> getProjectServicesByProjectId(Long id);
-   Optional<ProjectServices> findByServiceAndProject(Service service, Project project);
+   ProjectServices findByServiceAndProject(Service service, Project project);
 }
