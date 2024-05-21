6be232ca45fa7b05cdf2f0edec378383fa385df9
Victor Rentea <victorrentea@gmail.com>
2024-05-21 14:52:42 +0300

    bine. Optional impinge decizia "ce fac daca nu-i" in client.

--- src/main/java/victor/training/cleancode/optional/Optional_Intro.java
+++ src/main/java/victor/training/cleancode/optional/Optional_Intro.java
@@ -5,6 +5,7 @@ import victor.training.cleancode.exception.model.Customer;
 import victor.training.cleancode.exception.model.MemberCard;
 
 import java.util.Map;
+import java.util.Optional;
 
 @SuppressWarnings("ConstantConditions")
 public class Optional_Intro {
@@ -17,18 +18,21 @@ public class Optional_Intro {
   }
 
   public static String getDiscountLine(Customer customer) {
-    Discount discount = computeDiscount(customer.getMemberCard());
-    return "You got a discount of %" + discount.globalPercentage();
+    Optional<Discount> discount = computeDiscount(customer.getMemberCard());
+    if (discount.isPresent())
+      return "You got a discount of %" + discount.get().globalPercentage();
+    else
+      return "Earn more points to get a discount!"; // FOMO
   }
 
-  private static Discount computeDiscount(MemberCard card) {
+  private static Optional<Discount> computeDiscount(MemberCard card) {
     if (card.getFidelityPoints() >= 100) {
-      return new Discount(5, Map.of());
+      return Optional.of(new Discount(5, Map.of()));
     }
     if (card.getFidelityPoints() >= 50) {
-      return new Discount(3, Map.of());
+      return Optional.of(new Discount(3, Map.of()));
     }
-    return new Discount(0, Map.of());
+    return Optional.empty();
   }
 
   public record Discount(int globalPercentage, Map<String, Integer> categoryDiscounts) {
