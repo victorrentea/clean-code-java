ea9e40791cbecd01d83538b22f26b110a4212cc5
Victor Rentea <victorrentea@gmail.com>
2024-05-21 14:24:13 +0300

    rupe bucati streamurile lungi.
    dupa 2-3 "puncte", sau la "OFTAT" extragi variabila, metoda,...

--- src/main/java/victor/training/cleancode/fp/FunctionalChainsaw.java
+++ src/main/java/victor/training/cleancode/fp/FunctionalChainsaw.java
@@ -5,10 +5,13 @@ import victor.training.cleancode.fp.support.OrderLine;
 import victor.training.cleancode.fp.support.Product;
 import victor.training.cleancode.fp.support.ProductRepo;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
+import static java.util.function.Predicate.not;
 import static java.util.stream.Collectors.*;
 
 public class FunctionalChainsaw { // ... Massacre
@@ -19,17 +22,27 @@ public class FunctionalChainsaw { // ... Massacre
   }
 
   public List<Product> getFrequentOrderedProducts(List<Order> orders) {
-		return orders.stream()
-				.filter(Order::isActive)
-				.filter(o -> o.creationDate().isAfter(LocalDate.now().minusYears(1)))
-				.flatMap(o -> o.orderLines().stream())
-				.collect(groupingBy(OrderLine::product, summingInt(OrderLine::itemCount)))
-				.entrySet()
+		List<Product> topProducts = numberOfRecentProducts(orders).entrySet()
 				.stream()
 				.filter(e -> e.getValue() >= 10)
 				.map(Entry::getKey)
-				.filter(p -> !p.isDeleted())
-				.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
+				.toList();
+//		if (stream.count() > 100) {
+//			System.out.println("yeeei am succes!");
+//		}
+		List<Long> hiddenProductIds = productRepo.getHiddenProductIds();
+		return topProducts.stream()
+				.filter(not(Product::isDeleted))
+				.filter(p -> !hiddenProductIds.contains(p.getId()))
 				.collect(toList());
 	}
+
+	private Map<Product, Integer> numberOfRecentProducts(List<Order> orders) {
+		return orders.stream()
+				.filter(Order::isActive)
+				.filter(Order::isRecent)
+				.flatMap(o -> o.orderLines().stream())
+				.collect(groupingBy(OrderLine::product, summingInt(OrderLine::itemCount)));
+	}
+
 }
