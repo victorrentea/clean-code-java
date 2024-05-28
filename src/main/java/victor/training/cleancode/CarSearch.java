a3003c32a2d80f86a41c04586accf4258870ca21
Victor Rentea <victorrentea@gmail.com>
2024-05-28 11:39:05 +0300

    am introdus un obiect Interval imutabil💖 cu care am simplificat un apel de metoda

--- src/main/java/victor/training/cleancode/CarSearch.java
+++ src/main/java/victor/training/cleancode/CarSearch.java
@@ -1,48 +1,48 @@
 package victor.training.cleancode;
 
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 class CarSearch {
 
   // run tests
   public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> carModels) {
+
     List<CarModel> results = carModels.stream()
-        .filter(getCarModelPredicate(criteria))
+        .filter(carModel -> intersectsYears(criteria, carModel))
         .collect(Collectors.toList());
     System.out.println("More filtering logic ...");
     return results;
   }
 
-  private Predicate<CarModel> getCarModelPredicate(CarSearchCriteria criteria) {
-    // oare ruleaza vreodata?
-    // de cate ori ruleaza?
-    // in ce thread ruleaza?
-    // pe ce masina ruleaza
-    // in ce tx ruleaza?
-    return carModel -> MathUtil.intervalsIntersect(
-        criteria.getStartYear(), criteria.getEndYear(),
-        carModel.getStartYear(), carModel.getEndYear());
+  private boolean intersectsYears(CarSearchCriteria criteria, CarModel carModel) {
+    int start1 = criteria.getStartYear();
+    int end1 = criteria.getEndYear();
+    int start2 = carModel.getStartYear();
+    int end2 = carModel.getEndYear();
+    return MathUtil.intervalsIntersect(new Interval(start1, end1), new Interval(start2, end2));
   }
+
 }
 
 class SomeOtherClientCode {
   private void applyLengthFilter() { // pretend
-    System.out.println(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+    System.out.println(MathUtil.intervalsIntersect(new Interval(1000, 1600), new Interval(1250, 2000)));
   }
   private void applyCapacityFilter() { // pretend
-    System.out.println(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+    System.out.println(MathUtil.intervalsIntersect(new Interval(1000, 1600), new Interval(1250, 2000)));
   }
 }
 
 class MathUtil {
-
-  public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
-    return start1 <= end2 && start2 <= end1;
+  public static boolean intervalsIntersect(
+      Interval interval1, Interval interval2) {
+    return interval1.start() <= interval2.end()
+           && interval2.start() <= interval1.end();
   }
 }
-
+record Interval(int start, int end) {
+}
 
 class CarSearchCriteria { // a DTO received from JSON
   private final int startYear;
