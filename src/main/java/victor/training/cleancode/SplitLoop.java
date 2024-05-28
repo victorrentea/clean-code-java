e36c6f23ec1a52a91569e67468ef3ac98b9467ef
Victor Rentea <victorrentea@gmail.com>
2024-05-28 14:24:57 +0300

    split loop -> Stream

--- src/main/java/victor/training/cleancode/SplitLoop.java
+++ src/main/java/victor/training/cleancode/SplitLoop.java
@@ -1,78 +1,87 @@
 package victor.training.cleancode;
 
 import lombok.Builder;
-import lombok.Value;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static java.util.function.Predicate.not;
 
 /**
  * Break the loops and refactor to use .stream to compute stuff.
  */
 public class SplitLoop {
-    // see tests
-    public String computeStats(List<Employee> employees) {
-        long averageAge = 0;
-        double averageSalary = 0;
-        List<Integer> consultantIds = new ArrayList<>();
+  // see tests
+  public String computeStats(List<Employee> employees) {
+    long averageAge = employees.stream()
+        .filter(not(Employee::consultant))
+        .mapToLong(Employee::age)
+        .sum();
 
-        for (Employee employee : employees) {
-            if (!employee.consultant()) {
-                averageAge += employee.age();
-            } else {
-                consultantIds.add(employee.id());
-            }
-            averageSalary += employee.salary();
+    List<Integer> consultantIds = employees.stream()
+        .filter(Employee::consultant)
+        .map(Employee::id)
+//        .collect(Collectors.toList()); // java <17 naspa ca da ArrayList mutabil inapoi
+        .toList();
+
+
+    double averageSalary = employees
+        .stream()
+        .mapToDouble(Employee::salary)
+        .average()
+        .orElse(0);
+
+    averageAge = averageAge / employees.stream().filter(e -> !e.consultant()).count();
+//    averageSalary = averageSalary / employees.size();
+    System.out.println("Consultant IDs: " + consultantIds);
+    return "Average age = " + averageAge + "; Average salary = " + averageSalary;
+  }
+
+
+  // ======= hard core =========
+
+  EmployeeService employeeService;
+
+  public String computeStatsHard(List<Employee> employees) {
+    long totalEmpAge = 0;
+    double totalConsultantSalary = 0;
+    for (Employee employee : employees) {
+      if (!employee.consultant()) {
+        totalEmpAge += employee.age();
+        continue;
+      }
+      if (employee.id() == null) {
+        return "Employee(s) not persisted";
+      }
+
+      Integer salary = employee.salary();
+      if (salary == null) {
+        salary = employeeService.retrieveSalary(employee.id());
+        if (salary == null) {
+          throw new RuntimeException("NO salary found for employee " + employee.id());
         }
-        averageAge = averageAge / employees.stream().filter(e -> !e.consultant()).count();
-        averageSalary = averageSalary / employees.size();
-        System.out.println("Consultant IDs: " + consultantIds);
-        return "Average age = " + averageAge + "; Average salary = " + averageSalary;
+      }
+      totalConsultantSalary += salary;
     }
 
-
-    // ======= hard core =========
-
-    EmployeeService employeeService;
-
-    public String computeStatsHard(List<Employee> employees) {
-        long totalEmpAge = 0;
-        double totalConsultantSalary = 0;
-        for (Employee employee : employees) {
-            if (!employee.consultant()) {
-                totalEmpAge += employee.age();
-                continue;
-            }
-            if (employee.id() == null) {
-                return "Employee(s) not persisted";
-            }
-
-            Integer salary = employee.salary();
-            if (salary == null) {
-                salary = employeeService.retrieveSalary(employee.id());
-                if (salary == null) {
-                    throw new RuntimeException("NO salary found for employee " + employee.id());
-                }
-            }
-            totalConsultantSalary += salary;
-        }
-
-        long averageAge = 0;
-        if (totalEmpAge != 0) {
-            averageAge = totalEmpAge / employees.stream().filter(e -> !e.consultant()).count();
-        }
-        double averageConsultantSalary = 0;
-        if (totalConsultantSalary != 0) {
-            averageConsultantSalary = totalConsultantSalary / employees.size();
-        }
-        return "Average employee age = " + averageAge + "; Average consultant salary = " + averageConsultantSalary;
+    long averageAge = 0;
+    if (totalEmpAge != 0) {
+      averageAge = totalEmpAge / employees.stream().filter(e -> !e.consultant()).count();
     }
-
+    double averageConsultantSalary = 0;
+    if (totalConsultantSalary != 0) {
+      averageConsultantSalary = totalConsultantSalary / employees.size();
+    }
+    return "Average employee age = " + averageAge + "; Average consultant salary = " + averageConsultantSalary;
+  }
 
 
 }
+
 interface EmployeeService {
-    Integer retrieveSalary(int employeeId);
+  Integer retrieveSalary(int employeeId);
 }
 
 @Builder(toBuilder = true)
