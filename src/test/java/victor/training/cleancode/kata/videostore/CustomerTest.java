6df306bf609fa64ec555f52dcc29db281bf31c80
Victor Rentea <victorrentea@gmail.com>
2024-05-28 14:33:23 +0300

    java 17 ftw

--- src/test/java/victor/training/cleancode/kata/videostore/CustomerTest.java
+++ src/test/java/victor/training/cleancode/kata/videostore/CustomerTest.java
@@ -14,12 +14,13 @@ public class CustomerTest {
         customer.addRental(new Movie("Sofia", Movie.CHILDRENS), 7);
         customer.addRental(new Movie("Inception", Movie.REGULAR), 5);
         
-        String expected = "Rental Record for John Doe\n"
-                + "	Star Wars	18.0\n"
-                + "	Sofia	7.5\n"
-                + "	Inception	6.5\n"
-                + "Amount owed is 32.0\n"
-                + "You earned 4 frequent renter points";
+        String expected = """
+            Rental Record for John Doe
+            	Star Wars	18.0
+            	Sofia	7.5
+            	Inception	6.5
+            Amount owed is 32.0
+            You earned 4 frequent renter points""";
         
         assertEquals(expected, customer.statement());
     }
