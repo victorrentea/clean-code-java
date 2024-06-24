2310d4170d9f0f11573fe61c77d4bc9bfca092bb
Victor Rentea <victorrentea@gmail.com>
2024-06-24 17:17:11 +0300

    warner the callers that there might be nothing coming back from my function, by returning a Optional

--- src/main/java/victor/training/cleancode/optional/Optional_Intro.java
+++ src/main/java/victor/training/cleancode/optional/Optional_Intro.java
@@ -5,6 +5,7 @@ import victor.training.cleancode.exception.model.Customer;
 import victor.training.cleancode.exception.model.MemberCard;
 
 import java.util.Map;
+import java.util.Optional;
 
 @SuppressWarnings("ConstantConditions")
 public class Optional_Intro {
@@ -12,21 +13,30 @@ public class Optional_Intro {
     // test: 60, 10, no MemberCard
     System.out.println(getDiscountLine(new Customer(
         new MemberCard("bar", 60))));
+    System.out.println(getDiscountLine(new Customer(
+        new MemberCard("bar", 10))));
   }
 
   public static String getDiscountLine(Customer customer) {
-    Discount discount = computeDiscount(customer.getMemberCard());
-    return "You got a discount of %" + discount.globalPercentage();
+    Optional<Discount> discount = computeDiscount(customer.getMemberCard());
+    if(discount.isPresent())
+      return "You got a discount of %" + discount.get().globalPercentage();
+    else
+      return "Earn more points to get a discount!";
   }
 
-  private static Discount computeDiscount(MemberCard card) {
+  private static Optional<Discount> computeDiscount(MemberCard card) {
     if (card.getFidelityPoints() >= 100) {
-      return new Discount(5, Map.of());
+      return Optional.of(new Discount(5, Map.of()));
     }
     if (card.getFidelityPoints() >= 50) {
-      return new Discount(3, Map.of());
+      return Optional.of(new Discount(3, Map.of()));
     }
-    return null;
+//    return null; // NPE in the client
+    // Null-Object Design Pattern. example: User.NULL_USER, User.ANONYMOUS
+//    return new Discount(0, Map.of()); // the caller might not expect a Discount(0). that's not a discount.
+//    return null; // this can get you fired. it's antisocial behavior.
+    return Optional.empty();
   }
 
   public record Discount(int globalPercentage, Map<String, Integer> categoryDiscounts) {
