bfe515b502791448010c33dab43861d3dee0e87d
Victor Rentea <victorrentea@gmail.com>
2024-06-24 10:21:48 +0300

    better names

--- src/main/java/victor/training/cleancode/Guards.java
+++ src/main/java/victor/training/cleancode/Guards.java
@@ -9,33 +9,33 @@ public class Guards {
   public static final int DEAD_PAY_AMOUNT = 1;
 
   public int getPayAmount(Marine marine, BonusPackage bonusPackage) {
-    int result;
+    int r;
     if (marine != null && !(bonusPackage.value() < 10 || bonusPackage.value() > 100)) {
       if (!marine.dead()) {
         if (!marine.retired()) {
           if (marine.yearsService() != null) {
-            result = marine.yearsService() * 100 + bonusPackage.value();
+            r = marine.yearsService() * 100 + bonusPackage.value();
             if (!marine.awards().isEmpty()) {
-              result += 1000;
+              r += 1000;
 
             }
             if (marine.awards().size() >= 3) {
-              result += 2000;
+              r += 2000;
             }
             // HEAVY core logic here, business-rules ...
           } else {
             throw new IllegalArgumentException("Any marine should have the years of service set");
           }
         } else {
-          result = retiredAmount();
+          r = retiredAmount();
         }
       } else {
-        result = DEAD_PAY_AMOUNT;
+        r = DEAD_PAY_AMOUNT;
       }
     } else{
       throw new IllegalArgumentException("Not applicable!");
     }
-    return result; // TODO ALT-ENTER move return closer
+    return r; // TODO ALT-ENTER move return closer
   }
 
   private int retiredAmount() {
