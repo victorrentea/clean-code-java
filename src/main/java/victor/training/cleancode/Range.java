76a9db9b4b6e1978c66d647c25bcd58df46afb5e
Victor Rentea <victorrentea@gmail.com>
2024-06-24 15:02:12 +0300

    validation close to date in the constructor of the new object.,

--- src/main/java/victor/training/cleancode/Range.java
+++ src/main/java/victor/training/cleancode/Range.java
@@ -1,6 +1,9 @@
 package victor.training.cleancode;
 
 public record Range(int start, int end) {
+  public Range {
+    if (start > end) throw new IllegalArgumentException("start larger than end");
+  }
   public boolean intersects(Range other) {
     return start <= other.end && other.start <= end;
   }
