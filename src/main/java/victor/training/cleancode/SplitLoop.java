942fc0a58790758236d6a91621b43e3b3b4174e5
Victor Rentea <victorrentea@gmail.com>
2024-06-24 18:10:46 +0300

    polish

--- src/main/java/victor/training/cleancode/SplitLoop.java
+++ src/main/java/victor/training/cleancode/SplitLoop.java
@@ -29,52 +29,10 @@ public class SplitLoop {
         System.out.println("Consultant IDs: " + consultantIds);
         return "Average age = " + averageAge + "; Average salary = " + averageSalary;
     }
-
-
-    // ======= hard core =========
-
-    EmployeeService employeeService;
-
-    public String computeStatsHard(List<Employee> employees) {
-        long totalEmpAge = 0;
-        double totalConsultantSalary = 0;
-        for (Employee employee : employees) {
-            if (!employee.consultant()) {
-                totalEmpAge += employee.age();
-                continue;
-            }
-            if (employee.id() == null) {
-                return "Employee(s) not persisted";
-            }
-
-            Integer salary = employee.salary();
-            if (salary == null) {
-                salary = employeeService.retrieveSalary(employee.id());
-                if (salary == null) {
-                    throw new RuntimeException("NO salary found for employee " + employee.id());
-                }
-            }
-            totalConsultantSalary += salary;
-        }
-
-        long averageAge = 0;
-        if (totalEmpAge != 0) {
-            averageAge = totalEmpAge / employees.stream().filter(e -> !e.consultant()).count();
-        }
-        double averageConsultantSalary = 0;
-        if (totalConsultantSalary != 0) {
-            averageConsultantSalary = totalConsultantSalary / employees.size();
-        }
-        return "Average employee age = " + averageAge + "; Average consultant salary = " + averageConsultantSalary;
-    }
-
-
-
-}
-interface EmployeeService {
-    Integer retrieveSalary(int employeeId);
 }
 
 @Builder(toBuilder = true)
-record Employee(Integer id, int age, Integer salary, boolean consultant) {
+record Employee(Integer id,
+                int age,
+                Integer salary, boolean consultant) {
 }
