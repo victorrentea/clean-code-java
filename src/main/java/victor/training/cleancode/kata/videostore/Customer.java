ce9015ed9012b648f6cabeae9a1a920b4fa836c3
Victor Rentea <victorrentea@gmail.com>
2024-06-25 17:11:57 +0300

    done

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -6,29 +6,50 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.joining;
+
 class Customer {
 	@Getter
 	private final String name;
-	private final List<MovieRental> rentals = new ArrayList<>(); // preserves order of elements
+	private final List<MovieRental> rentals = new ArrayList<>();
 
 	public Customer(String name) {
 		this.name = name;
 	}
+
 	public void addRental(Movie movie, int numDays) {
 		rentals.add(new MovieRental(movie, numDays));
 	}
 
 	public String statement() {
-		return "Rental Record for " + getName() + "\n"
-				+ getStatementLines()
-				+ "Amount owed is " + rentals.stream().mapToDouble(MovieRental::getRentalPrice).sum() + "\n"
-				+ "You earned " + rentals.stream().mapToInt(MovieRental::getFrequentRenterPoints).sum() + " frequent renter points";
+    return header() + body() + footer();
 	}
 
-	private String getStatementLines() {
+	private String header() {
+		return "Rental Record for " + name + "\n";
+	}
+
+	private String footer() {
+		return "Amount owed is " + totalPrice() + "\n"
+					 + "You earned " + totalPoints() + " frequent renter points";
+	}
+
+	private int totalPoints() {
+		return rentals.stream().mapToInt(MovieRental::getFrequentRenterPoints).sum();
+	}
+
+	private double totalPrice() {
+		return rentals.stream().mapToDouble(MovieRental::getRentalPrice).sum();
+	}
+
+	private String body() {
 		return rentals.stream()
-				.map(movieRental -> "\t" + movieRental.movie().title() + "\t" + movieRental.getRentalPrice() + "\n")
-				.collect(Collectors.joining());
+				.map(this::bodyLine)
+				.collect(joining());
+	}
+
+	private String bodyLine(MovieRental movieRental) {
+		return "\t" + movieRental.movie().title() + "\t" + movieRental.getRentalPrice() + "\n";
 	}
 
 }
--- src/main/java/victor/training/cleancode/kata/videostore/MovieRental.java
+++ src/main/java/victor/training/cleancode/kata/videostore/MovieRental.java
@@ -1,38 +1,42 @@
 package victor.training.cleancode.kata.videostore;
 
+import static victor.training.cleancode.kata.videostore.Movie.PriceCode.NEW_RELEASE;
+
 record MovieRental(Movie movie, int numDays) {
 
-    int getFrequentRenterPoints() {
-        if (movie().priceCode() == Movie.PriceCode.NEW_RELEASE && numDays() > 1) {
-            return 2;
-        }
-        return 1;
+  int getFrequentRenterPoints() {
+    if (movie().priceCode() == NEW_RELEASE && numDays() >= 2) {
+      return 2;
     }
+    return 1;
+  }
 
-    double getRentalPrice() {
-        return switch (movie.priceCode()) {
-            case REGULAR -> getRegularPrice();
-            case NEW_RELEASE -> getNewReleasePrice();
-            case CHILDREN -> getChildrenPrice();
-        };
-    }
+  double getRentalPrice() {
+    return switch (movie.priceCode()) {
+      case REGULAR -> getRegularPrice();
+      case NEW_RELEASE -> getNewReleasePrice();
+      case CHILDREN -> getChildrenPrice();
+    };
+  }
 
-    private double getChildrenPrice() {
-        double basePrice = 1.5;
-        if (numDays > 3)
-            return basePrice + (numDays - 3) * 1.5;
-        return basePrice;
+  private double getChildrenPrice() {
+    double basePrice = 1.5;
+    if (numDays > 3) {
+      return basePrice + (numDays - 3) * 1.5;
     }
+    return basePrice;
+  }
 
-    private double getNewReleasePrice() {
-        return (numDays * 3);
-    }
+  private double getNewReleasePrice() {
+    return (numDays * 3);
+  }
 
-    private double getRegularPrice() {
-        double rentalPrice = 2;
-        if (numDays > 2)
-            return rentalPrice + (numDays - 2) * 1.5;
-        return rentalPrice;
+  private double getRegularPrice() {
+    double rentalPrice = 2;
+    if (numDays > 2) {
+      return rentalPrice + (numDays - 2) * 1.5;
     }
+    return rentalPrice;
+  }
 
 }
