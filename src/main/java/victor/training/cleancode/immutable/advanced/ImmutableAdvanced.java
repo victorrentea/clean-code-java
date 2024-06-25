a8c386b92067cf65ee13c7cd281043e1196282f6
Victor Rentea <victorrentea@gmail.com>
2024-06-25 12:02:36 +0300

    solutions to increase the semantics of an immutable object "changes".

--- src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
+++ src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
@@ -1,12 +1,9 @@
 package victor.training.cleancode.immutable.advanced;
 
 import com.google.common.collect.ImmutableList;
+import lombok.Builder;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
 
 public class ImmutableAdvanced {
   public static void main(String[] args) {
@@ -15,22 +12,46 @@ public class ImmutableAdvanced {
     Immutable immutable = new Immutable(1, 2, list, new Other(15));
     System.out.println("Before: " + immutable);
 
-    wilderness(immutable);
+    // new variable
+    Immutable movedImmutable = wilderness(immutable);
 
-    System.out.println("After:  " + immutable);
+    System.out.println("After:  " + movedImmutable);
   }
 
-  private static void wilderness(Immutable immutable) {
+  private static Immutable wilderness(Immutable immutable) {
     // dark, deep logic not expected to change the immutable object x,y
-    immutable.list().clear(); // deprecated
+//    immutable.list().clear(); //
+//    return new Immutable( #1
+//        immutable.x() + 1,
+//        immutable.y() + 1,
+//        immutable.list(), // coupling to internals
+//        immutable.other());
+    // worse: #2
+//    return immutable.toBuilder()
+//        .x(immutable.x() + 1)
+//        .y(immutable.y() + 1)
+//        .build();
+
+    //better: #3
+//    return immutable.withXY(immutable.x()+1, immutable.y()+1);
+
+    // #4
+    return immutable.move(1, 1);
   }
 }
-
+//@Builder(toBuilder = true) // lombok; bad because it lacks semantics.
 record Immutable(
     Integer x,
     Integer y,
     ImmutableList<Integer> list,
-    Other other) { // deep immutable
+    Other other) {
+  public Immutable withXY(int newX, int newY) { // wither
+    return new Immutable(newX, newY, list, other);
+  } // deep immutable
+
+  public Immutable move(int dx, int dy) { // tells a story
+    return  new Immutable(x + dx, y + dy, list, other);
+  }
 //  Immutable {
 //    list = Collections.unmodifiableList(list); // OK #1
 //  }
