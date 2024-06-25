5babe9486314cad6cb412c294dba3c7bd1a04fca
Shulamit Eliyahu <Shulamit.Eliyahu@nice.com>
2024-06-25 15:14:14 +0300

    shula

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -10,8 +10,8 @@ class Customer {
 		this.name = name;
 	}
 
-    public void addRental(Movie m, int d) {
-		rentals.put(m, d);
+    public void addRental(Movie m, int rentalDays) {
+		rentals.put(m, rentalDays);
 	}
 
 	public String statement() {
@@ -27,12 +27,12 @@ class Customer {
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if (each.getPriceCode() != null &&
-				 (each.getPriceCode() == Movie.NEW_RELEASE)
+			if (each.priceCode() != null &&
+				 (each.priceCode() == Movie.NEW_RELEASE)
 				 && daysRented > 1)
 				frequentRenterPoints++;
 			// show figures line for this rental
-			result += "\t" + each.getTitle() + "\t" + price + "\n";
+			result += "\t" + each.title() + "\t" + price + "\n";
 			totalPrice += price;
 		}
 		// add footer lines
@@ -42,14 +42,14 @@ class Customer {
 	}
 
 	private static double calculateNewPrice(Movie each, double thisAmount, int daysRented) {
-		switch (each.getPriceCode()) {
+		switch (each.priceCode()) {
 			case Movie.REGULAR -> {
 				thisAmount += 2;
 				if (daysRented > 2)
 					thisAmount += (daysRented - 2) * 1.5;
 			}
 			case Movie.NEW_RELEASE -> thisAmount += daysRented * 3;
-			case Movie.CHILDRENS -> {
+			case Movie.CHILDREN -> {
 				thisAmount += 1.5;
 				if (daysRented > 3)
 					thisAmount += (daysRented - 3) * 1.5;
