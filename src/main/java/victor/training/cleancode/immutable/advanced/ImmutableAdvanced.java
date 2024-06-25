b5c6af32410875334c818e41adfb4f6c7b231506
Victor Rentea <victorrentea@gmail.com>
2024-06-25 12:07:13 +0300

    Immutable -> Point

--- src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
+++ src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
@@ -9,16 +9,17 @@ public class ImmutableAdvanced {
   public static void main(String[] args) {
     ImmutableList<Integer> list = Stream.of(1, 2, 3).collect(ImmutableList.toImmutableList()); // ArrayList
 
-    Immutable immutable = new Immutable(1, 2, list, new Other(15));
+    Immutable immutable = new Immutable(new Point(1, 2), list, new Other(15));
     System.out.println("Before: " + immutable);
 
     // new variable
-    Immutable movedImmutable = wilderness(immutable);
+    Point newPoint = wilderness(immutable);
+    Immutable movedImmutable = immutable.withPoint(newPoint);
 
     System.out.println("After:  " + movedImmutable);
   }
 
-  private static Immutable wilderness(Immutable immutable) {
+  private static Point wilderness(Immutable immutable) {
     // dark, deep logic not expected to change the immutable object x,y
 //    immutable.list().clear(); //
 //    return new Immutable( #1
@@ -36,22 +37,30 @@ public class ImmutableAdvanced {
 //    return immutable.withXY(immutable.x()+1, immutable.y()+1);
 
     // #4
-    return immutable.move(1, 1);
+    return immutable.point().move(1, 1);
+    // #5 group together the things that change together -> Point
+  }
+}
+record Point(int x, int y) {
+  public Point move(int dx, int dy) {
+    return new Point(x + dx, y + dy);
   }
 }
 //@Builder(toBuilder = true) // lombok; bad because it lacks semantics.
 record Immutable(
-    Integer x,
-    Integer y,
+    Point point,
     ImmutableList<Integer> list,
     Other other) {
-  public Immutable withXY(int newX, int newY) { // wither
-    return new Immutable(newX, newY, list, other);
-  } // deep immutable
+//  public Immutable withXY(int newX, int newY) { // wither
+//    return new Immutable(newX, newY, list, other);
+//  } // deep immutable
 
-  public Immutable move(int dx, int dy) { // tells a story
-    return  new Immutable(x + dx, y + dy, list, other);
+  public Immutable withPoint(Point newPoint) {
+    return new Immutable(newPoint, list, other);
   }
+//  public Immutable move(int dx, int dy) { // tells a story
+//    return  new Immutable(x + dx, y + dy, list, other);
+//  }
 //  Immutable {
 //    list = Collections.unmodifiableList(list); // OK #1
 //  }
