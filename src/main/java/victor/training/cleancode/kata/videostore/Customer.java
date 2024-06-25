1f0004f0a145f657c8a4b2f75ff288708e6638e5
amichaiu <amichai.ungar@nice.com>
2024-06-25 16:11:29 +0300

    third refactor

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -1,11 +1,14 @@
 package victor.training.cleancode.kata.videostore;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 class Customer {
-	private String name;
-	private List<MovieRental> rentals = new ArrayList<>(); // preserves order of elements
+	@Getter
+	private final String name;
+	private final List<MovieRental> rentals = new ArrayList<>(); // preserves order of elements
 
 	public Customer(String name) {
 		this.name = name;
@@ -14,36 +17,28 @@ class Customer {
 	public void addRental(Movie movie, int numDays) {
 		rentals.add(new MovieRental(movie, numDays));
 	}
-
-	public String getName() {
-		return name;
-	}
-
-
 	public String statement() {
 		double totalAmount = 0;
-		int frequentRenterPoints = 0;
 		StringBuilder result = new StringBuilder();
 		result.append("Rental Record for ").append(getName()).append("\n");
 		// loop over each movie rental
 		for (MovieRental movieRental : rentals) {
-			frequentRenterPoints = calculateFrequentRenterPoints(movieRental, frequentRenterPoints);
-			Movie movie = movieRental.movie();
 			double rentalPrice = movieRental.getRentalPrice();
-			result.append("\t").append(movie.title()).append("\t").append(rentalPrice).append("\n");
-			totalAmount += rentalPrice;
+			result.append("\t").append(movieRental.movie().title()).append("\t").append(rentalPrice).append("\n");
 		}
+		totalAmount += rentals.stream().mapToDouble(MovieRental::getRentalPrice).sum();
+		int frequentRenterPoints = rentals.stream().mapToInt(Customer::getFrequentRenterPoints).sum();
 		// add footer lines
 		result.append("Amount owed is ").append(totalAmount).append("\n");
 		result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
 		return result.toString();
 	}
 
-	private static int calculateFrequentRenterPoints(MovieRental movieRental, int frequentRenterPoints) {
+	private static int getFrequentRenterPoints(MovieRental movieRental) {
         if (movieRental.movie().priceCode() == Movie.PriceCode.NEW_RELEASE && movieRental.numDays() > 1) {
-			return frequentRenterPoints + 2;
+			return 2;
 		}
-		return frequentRenterPoints + 1;
+		return 1;
 	}
 
 }
