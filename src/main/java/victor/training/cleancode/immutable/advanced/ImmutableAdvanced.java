0d04908a37abb3d0870a4d5cee9fa1081accce4b
Victor Rentea <victorrentea@gmail.com>
2024-06-25 11:54:05 +0300

    ImmutableList

--- src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
+++ src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
@@ -1,5 +1,7 @@
 package victor.training.cleancode.immutable.advanced;
 
+import com.google.common.collect.ImmutableList;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
@@ -8,7 +10,7 @@ import static java.util.stream.Collectors.toList;
 
 public class ImmutableAdvanced {
   public static void main(String[] args) {
-    List<Integer> list = Stream.of(1, 2, 3).collect(toList()); // ArrayList
+    ImmutableList<Integer> list = Stream.of(1, 2, 3).collect(ImmutableList.toImmutableList()); // ArrayList
 
     Immutable immutable = new Immutable(1, 2, list, new Other(15));
     System.out.println("Before: " + immutable);
@@ -20,22 +22,22 @@ public class ImmutableAdvanced {
 
   private static void wilderness(Immutable immutable) {
     // dark, deep logic not expected to change the immutable object x,y
-    immutable.list().clear();
+    immutable.list().clear(); // deprecated
   }
 }
 
 record Immutable(
     Integer x,
     Integer y,
-    List<Integer> list,
+    ImmutableList<Integer> list,
     Other other) { // deep immutable
 //  Immutable {
 //    list = Collections.unmodifiableList(list); // OK #1
 //  }
-  public List<Integer> list() {
-    return Collections.unmodifiableList(list); // OK #2
+//  public List<Integer> list() {
+//    return Collections.unmodifiableList(list); // OK #2
 //    return new ArrayList<>(list);// NO: -performance -misleading for caller
-  }
+//  }
 }
 
 record Other(int a) {
