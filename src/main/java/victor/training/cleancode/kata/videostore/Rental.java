b0fb83c1808e3d8320b2a77a4be18c266f8b42e8
Victor Rentea <victorrentea@gmail.com>
2024-06-25 17:24:37 +0300

    yolo

--- src/main/java/victor/training/cleancode/kata/videostore/Rental.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Rental.java
@@ -2,6 +2,6 @@ package victor.training.cleancode.kata.videostore;
 
 public record Rental(Movie movie, int daysRented) {
     double calculatePrice() {
-        return movie().priceCode().calculatePrice(daysRented());
+        return movie.priceCode().calculatePrice(daysRented);
     }
 }
