eab34216153f8dfa40026c06791de261731f3492
Sagi Ifrah <Sagi.Ifrah@nice.com>
2024-06-25 16:39:51 +0300

    refactor map to record, convert for loop to stream

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -4,46 +4,49 @@ import lombok.Getter;
 
 import java.util.*;
 
-import static victor.training.cleancode.kata.videostore.PriceCode.NEW_RELEASE;
-
 class Customer {
 	@Getter
     private String name;
-	private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order of elements
+	//	private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order of elements
+	private final List<Rental> rentals = new ArrayList<>(); // preserves order of elements
 
 	public Customer(String name) {
 		this.name = name;
 	};
 
-	public void addRental(Movie m, int d) {
-		rentals.put(m, d);
+	public void addRental(Movie movie, int days) {
+		rentals.add(new Rental(movie, days));
 	}
 
     public String statement() {
 		double totalAmount = 0;
-        String result = "Rental Record for " + getName() + "\n";
+        StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n").append(getReportSummary());
 		// loop over each movie rental
-		Set<Movie> movies = rentals.keySet();
-        int frequentRenterPoints = getFrequentRenterPoints(movies);
-
-		for (Movie movie : movies) {
-			// determine amounts for every line
-			int daysRented = rentals.get(movie);
-			double price = movie.priceCode().getPrice(daysRented);
-			// show figures line for this rental
-			result += "\t" + movie.title() + "\t" + price + "\n";
-			totalAmount += price;
-		}
+        int frequentRenterPoints = getFrequentRenterPoints(rentals);
+		totalAmount = getTotalAmount();
 
 		// add footer lines
-		result += "Amount owed is " + totalAmount + "\n";
-		result += "You earned " + frequentRenterPoints + " frequent renter points";
+		result.append("Amount owed is ").append(totalAmount).append("\n");
+		result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
+		return result.toString();
+	}
+
+	private StringBuilder getReportSummary() {
+		StringBuilder result = new StringBuilder("");
+        // determine amounts for every line
+        // show figures line for this rental
+        rentals.forEach(rental -> result.append("\t").append(rental.movie().title()).append("\t")
+				.append(rental.movie().priceCode().getPrice(rental.daysRented())).append("\n"));
 		return result;
 	}
 
-	private int getFrequentRenterPoints(Set<Movie> movies) {
-		return movies.stream()
-				.mapToInt(movie -> (int) movie.priceCode().getFrequentRenterPoints(rentals.get(movie)))
+	private double getTotalAmount() {
+		return rentals.stream().mapToDouble(rental -> rental.movie().priceCode().getPrice(rental.daysRented())).sum();
+	}
+
+	private int getFrequentRenterPoints(List<Rental> rentals) {
+		return rentals.stream()
+				.mapToInt(rental -> (int) rental.movie().priceCode().getFrequentRenterPoints(rental.daysRented()))
 				.sum();
 
 	}
---
+++ src/main/java/victor/training/cleancode/kata/videostore/Rental.java
@@ -0,0 +1,4 @@
+package victor.training.cleancode.kata.videostore;
+
+
+public record Rental(Movie movie, int daysRented) {}
