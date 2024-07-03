eb72429ad8930a1bb1a00e98ef727b2f98235dd6
Victor Rentea <victorrentea@gmail.com>
2024-07-03 17:12:41 +0300

    if ca pe vremuri dar cu mesaj corect pus sa manipulam emotional clientu

--- src/main/java/victor/training/cleancode/optional/Optional_Intro.java
+++ src/main/java/victor/training/cleancode/optional/Optional_Intro.java
@@ -5,28 +5,32 @@ import victor.training.cleancode.exception.model.Customer;
 import victor.training.cleancode.exception.model.MemberCard;
 
 import java.util.Map;
+import java.util.Optional;
 
 @SuppressWarnings("ConstantConditions")
 public class Optional_Intro {
   public static void main(String[] args) {
     // test with 10 points or no MemberCard
-    System.out.println(getDiscountLine(new Customer(
-        new MemberCard("bar", 60))));
+    System.out.println(getDiscountLine(new Customer(new MemberCard("bar", 60))));
+    System.out.println(getDiscountLine(new Customer(new MemberCard("bar", 1))));
   }
 
   public static String getDiscountLine(Customer customer) {
-    Discount discount = computeDiscount(customer.getMemberCard());
-    return "You got a discount of %" + discount.globalPercentage();
+    Optional<Discount> discount = computeDiscount(customer.getMemberCard());
+    if (discount.isPresent()) {
+      return "You got a discount of %" + discount.get().globalPercentage();
+    }
+    return "Earn more points to get a discount"; // FOMO = Fear Of Missing Out
   }
 
-  private static Discount computeDiscount(MemberCard card) {
+  private static Optional<Discount> computeDiscount(MemberCard card) {
     if (card.getFidelityPoints() >= 100) {
-      return new Discount(5, Map.of());
+      return Optional.of(new Discount(5, Map.of()));
     }
     if (card.getFidelityPoints() >= 50) {
-      return new Discount(3, Map.of());
+      return Optional.of(new Discount(3, Map.of()));
     }
-    return null;
+    return Optional.empty();
   }
 
   public record Discount(int globalPercentage, Map<String, Integer> categoryDiscounts) {
