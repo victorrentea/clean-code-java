85388b30db1825253e5744eedb8c94fa95940930
Victor Rentea <victorrentea@gmail.com>
2024-07-04 12:24:25 +0300

    nu mutez ci intorc

--- src/main/java/victor/training/cleancode/fp/MutantPipeline.java
+++ src/main/java/victor/training/cleancode/fp/MutantPipeline.java
@@ -3,27 +3,31 @@ package victor.training.cleancode.fp;
 import victor.training.cleancode.fp.support.Order;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class MutantPipeline {
   public int totalActiveOrderPrice(List<Order> orders) {
-    int sum = 0; // stiva thread curent
-    orders.parallelStream()
+    // compute and return, don;t change
+    return orders.parallelStream()
         .filter(Order::isActive)
-//        .forEach(order -> sum += order.price()); // nu poate pt ca ar permite modificari concurente la thread stack
-        .forEach(order -> sum += order.price());
-    return sum;
+        .mapToInt(Order::price)
+        .sum();// js: .reduce((acc, order) => acc + order.price, 0);
   }
 
-
   public List<LocalDate> getShipDates(List<Order> orders) {
-    List<LocalDate> shipDates = new ArrayList<>();
-    orders.stream()
-        .filter(order -> order.isActive())
-        .forEach(order -> {
-          order.shipDate().ifPresent(date -> shipDates.add(date));
-        });
+    List<LocalDate> shipDates = orders.stream()
+        .filter(Order::isActive)
+//        .forEach(order -> {
+//          order.shipDate().ifPresent(date -> shipDates.add(date));
+//        });
+        // merge, nu mutezi nimic dupa ->
+//        .filter(order -> order.shipDate().isPresent())
+//        .map(order -> order.shipDate().get())
+        // mai simplu
+        .map(Order::shipDate)
+        .flatMap(Optional::stream)
+        .toList();
     return shipDates;
   }
 }
