cc50c3baecd38887172de2ad7e9d71c6d5808fd1
Victor Rentea <victorrentea@gmail.com>
2024-11-07 00:40:52 +0200

    ImmutableList ftw

--- src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
+++ src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
@@ -1,13 +1,14 @@
 package victor.training.cleancode.immutable.advanced;
 
-import java.util.List;
+import com.google.common.collect.ImmutableList;
+
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toList;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 
 public class ImmutableAdvanced {
   public static void main(String[] args) {
-    List<Integer> list = Stream.of(1, 2, 3).collect(toList()); // ArrayList
+    ImmutableList<Integer> list = Stream.of(1, 2, 3).collect(toImmutableList()); // ArrayList
 
     Immutable immutable = new Immutable(1, 2, list, new Other(15));
     System.out.println("Before: " + immutable);
@@ -26,21 +27,17 @@ public class ImmutableAdvanced {
 class Immutable { // shallow immutable
   private final Integer x;
   private final Integer y;
-  private final List<Integer> list;
+  private final ImmutableList<Integer> list; // NOT friends with Hibernate, but OK with Jackson, Mongo...
   private final Other other;
 
-  Immutable(Integer x, Integer y, List<Integer> list, Other other) {
+  Immutable(Integer x, Integer y, ImmutableList<Integer> list, Other other) {
     this.x = x;
     this.y = y;
-    this.list = List.copyOf(list);
+    this.list = list;
     this.other = other;
   }
 
-//  public Iterator<Integer> listIterator() {
-//    return list.iterator();
-//  }
-
-  public List<Integer> getList() {
+  public ImmutableList<Integer> getList() {
     return list;
   }
 
