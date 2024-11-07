52cee85533d593c789ca160ca7e8edfa51082a24
Mikhail Shnayderman <Mikhail.Shnayderman@nice.com>
2024-11-07 16:04:26 -0600

    refactor: Added PriceCode enum

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -27,7 +27,7 @@ class Customer {
 			// determine amounts for every line
 			int daysRented = rentals.get(movie);
 
-			double thisAmount = calculateAmount(movie, daysRented);
+			double thisAmount = movie.priceCode().calculateAmount(daysRented);
 
 			// add frequent renter points
 			frequentRenterPoints += calculateFrequentRentalPoints(movie.priceCode(), daysRented);
@@ -42,28 +42,8 @@ class Customer {
 		return result;
 	}
 
-	private static int calculateFrequentRentalPoints(int priceCode, int daysRented) {
-		return priceCode == Movie.NEW_RELEASE && daysRented > 1 ? 2 : 1;
+	private static int calculateFrequentRentalPoints(Movie.PriceCode priceCode, int daysRented) {
+		return priceCode == Movie.PriceCode.NEW_RELEASE && daysRented > 1 ? 2 : 1;
 	}
-
-	private static double calculateAmount(Movie each, int dr) {
-		double thisAmount = 0;
-
-		switch (each.priceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (dr > 2)
-					thisAmount += (dr - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += dr * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (dr > 3)
-					thisAmount += (dr - 3) * 1.5;
-				break;
-		}
-		return thisAmount;
-	}
+	
 }
