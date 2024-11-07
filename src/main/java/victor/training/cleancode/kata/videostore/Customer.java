7970dd483db478c17663a4feaf888c1f5829c007
Mikhail Shnayderman <Mikhail.Shnayderman@nice.com>
2024-11-07 15:46:52 -0600

    refactor: Extracted methods computing amounts

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -23,39 +23,47 @@ class Customer {
 		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
 		// loop over each movie rental
-		for (Movie each : rentals.keySet()) {
-			double thisAmount = 0;
+		for (Movie movie : rentals.keySet()) {
 			// determine amounts for every line
-			int dr = rentals.get(each);
-			switch (each.getPriceCode()) {
-				case Movie.REGULAR:
-					thisAmount += 2;
-					if (dr > 2)
-						thisAmount += (dr - 2) * 1.5;
-					break;
-				case Movie.NEW_RELEASE:
-					thisAmount += dr * 3;
-					break;
-				case Movie.CHILDRENS:
-					thisAmount += 1.5;
-					if (dr > 3)
-						thisAmount += (dr - 3) * 1.5;
-					break;
-			}
+			int daysRented = rentals.get(movie);
+
+			double thisAmount = calculateAmount(movie, daysRented);
+
 			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if (each.getPriceCode() != null &&
-				 (each.getPriceCode() == Movie.NEW_RELEASE)
-				 && dr > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints += calculateFrequentRentalPoints(movie.getPriceCode(), daysRented);
+
 			// show figures line for this rental
-			result += "\t" + each.getTitle() + "\t" + thisAmount + "\n";
+			result += "\t" + movie.getTitle() + "\t" + thisAmount + "\n";
 			totalAmount += thisAmount;
 		}
 		// add footer lines
 		result += "Amount owed is " + totalAmount + "\n";
 		result += "You earned " + frequentRenterPoints + " frequent renter points";
 		return result;
+	}
+
+	private static int calculateFrequentRentalPoints(int priceCode, int daysRented) {
+		return priceCode == Movie.NEW_RELEASE && daysRented > 1 ? 2 : 1;
+	}
+
+	private static double calculateAmount(Movie each, int dr) {
+		double thisAmount = 0;
+
+		switch (each.getPriceCode()) {
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (dr > 2)
+					thisAmount += (dr - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount += dr * 3;
+				break;
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (dr > 3)
+					thisAmount += (dr - 3) * 1.5;
+				break;
+		}
+		return thisAmount;
 	}
 }
