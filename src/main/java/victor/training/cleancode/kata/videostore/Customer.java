47032439a28c37c11fcb6f4add2be5fa4ec04f5d
alexbatt1980 <96565876+alexbatt1980@users.noreply.github.com>
2024-11-07 15:16:06 -0700

    move logic to enum

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -1,41 +1,41 @@
 package victor.training.cleancode.kata.videostore;
 
+import lombok.Getter;
+
 import java.util.*;
 
 class Customer {
-	private final String name;
-	private Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order of elements
+	@Getter
+    private final String name;
+	private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order of elements
 
 	public Customer(String name) {
 		this.name = name;
-	};
+	}
 
 	public void addRental(Movie m, int d) {
 		rentals.put(m, d);
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public String statement() {
+    public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
 		// loop over each movie rental
-		for (Movie each : rentals.keySet()) {
+		for (Movie movie : rentals.keySet()) {
 			// determine amounts for every line
-			int daysRental = rentals.get(each);
-			double thisAmount = calculateRentalCost(each, daysRental);
+			int daysRental = rentals.get(movie);
+
+			double thisAmount = movie.priceCategory().calculateRentalPrice(daysRental);
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two days new release rental
-			if (each.priceCategory() != null &&
-				 (each.priceCategory() == PriceCategory.NEW_RELEASE)
+			if (movie.priceCategory() != null &&
+				 (movie.priceCategory() == PriceCategory.NEW_RELEASE)
 				 && daysRental > 1)
 				frequentRenterPoints++;
 			// show figures line for this rental
-			result += "\t" + each.title() + "\t" + thisAmount + "\n";
+			result += "\t" + movie.title() + "\t" + thisAmount + "\n";
 			totalAmount += thisAmount;
 		}
 		// add footer lines
@@ -44,24 +44,4 @@ class Customer {
 		return result;
 	}
 
-	private static double calculateRentalCost(Movie each, int dr)
-	{
-		double thisAmount = 0;
-		switch (each.priceCategory()) {
-			case REGULAR:
-				thisAmount += 2;
-				if (dr > 2)
-					thisAmount += (dr - 2) * 1.5;
-				break;
-			case NEW_RELEASE:
-				thisAmount += dr * 3;
-				break;
-			case CHILDRENS:
-				thisAmount += 1.5;
-				if (dr > 3)
-					thisAmount += (dr - 3) * 1.5;
-				break;
-		}
-		return thisAmount;
-	}
 }
--- src/main/java/victor/training/cleancode/kata/videostore/PriceCategory.java
+++ src/main/java/victor/training/cleancode/kata/videostore/PriceCategory.java
@@ -1,7 +1,33 @@
 package victor.training.cleancode.kata.videostore;
 
 public enum PriceCategory {
-    CHILDRENS,
-    REGULAR,
-    NEW_RELEASE
+    CHILDRENS {
+        @Override
+        double calculateRentalPrice(int daysRented) {
+            double rentalCost = 1.5;
+            if (daysRented > 3) {
+                rentalCost += (daysRented - 3) * 1.5;
+            }
+            return rentalCost;
+        }
+    },
+    REGULAR {
+        @Override
+        double calculateRentalPrice(int daysRented) {
+            double rentalCost = 2;
+            if (daysRented > 2) {
+                rentalCost += (daysRented - 2) * 1.5;
+            }
+            return rentalCost;
+        }
+    },
+    NEW_RELEASE {
+        @Override
+        double calculateRentalPrice(int daysRented) {
+            return  daysRented * 3;
+        }
+    };
+
+
+    abstract double calculateRentalPrice(int daysRented);
 }
