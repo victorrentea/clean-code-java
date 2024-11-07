5767de04e19ef3ef257d6952cf3ac717e4392f91
ehowe <ehowe@nexidia.com>
2024-11-07 17:31:34 -0500

    more fun

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -5,43 +5,52 @@ import lombok.Getter;
 import java.util.*;
 
 class Customer {
-	@Getter
+    @Getter
     private final String name;
-	private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order of elements
+    private final List<Rental> rentals = new ArrayList<>(); // preserves order of elements
 
-	public Customer(String name) {
-		this.name = name;
-	}
+    public Customer(String name) {
+        this.name = name;
+    }
 
-	public void addRental(Movie m, int d) {
-		rentals.put(m, d);
-	}
+    public void addRental(Movie m, int daysRented) {
+        rentals.add(new Rental(m, daysRented));
+    }
 
     public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		String result = "Rental Record for " + getName() + "\n";
-		// loop over each movie rental
-		for (Movie movie : rentals.keySet()) {
-			// determine amounts for every line
-			int daysRental = rentals.get(movie);
-
-			double thisAmount = movie.priceCategory().calculateRentalPrice(daysRental);
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two days new release rental
-			if (movie.priceCategory() != null &&
-				 (movie.priceCategory() == PriceCategory.NEW_RELEASE)
-				 && daysRental > 1)
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        String result = "Rental Record for " + getName() + "\n";
+        // loop over each movie rental
+        for (Rental rental : rentals) {
+			Movie movie = rental.movie();
+            double movieCost = movie.priceCategory().calculateRentalPrice(rental.daysRented());
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two days new release rental
+			if (movie.priceCategory() == PriceCategory.NEW_RELEASE && rental.daysRented() > 1) {
 				frequentRenterPoints++;
-			// show figures line for this rental
-			result += "\t" + movie.title() + "\t" + thisAmount + "\n";
-			totalAmount += thisAmount;
-		}
-		// add footer lines
-		result += "Amount owed is " + totalAmount + "\n";
-		result += "You earned " + frequentRenterPoints + " frequent renter points";
-		return result;
-	}
-
+			}
+            // show figures line for this rental
+            result += "\t" + movie.title() + "\t" + movieCost + "\n";
+            totalAmount += movieCost;
+        }
+        for (Rental rental : rentals) {
+			Movie movie = rental.movie();
+            double movieCost = movie.priceCategory().calculateRentalPrice(rental.daysRented());
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two days new release rental
+			if (movie.priceCategory() == PriceCategory.NEW_RELEASE && rental.daysRented() > 1) {
+				frequentRenterPoints++;
+			}
+            // show figures line for this rental
+            result += "\t" + movie.title() + "\t" + movieCost + "\n";
+            totalAmount += movieCost;
+        }
+        // add footer lines
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        return result;
+    }
 }
