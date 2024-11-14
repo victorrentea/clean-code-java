c354613a0f8b37b795f4716642f79ec9f9a0dcdc
Victor Rentea <victorrentea@gmail.com>
2024-11-14 11:52:17 +0200

    deep immutable

--- src/main/java/victor/training/cleancode/PrimitiveObsession.java
+++ src/main/java/victor/training/cleancode/PrimitiveObsession.java
@@ -1,18 +1,26 @@
 package victor.training.cleancode;
 
+import java.util.Arrays;
 import java.util.Map;
 
 import static java.util.stream.Collectors.joining;
+import static victor.training.cleancode.PrimitiveObsession.PaymentMethod.CARD;
+import static victor.training.cleancode.PrimitiveObsession.PaymentMethod.CASH;
 
 public class PrimitiveObsession {
-
   public static void main(String[] args) {
-    new PrimitiveObsession().primitiveObsession("CARD");
+    new PrimitiveObsession().entryPoint("CARD");
+    new PrimitiveObsession().entryPoint("CArD");
+    new PrimitiveObsession().entryPoint("CREDIT");
+    new PrimitiveObsession().entryPoint("CARD ");
+    new PrimitiveObsession().entryPoint(" ");
+    new PrimitiveObsession().entryPoint(""); // SCARY
+    new PrimitiveObsession().entryPoint(null); // SCARY
   }
 
   //<editor-fold desc="fetchData()">
-  public Map<Long, Map<String, Integer>> fetchData(String paymentMethod) {
-    Long customerId = 1L;
+  public Map<CustomerId, Map<String, Integer>> fetchData(PaymentMethod paymentMethod) {
+    CustomerId customerId = new CustomerId(1L);
     Integer product1Count = 2;
     Integer product2Count = 4;
     return Map.of(customerId, Map.of(
@@ -20,19 +28,37 @@ public class PrimitiveObsession {
         "Chair", product2Count
     ));
   }
+
+  public void entryPoint(String paymentMethod) {// called with String, JSON.XML,,....
+    foo(PaymentMethod.valueOf(paymentMethod)); // conversion aka mapper/transformer
+  }
   //</editor-fold>
 
-  public void primitiveObsession(String paymentMethod) {
-    if (!"CARD".equals(paymentMethod) && !"CASH".equals(paymentMethod)) {
-      throw new IllegalArgumentException("Only CARD payment method is supported");
+  public void foo(PaymentMethod paymentMethod) {
+//    if (!paymentMethod.equals("CARD") && !"CASH".equals(paymentMethod)) {// BAD
+//    if (!"CARD".equals(paymentMethod) && !"CASH".equals(paymentMethod)) { // NULL-DRIVEN DEVELOPMENT
+//    if (paymentMethod != PaymentMethod.CARD && paymentMethod != PaymentMethod.CASH) {
+    if (!paymentMethod.oneOf(CARD, CASH)) {
+      throw new IllegalArgumentException("Only CARD or CASH payment method is supported");
     }
-    Map<Long, Map<String, Integer>> map = fetchData(paymentMethod);
+    Map<CustomerId, Map<String, Integer>> customerIdToProductCounts = fetchData(paymentMethod);
 
-    for (var e : map.entrySet()) { // iterating map entries 🤢
+    for (var e : customerIdToProductCounts.entrySet()) { // iterating map entries 🤢
       String pl = e.getValue().entrySet().stream()
           .map(entry -> entry.getValue() + " pcs. of " + entry.getKey())
           .collect(joining(", "));
-      System.out.println("cid=" + e.getKey() + " got " + pl);
+      System.out.println("cid=" + e.getKey().id() + " got " + pl);
     }
+  }
+
+  enum PaymentMethod {
+    CARD, CASH;
+
+    public boolean oneOf(PaymentMethod... values) {
+      return Arrays.stream(values).anyMatch(value -> value.equals(this));
+    }
+  }
+
+  record CustomerId(long id) {
   }
 }
--- src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
+++ src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
@@ -1,13 +1,15 @@
 package victor.training.cleancode.immutable.advanced;
 
+import com.google.common.collect.ImmutableList;
+
 import java.util.List;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toList;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 
 public class ImmutableAdvanced {
   public static void main(String[] args) {
-    List<Integer> list = Stream.of(1, 2, 3).collect(toList()); // ArrayList
+    ImmutableList<Integer> list = Stream.of(1, 2, 3).collect(toImmutableList()); // ArrayList
 
     Immutable immutable = new Immutable(1, 2, list, new Other(15));
     System.out.println("Before: " + immutable);
@@ -19,22 +21,28 @@ public class ImmutableAdvanced {
 
   private static void wilderness(Immutable immutable) {
     // dark, deep logic not expected to change the immutable object x,y
+//    immutable.getList().clear();
   }
 }
 
-class Immutable {
+class Immutable { // deep immutable
   private final Integer x;
   private final Integer y;
-  private final List<Integer> list;
+  private final ImmutableList<Integer> list; // 3
   private final Other other;
 
-  Immutable(Integer x, Integer y, List<Integer> list, Other other) {
+  Immutable(Integer x, Integer y, ImmutableList<Integer> list, Other other) {
     this.x = x;
     this.y = y;
-    this.list = list;
+    this.list = list; //2 immutable clone
+//    this.list = List.copyOf(list); //2 immutable clone
     this.other = other;
   }
 
+  // 1
+//  public List<Integer> getList() {
+//    return Collections.unmodifiableList(list);
+//  }
   public List<Integer> getList() {
     return list;
   }
@@ -57,18 +65,5 @@ class Immutable {
   }
 }
 
-class Other {
-  private int a;
-
-  public Other(int a) {
-    this.a = a;
-  }
-
-  public int getA() {
-    return a;
-  }
-
-  public void setA(int a) {
-    this.a = a;
-  }
+record Other(int a) {
 }
