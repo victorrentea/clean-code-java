453934b39b5fb6bf27cb5710457c5cf804ec449e
jaffaskree <benoit.antoine@weben.be>
2024-11-15 12:42:00 +0100

    wip

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -1,46 +1,38 @@
 package victor.training.cleancode.kata.videostore;
 
+import lombok.Getter;
+
 import java.util.*;
 
 class Customer {
-	private final String name;
-	private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order of elements
+	private static final double DEFAULT_RATE = 1.5;
+	record Rental(Movie movie, int daysDuration){}
+	@Getter
+    private final String name;
+	private final List<Rental> rentals = new ArrayList<>();
+
 
 	public Customer(String name) {
 		this.name = name;
 	}
 
-	public void addRental(Movie movie, int d) {
-		rentals.put(movie, d);
+	public void addRental(Movie movie, int daysDuration) {
+		rentals.add(new Rental(movie, daysDuration));
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public String statement() {
+    public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
+
 		// loop over each movie rental
-		for (Movie movieRental : rentals.keySet()) {
-			double thisAmount = 0;
-			// determine amounts for every line
-			int dr = rentals.get(movieRental);
-			switch (movieRental.getMovieType()) {
-				case REGULAR:
-					thisAmount = calculateForRegular(thisAmount, dr);
-					break;
-				case NEW_RELEASE:
-					thisAmount = calculateForNewRelease(thisAmount, dr);
-					break;
-				case CHILDREN:
-					thisAmount = calculateForChildren(thisAmount, dr);
-					break;
-			}
-			frequentRenterPoints = calculateRenterPoints(movieRental, frequentRenterPoints, dr);
+		for (Rental rental : rentals) {
+            // determine amounts for every line
+			int daysDuration = rental.daysDuration;
+            double thisAmount = calculateRentalPrice(rental, daysDuration);
+			frequentRenterPoints = calculateRenterPoints(rental, frequentRenterPoints, daysDuration);
 			// show figures line for this rental
-			result.append("\t").append(movieRental.getTitle()).append("\t").append(thisAmount).append("\n");
+			result.append("\t").append(rental.movie.getTitle()).append("\t").append(thisAmount).append("\n");
 			totalAmount += thisAmount;
 		}
 		
@@ -50,34 +42,21 @@ class Customer {
 		return result.toString();
 	}
 
-	private static int calculateRenterPoints(Movie movieRental, int frequentRenterPoints, int dr) {
+	private static double calculateRentalPrice(Rental rental, int daysDuration) {
+		return switch (rental.movie.getMovieType()) {
+			case REGULAR -> daysDuration > 2 ? 2 + (daysDuration - 2) * DEFAULT_RATE : 2;
+			case NEW_RELEASE -> daysDuration * 3;
+			case CHILDREN -> daysDuration > 3 ? 1.5 + (daysDuration - 3) * DEFAULT_RATE : 1.5;
+		};
+	}
+
+	private static int calculateRenterPoints(Rental rental, int frequentRenterPoints, int dr) {
 		// add frequent renter points
 		frequentRenterPoints++;
-		// add bonus for a two day new release rental
-		if ((movieRental.getMovieType() == MovieType.NEW_RELEASE )  && dr > 1)
+		// add bonus for a two days new release rental
+		if ((rental.movie.getMovieType() == MovieType.NEW_RELEASE )  && dr > 1)
 			frequentRenterPoints++;
 		return frequentRenterPoints;
 	}
 
-	private static final double DEFAULT_RATE = 1.5;
-
-	private static double calculateForChildren(double thisAmount, int dr) {
-		 double newAmount = thisAmount + 1.5;
-		if (dr > 3)
-			newAmount += (dr - 3) * DEFAULT_RATE;
-		return newAmount;
-	}
-
-	private static double calculateForNewRelease(double thisAmount, int dr) {
-
-		return thisAmount += dr * 3;
-
-	}
-
-	private static double calculateForRegular(double thisAmount, int dr) {
-		thisAmount += 2;
-		if (dr > 2)
-			thisAmount += (dr - 2) * DEFAULT_RATE;
-		return thisAmount;
-	}
 }
