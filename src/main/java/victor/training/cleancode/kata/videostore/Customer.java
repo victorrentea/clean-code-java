d975cfe2cdb1048bb733f13a652f33c604017494
Vyacheslav Tokarev <vyacheslav.tokarev@gmail.com>
2024-11-15 12:40:37 +0100

    Uraa

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -19,49 +19,36 @@ class Customer {
 	}
 
     public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		String result = "Rental Record for " + getName() + "\n";
-		// loop over each movie rental
-		for (Rental rental : rentals) {
-			double thisAmount = 0;
-			// determine amounts for every line
-			int durationOfRental = rental.duration();
-			thisAmount = getPrice(rental.movie(), thisAmount, durationOfRental);
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if (rental.movie().getPriceCode() != null &&
-				(rental.movie().getPriceCode() == PriceCategory.NEW_RELEASE)
-				&& durationOfRental > 1)
-				frequentRenterPoints++;
-			// show figures line for this rental
-			result += "\t" + rental.movie().getTitle() + "\t" + thisAmount + "\n";
-			totalAmount += thisAmount;
+		return header() +  body() + footer();
+	}
 
-                        }
+	private String footer() {
+		String result = "";
+		int frequentRenterPoints = rentals.stream()
+				.mapToInt(Rental::frequentRenterPoints)
+				.sum();
+
+		double totalAmount = rentals.stream()
+				.mapToDouble(Rental::price)
+				.sum();
+
 		// add footer lines
 		result += "Amount owed is " + totalAmount + "\n";
 		result += "You earned " + frequentRenterPoints + " frequent renter points";
 		return result;
 	}
 
-	private static double getPrice(Movie each, double thisAmount, int dr) {
-		switch (each.getPriceCode()) {
-			case REGULAR:
-				thisAmount += 2;
-				if (dr > 2)
-					thisAmount += (dr - 2) * 1.5;
-				break;
-			case NEW_RELEASE:
-				thisAmount += dr * 3;
-				break;
-			case CHILDRENS:
-				thisAmount += 1.5;
-				if (dr > 3)
-					thisAmount += (dr - 3) * 1.5;
-				break;
+	private String body() {
+		String result = "";
+		for (Rental rental : rentals) {
+			result += "\t" + rental.movie().getTitle() + "\t" + rental.price() + "\n";
 		}
-		return thisAmount;
+		return result;
 	}
+
+	private String header() {
+		String result = "Rental Record for " + getName() + "\n";
+		return result;
+	}
+
 }
--- src/main/java/victor/training/cleancode/kata/videostore/Rental.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Rental.java
@@ -1,4 +1,29 @@
 package victor.training.cleancode.kata.videostore;
 
 public record Rental(Movie movie, int duration) {
+    double price() {
+        double price = 0;
+        switch (movie.getPriceCode()) {
+            case REGULAR -> {
+                price += 2;
+                if (duration > 2)
+                    price += (duration - 2) * 1.5;
+            }
+            case NEW_RELEASE -> price += duration * 3;
+            case CHILDRENS -> {
+                price += 1.5;
+                if (duration > 3)
+                    price += (duration - 3) * 1.5;
+            }
+        }
+        return price;
+    }
+
+    int frequentRenterPoints() {
+        int points = 1;
+        // add bonus for a two day new release rental
+        if ((movie().getPriceCode() == PriceCategory.NEW_RELEASE) && duration() > 1)
+            points++;
+        return points;
+    }
 }
