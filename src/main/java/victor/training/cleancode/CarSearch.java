77035617c95e97f914954d1143c75bee782a0f47
Victor Rentea <victorrentea@gmail.com>
2024-11-25 14:28:02 +0200

    we identified a new concept "interval" and we captured it in a new Value Object (DESIGN PATTERN) - we added logic to it (intersects) - we used it to simplify other larger structures

--- src/main/java/victor/training/cleancode/CarSearch.java
+++ src/main/java/victor/training/cleancode/CarSearch.java
@@ -1,35 +1,48 @@
 package victor.training.cleancode;
 
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 class CarSearch {
 
   // run tests
-  public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> carModels) {
+  public List<CarModel> filterCarModels(
+      CarSearchCriteria criteria,
+      List<CarModel> carModels) {
+
     List<CarModel> results = carModels.stream()
-        .filter(carModel -> MathUtil.intervalsIntersect(
-            criteria.getStartYear(), criteria.getEndYear(),
-            carModel.getStartYear(), carModel.getEndYear()))
+        .filter(carModel -> criteria.getYearInterval().intersects(carModel.getYearInterval()))
         .collect(Collectors.toList());
     System.out.println("More filtering logic ...");
     return results;
   }
+
 }
 
 class SomeOtherClientCode {
   private void applyLengthFilter() { // pretend
-    System.out.println(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+    System.out.println(new Interval(1000, 1600).intersects(new Interval(1250, 2000)));
   }
+
   private void applyCapacityFilter() { // pretend
-    System.out.println(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+    System.out.println(new Interval(1000, 1600).intersects(new Interval(1250, 2000)));
   }
 }
 
-class MathUtil {
+class TimeUtils {
 
-  public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
-    return start1 <= end2 && start2 <= end1;
+}
+
+//class Period,Interval,Range
+// Value Object = Immutable, lacks identity (no PK)
+// eg> Money{amount, currency}
+@Embeddable
+record Interval(int start, int end) {
+  public boolean intersects(Interval interval2) {
+    return start <= interval2.end && interval2.start <= end;
   }
 }
 
@@ -37,6 +50,7 @@ class MathUtil {
 class CarSearchCriteria { // a DTO received from JSON
   private final int startYear;
   private final int endYear;
+  //  private final Interval yearInterval;
   private final String make;
 
   public CarSearchCriteria(int startYear, int endYear, String make) {
@@ -44,6 +58,11 @@ class CarSearchCriteria { // a DTO received from JSON
     if (startYear > endYear) throw new IllegalArgumentException("start larger than end");
     this.startYear = startYear;
     this.endYear = endYear;
+  }
+
+  //  @JsonIgnore
+  public Interval getYearInterval() { // helper methods in the DTO
+    return new Interval(getStartYear(), getEndYear());
   }
 
   public int getStartYear() {
@@ -60,13 +79,15 @@ class CarSearchCriteria { // a DTO received from JSON
 }
 
 // @Entity
-class CarModel { // the Entity Model👑
+class CarModel/* implements Period*/ { // the Entity Model👑
   // @Id
   private Long id;
   private String make;
   private String model;
-  private int startYear;
-  private int endYear;
+  //  private int startYear;
+//  private int endYear;
+  @Embedded // the 2 fields are persisted in the same CAR_MODEL table.
+  private Interval yearInterval; //+semantic rich, +less fields
 
   protected CarModel() {
   } // for Hibernate
@@ -75,20 +96,32 @@ class CarModel { // the Entity Model👑
     this.make = make;
     this.model = model;
     if (startYear > endYear) throw new IllegalArgumentException("start larger than end");
-    this.startYear = startYear;
-    this.endYear = endYear;
+//    this.startYear = startYear;
+//    this.endYear = endYear;
+    this.yearInterval = new Interval(startYear, endYear);
   }
+
+  Interval getYearInterval() {
+    return yearInterval;
+  }
+
+  // polluting the Domain Model with API structures
+//  public boolean intersectYears(CarSearchCriteria criteria) {
+//    return MathUtil.intervalsIntersect(
+//        criteria.getStartYear(), criteria.getEndYear(),
+//        getStartYear(), getEndYear());
+//  }
 
   public Long getId() {
     return id;
   }
 
   public int getEndYear() {
-    return endYear;
+    return yearInterval.end();
   }
 
   public int getStartYear() {
-    return startYear;
+    return yearInterval.start();
   }
 
   public String getMake() {
