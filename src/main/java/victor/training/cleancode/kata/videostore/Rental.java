b334a98c032992b1aa34182f52ee6c495db502a4
Victor Rentea <victorrentea@gmail.com>
2024-11-26 16:58:56 +0200

    a bit of verbosity could be better than encrypting the code with the operator in the end they did choose Java as the language not Python

--- src/main/java/victor/training/cleancode/kata/videostore/Rental.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Rental.java
@@ -3,12 +3,30 @@ package victor.training.cleancode.kata.videostore;
 public record Rental(Movie movie, int rentalDays) {
 
     public double calculatePrice() {
-        // Dear Victor, we love you. We know that this can be extracted to meaningful methods which are more readable, but this is enough for today.
+        // Dear Victor, we love you. We know that this can be extracted
+        // to meaningful methods which are more readable,
+        // but this is enough for today. We shall fix it on Monday™️
+
+        // we added this already to the tech debt backlog.
+        // The place wehere all good ideas go to die. without us feeling remorse.
         return switch (movie.movieType()) {
-            case REGULAR -> (rentalDays <= 2) ? 2 : 2 + (rentalDays - 2) * 1.5;
+            case REGULAR -> calculateRegularPrice();
             case NEW_RELEASE -> rentalDays * 3.0;
-            case CHILDREN -> (rentalDays <= 3) ? 1.5 : 1.5 + (rentalDays - 3) * 1.5;
+            case CHILDREN -> calculateChildrenPrice();
         };
+    }
+
+    private double calculateChildrenPrice() {
+        //more verbose but easier to understand. and closer to the spec and the PO 💖
+        double basePrice = 1.5;
+        if (rentalDays > 3) {
+            basePrice += (rentalDays - 3) * 1.5;
+        }
+        return basePrice;
+    }
+
+    private double calculateRegularPrice() {
+        return (rentalDays <= 2) ? 2 : 2 + (rentalDays - 2) * 1.5;
     }
 
     public int calculateFrequentRenterPoints() {
