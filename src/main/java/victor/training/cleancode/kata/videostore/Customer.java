43680a120185683d64a835ed54487536c77824ee
Eliad.Gabriel <eliad.gabriel@nice.com>
2024-11-26 16:26:54 +0200

    Amazing refactor3

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -4,28 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static victor.training.cleancode.kata.videostore.PriceCode.NEW_RELEASE;
-
 class Customer {
     private final String name;
     private final List<RentalMovie> rentalMovies = new ArrayList<>(); // preserves order of elements
 
     public Customer(String name) {
         this.name = name;
-    }
-
-    private static double getRentalCostByPriceCode(int rentalDays, PriceCode priceCode) {
-        return switch (priceCode) {
-            case REGULAR -> calculateRentalCost(rentalDays, 2, 2);
-            case NEW_RELEASE -> rentalDays * 3;
-            case CHILDREN -> calculateRentalCost(rentalDays, 3, 1.5);
-        };
-    }
-
-    private static double calculateRentalCost(int rentalDays, int days, double initialAmount) {
-        if (rentalDays > days)
-            return initialAmount + (rentalDays - days) * 1.5;
-        return initialAmount;
     }
 
     public void addRental(Movie movie, int days) {
@@ -43,23 +27,20 @@ class Customer {
     private int getFrequentRenterPoints() {
         return rentalMovies.size() +
                 (int) rentalMovies.stream()
-                        .filter(rentalMovie -> rentalMovie.movie().priceCode() != null &&
-                                rentalMovie.movie().priceCode() == NEW_RELEASE &&
-                                rentalMovie.days() > 1)
+                        .filter(RentalMovie::shouldAddBonus)
                         .count();
     }
 
     private String getCostPrints() {
         return rentalMovies.stream()
                 .map(rentalMovie ->
-                        "\t" + rentalMovie.movie().title() + "\t" + getRentalCostByPriceCode(rentalMovie.days(), rentalMovie.movie().priceCode()) + "\n")
+                        "\t" + rentalMovie.movie().title() + "\t" + rentalMovie.getRentalCostByPriceCode() + "\n")
                 .collect(Collectors.joining());
     }
 
     private double getTotalCost() {
         return rentalMovies.stream()
-                .mapToDouble(rentalMovie ->
-                        getRentalCostByPriceCode(rentalMovie.days(), rentalMovie.movie().priceCode()))
+                .mapToDouble(RentalMovie::getRentalCostByPriceCode)
                 .sum();
     }
 
--- src/main/java/victor/training/cleancode/kata/videostore/RentalMovie.java
+++ src/main/java/victor/training/cleancode/kata/videostore/RentalMovie.java
@@ -1,4 +1,23 @@
 package victor.training.cleancode.kata.videostore;
 
+import static victor.training.cleancode.kata.videostore.PriceCode.NEW_RELEASE;
+
 public record RentalMovie(Movie movie, int days) {
+    private static double calculateRentalCost(int rentalDays, int days, double initialAmount) {
+        if (rentalDays > days)
+            return initialAmount + (rentalDays - days) * 1.5;
+        return initialAmount;
+    }
+
+    public boolean shouldAddBonus() {
+        return movie.priceCode() == NEW_RELEASE && days > 1;
+    }
+
+    public double getRentalCostByPriceCode() {
+        return switch (movie().priceCode()) {
+            case REGULAR -> calculateRentalCost(days, 2, 2);
+            case NEW_RELEASE -> days * 3;
+            case CHILDREN -> calculateRentalCost(days, 3, 1.5);
+        };
+    }
 }
