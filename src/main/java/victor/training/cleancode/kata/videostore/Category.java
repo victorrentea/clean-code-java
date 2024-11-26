e7ce10bd5baf704fd92281f9cb98f51f3c29912c
shani.shalel <S123456s>
2024-11-26 15:41:22 +0200

    Made changes until calculate bonus method

--- src/main/java/victor/training/cleancode/kata/videostore/Category.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Category.java
@@ -2,17 +2,23 @@ package victor.training.cleancode.kata.videostore;
 
 import lombok.Getter;
 
+@Getter
 public enum Category {
-    CHILDREN(2),
-    REGULAR(0),
-    NEW_RELEASE(1);
+    CHILDREN(2, 3, 1.5, 1.5),
+    REGULAR(0, 2, 1.5, 2),
+    NEW_RELEASE(1, 0, 3, 0);
 
-    @Getter
     private final int code;
 
+    private final int discountFactor;
+    private final double multiplicationFactor;
+    private final double basePrice;
 
-    Category(int value) {
+    Category(int value, int discount, double multiplicationFactor, double basePrice) {
         this.code = value;
+        this.discountFactor = discount;
+        this.multiplicationFactor = multiplicationFactor;
+        this.basePrice = basePrice;
     }
 
 
--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -1,61 +1,59 @@
 package victor.training.cleancode.kata.videostore;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-import static victor.training.cleancode.kata.videostore.Category.*;
+import static victor.training.cleancode.kata.videostore.Category.NEW_RELEASE;
 
 class Customer {
-	private String name;
-	private Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order of elements
+    private final String name;
+    private final Map<Movie, Integer> rentals = new LinkedHashMap<>();
 
-	public Customer(String name) {
-		this.name = name;
-	};
+    public Customer(String name) {
+        this.name = name;
+    }
 
-	public void addRental(Movie m, int d) {
-		rentals.put(m, d);
-	}
+    public void addRental(Movie movie, int numberOfRentals) {
+        this.rentals.put(movie, numberOfRentals);
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		String result = "Rental Record for " + getName() + "\n";
-		// loop over each movie rental
-		for (Movie movie : rentals.keySet()) {
-			double thisAmount = 0;
-			// determine amounts for every line
-			int numberOfRentals = rentals.get(movie);
-			switch (movie.category) {
-				case REGULAR -> {
-					thisAmount += 2;
-					if (numberOfRentals > 2)
-						thisAmount += (numberOfRentals - 2) * 1.5;
-				}
-				case NEW_RELEASE -> thisAmount += numberOfRentals * 3;
-				case CHILDREN -> {
-					thisAmount += 1.5;
-					if (numberOfRentals > 3)
-						thisAmount += (numberOfRentals - 3) * 1.5;
-				}
-			}
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if (movie.getPriceCode() != null &&
-				 (movie.getPriceCode() == NEW_RELEASE.getCode())
-				 && numberOfRentals > 1)
-				frequentRenterPoints++;
-			// show figures line for this rental
-			result += "\t" + movie.getTitle() + "\t" + thisAmount + "\n";
-			totalAmount += thisAmount;
-		}
-		// add footer lines
-		result += "Amount owed is " + totalAmount + "\n";
-		result += "You earned " + frequentRenterPoints + " frequent renter points";
-		return result;
-	}
+    public String statement() {
+        double totalAmount = 0;
+        int renterPoints = 0;
+        String result = "Rental Record for " + getName() + "\n";
+
+        // loop over each movie rental
+        for (Movie movie : rentals.keySet()) {
+            double totalPrice = 0;
+            int numberOfRentals = rentals.get(movie);
+
+            totalPrice += calculatePrice(numberOfRentals, movie.category);
+
+            renterPoints += 1 + calculateBonus(movie, numberOfRentals);
+            // show figures line for this rental
+            result += "\t" + movie.getTitle() + "\t" + totalPrice + "\n";
+            totalAmount += totalPrice;
+        }
+        // add footer lines
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + renterPoints + " frequent renter points";
+        return result;
+    }
+
+    private int calculateBonus(Movie movie, int numberOfRentals) {
+        return movie.getPriceCode() != null &&
+                (movie.getPriceCode() == NEW_RELEASE.getCode())
+                && numberOfRentals > 1 ? 1 : 0;
+    }
+
+    private double calculatePrice(int numberOfRentals, Category category) {
+        double price = numberOfRentals > category.getDiscountFactor() ?
+                (numberOfRentals - category.getDiscountFactor()) * category.getMultiplicationFactor() :
+                0;
+        return price + category.getBasePrice();
+    }
 }
