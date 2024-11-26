e9596fc60f494a94771e93239e261bbddbdc0544
avihooCohenNice <Avihoo.Cohen@nice.com>
2024-11-26 14:49:00 +0200

    FIX-Issues

--- src/main/java/victor/training/cleancode/kata/videostore/PriceCategory.java
+++ src/main/java/victor/training/cleancode/kata/videostore/PriceCategory.java
@@ -1,24 +1,18 @@
 package victor.training.cleancode.kata.videostore;
 
+import static victor.training.cleancode.kata.videostore.Rental.getRentalCost;
+
 public enum PriceCategory {
     CHILDRENS {
         @Override
         double calculateRentalPrice(int daysRented) {
-            double rentalCost = 1.5;
-            if (daysRented > 3) {
-                rentalCost += (daysRented - 3) * 1.5;
-            }
-            return rentalCost;
+            return getRentalCost(daysRented, 1.5, 3);
         }
     },
     REGULAR {
         @Override
         double calculateRentalPrice(int daysRented) {
-            double rentalCost = 2;
-            if (daysRented > 2) {
-                rentalCost += (daysRented - 2) * 1.5;
-            }
-            return rentalCost;
+            return getRentalCost(daysRented,2, 2);
         }
     },
     NEW_RELEASE {
@@ -27,6 +21,7 @@ public enum PriceCategory {
             return  daysRented * 3;
         }
     };
+
 
     abstract double calculateRentalPrice(int daysRented);
 }
--- src/main/java/victor/training/cleancode/kata/videostore/Rental.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Rental.java
@@ -1,13 +1,25 @@
 package victor.training.cleancode.kata.videostore;
 
+import static victor.training.cleancode.kata.videostore.PriceCategory.NEW_RELEASE;
+
 public record Rental(Movie movie, int daysRented) {
   double amount() {
     return movie.priceCategory().calculateRentalPrice(daysRented);
   }
+  public static double getRentalCost(final int daysRented, final double rentalCost, final int maxRentDays) {
+    return getCost(daysRented, rentalCost, maxRentDays);
+  }
 
+  private static double getCost(final int daysRented,
+                                double rentalCost, final int maxRentDays) {
+    if ( daysRented > maxRentDays) {
+      rentalCost += ( daysRented - maxRentDays) * rentalCost;
+    }
+    return rentalCost;
+  }
   int renterPoints() {
     int points = 1;
-    if (movie().priceCategory() == PriceCategory.NEW_RELEASE && daysRented() > 1) {
+    if (movie.priceCategory() == NEW_RELEASE && daysRented >= 2) {
       points++;
     }
     return points;
