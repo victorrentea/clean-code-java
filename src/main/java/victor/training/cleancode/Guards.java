f33ff06be54c49a29b70309d75e3e6ea163e948c
Victor Rentea <victorrentea@gmail.com>
2025-01-20 11:44:18 +0200

    refactored together

--- src/main/java/victor/training/cleancode/Guards.java
+++ src/main/java/victor/training/cleancode/Guards.java
@@ -7,28 +7,25 @@ public class Guards {
   public static final int DEAD_PAY_AMOUNT = 1;
 
   public int getPayAmount(Marine marine, BonusPackage bonusPackage) {
-
-    if (!(marine != null && !(bonusPackage.value() < 10 || bonusPackage.value() > 100))) {
+    if (marine == null || (bonusPackage.value() < 10 || bonusPackage.value() > 100)) {
       throw new IllegalArgumentException("Not applicable!");
     }
-    int result;
-    if (marine.dead()) return DEAD_PAY_AMOUNT;
-
+    if (marine.dead()) {
+      return DEAD_PAY_AMOUNT;
+    }
     if (marine.retired()) {
       return retiredAmount();
     }
-    if (marine.yearsOfService() != null) {
-      result = marine.yearsOfService() * 100 + bonusPackage.value();
-      if (!marine.awards().isEmpty()) {
-        result += 1000;
-      }
-      if (marine.awards().size() >= 3) {
-        result += 2000;
-      }
-    } else {
+    if (marine.yearsOfService() == null) {
       throw new IllegalArgumentException("Any marine should have the years of service set");
     }
-
+    int result = marine.yearsOfService() * 100 + bonusPackage.value();
+    if (!marine.awards().isEmpty()) {
+      result += 1000;
+    }
+    if (marine.awards().size() >= 3) {
+      result += 2000;
+    }
     return result;
   }
 
