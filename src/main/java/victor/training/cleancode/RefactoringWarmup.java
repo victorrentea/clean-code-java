302ec0ab35cbf3d61f0d341d95a9b4ceaf59cd88
Victor Rentea <victorrentea@gmail.com>
2025-01-20 11:52:02 +0200

    polish

--- src/main/java/victor/training/cleancode/RefactoringWarmup.java
+++ src/main/java/victor/training/cleancode/RefactoringWarmup.java
@@ -1,51 +1,16 @@
 package victor.training.cleancode;
 
 
-import java.util.Arrays;
 import java.util.List;
 
 class RefactoringWarmup {
   public static void main(String[] args) {
-    System.out.println(new One(new Two()).f());
-    loop();
-  }
-
-  private static void loop() {
-    List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-    double ssq = 0;
-    for (Integer number : numbers) {
-      if (number % 2 == 0) {
-        ssq += number * number;
-      }
-    }
-    System.out.println(Math.sqrt(ssq));
+    Two two = new Two();
+    System.out.println(two.loop(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)));
+    System.out.println(new One(two).f());
+    System.out.println(two.g(new R(1)));
   }
 }
-
-// TODO: Practice Refactoring
-//  * How to?
-//    - Select text > Hover
-//    - Right-click > Refactor
-//    - Ctrl-Alt-Shift-T/^T to
-//    - Keys: [Ctrl-Alt / Opt-Cmd] + [V]ariable/[M]ethod/[P]arameter/i[N]line
-//  * What? // after every action undo/revert to start clean
-//    - Inline[N] Variable 'b'
-//    - Extract [V]ariable '1', '3 * two.g()'
-//    - Extract [M]ethod 'System.out..'
-//    - Inline[N] Method 'g'
-//    - Extract [P]arameter '1', 'r.x()'
-//    - Inline[N] Parameter 'c'
-//    - Change Signature 'g': add 1 param with default as 1st arg
-//    - Extract Interface 'Two'->ITwo; - Inline to Anonymous Class to destroy interface
-//    - Rename 'g' -> 'h' by Shift-F6 or just edit>Alt-Enter>Rename
-//    - Move Method 'g' into R
-//    - Preview method/class: Ctrl-Shift-I
-//    - Quickfix for->stream
-//    - Change inspection severity & highlighting
-//       * Download "aggressive_refactoring.xml" from https://victorrentea.ro
-//       and import it in Settings>Editor>Inspections
-
-record R(int x) {}
 
 class One {
   private final Two two;
@@ -66,7 +31,39 @@ class Two {
     return 1 + b + r.x();
   }
 
-  public void unknown() {
+  public double loop(List<Integer> numbers) {
     System.out.println("b=" + 987);
+    double ssq = 0;
+    for (Integer number : numbers) {
+      if (number % 2 == 0) {
+        ssq += number * number;
+      }
+    }
+    return Math.sqrt(ssq);
   }
 }
+
+record R(int x) {
+}
+
+// TODO: Practice Refactoring
+//  * How to?
+//    - Select text > Right Click > Refactor
+//    - Ctrl-Alt-Shift-T/^T to
+//    - Keys: [Ctrl-Alt / Opt-Cmd] + [V]ariable/[M]ethod/[P]arameter/i[N]line
+//  * What? // after every action undo/revert to start clean
+//    - Inline Variable 'b'
+//    - Extract Variable '1', '3 * two.g()'
+//    - Extract Method 'System.out..' (+replace duplicate)
+//    - Inline Method 'g'
+//    - Extract Parameter '1', 'r.x()'
+//    - Inline Parameter 'c'
+//    - Change Signature 'g': add 1 param with default as 1st arg
+//    - Extract Interface 'Two'->ITwo; + Inline back to destroy interface
+//    - Rename 'g' -> 'h' by Shift-F6 or just edit>Alt-Enter>Rename
+//    - Move Method 'g' into R
+//    - Preview method/class: Ctrl-Shift-I
+//    - Quickfix (Alt/Option -Enter) for->stream
+//    - Edit inspection severity & highlighting
+//       * Download "aggressive_refactoring.xml" from https://victorrentea.ro
+//       and import it in Settings>Editor>Inspections
