39e0d4abd4d52415f33f486feb1d36fbad20f76a
Victor Rentea <victorrentea@gmail.com>
2025-01-21 11:32:58 +0200

    fixed perf

--- src/main/java/victor/training/cleancode/fp/PureFunction.java
+++ src/main/java/victor/training/cleancode/fp/PureFunction.java
@@ -61,10 +61,15 @@ class PureFunction {
 //    }
 
     // BUG: se cheama trhird party de fiecare data, chiar daca avem deja pretul in internalPrices
-    return products.stream()
-        .collect(ImmutableMap.toImmutableMap(Product::getId,
-            product -> internalPrices.getOrDefault(product.getId(), thirdPartyPricesApi.fetchPrice(product.getId()))));
+    return products.stream().collect(ImmutableMap.toImmutableMap(Product::getId,
+        product -> resolvePrice(internalPrices, product)));
+  }
 
+  private Double resolvePrice(Map<Long, Double> internalPrices, Product product) {
+    if (internalPrices.containsKey(product.getId())) {
+      return internalPrices.get(product.getId());
+    }
+    return thirdPartyPricesApi.fetchPrice(product.getId());
   }
 
   // prices, usedCoupons = f(customer, products, internalPrices)
