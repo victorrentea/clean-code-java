526ffde0cb930e250fdcc542dfbb915b3d08b91b
Victor Rentea <victorrentea@gmail.com>
2025-01-21 10:06:07 +0200

    immutable

---
+++ src/main/java/victor/training/cleancode/refactoring/MustHaveImmuabileInAsyncronism.java
@@ -0,0 +1,15 @@
+package victor.training.cleancode.refactoring;
+
+public class MustHaveImmuabileInAsyncronism {
+//  public static void main(String[] args) {
+//    await f(d);
+//    await g(d);
+//  }
+//  // RACE CONDITION: if 2 threads call f and g on the same Data object, the result is unpredictable
+//  async f(Data data) {
+//    data.setA(1);
+//  }
+//  async g(Data data) {
+//    data.setA(2);
+//  }
+}
