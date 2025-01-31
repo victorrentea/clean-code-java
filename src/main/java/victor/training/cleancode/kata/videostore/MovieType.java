6c5e1a42b4d728968dd5ad2bb0fbf022cdc98b01
Victor Rentea <victorrentea@gmail.com>
2025-01-31 18:35:49 +0200

    Revert "end clean code"
    This reverts commit 7f2f4e81164fb07ab9032cd6cadfd57d05e4c499.

--- src/main/java/victor/training/cleancode/kata/videostore/MovieType.java
+++
@@ -1,8 +0,0 @@
-package victor.training.cleancode.kata.videostore;
-
-public enum MovieType {
-  REGULAR,
-  NEW_RELEASE,
-  CHILDREN,
-  HORROR;
-}
--- src/test/java/victor/training/cleancode/kata/videostore/CustomerTest.java
+++ src/test/java/victor/training/cleancode/kata/videostore/CustomerTest.java
@@ -10,10 +10,10 @@ public class CustomerTest {
     @Test
     public void characterizationTest() {
         Customer customer = new Customer("John Doe");
-        customer.addRental(new Movie("Star Wars", MovieType.NEW_RELEASE), 6);
-        customer.addRental(new Movie("Sofia", MovieType.CHILDREN), 7);
-        customer.addRental(new Movie("Inception", MovieType.REGULAR), 5);
-        customer.addRental(new Movie("Wicked", MovieType.CHILDREN), 3);
+        customer.addRental(new Movie("Star Wars", Movie.NEW_RELEASE), 6);
+        customer.addRental(new Movie("Sofia", Movie.CHILDRENS), 7);
+        customer.addRental(new Movie("Inception", Movie.REGULAR), 5);
+        customer.addRental(new Movie("Wicked", Movie.CHILDRENS), 3);
 
         String expected = """
             Rental Record for John Doe
