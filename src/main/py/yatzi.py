3f17fc5625c5171ea67bea2f94ba67597c489382
Victor Rentea <victorrentea@gmail.com>
2025-01-31 18:35:49 +0200

    Revert "cleanup"
    This reverts commit 85b2f16e1032dbea4efc4a51b2b09723639a21ad.

--- src/main/py/yatzi.py
+++ src/main/py/yatzi.py
@@ -1,66 +1,159 @@
-# spec: https://sammancoaching.org/kata_descriptions/yatzy.html
-
 class Yatzy:
-    def __init__(self, d1=0, d2=0, d3=0, d4=0, d5=0):
-        self.dice = [d1, d2, d3, d4, d5]
 
     @staticmethod
     def chance(d1, d2, d3, d4, d5):
-        return sum([d1, d2, d3, d4, d5])
+        total = 0
+        total += d1
+        total += d2
+        total += d3
+        total += d4
+        total += d5
+        return total
 
     @staticmethod
     def yatzy(dice):
-        counts = [0] * 7
+        counts = [0] * (len(dice) + 1)
         for die in dice:
-            counts[die] += 1
-        # TODO puteti cu dictioar in loc de array. adic in loc de [0X,5,0,0,0,0,0] sa tineti {1:5}?
-        print(counts)
+            counts[die - 1] += 1
+        for i in range(len(counts)):
+            if counts[i] == 5:
+                return 50
+        return 0
 
-        return 50 if counts.count(5) == 1 else 0
+    @staticmethod
+    def ones(d1, d2, d3, d4, d5):
+        sum = 0
+        if (d1 == 1):
+            sum += 1
+        if (d2 == 1):
+            sum += 1
+        if (d3 == 1):
+            sum += 1
+        if (d4 == 1):
+            sum += 1
+        if (d5 == 1):
+            sum += 1
 
-    # idee FOARTE BUNA !
-    def count_appearances(self, dice_face):
-        return self.dice.count(dice_face) * dice_face
+        return sum
 
-    def ones(self):
-        return self.count_appearances(1)
+    @staticmethod
+    def twos(d1, d2, d3, d4, d5):
+        sum = 0
+        if (d1 == 2):
+            sum += 2
+        if (d2 == 2):
+            sum += 2
+        if (d3 == 2):
+            sum += 2
+        if (d4 == 2):
+            sum += 2
+        if (d5 == 2):
+            sum += 2
+        return sum
 
-    def twos(self):
-        return self.count_appearances(2)
+    @staticmethod
+    def threes(d1, d2, d3, d4, d5):
+        s = 0
+        if (d1 == 3):
+            s += 3
+        if (d2 == 3):
+            s += 3
+        if (d3 == 3):
+            s += 3
+        if (d4 == 3):
+            s += 3
+        if (d5 == 3):
+            s += 3
+        return s
 
-    def threes(self):
-        return self.count_appearances(3)
+    def __init__(self, d1=0, d2=0, d3=0, d4=0, _5=0):
+        self.dice = [0] * 5
+        self.dice[0] = d1
+        self.dice[1] = d2
+        self.dice[2] = d3
+        self.dice[3] = d4
+        self.dice[4] = _5
 
     def fours(self):
-        return self.count_appearances(4)
+        sum = 0
+        for at in range(5):
+            if (self.dice[at] == 4):
+                sum += 4
+        return sum
 
     def fives(self):
-        return self.count_appearances(5)
+        s = 0
+        i = 0
+        for i in range(len(self.dice)):
+            if (self.dice[i] == 5):
+                s = s + 5
+        return s
 
     def sixes(self):
-        return self.count_appearances(6)
+        sum = 0
+        for at in range(len(self.dice)):
+            if (self.dice[at] == 6):
+                sum = sum + 6
+        return sum
 
-    def score_pair(self):
-        counts = {face: self.dice.count(face) for face in range(1, 7)}
-        pair_faces = [face for face, count in counts.items() if count >= 2]
-        return 2 * max(pair_faces) or 0
+    def score_pair(self, d1, d2, d3, d4, d5):
+        counts = [0] * 6
+        counts[d1 - 1] += 1
+        counts[d2 - 1] += 1
+        counts[d3 - 1] += 1
+        counts[d4 - 1] += 1
+        counts[d5 - 1] += 1
+        at = 0
+        for at in range(6):
+            if (counts[6 - at - 1] == 2):
+                return (6 - at) * 2
+        return 0
 
-    def two_pair(self):
-        counts = {face: self.dice.count(face) for face in range(1, 7)}
-        pair_faces = [face for face, count in counts.items() if count >= 2]
-        return 2 * sum(pair_faces) if len(pair_faces) == 2 else 0
+    @staticmethod
+    def two_pair(d1, d2, d3, d4, d5):
+        counts = [0] * 6
+        counts[d1 - 1] += 1
+        counts[d2 - 1] += 1
+        counts[d3 - 1] += 1
+        counts[d4 - 1] += 1
+        counts[d5 - 1] += 1
+        n = 0
+        score = 0
+        for i in range(6):
+            if (counts[6 - i - 1] >= 2):
+                n = n + 1
+                score += (6 - i)
 
-    def four_of_a_kind(self):
-        counts = {face: self.dice.count(face) for face in range(1, 7)}
-        face4 = [face for face, count in counts.items() if count >= 4]
-        return 4 * face4[0] if face4 else 0
+        if (n == 2):
+            return score * 2
+        else:
+            return 0
 
-    def three_of_a_kind(self):
-        counts = {face: self.dice.count(face) for face in range(1, 7)}
-        face3 = [face for face, count in counts.items() if count >= 3]
-        return 3 * face3[0] if face3 else 0
+    @staticmethod
+    def four_of_a_kind(_1, _2, d3, d4, d5):
+        tallies = [0] * 6
+        tallies[_1 - 1] += 1
+        tallies[_2 - 1] += 1
+        tallies[d3 - 1] += 1
+        tallies[d4 - 1] += 1
+        tallies[d5 - 1] += 1
+        for i in range(6):
+            if (tallies[i] >= 4):
+                return (i + 1) * 4
+        return 0
 
-    # https://chatgpt.com/share/678fb401-c858-8006-8fc0-2483318ee125
+    @staticmethod
+    def three_of_a_kind(d1, d2, d3, d4, d5):
+        t = [0] * 6
+        t[d1 - 1] += 1
+        t[d2 - 1] += 1
+        t[d3 - 1] += 1
+        t[d4 - 1] += 1
+        t[d5 - 1] += 1
+        for i in range(6):
+            if (t[i] >= 3):
+                return (i + 1) * 3
+        return 0
 
     @staticmethod
     def smallStraight(d1, d2, d3, d4, d5):
@@ -142,20 +235,20 @@ def test_yatzy_scores_50():
 
 
 def test_1s():
-    assert Yatzy(1, 2, 3, 4, 5).ones() == 1
-    assert 2 == Yatzy(1, 2, 1, 4, 5).ones()
-    assert 0 == Yatzy(6, 2, 2, 4, 5).ones()
-    assert 4 == Yatzy(1, 2, 1, 1, 1).ones()
+    assert Yatzy.ones(1, 2, 3, 4, 5) == 1
+    assert 2 == Yatzy.ones(1, 2, 1, 4, 5)
+    assert 0 == Yatzy.ones(6, 2, 2, 4, 5)
+    assert 4 == Yatzy.ones(1, 2, 1, 1, 1)
 
 
 def test_2s():
-    assert 4 == Yatzy(1, 2, 3, 2, 6).twos()
-    assert 10 == Yatzy(2, 2, 2, 2, 2).twos()
+    assert 4 == Yatzy.twos(1, 2, 3, 2, 6)
+    assert 10 == Yatzy.twos(2, 2, 2, 2, 2)
 
 
 def test_threes():
-    assert 6 == Yatzy(1, 2, 3, 2, 3).threes()
-    assert 12 == Yatzy(2, 3, 3, 3, 3).threes()
+    assert 6 == Yatzy.threes(1, 2, 3, 2, 3)
+    assert 12 == Yatzy.threes(2, 3, 3, 3, 3)
 
 
 def test_fours_test():
@@ -173,32 +266,32 @@ def test_fives():
 def test_sixes_test():
     assert 0 == Yatzy(4, 4, 4, 5, 5).sixes()
     assert 6 == Yatzy(4, 4, 6, 5, 5).sixes()
-    assert 18 == Yatzy(6, 5, 6, 6, 5).sixes()  # de ce e altfel? ctor
+    assert 18 == Yatzy(6, 5, 6, 6, 5).sixes()
 
 
 def test_one_pair():
-    assert 6 == Yatzy(3, 4, 3, 5, 6).score_pair()  # de ce e altfel? param de metoda
-    assert 10 == Yatzy(5, 3, 3, 3, 5).score_pair()
-    assert 12 == Yatzy(5, 3, 6, 6, 5).score_pair()
+    assert 6 == Yatzy().score_pair(3, 4, 3, 5, 6)
+    assert 10 == Yatzy().score_pair(5, 3, 3, 3, 5)
+    assert 12 == Yatzy().score_pair(5, 3, 6, 6, 5)
 
 
 def test_two_Pair():
-    assert 16 == Yatzy(3, 3, 5, 4, 5).two_pair()
-    assert 18 == Yatzy(3, 3, 6, 6, 6).two_pair()
-    assert 0 == Yatzy(3, 3, 6, 5, 4).two_pair()
+    assert 16 == Yatzy().two_pair(3, 3, 5, 4, 5)
+    assert 18 == Yatzy().two_pair(3, 3, 6, 6, 6)
+    assert 0 == Yatzy().two_pair(3, 3, 6, 5, 4)
 
 
 def test_three_of_a_kind():
-    assert 9 == Yatzy(3, 3, 3, 4, 5).three_of_a_kind()
-    assert 15 == Yatzy(5, 3, 5, 4, 5).three_of_a_kind()
-    assert 9 == Yatzy(3, 3, 3, 3, 5).three_of_a_kind()
+    assert 9 == Yatzy().three_of_a_kind(3, 3, 3, 4, 5)
+    assert 15 == Yatzy().three_of_a_kind(5, 3, 5, 4, 5)
+    assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 3, 5)
 
 
 def test_four_of_a_knd():
-    assert 12 == Yatzy(3, 3, 3, 3, 5).four_of_a_kind()
-    assert 20 == Yatzy(5, 5, 5, 4, 5).four_of_a_kind()
-    assert 12 == Yatzy(3, 3, 3, 3, 3).four_of_a_kind()
-    assert 0 == Yatzy(3, 3, 3, 2, 1).four_of_a_kind()
+    assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 5)
+    assert 20 == Yatzy.four_of_a_kind(5, 5, 5, 4, 5)
+    assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 3)
+    assert 0 == Yatzy.four_of_a_kind(3, 3, 3, 2, 1)
 
 
 def test_smallStraight():
