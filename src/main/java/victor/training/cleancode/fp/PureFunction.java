56421758d6193bcde8b31b84d6a425ad296a4f7e
Victor Rentea <victorrentea@gmail.com>
2025-01-31 18:35:49 +0200

    Revert "+py"
    This reverts commit ac239d9248bbc5ee9e857d66e90f9eb8df84c770.

--- src/main/java/victor/training/cleancode/fp/PureFunction.java
+++ src/main/java/victor/training/cleancode/fp/PureFunction.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-f
+
 @RequiredArgsConstructor
 class PureFunction {
   private final CustomerRepo customerRepo;
