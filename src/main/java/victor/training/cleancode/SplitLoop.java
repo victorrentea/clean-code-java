f0af383964b4f52b8af59f9a2d4b27b59ccd5d23
Victor Rentea <victorrentea@gmail.com>
2025-01-31 18:35:56 +0200

    Revert "Guards, intro"
    This reverts commit 26ab1d1d3ea4d94cd0d5bac7e2ff852a341d2146.

--- src/main/java/victor/training/cleancode/SplitLoop.java
+++ src/main/java/victor/training/cleancode/SplitLoop.java
@@ -2,18 +2,20 @@ package victor.training.cleancode;
 
 import lombok.Builder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SplitLoop {
   // TODO Split loops and refactor to .stream. Run Tests✅
   public String computeStats(List<Employee> employees) {
-    double totalConsultantSalary = employees.stream()
-        .filter(Employee::consultant)
-        .mapToDouble(Employee::salary)
-        .sum();
-    // Stream
-    List<Integer> employeeIds = employees.stream()
-        .map(Employee::id).toList();
+    List<Integer> employeeIds = new ArrayList<>();
+    double totalConsultantSalary = 0;
+    for (Employee employee : employees) {
+      if (employee.consultant()) {
+        totalConsultantSalary += employee.salary();
+      }
+      employeeIds.add(employee.id());
+    }
     System.out.println("Employee IDs: " + employeeIds);
     return "Total consultant salary: " + totalConsultantSalary + "; ids: " + employeeIds;
   }
