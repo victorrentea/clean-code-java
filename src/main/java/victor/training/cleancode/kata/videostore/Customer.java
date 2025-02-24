e7ebd1c96ecfa8c5838ab8e6a97657b2263b2c79
Sebastien Santerre <sebastien.santerre@plusgrade.com>
2025-02-24 17:22:41 -0500

    getFrequentRenterPoints

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -7,73 +7,92 @@ import java.util.List;
 
 import static victor.training.cleancode.kata.videostore.MovieType.NEW_RELEASE;
 
-class Customer {
-	@Getter
+class Customer
+{
+  @Getter
   private final String name;
-	private final List<MovieRental> rentals = new ArrayList<>();
+  private final List<MovieRental> rentals = new ArrayList<>();
 
-	public Customer(String name) {
-		this.name = name;
-	}
+  public Customer( String name )
+  {
+    this.name = name;
+  }
 
-  public void addRental(Movie movie, int daysRented) {
-		rentals.add(new MovieRental( movie, daysRented ));
-	}
+  public void addRental( Movie movie, int daysRented )
+  {
+    rentals.add( new MovieRental( movie, daysRented ) );
+  }
 
-  public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		String result = "Rental Record for " + getName() + "\n";
+  public String statement()
+  {
+    double totalAmount = 0;
+    String result = "Rental Record for " + getName() + "\n";
 
+//		rentals.stream()
+//			.map( Customer::computeAmount )
+//			.mapToDouble( a -> a )
+//			.sum();
+
+
+    // Calculate frequent renter point
+    int frequentRenterPoints = getFrequentRenterPoints();
+
+    // Calculate total amount
     for ( MovieRental movieRental : rentals )
     {
-			double thisAmount = computeAmount( movieRental );
-			frequentRenterPoints++;
-			
-      if ( isEligibleForBonus( movieRental ) )
-      {
-        frequentRenterPoints++;
-      }
       // show figures line for this rental
-      result += "\t" + movieRental.movie().title() + "\t" + thisAmount + "\n";
-      totalAmount += thisAmount;
+      double rentAmount = computeAmount( movieRental );
+      result += "\t" + movieRental.movie().title() + "\t" + rentAmount + "\n";
+      totalAmount += rentAmount;
     }
-		// add footer lines
-		result += "Amount owed is " + totalAmount + "\n";
-		result += "You earned " + frequentRenterPoints + " frequent renter points";
-		return result;
-	}
 
-	private static double computeAmount( MovieRental movieRental )
-	{
-		double amount = 0;
-		int daysRented = movieRental.daysRented();
+    // add footer lines
+    result += "Amount owed is " + totalAmount + "\n";
+    result += "You earned " + frequentRenterPoints + " frequent renter points";
+    return result;
+  }
 
-		switch ( movieRental.movie().movieType() )
-		{
-			case REGULAR:
-				amount += 2;
-				if ( daysRented > 2 )
-				{
-					amount += ( daysRented - 2 ) * 1.5;
-				}
-				break;
-			case NEW_RELEASE:
-				amount += daysRented * 3;
-				break;
-			case CHILDREN:
-				amount += 1.5;
-				if ( daysRented > 3 )
-				{
-					amount += ( daysRented - 3 ) * 1.5;
-				}
-				break;
-		}
-		return amount;
-	}
+  private int getFrequentRenterPoints( )
+  {
+    long count = rentals.stream()
+      .map( Customer::isEligibleForBonus )
+      .filter( r -> r )
+      .count();
 
-	private static boolean isEligibleForBonus( MovieRental movieRental )
-	{
-		return ( movieRental.movie().movieType() == NEW_RELEASE ) && movieRental.daysRented() > 1;
-	}
+    return ( int ) count + rentals.size();
+  }
+
+  private static double computeAmount( MovieRental movieRental )
+  {
+    double amount = 0;
+    int daysRented = movieRental.daysRented();
+
+    switch ( movieRental.movie().movieType() )
+    {
+      case REGULAR:
+        amount += 2;
+        if ( daysRented > 2 )
+        {
+          amount += ( daysRented - 2 ) * 1.5;
+        }
+        return amount;
+      case NEW_RELEASE:
+        amount += daysRented * 3;
+        return amount;
+      case CHILDREN:
+        amount += 1.5;
+        if ( daysRented > 3 )
+        {
+          amount += ( daysRented - 3 ) * 1.5;
+        }
+        return amount;
+    }
+
+    return amount;
+  }
+
+  private static boolean isEligibleForBonus( MovieRental movieRental )
+  {
+    return ( movieRental.movie().movieType() == NEW_RELEASE ) && movieRental.daysRented() > 1;
+  }
 }
