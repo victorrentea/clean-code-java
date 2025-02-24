69f5047aa0f2ee4ae5f76dd235059ccba4d4ac7b
kirill.parhomenco <kirill.parhomenco@plusgrade.com>
2025-02-24 17:55:08 -0500

    done

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -1,16 +1,13 @@
 package victor.training.cleancode.kata.videostore;
 
-import victor.training.cleancode.kata.videostore.Movie.PriceCategory;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Customer
 {
   private final String name;
   private final List<Rental> rentals = new ArrayList<>();
-  private int frequentRenterPoints = 0;
-  // preserves order of elements TODO find a better way to store this
 
   public Customer( String name )
   {
@@ -24,48 +21,19 @@ class Customer
 
   public String rentMovies()
   {
-    double totalAmount = getTotalAmount();
-
-    addFrequentRenterPoints();
-    return generateReceipt( totalAmount );
+    String rentedList = rentals.stream().map( Rental::toString ).collect( Collectors.joining() );
+    return "Rental Record for " + name + "\n" + rentedList +
+      "Amount owed is " + getTotalAmount() + "\n" +
+      "You earned " + addFrequentRenterPoints() + " frequent renter points";
   }
 
   private double getTotalAmount()
   {
-    // loop over each movie rental
-    double totalAmount = 0;
-    for ( Rental rental : rentals )
-    {
-      totalAmount += rental.getCost();
-    }
-    return totalAmount;
+    return rentals.stream().mapToDouble( Rental::cost ).sum();
   }
 
-  private String generateReceipt( double totalAmount )
+  private int addFrequentRenterPoints()
   {
-    StringBuilder result = new StringBuilder( "Rental Record for " + name + "\n" );
-    for ( Rental rental : rentals )
-    {
-      result.append( rental );
-    }
-
-    // add footer lines
-    result.append( "Amount owed is " ).append( totalAmount ).append( "\n" );
-    result.append( "You earned " ).append( frequentRenterPoints ).append( " frequent renter points" );
-    return result.toString();
-  }
-
-  private void addFrequentRenterPoints()
-  {
-    for ( Rental rental : rentals )
-    {
-      // add frequent renter points
-      frequentRenterPoints++;
-      // add bonus for a two-day new release rental
-      if ( rental.movie().priceCategory() == PriceCategory.NEW_RELEASE && rental.days() > 1 )
-      {
-        frequentRenterPoints++;
-      }
-    }
+    return rentals.stream().mapToInt( Rental::loyaltyPoints ).sum();
   }
 }
--- src/main/java/victor/training/cleancode/kata/videostore/Rental.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Rental.java
@@ -1,36 +1,54 @@
 package victor.training.cleancode.kata.videostore;
 
-public record Rental(Movie movie, int days )
-{
-    public double getCost()
-    {
-        double cost = 0;
-        switch ( movie.priceCategory() )
-        {
-            case REGULAR ->
-            {
-                cost += 2;
-                if ( days> 2 )
-                {
-                    cost += ( days - 2 ) * 1.5;
-                }
-            }
-            case NEW_RELEASE -> cost += days * 3;
-            case CHILDREN ->
-            {
-                cost += 1.5;
-                if ( days > 3 )
-                {
-                    cost += ( days - 3 ) * 1.5;
-                }
-            }
-        }
-        return cost;
-    }
+import victor.training.cleancode.kata.videostore.Movie.PriceCategory;
 
-    @Override
-    public String toString()
+public record Rental( Movie movie, int days )
+{
+  public double cost()
+  {
+    return switch ( movie.priceCategory() )
     {
-        return String.format("\t%s\t%s\n", movie().title(), getCost() );
+      case REGULAR -> getCostForRegular();
+
+      case NEW_RELEASE -> days * 3;
+
+      case CHILDREN -> getCostForChildren();
+
+    };
+  }
+
+  private double getCostForRegular()
+  {
+    double cost = 2;
+    if ( days > 2 )
+    {
+      cost += ( days - 2 ) * 1.5;
     }
+    return cost;
+  }
+
+  private double getCostForChildren()
+  {
+    double cost = 1.5;
+    if ( days > 3 )
+    {
+      cost += ( days - 3 ) * 1.5;
+    }
+    return cost;
+  }
+
+  @Override
+  public String toString()
+  {
+    return String.format( "\t%s\t%s\n", movie().title(), cost() );
+  }
+
+  public int loyaltyPoints()
+  {
+    if ( movie().priceCategory() == PriceCategory.NEW_RELEASE && days() > 1 )
+    {
+      return 2;
+    }
+    return 1;
+  }
 }
