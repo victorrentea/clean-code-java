eb467a4a343e8dfc337a11cdcd24929d4c41758e
Shaikh Saad Bin Kader <saad.kader@plusgrade.com>
2025-02-24 14:46:28 -0700

    Initial commit

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -1,10 +1,15 @@
 package victor.training.cleancode.kata.videostore;
 
+import lombok.Getter;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 class Customer {
-	private final String name;
+
+	@Getter
+  private final String name;
+
 	private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order of elements TODO find a better way to store this
 
 	public Customer(String name) {
@@ -15,48 +20,53 @@ class Customer {
 		rentals.put(m, d);
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public String statement() {
+  public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
+
 		String result = "Rental Record for " + getName() + "\n";
 		// loop over each movie rental
+
 		for (Movie each : rentals.keySet()) {
 			double thisAmount = 0;
 			// determine amounts for every line
 			int dr = rentals.get(each);
-			switch (each.getPriceCode()) {
-				case Movie.REGULAR:
-					thisAmount += 2;
-					if (dr > 2)
-						thisAmount += (dr - 2) * 1.5;
-					break;
-				case Movie.NEW_RELEASE:
-					thisAmount += dr * 3;
-					break;
-				case Movie.CHILDRENS:
-					thisAmount += 1.5;
-					if (dr > 3)
-						thisAmount += (dr - 3) * 1.5;
-					break;
-			}
+
+			thisAmount = getCodeValue( each, dr );
+
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if (each.getPriceCode() != null &&
-				 (each.getPriceCode() == Movie.NEW_RELEASE)
+			if (each.priceCode() != null &&
+				 (each.priceCode() == Movie.NEW_RELEASE)
 				 && dr > 1)
 				frequentRenterPoints++;
 			// show figures line for this rental
-			result += "\t" + each.getTitle() + "\t" + thisAmount + "\n";
+			result += "\t" + each.title() + "\t" + thisAmount + "\n";
 			totalAmount += thisAmount;
 		}
+
 		// add footer lines
 		result += "Amount owed is " + totalAmount + "\n";
 		result += "You earned " + frequentRenterPoints + " frequent renter points";
 		return result;
+	}
+
+	private static double getCodeValue( Movie each, int dr )
+	{
+		switch ( each.priceCode()) {
+			case Movie.REGULAR:
+				if ( dr > 2)
+					return 2 + ( dr - 2) * 1.5;
+
+			case Movie.NEW_RELEASE:
+				return dr * 3;
+
+			case Movie.CHILDREN:
+				if ( dr > 3)
+					return  1.5 + ( dr - 3) * 1.5;
+		}
+
+		return 1.5;
 	}
 }
