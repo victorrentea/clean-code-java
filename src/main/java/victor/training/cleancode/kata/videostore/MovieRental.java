dbb889c4541dc587db47ec5d3b96c49c6b4d2c6f
Shaikh Saad Bin Kader <saad.kader@plusgrade.com>
2025-02-24 15:56:36 -0700

    Improved MovieRental

--- src/main/java/victor/training/cleancode/kata/videostore/MovieRental.java
+++ src/main/java/victor/training/cleancode/kata/videostore/MovieRental.java
@@ -16,30 +16,26 @@ public record MovieRental(Movie movie, int daysRented)
 
   public double computeAmount()
   {
-    double amount = 0;
     int daysRented = daysRented();
 
     switch ( movie().movieType() )
     {
       case REGULAR:
-        amount += 2;
         if ( daysRented > 2 )
         {
-          amount += ( daysRented - 2 ) * 1.5;
+          return 2 + ( daysRented - 2 ) * 1.5;
         }
-        return amount;
+
       case NEW_RELEASE:
-        amount += daysRented * 3;
-        return amount;
+        return daysRented * 3;
+
       case CHILDREN:
-        amount += 1.5;
         if ( daysRented > 3 )
         {
-          amount += ( daysRented - 3 ) * 1.5;
+          return 1.5 + ( daysRented - 3 ) * 1.5;
         }
-        return amount;
     }
 
-    return amount;
+    return 1.5;
   }
 }
