42aa3320746892239b76a422289c1ab0feccb0b0
alasoua-pg <ala.soua@plusgrade.com>
2025-02-24 17:26:54 -0500

    Revert "wip"
    This reverts commit 03b7fe3d3c5c6cab8c2bbf7cd9521f9bdc4a8bd5.

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@
 class Customer {
 	@Getter
 	private final String name;
@@ -14,8 +15,8 @@ class Customer {
 		this.name = name;
 	}
 
-	public void addRental(Movie m, int days) {
-		rentals.put(m, days);
+	public void addRental(Movie m, int d) {
+		rentals.put(m, d);
 	}
 
 	public String statement() {
@@ -23,18 +24,35 @@ class Customer {
 		int frequentRenterPoints = 0;
 		StringBuilder result = new StringBuilder( "Rental Record for " + getName() + "\n" );
 		// loop over each movie rental
-		for (Movie movie : rentals.keySet()) {
-			int dr = rentals.get(movie);
-			double thisAmount = movie.getRentCost(dr);
+		for (Movie each : rentals.keySet()) {
+			double thisAmount = 0;
+			// determine amounts for every line
+			int dr = rentals.get(each);
+			switch (each.priceCode()) {
+				case Movie.REGULAR:
+					thisAmount += 2;
+					if (dr > 2)
+						thisAmount += (dr - 2) * 1.5;
+					break;
 
+				case Movie.NEW_RELEASE:
+					thisAmount += dr * 3;
+					break;
+
+				case Movie.CHILDREN:
+					thisAmount += 1.5;
+					if (dr > 3)
+						thisAmount += (dr - 3) * 1.5;
+					break;
+
+			}
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two-day new release rental
-			if ( movie.priceCode() == Movie.NEW_RELEASE && dr > 1 )
+			if ( each.priceCode() == Movie.NEW_RELEASE && dr > 1 )
 				frequentRenterPoints++;
-
 			// show figures line for this rental
-			result.append( "\t" ).append( movie.title() ).append( "\t" ).append( thisAmount ).append( "\n" );
+			result.append( "\t" ).append( each.title() ).append( "\t" ).append( thisAmount ).append( "\n" );
 			totalAmount += thisAmount;
 		}
 		// add footer lines
