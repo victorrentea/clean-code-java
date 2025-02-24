f993e0731834c395731f3beacae46f62d2b99740
Shaikh Saad Bin Kader <saad.kader@plusgrade.com>
2025-02-24 14:46:28 -0700

    Initial commit

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -1,10 +1,15 @@
 package victor.training.cleancode.kata.videostore;
 
+import lombok.Getter;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 class Customer {
-	private final String name;
+
+	@Getter
+  private final String name;
+
 	private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order of elements TODO find a better way to store this
 
 	public Customer(String name) {
@@ -15,20 +20,19 @@ class Customer {
 		rentals.put(m, d);
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public String statement() {
+  public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
+
 		String result = "Rental Record for " + getName() + "\n";
 		// loop over each movie rental
+
 		for (Movie each : rentals.keySet()) {
 			double thisAmount = 0;
 			// determine amounts for every line
 			int dr = rentals.get(each);
-			switch (each.getPriceCode()) {
+
+			switch (each.priceCode()) {
 				case Movie.REGULAR:
 					thisAmount += 2;
 					if (dr > 2)
@@ -37,7 +41,7 @@ class Customer {
 				case Movie.NEW_RELEASE:
 					thisAmount += dr * 3;
 					break;
-				case Movie.CHILDRENS:
+				case Movie.CHILDREN:
 					thisAmount += 1.5;
 					if (dr > 3)
 						thisAmount += (dr - 3) * 1.5;
@@ -46,14 +50,15 @@ class Customer {
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if (each.getPriceCode() != null &&
-				 (each.getPriceCode() == Movie.NEW_RELEASE)
+			if (each.priceCode() != null &&
+				 (each.priceCode() == Movie.NEW_RELEASE)
 				 && dr > 1)
 				frequentRenterPoints++;
 			// show figures line for this rental
-			result += "\t" + each.getTitle() + "\t" + thisAmount + "\n";
+			result += "\t" + each.title() + "\t" + thisAmount + "\n";
 			totalAmount += thisAmount;
 		}
+
 		// add footer lines
 		result += "Amount owed is " + totalAmount + "\n";
 		result += "You earned " + frequentRenterPoints + " frequent renter points";
