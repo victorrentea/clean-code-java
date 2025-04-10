727ce311c7f0291a5a5cd3aa16fc930d66d1a1c8
csaba.somogyi <csaba.somogyi@accenture.com>
2025-04-10 15:10:06 +0300

    started modifying Customer and Rental classes

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -1,22 +1,61 @@
 package victor.training.cleancode.kata.videostore;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import lombok.Value;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Value
 class Rental {
-    //TODO...
+    private final Movie movie;
+    private final Integer daysRented;
+
+    public Rental(Movie movie, Integer daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return this.movie;
+    }
+
+    public Integer getDaysRented() {
+        return this.daysRented;
+    }
+
+    public Double getRentalPrice(){
+        double rentalPrice = 0;
+
+        switch (this.getMovie().getMovieType()) {
+            case Movie.REGULAR:
+                rentalPrice += 2;
+                if (daysRented > 2)
+                    rentalPrice += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                rentalPrice += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                rentalPrice += 1.5;
+                if (daysRented > 3)
+                    rentalPrice += (daysRented - 3) * 1.5;
+                break;
+        }
+
+        return rentalPrice;
+    }
 }
 
 class Customer {
     private final String name;
-    private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order of elements TODO find a better way to store this
+    private final List<Rental> rentals = new ArrayList<>(); // preserves order of elements TODO find a better way to store this
 
     public Customer(String name) {
         this.name = name;
     }
 
-    public void addRental(Movie m, int d) {
-        rentals.put(m, d);
+    public void addRental(Movie movie, int daysRented) {
+        rentals.add(new Rental( movie, daysRented ));
     }
 
     public String getName() {
@@ -28,39 +67,45 @@ class Customer {
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + name + "\n";
         // loop over each movie rental
-        for (Movie each : rentals.keySet()) {
-            double thisAmount = 0;
-            // determine amounts for every line
-            int dr = rentals.get(each);
-            switch (each.getMovieType()) {
+        for (Rental rentedMovie : rentals) {
+            double rentalPrice = rentedMovie.getRentalPrice();
+            /*// determine amounts for every line
+            int daysRented = rentals.get(rentedMovie);
+            switch (rentedMovie.getMovie().getMovieType()) {
                 case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (dr > 2)
-                        thisAmount += (dr - 2) * 1.5;
+                    rentalPrice += 2;
+                    if (daysRented > 2)
+                        rentalPrice += (daysRented - 2) * 1.5;
                     break;
                 case Movie.NEW_RELEASE:
-                    thisAmount += dr * 3;
+                    rentalPrice += daysRented * 3;
                     break;
                 case Movie.CHILDREN:
-                    thisAmount += 1.5;
-                    if (dr > 3)
-                        thisAmount += (dr - 3) * 1.5;
+                    rentalPrice += 1.5;
+                    if (daysRented > 3)
+                        rentalPrice += (daysRented - 3) * 1.5;
                     break;
-            }
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if (each.getMovieType() != null &&
-                    (each.getMovieType() == Movie.NEW_RELEASE)
-                    && dr > 1)
-                frequentRenterPoints++;
+            }*/
+            frequentRenterPoints = computeFrequentRenterPoints(rentedMovie, frequentRenterPoints);
             // show figures line for this rental
-            result += "\t" + each.getTitle() + "\t" + thisAmount + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + rentedMovie.getMovie().getTitle() + "\t" + rentalPrice + "\n";
+            totalAmount += rentalPrice;
         }
         // add footer lines
         result += "Amount owed is " + totalAmount + "\n";
         result += "You earned " + frequentRenterPoints + " frequent renter points";
         return result;
+    }
+
+    private static int computeFrequentRenterPoints(Rental rentedMovie, int frequentRenterPoints) {
+        // add frequent renter points
+        frequentRenterPoints++;
+        int daysRented = rentedMovie.getDaysRented();
+        // add bonus for a two days new release rental
+        if (rentedMovie.getMovie().getMovieType() != null &&
+                (rentedMovie.getMovie().getMovieType() == Movie.NEW_RELEASE)
+                && daysRented > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
     }
 }
