734ea81b0a024531e18e2c8155c8f7f82112de20
Victor Rentea <victorrentea@gmail.com>
2025-04-10 14:35:37 +0300

    C# main test

--- cs/1_Original/Soat.CleanCode.VideoStore.Original/Customer.cs
+++ cs/1_Original/Soat.CleanCode.VideoStore.Original/Customer.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿    using System.Collections.Generic;
 using System.Globalization;
 
 namespace Soat.CleanCode.VideoStore.Original
---
+++ cs/1_Original/Soat.CleanCode.VideoStore.Original/Program.cs
@@ -0,0 +1,47 @@
+using System;
+using Soat.CleanCode.VideoStore.Original;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var customer = new Customer("John Doe");
+            customer.AddRental(new Rental(new Movie("Star Wars", Movie.NEW_RELEASE), 6));
+            customer.AddRental(new Rental(new Movie("Sofia", Movie.CHILDREN), 7));
+            customer.AddRental(new Rental(new Movie("Inception", Movie.REGULAR), 5));
+            customer.AddRental(new Rental(new Movie("Wicked", Movie.CHILDREN), 3));
+
+            var expected =
+                @"Rental Record for John Doe
+	Star Wars	18.0
+	Sofia	7.5
+	Inception	6.5
+	Wicked	1.5
+You owed 33.5
+You earned 5 frequent renter points 
+";
+
+            var actual = customer.Statement();
+
+
+
+            if (actual.Equals(expected))
+            {
+                Console.WriteLine("Test passed");
+            }
+            else
+            {
+                Console.WriteLine("Tests failed.\n***Expected output:");
+                Console.WriteLine(expected);
+                Console.WriteLine("\n***Actual output:");
+                Console.WriteLine(actual);
+            }
+
+
+        }
+    }
+
+}
