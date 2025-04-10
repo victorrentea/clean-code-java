80547e7f4aa6dd155dd1700f99be354c0cd84d98
Császár <b.csaszar@accenture.com>
2025-04-10 15:52:18 +0300

    yolo

--- cs/1_Original/Soat.CleanCode.VideoStore.Original/Customer.cs
+++ cs/1_Original/Soat.CleanCode.VideoStore.Original/Customer.cs
@@ -6,7 +6,6 @@ namespace Soat.CleanCode.VideoStore.Original
     public class Customer
     {
         private readonly List<Rental> _rentals = new List<Rental>();
-        private int FrequentRenterPoints = 0;
         public string Name { get; }
 
         public Customer(string name)
@@ -21,66 +20,51 @@ namespace Soat.CleanCode.VideoStore.Original
 
         public string Statement()
         {
-            var (totalAmount, result) = DeterminesAmountOfRentals();
-
-            result += "You owed " + totalAmount.ToString("0.0", CultureInfo.InvariantCulture) + "\r\n";
-            result += "You earned " + FrequentRenterPoints.ToString() + " frequent renter points \r\n";
-
-            return result;
-        }
-
-        private (decimal, string) DeterminesAmountOfRentals()
-        {
+            var frequentRenterPoints = 0;
             var totalAmount = 0m;
-            var result = "Rental Record for " + Name + "\r\n";
-            foreach (var rental in _rentals)
+            var result = "Rental Record for " + Name + "\n";
+            foreach (var each in _rentals)
             {
-                //dtermines the amount for each line
-                var thisAmount = CalculateThisAmount(rental);
-                CalculateFrequentRenterPoints(rental);
+                var thisAmount = 0m;
 
-                result += "\t" + rental.Movie.Title + "\t" + thisAmount.ToString("0.0", CultureInfo.InvariantCulture) + "\r\n";
+                //dtermines the amount for each line
+                switch (each.Movie.PriceCode)
+                {
+                    case Movie.REGULAR:
+                        thisAmount += 2;
+                        if (each.DaysRented > 2)
+                        {
+                            thisAmount += (each.DaysRented - 2) * 1.5m;
+                        }
+                        break;
+                    case Movie.NEW_RELEASE:
+                        thisAmount += each.DaysRented * 3;
+                        break;
+                    case Movie.CHILDREN:
+                        thisAmount += 1.5m;
+                        if (each.DaysRented > 3)
+                        {
+                            thisAmount += (each.DaysRented - 3) * 1.5m;
+                        }
+                        break;
+                }
+
+                frequentRenterPoints++;
+
+                if (each.Movie.PriceCode == Movie.NEW_RELEASE
+                    && each.DaysRented > 1)
+                {
+                    frequentRenterPoints++;
+                }
+
+                result += "\t" + each.Movie.Title + "\t" + thisAmount.ToString("0.0", CultureInfo.InvariantCulture) + "\n";
                 totalAmount += thisAmount;
             }
-            return (totalAmount, result);
-        }
 
-        private void CalculateFrequentRenterPoints(Rental each)
-        {
-            FrequentRenterPoints++;
+            result += "You owed " + totalAmount.ToString("0.0", CultureInfo.InvariantCulture) + "\n";
+            result += "You earned " + frequentRenterPoints.ToString() + " frequent renter points \n";
 
-            if (each.Movie.PriceCode == Movie.NEW_RELEASE
-                && each.DaysRented > 1)
-            {
-                FrequentRenterPoints++;
-            }
-        }
-
-        private decimal CalculateThisAmount(Rental each)
-        {
-            var thisAmount = 0m;
-
-            switch (each.Movie.PriceCode)
-            {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.DaysRented > 2)
-                    {
-                        thisAmount += (each.DaysRented - 2) * 1.5m;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.DaysRented * 3;
-                    break;
-                case Movie.CHILDREN:
-                    thisAmount += 1.5m;
-                    if (each.DaysRented > 3)
-                    {
-                        thisAmount += (each.DaysRented - 3) * 1.5m;
-                    }
-                    break;
-            }
-            return thisAmount;
+            return result;
         }
     }
 }
