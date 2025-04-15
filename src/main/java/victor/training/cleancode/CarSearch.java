cbe1ae6184b8a2600d3ab93586a4844332fd9e90
Victor Rentea <victorrentea@gmail.com>
2025-04-15 13:18:58 +0300

    I acked the presence of this thing called Interval to group two ints intoa a new IMMUTABLE VALUE OBJECT

--- src/main/java/victor/training/cleancode/CarSearch.java
+++ src/main/java/victor/training/cleancode/CarSearch.java
@@ -7,30 +7,66 @@ class CarSearch {
 
   // run tests
   public List<CarModel> filterCarModels(CarSearchCriteria criteria, List<CarModel> carModels) {
+//    Year year = Year.of(2024)
     List<CarModel> results = carModels.stream()
-        .filter(carModel -> MathUtil.intervalsIntersect(
-            criteria.getStartYear(), criteria.getEndYear(),
-            carModel.getStartYear(), carModel.getEndYear()))
+        .filter(carModel -> yearsIntersect(criteria, carModel))
         .collect(Collectors.toList());
     System.out.println("More filtering logic ...");
     return results;
   }
+
+  private boolean yearsIntersect(CarSearchCriteria criteria, CarModel carModel) {
+    return new Interval(criteria.getStartYear(), criteria.getEndYear()).doesIntersect(
+        new Interval(carModel.getStartYear(), carModel.getEndYear()));
+  }
 }
 
-class SomeOtherClientCode {
-  private void applyLengthFilter() { // pretend
-    System.out.println(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+// any name you invent, look for 5 alternatives. brcause devs suck at names.
+//class YearRange {
+//class ManufacturerYearRange {
+//class IntervalWindow {
+//class YearsWindow {
+//class IntWindow {
+//class Range {
+
+// every little class (not a Mongo Document) should be IMMUTABLE.
+// why?
+// - thread safety - do I do heavy multi threaded flows in microservices ?
+// - code safety: to call a method without worrying of it changing the state of my parameter
+class Interval {
+  private final int start;
+  private final int end;
+
+  Interval(int start, int end) {
+    if (start > end) { // self-validating constructor - scary for most developers. wild: < 5%
+      throw new IllegalArgumentException("start larger than end");
+    }
+    this.start = start;
+    this.end = end;
   }
-  private void applyCapacityFilter() { // pretend
-    System.out.println(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+
+  //  public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
+  public boolean doesIntersect(Interval interval2) {
+    return start <= interval2.end && interval2.start <= end;
+  }
+
+  // uniform interface principle: you should not be able to tell the difference
+  // between a getter and a method returning a "derived value" from the objects fields
+  public int length() {
+    return end - start;
+  }
+
+  public int getStart() {
+    return start;
+  }
+
+  public int getEnd() {
+    return end;
   }
 }
+
 
 class MathUtil {
-
-  public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
-    return start1 <= end2 && start2 <= end1;
-  }
 }
 
 
@@ -59,9 +95,8 @@ class CarSearchCriteria { // a DTO received from JSON
   }
 }
 
-// @Entity
+//@Document
 class CarModel { // the Entity Model👑 test
-  // @Id
   private Long id;
   private String make;
   private String model;
