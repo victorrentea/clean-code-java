92251a8654150418dcfb5e43683323b058b93afa
Victor Rentea <victorrentea@gmail.com>
2025-04-29 11:50:40 +0300

    deep immutable object, geek style

--- src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
+++ src/main/java/victor/training/cleancode/immutable/advanced/ImmutableAdvanced.java
@@ -1,19 +1,18 @@
 package victor.training.cleancode.immutable.advanced;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import com.google.common.collect.ImmutableList;
+
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toList;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 
 public class ImmutableAdvanced {
   public static void main(String[] args) {
-    List<Integer> list = Stream.of(1, 2, 3).collect(toList()); // ArrayList
+    var list = Stream.of(1, 2, 3).collect(toImmutableList()); // ArrayList
 
     Immutable immutable = new Immutable(1, 2, list, new Other(15));
     System.out.println("Before: " + immutable);
-    list.clear();
+//    list.clear();
 
     wilderness(immutable);
 
@@ -22,50 +21,58 @@ public class ImmutableAdvanced {
 
   private static void wilderness(Immutable immutable) {
     // dark, deep logic not expected to change the immutable object x,y
-//    immutable.getList().clear();
+//    immutable.list().clear();
   }
 }
 
-class Immutable {
-  private final Integer x;
-  private final Integer y;
-  //  private final ImmutableList<Integer> list; // best from guava
-  private final List<Integer> list;
-  private final Other other;
-
-  Immutable(Integer x, Integer y, List<Integer> list, Other other) {
-    this.x = x;
-    this.y = y;
-    this.list = new ArrayList<>(list);// protect against others keeping a ref to the mutable arraylist.
-    this.other = other;
-  }
-
-//  public Stream<Integer> getList() {
-//  public Iterator<Integer> getList() {
-//  public Iterable<Integer> getList() { // okish
-
-  public List<Integer> getList() {
-//    return new ArrayList<>(list); // malloc
-    return Collections.unmodifiableList(list); // immutable decorator
-  }
-
-  public Integer getX() {
-    return x;
-  }
-
-  public Integer getY() {
-    return y;
-  }
-
-  public Other getOther() {
-    return other;
-  }
-
-  @Override
-  public String toString() {
-    return "Immutable{x=%d, y=%d, numbers=%s, other=%s}".formatted(x, y, list, other);
-  }
+record Immutable(
+    Integer x,
+    Integer y,
+    ImmutableList<Integer> list,
+    Other other) {
 }
+
+//class Immutable {
+//  private final Integer x;
+//  private final Integer y;
+//  //  private final ImmutableList<Integer> list; // best from guava
+//  private final List<Integer> list;
+//  private final Other other;
+//
+//  Immutable(Integer x, Integer y, List<Integer> list, Other other) {
+//    this.x = x;
+//    this.y = y;
+//    this.list = list;// protect against others keeping a ref to the mutable arraylist.
+//    this.other = other;
+//  }
+//
+////  public Stream<Integer> getList() {
+////  public Iterator<Integer> getList() {
+////  public Iterable<Integer> getList() { // okish
+//
+//  public List<Integer> getList() {
+
+////    return new ArrayList<>(list); // malloc
+//    return Collections.unmodifiableList(list); // immutable decorator
+//  }
+//
+//  public Integer getX() {
+//    return x;
+//  }
+//
+//  public Integer getY() {
+//    return y;
+//  }
+//
+//  public Other getOther() {
+//    return other;
+//  }
+//
+//  @Override
+//  public String toString() {
+//    return "Immutable{x=%d, y=%d, numbers=%s, other=%s}".formatted(x, y, list, other);
+//  }
+//}
 
 record Other(int a) { // j17+
 }
