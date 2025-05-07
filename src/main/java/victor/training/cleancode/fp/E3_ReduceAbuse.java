98227599675bbcdbbb522894e6d747b54a5338b6
Victor Rentea <victorrentea@gmail.com>
2025-05-07 13:56:32 +0100

    prep= devoxx uk

--- src/main/java/victor/training/cleancode/fp/E3_ReduceAbuse.java
+++ src/main/java/victor/training/cleancode/fp/E3_ReduceAbuse.java
@@ -7,8 +7,10 @@ import java.util.List;
 public class E3_ReduceAbuse {
   public Order getLastPremiumOrder(List<Order> orders) {
     return orders.stream().reduce(null, (prev, order) ->
-        order.orderLines().stream().anyMatch(line -> line.product().isPremium()) &&
-        (prev == null || order.creationDate().isAfter(prev.creationDate())) ? order : prev
+        order.orderLines().stream()
+            .anyMatch(line -> line.product().isPremium()) &&
+        (prev == null || order.creationDate().isAfter(prev.creationDate())) ?
+            order : prev
     );
   }
 }
