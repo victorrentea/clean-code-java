6d72c6c49a0f72f4a44b989db5d1eeba58c8c605
Victor Rentea <victorrentea@gmail.com>
2025-05-07 14:17:29 +0100

    prep= devoxx uk

--- src/main/java/victor/training/cleancode/fp/E2_FunctionalChainsaw.java
+++ src/main/java/victor/training/cleancode/fp/E2_FunctionalChainsaw.java
@@ -11,21 +11,21 @@ import static java.util.stream.Collectors.*;
 
 @RequiredArgsConstructor
 public class E2_FunctionalChainsaw/*Massacre*/ {
-	private final ProductRepo productRepo;
-	private final OrderRepo orderRepo;
+  private final ProductRepo productRepo;
+  private final OrderRepo orderRepo;
 
-	public List<Product> getHotProducts() {
-		return orderRepo.findAll().stream()
-				.filter(Order::isActive)
-				.filter(o -> o.creationDate().isAfter(LocalDate.now().minusMonths(1)))
-				.flatMap(o -> o.orderLines().stream())
-				.collect(groupingBy(OrderLine::product, summingInt(OrderLine::itemCount)))
-				.entrySet()
-				.stream()
-				.filter(e -> e.getValue() >= 10)
-				.map(Entry::getKey)
-				.filter(p -> !p.isDeleted())
-				.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
-				.collect(toList());
-	}
+  public List<Product> getHotProducts() {
+    return orderRepo.findAll().stream()
+        .filter(Order::isActive)
+        .filter(o -> o.creationDate().isAfter(LocalDate.now().minusMonths(1)))
+        .flatMap(o -> o.orderLines().stream())
+        .collect(groupingBy(OrderLine::product, summingInt(OrderLine::itemCount)))
+        .entrySet()
+        .stream()
+        .filter(e -> e.getValue() >= 10)
+        .map(Entry::getKey)
+        .filter(p -> !p.isDeleted())
+        .filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
+        .collect(toList());
+  }
 }
