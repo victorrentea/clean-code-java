07c3fef40e3c21911011d614f8bcdd4faa5c27fc
Victor Rentea <victorrentea@gmail.com>
2025-05-09 13:58:01 +0200

    yolo

--- src/main/java/victor/training/cleancode/fp/MemoryChurn.kt
+++ src/main/java/victor/training/cleancode/fp/MemoryChurn.kt
@@ -2,6 +2,7 @@ package victor.training.cleancode.fp
 
 fun main() {
     val list50K = (1..50_000).toList()
-    val rez = list50K.fold(mutableListOf<Int>()) { list, e -> list + e * e }
+    val rez = list50K.fold(listOf<Int>())
+    { list, e -> list + e * e }
     println(rez.size)
 }
