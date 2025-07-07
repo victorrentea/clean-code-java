e0bb181929513274487e58a2a2e04ac72415c0c7
Victor Rentea <victorrentea@gmail.com>
2025-07-07 23:39:22 +0200

    wip

--- src/main/java/victor/training/cleancode/ExtractSimilarSyntax.java
+++ src/main/java/victor/training/cleancode/ExtractSimilarSyntax.java
@@ -1,34 +1,32 @@
 package victor.training.cleancode;
 
-class ParameterizeExtractSimilarCode {
-
-    public void f(int n) {
-        System.out.println("Logic F");
-        for (int i = 0; i < 4; i++) {
-            if (n + i < 0) {
-                System.out.println("CodeO" + i);
-            } else {
-                throw new IllegalArgumentException();
-            }
-        }
+class ClassA {
+  public void f(int n) {
+    System.out.println("Logic F");
+    for (int i = 0; i < 4; i++) {
+      if (n + i < 0) {
+        System.out.println("CodeO" + i);
+      } else {
+        throw new IllegalArgumentException();
+      }
     }
+  }
 }
-// ========= separate class =========
 
-class SomeOtherClass {
-    public void g(int n) {
-        System.out.println("Logic G");
-        try {
-            for (int j = 0; j < 4; j++) {
-                if (n + j < 0) {
-                    System.out.println("Code0" + j);
-                } else {
-                    throw new IllegalArgumentException();
-                }
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("Rethrow", e);
+class ClassB {
+  public void g(int n) {
+    System.out.println("Logic G");
+    try {
+      for (int j = 0; j < 4; j++) {
+        if (n + j < 0) {
+          System.out.println("Code0" + j);
+        } else {
+          throw new IllegalArgumentException();
         }
+      }
+    } catch (Exception e) {
+      throw new RuntimeException("Rethrow", e);
     }
+  }
 
 }
