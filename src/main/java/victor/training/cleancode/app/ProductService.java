1b46e356a04c91a1e0a21c0661587bea41eedd39
Victor Rentea <victorrentea@gmail.com>
2025-07-08 13:11:48 +0200

    inline a supposedly stupid interface

--- src/main/java/victor/training/cleancode/app/ProductService.java
+++ src/main/java/victor/training/cleancode/app/ProductService.java
@@ -3,6 +3,7 @@ package victor.training.cleancode.app;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import victor.training.cleancode.infra.RapexApiClient;
 import victor.training.cleancode.support.*;
 
 // TODO 4
@@ -12,7 +13,7 @@ import victor.training.cleancode.support.*;
 public class ProductService {
   private final SupplierRepo supplierRepo;
   private final ProductRepo productRepo;
-  private final SafetyCheckerService safetyCheckerService;
+  private final RapexApiClient safetyCheckerService;
 
   public Long createProduct(ProductDto productDto) {
     log.info("Creating product {}", productDto);
--- src/main/java/victor/training/cleancode/infra/RapexApiClient.java
+++ src/main/java/victor/training/cleancode/infra/RapexApiClient.java
@@ -6,20 +6,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import victor.training.cleancode.app.SafetyCheckerService;
 
 import java.net.URL;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service // Adapter pattern
-public class RapexApiClient implements SafetyCheckerService {
+public class RapexApiClient {
   private final RestTemplate restTemplate;
   @Value("${rapex.service.url.base}")
   private URL baseUrl;
 
   @SuppressWarnings("DataFlowIssue")
-  @Override
   public boolean isSafe(@NotNull String barcode) {
     SafetyResponse response = restTemplate.getForEntity(
             baseUrl + "/product/{barcode}/safety",
