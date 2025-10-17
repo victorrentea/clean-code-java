7fa8d831de3515204f1ac8ae75c5abc14681cb70
Beatriz Avila <beatriz.avila@swift.com>
2025-10-17 14:53:54 +0200

    Created Rental class with Movie and days

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -1,21 +1,22 @@
 package victor.training.cleancode.kata.videostore;
 
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
-import static victor.training.cleancode.kata.videostore.PriceCode.*;
+import static victor.training.cleancode.kata.videostore.PriceCode.NEW_RELEASE;
 
 class Customer {
     private final String name;
-    private final Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order of elements TODO find a better way to store this
+    private final List<Rental> rentals = new LinkedList<>(); // preserves order of elements TODO find a better way to store this
 
     public Customer(String name) {
         this.name = name;
     }
 
-
     public void addRental(Movie m, int price) {
-        rentals.put(m, price);
+        rentals.add(new Rental(m, price));
     }
 
     public String getName() {
@@ -29,10 +30,11 @@ class Customer {
 
         // loop over movie rental
 		Map<Movie, Double> finalPricePerMovie = new LinkedHashMap<>();
-        for (Movie movie : rentals.keySet()) {
+        for (Rental rental : rentals) {
             double thisAmount = 0;
             // determine amounts for every line
-            int days = rentals.get(movie);
+            int days = rental.days();
+            Movie movie = rental.movie();
             switch (movie.priceCode()) {
                 case REGULAR:
                     thisAmount += 2;
@@ -51,9 +53,7 @@ class Customer {
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if (movie.priceCode() != null &&
-                    (movie.priceCode() == NEW_RELEASE)
-                    && days > 1)
+            if (movie.priceCode() == NEW_RELEASE && days > 1)
                 frequentRenterPoints++;
             // show figures line for this rental
             result += "\t" + movie.title() + "\t" + thisAmount + "\n";
