2633a5cdf318a2c4309bc66825fc62b062fe4da9
Beatriz Avila <beatriz.avila@swift.com>
2025-10-17 13:32:48 +0200

    Test commit

--- src/test/java/victor/training/cleancode/kata/projectservice/ExerciseTest.java
+++ src/test/java/victor/training/cleancode/kata/projectservice/ExerciseTest.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
-
+// Test commit (Beatriz)
 @ExtendWith(MockitoExtension.class)
 class ExerciseTest {
   @Mock
