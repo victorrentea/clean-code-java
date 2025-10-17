149f249d377d6bd66e88e85c32c4f8e064c26c56
nlecroar <nlecroar26@gmail.com>
2025-10-17 14:14:14 +0200

    Renaming + introduce record for Movie

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -13,10 +13,9 @@ class Customer {
         this.name = name;
     }
 
-    ;
 
-    public void addRental(Movie m, int d) {
-        rentals.put(m, d);
+    public void addRental(Movie m, int price) {
+        rentals.put(m, price);
     }
 
     public String getName() {
@@ -31,31 +30,31 @@ class Customer {
         for (Movie each : rentals.keySet()) {
             double thisAmount = 0;
             // determine amounts for every line
-            int dr = rentals.get(each);
-            switch (each.getPriceCode()) {
+            int price = rentals.get(each);
+            switch (each.priceCode()) {
                 case REGULAR:
                     thisAmount += 2;
-                    if (dr > 2)
-                        thisAmount += (dr - 2) * 1.5;
+                    if (price > 2)
+                        thisAmount += (price - 2) * 1.5;
                     break;
                 case NEW_RELEASE:
-                    thisAmount += dr * 3;
+                    thisAmount += price * 3;
                     break;
                 case CHILDRENS:
                     thisAmount += 1.5;
-                    if (dr > 3)
-                        thisAmount += (dr - 3) * 1.5;
+                    if (price > 3)
+                        thisAmount += (price - 3) * 1.5;
                     break;
             }
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if (each.getPriceCode() != null &&
-                    (each.getPriceCode() == NEW_RELEASE)
-                    && dr > 1)
+            if (each.priceCode() != null &&
+                    (each.priceCode() == NEW_RELEASE)
+                    && price > 1)
                 frequentRenterPoints++;
             // show figures line for this rental
-            result += "\t" + each.getTitle() + "\t" + thisAmount + "\n";
+            result += "\t" + each.title() + "\t" + thisAmount + "\n";
             totalAmount += thisAmount;
         }
         // add footer lines
--- src/main/java/victor/training/cleancode/kata/videostore/Movie.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Movie.java
@@ -1,20 +1,5 @@
 package victor.training.cleancode.kata.videostore;
 
-public class Movie {
-    private final String title;
-    private final PriceCode priceCode;
-
-    public Movie(String title, PriceCode priceCode) {
-        this.title = title;
-        this.priceCode = priceCode;
-    }
-
-    public PriceCode getPriceCode() {
-        return priceCode;
-    }
-
-    public String getTitle() {
-        return title;
-    }
+public record Movie(String title, PriceCode priceCode) {
 
 }
