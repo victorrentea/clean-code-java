4b1b369398de0665437ee519cc9f8cebcc297547
Victor Rentea <victorrentea@gmail.com>
2025-10-17 17:29:33 +0300

    good

--- src/main/java/victor/training/cleancode/kata/videostore/Customer.java
+++ src/main/java/victor/training/cleancode/kata/videostore/Customer.java
@@ -1,37 +1,40 @@
 package victor.training.cleancode.kata.videostore;
 
+import victor.training.cleancode.kata.videostore.Statement.MoviePrice;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 class Customer {
-    private final String name;
-    private final List<RentedMovie> rentedMovies = new ArrayList<>();
+  private final String name;
+  private final List<RentedMovie> rentedMovies = new ArrayList<>();
 
-    public Customer(String name) {
-        this.name = Objects.requireNonNull(name);
-    }
+  public Customer(String name) {
+    this.name = Objects.requireNonNull(name);
+  }
 
-    public void addRental(Movie movie, int daysOfRental) {
-        rentedMovies.add(new RentedMovie(movie, daysOfRental));
-    }
+  public void addRental(Movie movie, int daysOfRental) {
+    rentedMovies.add(new RentedMovie(movie, daysOfRental));
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String statement() {
-        List<Statement.MoviePrice> moviePrices = rentedMovies.stream()
-                .map(m -> new Statement.MoviePrice(m.movie().title(), m.computePrice()))
-                .toList();
-        Statement statement = new Statement(name, moviePrices, computeFrequentRenterPoints());
-        return statement.format();
-    }
+  public String statement() {
+    List<MoviePrice> moviePrices = rentedMovies.stream()
+        .map(m -> new MoviePrice(m.movie().title(), m.computePrice()))
+        .toList();
+    Statement statement = new Statement(name, moviePrices, computeFrequentRenterPoints());
+    return statement.format();
+  }
 
-    private long computeFrequentRenterPoints() {
-		long frequentRenterBonusPoints =  rentedMovies.stream()
-				.filter(RentedMovie::isEligibleForFrequentRenterBonus).count();
-		long frequentRenterPoints = rentedMovies.size();
-        return frequentRenterBonusPoints + frequentRenterPoints;
-    }
+  private long computeFrequentRenterPoints() {
+//    long frequentRenterBonusPoints = rentedMovies.stream()
+//        .filter(RentedMovie::isEligibleForFrequentRenterBonus).count();
+//    long frequentRenterPoints = rentedMovies.size();
+//    return frequentRenterBonusPoints + frequentRenterPoints;
+    return rentedMovies.stream().mapToInt(RentedMovie::getFrequentRenterPoints).sum();
+  }
 }
--- src/main/java/victor/training/cleancode/kata/videostore/RentedMovie.java
+++ src/main/java/victor/training/cleancode/kata/videostore/RentedMovie.java
@@ -5,34 +5,40 @@ import java.util.Objects;
 import static victor.training.cleancode.kata.videostore.MoviePricingCategory.NEW_RELEASE;
 
 record RentedMovie(Movie movie, int daysOfRental) {
-    RentedMovie {
-        Objects.requireNonNull(movie);
-        if (daysOfRental <= 0) {
-            throw new IllegalArgumentException("The number of rental days cannot be smaller or equals than 0");
-        }
+  RentedMovie {
+    Objects.requireNonNull(movie);
+    if (daysOfRental <= 0) {
+      throw new IllegalArgumentException("The number of rental days cannot be smaller or equals than 0");
     }
+  }
 
-    public double computePrice() {
-        return switch (movie().pricingCategory()) {
-            case REGULAR -> {
-                double price = 2;
-                if (daysOfRental() > 2)
-                    price += (daysOfRental() - 2) * 1.5;
-                yield price;
-            }
-            case NEW_RELEASE -> daysOfRental() * 3;
-            case CHILDREN -> {
-                double price = 1.5;
-                if (daysOfRental() > 3)
-                    price += (daysOfRental() - 3) * 1.5;
-                yield price;
-            }
-        };
-    }
+  public double computePrice() {
+    return switch (movie().pricingCategory()) {
+      case REGULAR -> {
+        double price = 2;
+        if (daysOfRental() > 2)
+          price += (daysOfRental() - 2) * 1.5;
+        yield price;
+      }
+      case NEW_RELEASE -> daysOfRental() * 3;
+      case CHILDREN -> {
+        double price = 1.5;
+        if (daysOfRental() > 3)
+          price += (daysOfRental() - 3) * 1.5;
+        yield price;
+      }
+    };
+  }
 
-    public boolean isEligibleForFrequentRenterBonus() {
-        // add bonus for a two day new release rental
-        return movie.pricingCategory() == NEW_RELEASE
-                && daysOfRental > 1;
+  private boolean isEligibleForFrequentRenterBonus() {
+    return movie.pricingCategory() == NEW_RELEASE
+           && daysOfRental > 1;
+  }
+
+  public int getFrequentRenterPoints() {
+    if (isEligibleForFrequentRenterBonus()) {
+      return 2;
     }
+    return 1;
+  }
 }
