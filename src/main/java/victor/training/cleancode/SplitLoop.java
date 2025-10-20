53bb97fcded83d70b0afdc138c1c4bc10ef89516
Victor Rentea <victorrentea@gmail.com>
2025-10-20 11:18:23 +0300

    for ->stream()

--- src/main/java/victor/training/cleancode/SplitLoop.java
+++ src/main/java/victor/training/cleancode/SplitLoop.java
@@ -4,20 +4,22 @@ import lombok.Builder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SplitLoop {
 
   public String computeStats(List<Employee> employees) {
-    List<Integer> employeeIds = new ArrayList<>();
-    double totalConsultantSalary = 0;
-    for (Employee employee : employees) { // SRP: sa faci 1 lucru odata
-      if (employee.consultant()) {
-        totalConsultantSalary += employee.salary();
-      }
-    }
-    for (Employee employee : employees) { // SRP: sa faci 1 lucru odata
-      employeeIds.add(employee.id());
-    }
+    double totalConsultantSalary = employees.stream()
+        .filter(Employee::consultant)
+        .mapToDouble(Employee::salary) // 💩
+        .sum();
+    // TS = employees.filter(e=>e.consultant).reduce(0, (acc,e)=>acc+e.salary)
+    // PY = sum(e.salary for e in employees if e.consultant)
+
+    // SRP: sa faci 1 lucru odata
+    List<Integer> employeeIds = employees.stream().map(Employee::id).toList();
+    // TS: = employees.map(e=>e.id)
+    // PY: = [e.id for e in employees]
     System.out.println("Employee IDs: " + employeeIds);
     return "Total consultant salary: " + totalConsultantSalary + "; ids: " + employeeIds;
   }
